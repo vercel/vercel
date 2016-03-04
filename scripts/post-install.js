2691cd67bf733fd4525da8eede85f68fd2e496bd
Guillermo Rauch <rauchg@gmail.com>
2016-03-04 05:39:26 -0800

    error-handling

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 import login from '../lib/login';
+import chalk from 'chalk';
 
-login().then(() => {
+console.log(`> ${chalk.bold('Authenticating…')}`);
+
+login()
+.then(() => {
   console.log('> Logged in successfully. Token saved in ~/.now.json');
   process.exit(0);
+})
+.catch((err) => {
+  console.error(`> Error. Login filed. Run \`now --login\` to retry.`);
 });
