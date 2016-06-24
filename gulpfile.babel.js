43c224c43678aa869a9716e82924ece765a39ce8
Leo Lamprecht <mindrun@icloud.com>
2016-06-24 13:40:47 +0200

    Clean up gulpfile

---
+++ gulpfile.babel.js
@@ -0,0 +1,33 @@
+import gulp from 'gulp';
+import del from 'del';
+import ext from 'gulp-ext';
+import babel from 'gulp-babel';
+import uglify from 'gulp-uglify';
+import help from 'gulp-task-listing';
+
+gulp.task('help', help);
+
+gulp.task('compile', [
+  'compile-lib',
+  'compile-bin'
+]);
+
+gulp.task('compile-lib', () =>
+  gulp.src('lib/**/*.js')
+  .pipe(babel())
+  .pipe(uglify())
+  .pipe(gulp.dest('build/lib')));
+
+gulp.task('compile-bin', () =>
+  gulp.src('bin/*')
+  .pipe(babel())
+  .pipe(uglify())
+  .pipe(ext.crop())
+  .pipe(gulp.dest('build/bin')));
+
+gulp.task('watch-lib', () => gulp.watch('lib/*.js', ['compile-lib']));
+gulp.task('watch-bin', () => gulp.watch('bin/*', ['compile-bin']));
+gulp.task('clean', () => del(['build']));
+
+gulp.task('watch', ['watch-lib', 'watch-bin']);
+gulp.task('default', ['compile', 'watch']);
