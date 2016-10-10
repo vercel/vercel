d0e30a68aefb6acaf24fea9d6f76623ea09af98a
Leo Lamprecht <mindrun@icloud.com>
2016-10-10 17:43:03 +0200

    Replace standard with XO

--- test/to-host.js
+++ test/to-host.js
@@ -1,26 +1,26 @@
-import test from 'ava';
-import toHost from '../lib/to-host';
+import test from 'ava'
+import toHost from '../lib/to-host'
 
 test('simple', async t => {
-  t.is(toHost('zeit.co'), 'zeit.co');
-});
+  t.is(toHost('zeit.co'), 'zeit.co')
+})
 
 test('leading //', async t => {
-  t.is(toHost('//zeit-logos-rnemgaicnc.now.sh'), 'zeit-logos-rnemgaicnc.now.sh');
-});
+  t.is(toHost('//zeit-logos-rnemgaicnc.now.sh'), 'zeit-logos-rnemgaicnc.now.sh')
+})
 
 test('leading http://', async t => {
-  t.is(toHost('http://zeit-logos-rnemgaicnc.now.sh'), 'zeit-logos-rnemgaicnc.now.sh');
-});
+  t.is(toHost('http://zeit-logos-rnemgaicnc.now.sh'), 'zeit-logos-rnemgaicnc.now.sh')
+})
 
 test('leading https://', async t => {
-  t.is(toHost('https://zeit-logos-rnemgaicnc.now.sh'), 'zeit-logos-rnemgaicnc.now.sh');
-});
+  t.is(toHost('https://zeit-logos-rnemgaicnc.now.sh'), 'zeit-logos-rnemgaicnc.now.sh')
+})
 
 test('leading https:// and path', async t => {
-  t.is(toHost('https://zeit-logos-rnemgaicnc.now.sh/path'), 'zeit-logos-rnemgaicnc.now.sh');
-});
+  t.is(toHost('https://zeit-logos-rnemgaicnc.now.sh/path'), 'zeit-logos-rnemgaicnc.now.sh')
+})
 
 test('simple and path', async t => {
-  t.is(toHost('zeit.co/test'), 'zeit.co');
-});
+  t.is(toHost('zeit.co/test'), 'zeit.co')
+})
