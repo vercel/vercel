5c9468d9e2a311991e6f83706dc1a3f3c55e6b63
Guillermo Rauch <rauchg@gmail.com>
2017-02-13 22:41:20 -0300

    more reliable build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ rm -rf build
 mkdir -p build/{lib,bin}
 find lib/** -type d -exec mkdir -p build/{} \;
 find bin/** -type d -exec mkdir -p build/{} \;
-find lib/** -type f -exec async-to-gen --out-file build/{} {} \;
-find bin/** -type f -exec async-to-gen --out-file build/{} {} \;
+find lib/** -type f -exec node_modules/.bin/async-to-gen --out-file build/{} {} \;
+find bin/** -type f -exec node_modules/.bin/async-to-gen --out-file build/{} {} \;
 cp lib/utils/billing/*.json build/lib/utils/billing/
 cp package.json build/
