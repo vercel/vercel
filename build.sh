32ea71ed3482daeee1f091b85919bf1caef5f24b
Matheus Fernandes <github@matheus.top>
2017-02-21 17:43:32 -0300

    Auto generate a `pkg.json` based on `package.json`

--- build.sh
+++ build.sh
@@ -8,3 +8,7 @@ find lib/** -type f -exec node_modules/.bin/async-to-gen --out-file build/{} {} 
 find bin/** -type f -exec node_modules/.bin/async-to-gen --out-file build/{} {} \;
 chmod +x build/bin/now.js
 cp lib/utils/billing/*.json build/lib/utils/billing/
+
+# extracts `name` and `version` from ./package.json
+# and ouputs it to build/pkg.json
+echo "{`grep -e '\"version\|name\"' ./package.json | sed -e 's/ //g' | tr -d '\n' | sed -e 's/.$//g'`}" > build/pkg.json
