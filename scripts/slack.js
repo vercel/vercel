f3736dc6b985a267d76b9cac014ee099d5d645ea
Matheus Fernandes <github@matheus.top>
2017-04-12 17:43:19 -0700

    Increase the timeout for "unwanted" builds

--- scripts/slack.js
+++ scripts/slack.js
@@ -70,7 +70,7 @@ fetch(`https://api.github.com/repos/${repo}/commits/${commit}`, opts)
           const message = `:white_check_mark: Build succeded on Node ${currentNodeVersion} (commit <https://github.com/${repo}/commit/${commit}|${commit.substr(0, 7)}> by <${res.authorUrl}|${res.authorName}>)`;
           await slackup({ apiKey, channel, type: 'message', message });
         },
-        3000
+        10000
       );
     }
   })
