f6063e438e289268cef18ae08590bf67d7d789f7
Matheus Fernandes <github@matheus.top>
2017-04-29 02:41:26 -0700

    Add stamp to domain purchase

--- bin/domains/buy.js
+++ bin/domains/buy.js
@@ -7,18 +7,21 @@ const param = require('../../lib/utils/output/param');
 const info = require('../../lib/utils/output/info');
 const uid = require('../../lib/utils/output/uid');
 const success = require('../../lib/utils/output/success');
+const stamp = require('../../lib/utils/output/stamp');
 const promptBool = require('../../lib/utils/input/prompt-bool');
 const eraseLines = require('../../lib/utils/output/erase-lines');
 const treatBuyError = require('../../lib/utils/domains/treat-buy-error');
 
 module.exports = async function({domains, args, currentTeam, user}) {
   const name = args[0];
+  let elapsed
 
   if (!name) {
     return error(`Missing domain name. Run ${cmd('now domains help')}`);
   }
 
   const nameParam = param(name);
+  elapsed = stamp()
   let stopSpinner = wait(`Checking availability for ${nameParam}`);
 
   const price = await domains.price(name);
@@ -27,10 +30,10 @@ module.exports = async function({domains, args, currentTeam, user}) {
   stopSpinner();
 
   if (! available) {
-    return error(`The domain ${nameParam} is ${italic('unavailable')}!`);
+    return error(`The domain ${nameParam} is ${italic('unavailable')}! ${elapsed()}`);
   }
 
-  info(`The domain ${nameParam} is ${italic('available')}!`);
+  info(`The domain ${nameParam} is ${italic('available')}! ${elapsed()}`);
   const confirmation = await promptBool(`Buy now for ${bold(`$${price}`)} (${
     bold(
       (currentTeam && currentTeam.slug) || user.username || user.email
@@ -43,6 +46,7 @@ module.exports = async function({domains, args, currentTeam, user}) {
   }
 
   stopSpinner = wait('Purchasing');
+  elapsed = stamp()
   let domain;
   try {
     domain = await domains.buy(name);
@@ -53,7 +57,7 @@ module.exports = async function({domains, args, currentTeam, user}) {
 
   stopSpinner();
 
-  success(`Domain purchased and created ${uid(domain.uid)}`);
+  success(`Domain purchased and created ${uid(domain.uid)} ${elapsed()}`);
   info(
     `You may now use your domain as an alias to your deployments. Run ${cmd('now alias help')}`
   );
