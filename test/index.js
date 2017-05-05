4b8ea75aef96fe672c3935e09f17d1495388d467
Nathan Rajlich <nathan@tootallnate.net>
2017-05-05 10:36:10 -0700

    test: restore "build" prefix
    Fixes Node.js <= v6

--- test/index.js
+++ test/index.js
@@ -6,12 +6,12 @@ const test = require('ava')
 const { asc: alpha } = require('alpha-sort')
 
 // Ours
-const hash = require('../lib/hash')
-const readMetadata = require('../lib/read-metadata')
+const hash = require('../build/lib/hash')
+const readMetadata = require('../build/lib/read-metadata')
 const {
   npm: getNpmFiles_,
   docker: getDockerFiles
-} = require('../lib/get-files')
+} = require('../build/lib/get-files')
 
 const prefix = join(__dirname, '_fixtures') + '/'
 const base = path => path.replace(prefix, '')
