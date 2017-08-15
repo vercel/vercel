340b8721ac91f47fc694a74e55b6032e70f41f39
Leo Lamprecht <mindrun@icloud.com>
2017-08-15 16:37:09 +0200

    Made sure that shortlinks point to the right place

--- src/get-default-auth-cfg.js
+++ src/get-default-auth-cfg.js
@@ -1,6 +1,6 @@
 const getDefaultAuthCfg = () => ({
   _:
-    'This is your Now credentials file. DON\'T SHARE! More: https://git.io/now-global-config',
+    'This is your Now credentials file. DON\'T SHARE! More: https://git.io/v7dds',
   credentials: []
 })
 
