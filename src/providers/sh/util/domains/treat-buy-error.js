b033558aebf0237ee94db3b34b6c78af43e95b23
Matheus Fernandes <github@matheus.top>
2017-09-19 01:55:28 +0200

    Add missing `console.log`

--- src/providers/sh/util/domains/treat-buy-error.js
+++ src/providers/sh/util/domains/treat-buy-error.js
@@ -3,27 +3,27 @@ const error = require('../../../../util/output/error')
 module.exports = function(err) {
   switch (err.code) {
     case 'invalid_domain': {
-      error('Invalid domain')
+      console.error(error('Invalid domain'))
       break
     }
     case 'not_available': {
-      error("Domain can't be purchased at this time")
+      console.error(error("Domain can't be purchased at this time"))
       break
     }
     case 'service_unavailabe': {
-      error('Purchase failed – Service unavailable')
+      console.error(error('Purchase failed – Service unavailable'))
       break
     }
     case 'unexpected_error': {
-      error('Purchase failed – Unexpected error')
+      console.error(error('Purchase failed – Unexpected error'))
       break
     }
     case 'forbidden_premium': {
-      error('A coupon cannot be used to register a premium domain')
+      console.error(error('A coupon cannot be used to register a premium domain'))
       break
     }
     default: {
-      error(err.message)
+      console.error(error(err.message))
     }
   }
 }
