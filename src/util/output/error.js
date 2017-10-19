697fd8b7c9e67aea9db82ae5858819838dbdf4d0
Leo Lamprecht <mindrun@icloud.com>
2017-10-20 00:49:09 +0200

    Only output error slug if it's available (#939)

--- src/util/output/error.js
+++ src/util/output/error.js
@@ -6,11 +6,11 @@ module.exports = (...input) => {
 
   if (typeof input[0] === 'object') {
     const {slug, message} = input[0]
+    messages = [ message ]
 
-    messages = [
-      message,
-      `> More details: https://err.sh/now-cli/${slug}`
-    ]
+    if (slug) {
+      messages.push(`> More details: https://err.sh/now-cli/${slug}`)
+    }
   }
 
   return `${red('> Error!')} ${messages.join('\n')}`
