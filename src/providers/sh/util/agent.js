38d2af83ee3fbf8c80e13acdc3657fcdd2110396
Pranay Prakash <pranay.gp@gmail.com>
2017-10-28 16:10:47 -0500

    first working version

--- src/providers/sh/util/agent.js
+++ src/providers/sh/util/agent.js
@@ -5,6 +5,10 @@ const https = require('https')
 
 // Packages
 const fetch = require('node-fetch')
+const { fetch: fetchH2, JsonBody, StreamBody, disconnect } = require('fetch-h2')
+const Sema = require('async-sema');
+
+const USE_HTTP2 = true;
 
 /**
  * Returns a `fetch` version with a similar
@@ -22,6 +26,7 @@ module.exports = class Agent {
     this._url = url
     const parsed = parse(url)
     this._protocol = parsed.protocol
+    this._sema = new Sema(20)
     this._debug = debug
     if (tls) {
       this._initAgent()
@@ -46,7 +51,14 @@ module.exports = class Agent {
     }
   }
 
-  fetch(path, opts = {}) {
+  setConcurrency({maxStreams, capacity}) {
+    this._sema = new Sema(maxStreams || 20, {capacity})
+  }
+
+  async fetch(path, opts = {}) {
+    await this._sema.v()
+    // console.log(path, this._sema.nrWaiting())
+
     if (!this._agent) {
       if (this._debug) {
         console.log('> [debug] re-initializing agent')
@@ -61,14 +73,26 @@ module.exports = class Agent {
 
     if (body && typeof body === 'object' && typeof body.pipe !== 'function') {
       opts.headers['Content-Type'] = 'application/json'
-      opts.body = JSON.stringify(body)
+      if (USE_HTTP2) {
+        opts.body = new JsonBody(body)
+      } else {
+        opts.body = JSON.stringify(body)
+      }
     }
 
-    if (opts.body && typeof body.pipe !== 'function') {
+    if(USE_HTTP2 && body && typeof body === 'object' && typeof body.pipe === 'function') {
+      opts.body = new StreamBody(body)
+    }
+
+    if (!USE_HTTP2 && opts.body && typeof body.pipe !== 'function') {
       opts.headers['Content-Length'] = Buffer.byteLength(opts.body)
     }
 
-    return fetch(this._url + path, opts)
+    if (USE_HTTP2) {
+      return fetchH2(this._url + path, opts).then(res => this._sema.p() || res)
+    } else {
+      return fetch(this._url + path, opts)
+    }
   }
 
   close() {
@@ -78,6 +102,9 @@ module.exports = class Agent {
 
     if (this._agent) {
       this._agent.destroy()
+    }
+    if (USE_HTTP2) {
+      disconnect(this._url)
     }
   }
 }
