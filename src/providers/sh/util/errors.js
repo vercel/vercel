4f2de9dd5d406f72c413663b1e75b2075a137c99
Joe Lim <xjlim@users.noreply.github.com>
2017-11-10 21:33:14 -0800

    Update errors.js

--- src/providers/sh/util/errors.js
+++ src/providers/sh/util/errors.js
@@ -5,18 +5,15 @@ const DNS_VERIFICATION_ERROR = `Please make sure that your nameservers point to 
   'zeit.world'
 )}.
 > Examples: (full list at ${chalk.underline('https://zeit.world')})
-> ${chalk.gray('-')} ${chalk.underline('california.zeit.world')}    ${chalk.dim(
-  '173.255.215.107'
+> ${chalk.gray('-')} ${chalk.underline('a.zeit.world')}    ${chalk.dim(
+  '96.45.80.1'
 )}
-> ${chalk.gray('-')} ${chalk.underline('newark.zeit.world')}        ${chalk.dim(
-  '173.255.231.87'
+> ${chalk.gray('-')} ${chalk.underline('b.zeit.world')}    ${chalk.dim(
+  '46.31.236.1'
 )}
-> ${chalk.gray('-')} ${chalk.underline('london.zeit.world')}        ${chalk.dim(
-  '178.62.47.76'
-)}
-> ${chalk.gray('-')} ${chalk.underline('singapore.zeit.world')}     ${chalk.dim(
-  '119.81.97.170'
-)}`
+> ${chalk.gray('-')} ${chalk.underline('c.zeit.world')}    ${chalk.dim(
+  '43.247.170.1'
+)}`;
 
 const DOMAIN_VERIFICATION_ERROR =
   DNS_VERIFICATION_ERROR +
