7ed32032874997cf928aa2674a30167841b83ef9
Matheus Fernandes <github@matheus.top>
2018-02-10 02:01:20 -0200

    We have to treat `switch` differently, unfortunately

--- src/providers/sh/commands/teams.js
+++ src/providers/sh/commands/teams.js
@@ -79,10 +79,11 @@ const main = async ctx => {
   const isSwitch = argv._[0] && argv._[0] === 'switch'
 
   argv._ = argv._.slice(1)
-  subcommand = argv._.shift()
 
   if (isSwitch) {
     subcommand = 'switch'
+  } else {
+    subcommand = argv._.shift()
   }
 
   if (argv.help || !subcommand) {
