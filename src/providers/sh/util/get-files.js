7d62fe5b54da30ecdf8e1c3e9c5ffe80fa580ffc
Leo Lamprecht <mindrun@icloud.com>
2018-02-26 16:46:54 -0800

    Ignore dockerfile as well

--- src/providers/sh/util/get-files.js
+++ src/providers/sh/util/get-files.js
@@ -165,7 +165,7 @@ async function staticFiles(
 
     const filter = ignore()
       .add(IGNORED + '\n' + clearRelative(gitIgnore))
-      .add([ 'now.json', 'package.json' ])
+      .add([ 'now.json', 'package.json', 'Dockerfile' ])
       .createFilter()
 
     const prefixLength = path.length + 1
