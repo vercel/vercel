ae8f037ff31d13afc74e251fd4611184ebfacee9
Leo Lamprecht <mindrun@icloud.com>
2018-03-06 15:33:42 -0800

    Compare client version

--- test/integration.js
+++ test/integration.js
@@ -4,9 +4,11 @@ const path = require('path')
 // Packages
 const crossSpawn = require('cross-spawn')
 const test = require('ava')
+const semVer = require('semver')
 
 // Utilities
 const logo = require('../src/util/output/logo')
+const pkg = require('../package')
 
 const binary = {
   darwin: 'now-macos',
@@ -17,23 +19,26 @@ const binary = {
 const binaryPath = path.resolve(__dirname, '../packed/' + binary)
 const deployHelpMessage = `${logo} now [options] <command | path>`
 
-test('packed "now help" prints deploy help message', async t => {
-  if (!process.env.CI) {
-    t.true(true)
-    return
-  }
-
-  const {stdout, code} = await spawn(binaryPath, ['help'])
+test('print the deploy help message', async t => {
+  const { stdout, code } = await spawn(binaryPath, ['help'])
 
   t.is(code, 0)
-  t.true(stdout.length > 1)
   t.true(stdout.includes(deployHelpMessage))
+})
+
+test('output the version', async t => {
+  const { stdout, code } = await spawn(binaryPath, ['--version'])
+  const version = stdout.trim()
+
+  t.is(code, 0)
+  t.truthy(semVer.valid(version))
+  t.is(version, pkg.version)
 })
 
 const spawn = (command, args) => new Promise((resolve, reject) => {
   const child = crossSpawn.spawn(command, args)
-
   let stdout = ''
+
   child.stdout.on('data', data => {
     stdout += data
   })
