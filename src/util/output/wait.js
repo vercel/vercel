5d5d7694da519061f2f8febc9a00ff0cb794ae41
Leo Lamprecht <mindrun@icloud.com>
2018-03-14 13:37:47 -0700

    Removed semis

--- src/util/output/wait.js
+++ src/util/output/wait.js
@@ -3,24 +3,27 @@ const { gray } = require('chalk')
 const eraseLines = require('./erase-lines')
 
 const wait = (msg, timeOut = 300, ora = ora2) => {
-  let running = false;
-  let spinner;
-  let stopped = false;
+  let running = false
+  let spinner
+  let stopped = false
 
   setTimeout(() => {
-    if (stopped) return;
+    if (stopped) return
+    
     spinner = ora(gray(msg))
     spinner.color = 'gray'
     spinner.start()
-    running = true;
-  }, timeOut);
+    
+    running = true
+  }, timeOut)
 
   return () => {
-    stopped = true;
+    stopped = true
+    
     if (running) {
       spinner.stop()
       process.stdout.write(eraseLines(1))
-      running = false;
+      running = false
     }
   }
 }
