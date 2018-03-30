af1dca2c9788cc8a61db207258ce31c5f35c2381
Javi Velasco <javier.velasco86@gmail.com>
2018-03-30 14:58:40 -0700

    Refactor alias (#1216)

--- src/providers/sh/commands/alias.js
+++ src/providers/sh/commands/alias.js
@@ -1,29 +1,30 @@
 // @flow
 
 // Packages
-const chalk = require('chalk')
 const arg = require('arg')
-const table = require('text-table')
-const ms = require('ms')
+const chalk = require('chalk')
 const fs = require('fs-extra')
+const ms = require('ms')
+const path = require('path');
 const plural = require('pluralize')
-const {resolve, basename} = require('path');
+const table = require('text-table')
 
 // Utilities
-const strlen = require('../util/strlen')
-const Now = require('../util/')
-const NowAlias = require('../util/alias')
+const { handleError } = require('../util/error')
+const argCommon = require('../util/arg-common')()
 const cmd = require('../../../util/output/cmd')
 const createOutput = require('../../../util/output')
-const argCommon = require('../util/arg-common')()
-const wait = require('../../../util/output/wait')
-const { handleError } = require('../util/error')
-const toHost = require('../util/to-host')
-const logo = require('../../../util/output/logo')
 const elapsed = require('../../../util/output/elapsed')
-const promptBool = require('../../../util/input/prompt-bool')
 const getContextName = require('../util/get-context-name')
+const humanizePath = require('../../../util/humanize-path')
 const isValidDomain = require('../util/domains/is-valid-domain')
+const logo = require('../../../util/output/logo')
+const Now = require('../util/')
+const NowAlias = require('../util/alias')
+const promptBool = require('../../../util/input/prompt-bool')
+const strlen = require('../util/strlen')
+const toHost = require('../util/to-host')
+const wait = require('../../../util/output/wait')
 
 const help = () => {
   console.log(`
@@ -125,20 +126,23 @@ module.exports = async function main(ctx: any): Promise<number> {
 
   const debugEnabled = argv['--debug']
   const output = createOutput({ debug: debugEnabled })
-  const args = argv._.slice(1)
+  let args
 
   switch (subcommand) {
     case 'ls':
     case 'list':
+      args = argv._.slice(1);
       return ls(ctx, argv, args, output);
     case 'rm':
     case 'remove':
+      args = argv._.slice(1)
       return rm(ctx, argv, args, output);
     case 'set':
-      return set(ctx, argv, ['set', ...args], output);
-    // default to set, and populate it to emulate it as one of the args
+      args = argv._.slice(1)
+      return set(ctx, argv, args, output);
     default:
-      return set(ctx, argv, ['set', ...args], output);
+      args = argv._
+      return set(ctx, argv, args, output);
   }
 }
 
@@ -381,17 +385,7 @@ async function rm (ctx, opts, args, output): Promise<number> {
 }
 
 async function set(ctx, opts, args, output): Promise<number> {
-  const { log, warn, error, debug } = output;
-  if (args.length > 2) {
-    error(`${cmd('now alias <deployment> <target>')} accepts at most two arguments`);
-    return 1;
-  }
-
-  // deployment id and target alias
-  let [id, target] = args;
-  let targets = [target]
-
-  // set up the context
+  // Prepare the context
   const {authConfig: { credentials }, config: { sh }} = ctx
   const {token} = credentials.find(item => item.provider === 'sh')
   const { user, currentTeam } = sh;
@@ -400,177 +394,314 @@ async function set(ctx, opts, args, output): Promise<number> {
   const { ['--debug']: debugEnabled } = opts;
   const now = new Now({ apiUrl, token, debug: debugEnabled, currentTeam })
 
-  // we will attempt to fetch the deployment based on the
-  // current directory, if applicalbe
-  let deployment;
-
-  // there's a chance that the user did `now alias <target>`
-  // directly, because he is in a directory
-  // note: this entire branch will be deprecated soon
-  if (id != null && target == null) {
-    debug(`only got one argument, assuming ${id} is the target`);
-    target = id;
-    targets = [target];
-    id = null;
-  }
-
-  // if both are null, we attempt to determine if we are in a
-  // deployment directory
-  // note: this entire branch will be deprecated soon
-  if (id == null) {
-    debug('no known deployment id, attempting to get it from current directory');
-    const cfg = await maybeGetDeploymentConfig(process.cwd(), opts['--local-config']);
-    if (cfg) {
-      const aliases = cfg.aliases || cfg.alias;
-
-      if (cfg.aliases) {
-        warn('The `aliases` field has been deprecated in favor of `alias`');
-      }
-
-      if (!aliases) {
-        error('Attempted to alias the deployment in the current directory, but no `alias` found in config');
-        help();
-        now.close();
-        return 1;
-      }
-
-      if (aliases && typeof aliases !== 'string' && !Array.isArray(aliases)) {
-        error('Attempted to alias deployment, but the `alias` config is neither a string nor an array');
-        now.close();
-        return 1;
-      }
-
-      if (!aliases.length) {
-        error('Attempted to alias deployment, but the `alias` config is empty');
-        now.close();
-        return 1;
-      }
-
-      targets = Array.from(aliases);
-
-      const appName = cfg.name || basename(resolve(process.cwd(), opts['--local-config'] || ''));
-      debug(`will look for deployments matching app ${appName}`);
-      const deployments = await now.list(appName, { version: 3 });
-
-      // we select as a candidate the latest deployment of the same name
-      // as the current project / directory, deployed
-      deployment = deployments
-        .sort(sortRecent())
-        .filter(dep => dep.state === 'READY' && dep.creator.uid === user.uid)[0]
-
-      if (deployment) {
-        debug(`deployment found "${deployment.url}"`);
-        id = deployment.id;
-      } else {
-        debug(`no deployment found matching app ${appName}`);
-        help();
-        now.close();
-        return 1;
-      }
-    } else {
-      error(`${cmd('now alias')} requires two arguments or to be run in a deployment with \`alias\` config`);
-      help();
-      now.close();
-      return 1;
+  // Get deployments and targets from opts and args
+  const params = await getTargetsAndDeployment(now, output, args, opts, user, contextName)
+  if (params instanceof NowError) {
+    switch (params.code) {
+      case 'CANT_FIND_CONFIG':
+        output.error(`Couldn't find a configuration file at \n    ${params.meta.paths.join('\n    ')}.`)
+        break
+      case 'CANT_PARSE_CONFIG':
+        output.error(`Couldn't parse configuration file ${params.meta.file}.`);
+        break
+      case 'NO_ALIAS_IN_CONFIG':
+        output.error(`Could't find an alias property into the given configuration.`)
+        break
+      case 'WRONG_ALIAS_IN_CONFIG':
+        output.error(`Wrong value "${params.meta.value}" for alias found in config. It must be a string or array of string.`)
+        break
+      case 'CANT_FIND_A_DEPLOYMENT':
+        output.error(`Could't find a deployment to alias. Please specify one from the command line.`)
+        break
+      case 'DEPLOYMENT_NOT_FOUND':
+        output.error(`Failed to find deployment "${params.meta.id}" under ${chalk.bold(contextName)}`)
+        break
+      case 'DEPLOYMENT_PERMISSION_DENIED':
+        output.error(`No permission to access deployment "${params.meta.id}" under ${chalk.bold(contextName)}`)
+        break
+      case 'TOO_MANY_ARGS':
+        output.error(`${cmd('now alias <deployment> <target>')} accepts at most two arguments`);
+        break
     }
+    now.close()
+    return 1
   }
 
-  // validate targets
-  // note: we have multiple targets because of the config. for now,
-  // CLI users can only set one target
-  for (const target of targets) {
-    if (!isValidDomain(target)) {
-      error(`The target alias domain "${target}" is not valid`);
-      now.close();
-      return 1;
-    }
+  // Now we are sure we have values for deployment and targets
+  const { deployment, targets } = params
+
+  // Validate the resolved targets
+  const targetError = targetsAreValid(targets)
+  if (targetError instanceof NowError) {
+    output.error(`Invalid target ${targetError.meta.target}`);
+    now.close()
+    return 1
   }
 
-  // only fetch the deployment if we got it as a parameter,
-  // and we didn't guess it based on the local directory
-  if (!deployment) {
-    const depFetchStart = Date.now();
-    const cancelWait = wait(`Fetching deployment "${id}" in ${chalk.bold(contextName)}`);
-
-    try {
-      deployment = await now.findDeployment(id)
-      cancelWait();
-    } catch (err) {
-      cancelWait();
-      now.close();
-
-      if (err.status === 404) {
-        error(`Failed to find deployment "${id}" in ${chalk.bold(contextName)}`)
-        return 1;
-      } else if (err.status === 403) {
-        error(`No permission to access deployment "${id}" in ${chalk.bold(contextName)}`)
-        return 1;
-      } else {
-        // unexpected
-        throw err;
-      }
-    }
-
-    log(`Found deployment "${deployment.url}" in ${chalk.bold(contextName)} ${elapsed(Date.now() - depFetchStart)}`);
-  }
-
-  if (id == null && !opts['--rules']) {
-    error(`Invalid number of arguments. Usage: ${cmd('`now alias set <deployment> <domain>`')}`)
-    help();
-    now.close();
-    return 1;
-  }
-
-  // attempt the alias directly
-  log('will alias')
-
+  console.log({ deployment, targets })
   now.close();
   return 0
 }
 
-async function maybeGetDeploymentConfig (path, configFile) {
-  const read = async (file) => {
-    if (await fs.exists(file)) {
-      return JSON.parse(await fs.readFile(file));
-    } else {
-      return null;
-    }
-  }
+type NowErrorArgs<T, M> = {
+  code: T,
+  meta?: M,
+  message?: string
+}
 
-  // if the config file is supplied by the user
-  // via --local-config
-  if (configFile) {
-    return read(resolve(path, configFile));
-  } else {
-    const mainConfig = await read(resolve(path, 'now.json'));
-    if (mainConfig) return mainConfig;
-    const pkg = await read(resolve(path, 'package.json'));
-    return pkg ? pkg.now || null : null;
+// A generic error to rule them all
+class NowError<T, M = {}> extends Error {
+  meta: M;
+  code: T;
+
+  constructor({ code, message, meta }: NowErrorArgs<T, M>) {
+    super(message)
+    this.code = code;
+    if (meta) {
+      this.meta = meta;
+    }
   }
 }
 
-//async function updatePathAlias(alias, aliasName, rules, domains, output) {
-  //const start = new Date()
-  //const res = await alias.updatePathBasedroutes(
-    //String(aliasName),
-    //rules,
-    //domains
-  //)
-  //const elapsed = ms(new Date() - start)
-  //if (res.error) {
-    //throw responseError(res);
-  //} else {
-    //output.success(
-      //`${res.ruleCount} rules configured for ${chalk.underline(
-        //res.alias
-      //)} [${elapsed}]`
-    //)
-  //}
-//}
+async function readFileSafe(file): Promise<string | null> {
+  return (await fs.exists(file))
+    ? await fs.readFile(file)
+    : null
+}
 
-// sorts by most recent deployment
-function sortRecent() {
-  return function recencySort(a, b) {
-    return b.created - a.created;
+type Config = {
+  alias: Array<string> | string,
+  aliases: Array<string> | string,
+  name: string,
+}
+
+type ReadConfigErrors = NowError<'CANT_PARSE_CONFIG', { file: string }>
+
+async function readConfigFromFile(file): Promise<Config | null | ReadConfigErrors> {
+  const content = await readFileSafe(file)
+  if (content === null) {
+    return content
   }
+  
+  try {
+    return (JSON.parse(content): Config);
+  } catch (error) {
+    return new NowError({
+      code: 'CANT_PARSE_CONFIG',
+      meta: { file }
+    })
+  }
+}
+
+async function readConfigFromPackage(file): Promise<Config | null | ReadConfigErrors> {
+  const content = await readFileSafe(file)
+  if (content === null) {
+    return content
+  }
+
+  try {
+    return JSON.parse(content).now;
+  } catch (error) {
+    return new NowError({
+      code: 'CANT_PARSE_CONFIG',
+      meta: { file }
+    })
+  }
+}
+
+type GetConfigErrors =
+  ReadConfigErrors |
+  NowError<'CANT_FIND_CONFIG', {paths: string[]}>
+
+let config
+
+async function getConfig(output, configFile): Promise<Config | GetConfigErrors> {
+  const localPath = process.cwd()
+  
+  // If config was already read, just return it
+  if (config) {
+    return config
+  }
+
+  // First try with the config supplied by the user via --local-config
+  if (configFile) {
+    const localFilePath = path.resolve(localPath, configFile)
+    output.debug(`Found config in provided --local-config path ${localFilePath}`)
+    const localConfig = await readConfigFromFile(localFilePath)
+    if (localConfig || localConfig instanceof NowError) {
+      config = localConfig
+      return localConfig
+    }
+  }
+
+  // Then try with now.json in the same directory
+  const nowFilePath = path.resolve(localPath, 'now.json')
+  const mainConfig = await readConfigFromFile(nowFilePath);
+  if (mainConfig && !(mainConfig instanceof NowError)) {
+    output.debug(`Found config in file ${nowFilePath}`)
+    config = mainConfig
+    return mainConfig;
+  }
+  
+  // Finally try with the package
+  const pkgFilePath = path.resolve(localPath, 'package.json')
+  const pkgConfig = await readConfigFromPackage(pkgFilePath);
+  if (pkgConfig && !(pkgConfig instanceof NowError)) {
+    output.debug(`Found config in package ${pkgFilePath}`)
+    config = pkgConfig
+    return pkgConfig
+  }
+
+  // If we couldn't find the config anywhere return error
+  return new NowError({
+    code: 'CANT_FIND_CONFIG' ,
+    meta: { paths: [nowFilePath, pkgFilePath].map(humanizePath) }
+  })
+}
+
+type GetInferredTargetsError =
+  GetConfigErrors |
+  NowError<'NO_ALIAS_IN_CONFIG'> |
+  NowError<'WRONG_ALIAS_IN_CONFIG', {value: any}>
+
+async function getInferredTargets(output, opts): Promise<Array<string> | GetInferredTargetsError> {  
+  // Read the configuration file from the best guessed location
+  const configResult = await getConfig(output, opts['--local-config']);
+  if (configResult instanceof NowError) {
+    return configResult
+  }
+
+  // This field is deprecated, warn about it
+  if (configResult.aliases) {
+    output.warn('The `aliases` field has been deprecated in favor of `alias`');
+  }
+
+  // The aliases can be stored in both aliases or alias
+  const aliases = configResult.aliases || configResult.alias
+  if (!aliases) {
+    return new NowError({ code: 'NO_ALIAS_IN_CONFIG' })
+  }
+
+  // Check the type for the option aliases
+  if (typeof aliases !== 'string' && !Array.isArray(aliases)) {
+    return new NowError({ code: 'WRONG_ALIAS_IN_CONFIG', meta: { value: aliases }})
+  }
+
+  // Always resolve with an array
+  return (Array.from(aliases): Array<string>)
+}
+
+async function getAppName(output, opts): Promise<string> {
+  const config = await getConfig(output, opts['--local-config'])
+  return config instanceof NowError || !config.name
+    ? path.basename(path.resolve(process.cwd(), opts['--local-config'] || ''))
+    : config.name
+}
+
+type Deployment = {
+  uid: string,
+  url: string,
+  name: string,
+  created: number,
+  creator: { uid: string, },
+  state: 'FROZEN' | 'READY',
+  type: 'NPM',
+}
+
+async function getAppLastDeployment(output, now, appName, user, contextName): Promise<Deployment | null> {
+  output.debug(`Looking for deployments matching app ${appName}`)
+  const cancelWait = wait(`Fetching user deployments in ${chalk.bold(contextName)}`);
+  const deployments: Array<Deployment> = await now.list(appName, { version: 3 });
+  cancelWait()
+  return deployments
+    .sort((a, b) => b.created - a.created)
+    .filter(dep => dep.state === 'READY' && dep.creator.uid === user.uid)[0]
+}
+
+type FetchDeploymentErrors =
+  NowError<'DEPLOYMENT_NOT_FOUND', { id: string }> |
+  NowError<'DEPLOYMENT_PERMISSION_DENIED', { id: string }>
+
+async function fetchDeployment(output, now, contextName, id): Promise<Deployment | FetchDeploymentErrors> {
+  const cancelWait = wait(`Fetching deployment "${id}" in ${chalk.bold(contextName)}`);
+  try {
+    const deployment = await now.findDeployment(id)
+    cancelWait();
+    return deployment
+  } catch (err) {
+    cancelWait();
+    if (err.status === 404) {
+      return new NowError({ code: 'DEPLOYMENT_NOT_FOUND', meta: { id } })
+    } else if (err.status === 403) {
+      return new NowError({ code: 'DEPLOYMENT_PERMISSION_DENIED', meta: { id } })
+    } else {
+      throw err;
+    }
+  }
+}
+
+type SetAliasArgs = {
+  deployment: Deployment,
+  targets: Array<string>,
+}
+
+type SetAliasErrors = 
+  NowError<'CANT_FIND_DEPLOYMENT'> |
+  NowError<'TOO_MANY_ARGS'> |
+  GetInferredTargetsError | 
+  FetchDeploymentErrors
+
+async function getTargetsAndDeployment(now, output, args: Array<string>, opts, user, contextName): Promise<SetAliasArgs | SetAliasErrors> {
+  // When there are no args at all we try to get the targets from the config
+  if (args.length === 0) {
+    output.debug('Looking for targets in the configuration directory');
+    const targets = await getInferredTargets(output, opts)
+    if (targets instanceof NowError) {
+      return targets
+    }
+
+    const appName = await getAppName(output, opts)
+    const deployment = await getAppLastDeployment(output, now, appName, user)
+    return !deployment 
+      ? new NowError({ code: 'CANT_FIND_A_DEPLOYMENT' })
+      : { deployment, targets }
+  }
+
+  // When there is one arg we assume the user typed `now alias <target> because
+  // he is in the app directory so we can infer the deployment
+  if (args.length === 1) {
+    const targets = [args[0]]
+    const appName = await getAppName(output, opts)
+    const deployment = await getAppLastDeployment(output, now, appName, user)
+    return !deployment
+      ? new NowError({ code: 'CANT_FIND_A_DEPLOYMENT' })
+      : { deployment, targets }
+  }
+
+  // In any other case the user provided both id and deployment id so we can just
+  // fetch the deployment id to ensure it exists
+  if (args.length === 2) {
+    const [deploymentId, target] = args
+    const deployment = await fetchDeployment(output, now, contextName, deploymentId)
+    return !(deployment instanceof NowError)
+      ? { deployment, targets: [target] }
+      : deployment
+  }
+
+  return new NowError({ code: 'TOO_MANY_ARGS' })
+}
+
+type DomainValidationError = NowError<'INVALID_TARGET_DOMAIN', { target: string }>
+
+function targetsAreValid(targets): Array<string> | DomainValidationError {
+  for (const target of targets) {
+    if (!isValidDomain(target)) {
+      return new NowError({
+        code: 'INVALID_TARGET_DOMAIN',
+        meta: { target }
+      })
+    }
+  }
+
+  return targets
 }
