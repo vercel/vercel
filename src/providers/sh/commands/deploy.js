bbdc8dcaec72fac6903075fbaede3759590c146c
Igor Klopov <igor@klopov.com>
2018-04-01 04:42:52 +0300

    add polling (#1220)
    * add polling
    
    * hack to prevent partial json to be unluckily parsed in _flush
    
    * cleanupAndResolveCalled to prevent running twice
    
    * call onOpen only of going to 'log'. also call from cleanup
    
    * callOnOpenOnce, don't reject on stream error, polling to finish
    
    * fix eraseLines, make cleanup routine reject
    
    * remove build-logger.js
    
    * restored printEvent from deleted build-logger.js

--- src/providers/sh/commands/deploy.js
+++ src/providers/sh/commands/deploy.js
@@ -912,9 +912,29 @@ async function readMeta(
   }
 }
 
+function printEvent(type, text) {
+  if (type === 'command') {
+    log(`▲ ${text}`)
+  } else if (type === 'stdout' || type === 'stderr') {
+    text.split('\n').forEach(v => {
+      // strip out the beginning `>` if there is one because
+      // `log()` prepends its own and we don't want `> >`
+      log(v.replace(/^> /, ''))
+    })
+  }
+}
+
 async function printEvents(now, currentTeam = null, {
   onOpen = ()=>{}
 } = {}) {
+  let onOpenCalled = false
+  function callOnOpenOnce() {
+    if (onOpenCalled) return
+    onOpenCalled = true
+    onOpen()
+  }
+
+  let pollUrl = `/v1/now/deployments/${now.id}`
   let eventsUrl = `/v1/now/deployments/${now.id}/events?follow=1`
 
   if (currentTeam) {
@@ -933,64 +953,98 @@ async function printEvents(now, currentTeam = null, {
     }
 
     // if we are retrying, we clear past logs
-    if (!quiet && o) process.stdout.write(eraseLines(0))
+    if (!quiet && o) {
+      // o + 1 because current line is counted
+      process.stdout.write(eraseLines(o + 1))
+      o = 0
+    }
 
-    const res = await now._fetch(eventsUrl)
-    if (res.ok) {
-      const readable = await res.readable();
+    const eventsRes = await now._fetch(eventsUrl)
+    if (eventsRes.ok) {
+      const readable = await eventsRes.readable()
 
       // handle the event stream and make the promise get rejected
       // if errors occur so we can retry
       return new Promise((resolve, reject) => {
         const stream = readable.pipe(jsonlines.parse())
 
-        function cleanupAndResolve() {
+        let poller = (function startPoller() {
+          return setTimeout(async () => {
+            try {
+              const pollRes = await now._fetch(pollUrl)
+              if (!pollRes.ok) throw new Error(`Response ${pollRes.status}`)
+              const json = await pollRes.json()
+              if (json.state === 'READY') {
+                cleanup()
+                return
+              }
+              poller = startPoller()
+            } catch (error) {
+              cleanup(error)
+            }
+          }, 5000)
+        })()
+
+        let cleanupAndResolveCalled = false
+        function cleanup(error) {
+          if (cleanupAndResolveCalled) return
+          cleanupAndResolveCalled = true
+          callOnOpenOnce()
+          if (!error) log(chalk`{cyan Success!} Build complete`)
+          clearTimeout(poller)
           // avoid lingering events
           stream.removeListener('data', onData)
+          // prevent partial json from being parsed and error emitted.
+          // this can be reproduced by force placing stream.write('{{{') here
+          stream._emitInvalidLines = true
           // close the stream and resolve
           stream.end()
-          resolve()
+          if (error) {
+            reject(error)
+          } else {
+            resolve()
+          }
         }
 
         const onData = ({ type, event, text }) => {
-          // fire the open callback and ensure it's only fired once
-          onOpen()
-          onOpen = ()=>{}
-
           // if we are 'quiet' because we are piping, simply
           // wait for the first instance to be started
           // and ignore everything else
           if (quiet) {
             if (type === 'instance-start') {
-              cleanupAndResolve()
+              cleanup()
             }
             return
           }
 
           if (event === 'build-start') {
             o++
+            callOnOpenOnce()
             log('Building…')
           } else
-          if (event === 'build-stop' ||
-              event === 'build-complete') {
-            o++
-            log(chalk`{cyan Success!} Build complete`)
-            cleanupAndResolve()
+          if (event === 'build-complete') {
+            cleanup()
           } else
           if ([ 'command', 'stdout', 'stderr' ].includes(type)) {
             if (text.slice(-1) === '\n') text = text.slice(0, -1)
-            log(text)
+            o += text.split('\n').length
+            callOnOpenOnce()
+            printEvent(type, text)
           }
         }
+
         stream.on('data', onData)
         stream.on('error', err => {
-          reject(new Error(`Deployment event stream error: ${err.stack}`))
+          o++
+          callOnOpenOnce()
+          log(`Deployment event stream error: ${err.message}`)
         })
       })
     } else {
-      const err = new Error(`Deployment events status ${res.status}`)
+      callOnOpenOnce()
+      const err = new Error(`Deployment events status ${eventsRes.status}`)
 
-      if (res.status < 500) {
+      if (eventsRes.status < 500) {
         bail(err)
       } else {
         throw err
