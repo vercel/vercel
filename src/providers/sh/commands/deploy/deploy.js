c3124d93d7ceee4bcac77b5c4bdcfb3d0e5b551b
Javi Velasco <javier.velasco86@gmail.com>
2018-04-18 09:18:47 -0700

    Handle ENOENT error when deploying unexistent path

--- src/providers/sh/commands/deploy/deploy.js
+++ src/providers/sh/commands/deploy/deploy.js
@@ -405,13 +405,20 @@ async function sync({ contextName, output, token, config: { currentTeam, user },
       checkers.push(checkPath(paths[0]))
     } else {
       for (const path of paths) {
-        const fsData = await fs.lstat(path)
-
-        if (fsData.isFile()) {
-          continue
+        try {
+          const fsData = await fs.lstat(path)
+          if (fsData.isFile()) {
+            continue
+          }
+          checkers.push(checkPath(path))
+        } catch (error) {
+          if (error.code === 'ENOENT') {
+            output.error(error.message, 'path-not-deployable')
+            await exit(1)
+          } else {
+            throw error
+          }
         }
-
-        checkers.push(checkPath(path))
       }
     }
 
