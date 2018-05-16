d7ac562cc5680e553219c83a29caaba1f6fa641e
Leo Lamprecht <mindrun@icloud.com>
2018-05-16 20:53:26 +0200

    Removed type for now

--- src/providers/sh/commands/alias/get-domain-nameservers.js
+++ src/providers/sh/commands/alias/get-domain-nameservers.js
@@ -3,14 +3,10 @@ import wait from '../../../../util/output/wait'
 import { Now } from '../../util/types'
 import { DomainNameserversNotFound } from '../../util/errors'
 
-type NameserversResponse = {
-  nameservers: string[]
-}
-
 async function getDomainNameservers(now: Now, domain: string) {
   const cancelFetchingMessage = wait(`Fetching DNS nameservers for ${domain}`)
   try {
-    let { nameservers }: NameserversResponse = await now.fetch(`/whois-ns?domain=${encodeURIComponent(domain)}`)
+    let { nameservers } = await now.fetch(`/whois-ns?domain=${encodeURIComponent(domain)}`)
     cancelFetchingMessage()
     return nameservers.filter(ns => {
       // Temporary hack since sometimes we get a response that looks like: ['ns', 'ns', '', '']
