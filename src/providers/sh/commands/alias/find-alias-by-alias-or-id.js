a4935c33b4c17b13c4af66b015a3dda9edbc6182
Igor Klopov <igor@klopov.com>
2018-06-02 01:27:47 +0300

    reuse getSafeAlias function for all callers of findAliasByAliasOrId (#1385)

--- src/providers/sh/commands/alias/find-alias-by-alias-or-id.js
+++ src/providers/sh/commands/alias/find-alias-by-alias-or-id.js
@@ -2,6 +2,14 @@
 import { Now, Output } from '../../util/types'
 import type { Alias } from '../../util/types'
 
+function getSafeAlias(alias: string) {
+  return alias
+    .replace(/^https:\/\//i, '')
+    .replace(/^\.+/, '')
+    .replace(/\.+$/, '')
+    .toLowerCase()
+}
+
 export default async function findAliasByAliasOrId(output: Output, now: Now, aliasOrId: string): Promise<Alias> {
-  return now.fetch(`/now/aliases/${encodeURIComponent(aliasOrId)}`);
+  return now.fetch(`/now/aliases/${encodeURIComponent(getSafeAlias(aliasOrId))}`);
 }
