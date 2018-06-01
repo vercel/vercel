c312d42302d69c3c75449195f19df45793e8139e
Igor Klopov <igor@klopov.com>
2018-06-02 01:27:47 +0300

    reuse getSafeAlias function for all callers of findAliasByAliasOrId (#1385)

--- src/providers/sh/commands/alias/get-previous-alias.js
+++ src/providers/sh/commands/alias/get-previous-alias.js
@@ -4,15 +4,7 @@ import findAliasByAliasOrId from './find-alias-by-alias-or-id'
 import type { Alias } from '../../util/types'
 
 async function getPreviousAlias(output: Output, now: Now, alias: string): Promise<Alias | void> {
-  return findAliasByAliasOrId(output, now, getSafeAlias(alias))
-}
-
-function getSafeAlias(alias: string) {
-  return alias
-    .replace(/^https:\/\//i, '')
-    .replace(/^\.+/, '')
-    .replace(/\.+$/, '')
-    .toLowerCase()
+  return findAliasByAliasOrId(output, now, alias)
 }
 
 export default getPreviousAlias
