b29785d851c8e1531bf841ffd88e698390689700
Javi Velasco <javier.velasco86@gmail.com>
2018-06-07 10:52:24 +0200

    Ensure there is scale rules before trying to apply (#1389)

--- src/providers/sh/commands/alias/deployment-should-copy-scale.js
+++ src/providers/sh/commands/alias/deployment-should-copy-scale.js
@@ -3,7 +3,8 @@ import type { NpmDeployment, BinaryDeployment } from '../../util/types'
 import getScaleForDC from './get-scale-for-dc'
 
 function shouldCopyScalingAttributes(origin: NpmDeployment | BinaryDeployment, dest: NpmDeployment | BinaryDeployment) {
-  return getScaleForDC('bru1', origin).min !== getScaleForDC('bru1', dest).min ||
+  return Boolean(origin.scale) &&
+    getScaleForDC('bru1', origin).min !== getScaleForDC('bru1', dest).min ||
     getScaleForDC('bru1', origin).max !== getScaleForDC('bru1', dest).max ||
     getScaleForDC('sfo1', origin).min !== getScaleForDC('sfo1', dest).min ||
     getScaleForDC('sfo1', origin).max !== getScaleForDC('sfo1', dest).max
