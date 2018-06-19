a0820676262bb873f3c0e7b5a7e8a1444129da64
Pranay Prakash <pranay.gp@gmail.com>
2018-06-19 16:58:40 -0700

    get-state-change-from-polling -> shiksha

--- src/providers/sh/util/deploy/get-state-change-from-polling.js
+++ src/providers/sh/util/deploy/get-state-change-from-polling.js
@@ -1,32 +1,45 @@
 // @flow
-import sleep from 'then-sleep'
-import { Now } from '../../util/types'
-import createPollingFn from '../../../../util/create-polling-fn'
-import type { StateChangeEvent } from '../types'
-import getDeploymentByIdOrThrow from './get-deployment-by-id-or-throw'
+import { Now } from '../../util/types';
+import createPollingFn from '../../../../util/create-polling-fn';
+import type { StateChangeEvent } from '../types';
+import getDeploymentByIdOrThrow from './get-deployment-by-id-or-throw';
+import { map, pairwise, unshift, filter, delay } from 'shiksha';
 
-const POLLING_INTERVAL = 1000
+const POLLING_INTERVAL = 1000;
 
-async function* getStatusChangeFromPolling(
+const getStatusChangeFromPolling = (
   now: Now,
   contextName: string,
   idOrHost: string,
-  initialState: 'INITIALIZING' | 'FROZEN' | 'READY' | 'ERROR',
-): AsyncGenerator<StateChangeEvent, void, void> {
-  const pollDeployment = createPollingFn(getDeploymentByIdOrThrow, POLLING_INTERVAL)
-  let prevState = initialState
-  for await (const deployment of pollDeployment(now, contextName, idOrHost)) {
-    if (prevState !== deployment.state) {
-      await sleep(5000)
-      yield {
+  initialState: 'INITIALIZING' | 'FROZEN' | 'READY' | 'ERROR'
+): AsyncGenerator<StateChangeEvent, void, void> => {
+  const pollDeployment = createPollingFn(
+    getDeploymentByIdOrThrow,
+    POLLING_INTERVAL
+  );
+
+  return delay(
+    5000,
+    map(
+      ([, state]) => ({
         type: 'state-change',
         created: Date.now(),
-        payload: { value: deployment.state }
-      }
-    } else {
-      prevState = deployment.state
-    }
-  }
-}
+        payload: { value: state }
+      }),
+      filter(
+        ([prev, curr]) => prev !== curr,
+        unshift(
+          initialState,
+          pairwise(
+            map(
+              deployment => deployment.state,
+              pollDeployment(now, contextName, idOrHost)
+            )
+          )
+        )
+      )
+    )
+  );
+};
 
-export default getStatusChangeFromPolling
+export default getStatusChangeFromPolling;
