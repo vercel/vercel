8b79cc87dd328cbfe5b4eea0980c2dbf7e7a2190
Matheus Fernandes <matheus@zeit.co>
2018-07-08 20:46:04 -0700

    Revert "Upgrade to webpack 4 and latest Babel (#1436)"
    This reverts commit 7612d776477ab0e6853ef2a30ddc3cb001d12c63.

--- download/webpack.config.js
+++ download/webpack.config.js
@@ -2,35 +2,33 @@
 const path = require('path')
 
 module.exports = {
-  target: 'node',
-  node: {
-      __dirname: false,
-      __filename: false,
-      process: false
-  },
-  entry: [
-      './src/index.js'
-  ],
-  output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'download.js'
-  },
-  module: {
-      rules: [{
-        test: /.js$/,
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          query: {
-              plugins: [
-                  '@babel/transform-async-to-generator',
-                  '@babel/transform-runtime'
-              ],
-              presets: [
-                  '@babel/preset-env'
-              ]
-          }
-        }]
-      } ]
-  }
+    target: 'node',
+    node: {
+        __dirname: false,
+        __filename: false,
+        process: false
+    },
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'download.js'
+    },
+    module: {
+        loaders: [ {
+            test: /.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                plugins: [
+                    '@babel/transform-async-to-generator',
+                    '@babel/transform-runtime'
+                ],
+                presets: [
+                    '@babel/preset-env'
+                ]
+            }
+        } ]
+    }
 }
