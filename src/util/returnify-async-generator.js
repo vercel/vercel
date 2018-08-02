7dab4a01f20ea56ae6b5f29cc18bb9c0f56560ed
Pranay Prakash <pranay.gp@gmail.com>
2018-08-02 04:54:51 +0000

    add comment for returnify and exit when the iterator ends

--- src/util/returnify-async-generator.js
+++ src/util/returnify-async-generator.js
@@ -2,6 +2,11 @@
 
 type Maybe<T> = [Error, null] | [null, T]
 
+/*
+ * Returnify iterates through an async iterator and yields the values
+ * as Maybe monads. If the async iterator throws an error, we continue
+ * iterating through it after yielding a failed maybe monad.
+ */
 async function* returnify<T>(
   it: AsyncIterator<T>
 ): AsyncGenerator<Maybe<T>, void, void> {
@@ -10,6 +15,7 @@ async function* returnify<T>(
       for await (const v of it) {
         yield [null, v]
       }
+      break;
     } catch (e) {
       yield [(e: Error), null]
     }
