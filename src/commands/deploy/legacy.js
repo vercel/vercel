9ef1dbc0ead0e006a39383b2de6ef5b4e21e2786
Leo Lamprecht <mindrun@icloud.com>
2018-11-08 21:37:44 +0100

    legacy => old

--- src/commands/deploy/legacy.js
+++ src/commands/deploy/legacy.js
@@ -375,7 +375,7 @@ exports.pipe = async function main(
   ({ log, error, note, debug, warn } = output);
 
   warn(
-    'You are using a legacy version of the Now Platform. More: https://zeit.co/docs/v1-upgrade'
+    'You are using an old version of the Now Platform. More: https://zeit.co/docs/v1-upgrade'
   );
 
   const { authConfig: { token }, config } = ctx;
