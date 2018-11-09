d275744e04156e6a81d7f2bb35c95c8acc613c5d
Igor Klopov <igor@klopov.com>
2018-11-09 17:21:29 +0300

    [build-utils] use npm install if package-lock.json is around (#1)

--- now-build-utils/fs/run-user-scripts.js
+++ now-build-utils/fs/run-user-scripts.js
@@ -1,3 +1,4 @@
+const fs = require('fs-extra');
 const path = require('path');
 const { spawn } = require('child_process');
 
@@ -7,15 +8,43 @@ async function runShellScript (fsPath) {
   return true;
 }
 
+async function shouldUseNpm (destPath) {
+  while (true) {
+    if (await fs.exists(path.join(destPath, 'package.json'))) {
+      if (await fs.exists(path.join(destPath, 'package-lock.json'))) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    const newDestPath = path.dirname(destPath);
+    if (destPath === newDestPath) break;
+    destPath = newDestPath;
+  }
+
+  return false;
+}
+
 async function runNpmInstall (destPath, args = []) {
   console.log(`installing to ${destPath}`);
-  await spawnAsync('yarn', [ '--cwd', destPath ].concat(args), destPath);
+  if (await shouldUseNpm(destPath)) {
+    args = args.filter((a) => a !== '--prefer-offline');
+    await spawnAsync('npm', [ 'install' ].concat(args), destPath);
+  } else {
+    await spawnAsync('yarn', [ '--cwd', destPath ].concat(args), destPath);
+  }
 }
 
 async function runPackageJsonScript (destPath, scriptName) {
   try {
-    console.log(`running "yarn run ${scriptName}"`);
-    await spawnAsync('yarn', [ '--cwd', destPath, 'run', scriptName ], destPath);
+    if (await shouldUseNpm(destPath)) {
+      console.log(`running "npm run ${scriptName}"`);
+      await spawnAsync('npm', [ 'run', scriptName ], destPath);
+    } else {
+      console.log(`running "yarn run ${scriptName}"`);
+      await spawnAsync('yarn', [ '--cwd', destPath, 'run', scriptName ], destPath);
+    }
   } catch (error) {
     console.log(error.message);
     return false;
