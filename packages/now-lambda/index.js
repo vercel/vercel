c8fce906127ca48b15fcc18cfe2b32e07fa185e6
Igor Klopov <igor@klopov.com>
2018-11-13 23:42:59 +0300

    [now/lambda] take handler and runtime from now.json's config

--- packages/now-lambda/index.js
+++ packages/now-lambda/index.js
@@ -1,28 +1,12 @@
 const { Lambda } = require('@now/build-utils/lambda.js');
-const path = require('path');
 const streamToBuffer = require('@now/build-utils/fs/stream-to-buffer.js');
 
-exports.build = async ({ files, entrypoint }) => {
+exports.build = async ({ files, entrypoint, config }) => {
   if (!files[entrypoint]) throw new Error('Entrypoint not found in files');
-
-  // handler=launcher.main!runtime=nodejs8.10!name.zip
-  const config = path.basename(entrypoint).split('!').reduce((a, c) => {
-    const [k, v] = c.split('=');
-    // eslint-disable-next-line no-param-reassign
-    if (v) a[k] = v;
-    return a;
-  }, {});
-
-  if (!config.handler) throw new Error('Handler not found in config');
-  if (!config.runtime) throw new Error('Runtime not found in config');
-
+  const { handler, runtime } = config;
+  if (!handler) throw new Error('Handler not found in config');
+  if (!runtime) throw new Error('Runtime not found in config');
   const zipBuffer = await streamToBuffer(files[entrypoint].toStream());
-
-  const lambda = new Lambda({
-    zipBuffer,
-    handler: config.handler,
-    runtime: config.runtime,
-  });
-
+  const lambda = new Lambda({ zipBuffer, handler, runtime });
   return { [entrypoint]: lambda };
 };
