e55b84b64681512bc1f7e1052fe8c2be87163389
Igor Klopov <igor@klopov.com>
2018-11-16 17:45:21 +0300

    [build-utils] clear cache right away after npmInstall

--- packages/now-build-utils/fs/run-user-scripts.js
+++ packages/now-build-utils/fs/run-user-scripts.js
@@ -6,7 +6,9 @@ function spawnAsync(command, args, cwd) {
   return new Promise((resolve, reject) => {
     const child = spawn(command, args, { stdio: 'inherit', cwd });
     child.on('error', reject);
-    child.on('close', (code, signal) => (code !== 0 ? reject(new Error(`Exited with ${code || signal}`)) : resolve()));
+    child.on('close', (code, signal) => (code !== 0
+      ? reject(new Error(`Exited with ${code || signal}`))
+      : resolve()));
   });
 }
 
@@ -43,8 +45,10 @@ async function runNpmInstall(destPath, args = []) {
   if (await shouldUseNpm(destPath)) {
     commandArgs = args.filter(a => a !== '--prefer-offline');
     await spawnAsync('npm', ['install'].concat(commandArgs), destPath);
+    await spawnAsync('npm', ['cache', 'clean', '--force'], destPath);
   } else {
     await spawnAsync('yarn', ['--cwd', destPath].concat(commandArgs), destPath);
+    await spawnAsync('yarn', ['cache', 'clean'], destPath);
   }
 }
 
@@ -55,7 +59,11 @@ async function runPackageJsonScript(destPath, scriptName) {
       await spawnAsync('npm', ['run', scriptName], destPath);
     } else {
       console.log(`running "yarn run ${scriptName}"`);
-      await spawnAsync('yarn', ['--cwd', destPath, 'run', scriptName], destPath);
+      await spawnAsync(
+        'yarn',
+        ['--cwd', destPath, 'run', scriptName],
+        destPath,
+      );
     }
   } catch (error) {
     console.log(error.message);
