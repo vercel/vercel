a58451548f226408dc4108a0b2150f0b146eee03
Igor Klopov <igor@klopov.com>
2018-11-21 19:06:41 +0300

    simple tests for now/static-build

---
+++ packages/now-static-build/test/test.js
@@ -0,0 +1,20 @@
+/* global beforeAll, expect, it, jest */
+const path = require('path');
+const {
+  packAndDeploy,
+  testDeployment,
+} = require('../../../test/lib/deployment/test-deployment.js');
+
+jest.setTimeout(10 * 60 * 1000);
+let builderUrl;
+
+beforeAll(async () => {
+  const builderPath = path.resolve(__dirname, '..');
+  builderUrl = await packAndDeploy(builderPath);
+});
+
+it('should build 01-cowsay', async () => {
+  await expect(
+    testDeployment(builderUrl, path.resolve(__dirname, 'fixtures/01-cowsay')),
+  ).resolves.toBe(undefined);
+});
