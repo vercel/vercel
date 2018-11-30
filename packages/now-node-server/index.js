756c452203fb35c629728f4dc48bc7abda4ee2fe
Igor Klopov <igor@klopov.com>
2018-11-30 07:59:11 +0300

    bump ncc in node-server and node

--- packages/now-node-server/index.js
+++ packages/now-node-server/index.js
@@ -47,7 +47,7 @@ async function downloadInstallAndBundle(
       'package.json': new FileBlob({
         data: JSON.stringify({
           dependencies: {
-            '@zeit/ncc': '0.1.15',
+            '@zeit/ncc': '0.1.18',
           },
         }),
       }),
--- packages/now-node/index.js
+++ packages/now-node/index.js
@@ -45,7 +45,7 @@ async function downloadInstallAndBundle(
       'package.json': new FileBlob({
         data: JSON.stringify({
           dependencies: {
-            '@zeit/ncc': '0.1.15',
+            '@zeit/ncc': '0.1.18',
           },
         }),
       }),
