f328a53a3f6b6d3139f2e38ca06170130c135236
Igor Klopov <igor@klopov.com>
2018-12-03 04:51:20 +0300

    bump ncc to 0.2.0 (#110)

--- packages/now-node-server/index.js
+++ packages/now-node-server/index.js
@@ -47,7 +47,7 @@ async function downloadInstallAndBundle(
       'package.json': new FileBlob({
         data: JSON.stringify({
           dependencies: {
-            '@zeit/ncc': '0.1.18',
+            '@zeit/ncc': '0.2.0',
           },
         }),
       }),
