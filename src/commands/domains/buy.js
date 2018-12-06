39262225425c3c21166752359e10577b3eea0397
Javi Velasco <javier.velasco86@gmail.com>
2018-12-06 21:00:17 +0100

    Remove spaces from domains buy

--- src/commands/domains/buy.js
+++ src/commands/domains/buy.js
@@ -1,27 +1,19 @@
-//      
 import chalk from 'chalk';
 import psl from 'psl';
 
-
 import * as Errors from '../../util/errors';
+import Now from '../../util';
 import cmd from '../../util/output/cmd';
-import getScope from '../../util/get-scope';
 import getDomainPrice from '../../util/domains/get-domain-price';
 import getDomainStatus from '../../util/domains/get-domain-status';
-import Now from '../../util';
+import getScope from '../../util/get-scope';
 import param from '../../util/output/param';
 import promptBool from '../../util/input/prompt-bool';
 import purchaseDomain from '../../util/domains/purchase-domain';
 import stamp from '../../util/output/stamp';
-                                                          
 import wait from '../../util/output/wait';
 
-export default async function buy(
-  ctx            ,
-  opts                   ,
-  args          ,
-  output        
-)                  {
+export default async function buy(ctx, opts, args, output) {
   const { authConfig: { token }, config } = ctx;
   const { currentTeam } = config;
   const { apiUrl } = ctx;
@@ -33,7 +25,6 @@ export default async function buy(
     currentTeam
   });
 
-  // $FlowFixMe
   const now = new Now({ apiUrl, token, debug, currentTeam });
   const coupon = opts['--coupon'];
   const domainName = args[0];
@@ -45,9 +36,7 @@ export default async function buy(
 
   const { domain: rootDomain, subdomain } = psl.parse(domainName);
   if (subdomain || !rootDomain) {
-    output.error(
-      `Invalid domain name "${domainName}". Run ${cmd('now domains --help')}`
-    );
+    output.error(`Invalid domain name "${domainName}". Run ${cmd('now domains --help')}`);
     return 1;
   }
 
@@ -56,19 +45,20 @@ export default async function buy(
   if (domainPrice instanceof Errors.InvalidCoupon) {
     output.error(`The coupon ${param(coupon)} is not valid.`);
     return 1;
-  } if (domainPrice instanceof Errors.UsedCoupon) {
+  }
+
+  if (domainPrice instanceof Errors.UsedCoupon) {
     output.error(`The coupon ${param(coupon)} has already been used.`);
     return 1;
   }
+
   if (domainPrice instanceof Errors.UnsupportedTLD) {
     output.error(`The TLD for ${param(domainName)} is not supported.`);
     return 1;
   }
 
   if (domainPrice instanceof Errors.MissingCreditCard) {
-    output.print(
-      'You have no credit cards on file. Please add one in order to claim your free domain'
-    );
+    output.print('You have no credit cards on file. Please add one in order to claim your free domain');
     output.print(`Your card will ${chalk.bold('not')} be charged`);
     return 1;
   }
@@ -100,43 +90,35 @@ export default async function buy(
 
   const purchaseStamp = stamp();
   const stopPurchaseSpinner = wait('Purchasing');
-  const buyResult = await purchaseDomain(
-    output,
-    now,
-    domainName,
-    coupon,
-    price
-  );
+  const buyResult = await purchaseDomain(output, now, domainName, coupon, price);
   stopPurchaseSpinner();
 
   if (buyResult instanceof Errors.InvalidDomain) {
     output.error(`The domain ${buyResult.meta.domain} is not valid.`);
     return 1;
-  } if (buyResult instanceof Errors.DomainNotAvailable) {
+  }
+
+  if (buyResult instanceof Errors.DomainNotAvailable) {
     output.error(`The domain ${buyResult.meta.domain} is not available.`);
     return 1;
-  } if (buyResult instanceof Errors.DomainServiceNotAvailable) {
-    output.error(
-      `The domain purchase service is not available. Please try again later.`
-    );
+  }
+
+  if (buyResult instanceof Errors.DomainServiceNotAvailable) {
+    output.error(`The domain purchase service is not available. Please try again later.`);
     return 1;
-  } if (buyResult instanceof Errors.UnexpectedDomainPurchaseError) {
+  }
+
+  if (buyResult instanceof Errors.UnexpectedDomainPurchaseError) {
     output.error(`An unexpected error happened while performing the purchase.`);
     return 1;
-  } if (buyResult instanceof Errors.PremiumDomainForbidden) {
+  }
+
+  if (buyResult instanceof Errors.PremiumDomainForbidden) {
     output.error(`A coupon cannot be used to register a premium domain.`);
     return 1;
   }
 
-  console.log(
-    `${chalk.cyan('> Success!')} Domain ${param(
-      domainName
-    )} purchased ${purchaseStamp()}`
-  );
-  output.note(
-    `You may now use your domain as an alias to your deployments. Run ${cmd(
-      'now alias --help'
-    )}`
-  );
+  console.log(`${chalk.cyan('> Success!')} Domain ${param(domainName)} purchased ${purchaseStamp()}`);
+  output.note(`You may now use your domain as an alias to your deployments. Run ${cmd('now alias --help')}`);
   return 0;
 }
