9936e3528025dca42ee1b4fe99d688c75cf44340
Igor Klopov <igor@klopov.com>
2018-12-15 21:55:55 +0300

    tests: retry on ETIMEDOUT

--- test/lib/deployment/fetch-retry.js
+++ test/lib/deployment/fetch-retry.js
@@ -7,7 +7,12 @@ async function fetchRetry (...args) {
       return await fetch(...args);
     } catch (error) {
       if (error.code === 'ENOTFOUND') {
-        // getaddrinfo ENOTFOUND api.zeit.co like some transitional dns issue
+        // getaddrinfo ENOTFOUND api.zeit.co like some transient dns issue
+        throw canRetry(error);
+      } else
+      if (error.code === 'ETIMEDOUT') {
+        // request to https://api-gru1.zeit.co/v3/now/deployments/dpl_FBWWhpQomjgwjJLu396snLrGZYCm failed, reason:
+        // connect ETIMEDOUT 18.228.143.224:443
         throw canRetry(error);
       }
       throw error;
