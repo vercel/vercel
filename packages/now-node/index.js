77ee10cead4f73254be91519315e7ddd855a03e3
Igor Klopov <igor@klopov.com>
2018-12-17 09:07:25 +0300

    now/node: bump ncc

--- packages/now-node/index.js
+++ packages/now-node/index.js
@@ -45,7 +45,7 @@ async function downloadInstallAndBundle(
       'package.json': new FileBlob({
         data: JSON.stringify({
           dependencies: {
-            '@zeit/ncc': '0.5.3',
+            '@zeit/ncc': '0.6.0',
           },
         }),
       }),
