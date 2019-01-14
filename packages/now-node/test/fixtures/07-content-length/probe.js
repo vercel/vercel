b64ce0f3c0c010f87aa57d713ef16c5660c132b4
Igor Klopov <igor@klopov.com>
2019-01-14 03:27:15 +0300

    [now-node] fix test for content-length

--- packages/now-node/test/fixtures/07-content-length/probe.js
+++ packages/now-node/test/fixtures/07-content-length/probe.js
@@ -4,21 +4,24 @@ async function test1({ deploymentUrl, fetch, randomness }) {
   const bodyMustBe = `${randomness}:content-length`;
   const resp = await fetch(`https://${deploymentUrl}/test1.js`);
   assert.equal(resp.status, 401);
-  assert.equal(resp.headers.get('content-length'), bodyMustBe.length);
   assert.equal(await resp.text(), bodyMustBe);
+  assert.equal(resp.headers.get('content-length'), bodyMustBe.length);
 }
 
 async function test2({ deploymentUrl, fetch }) {
+  const bodyMustBe = '';
   const resp = await fetch(`https://${deploymentUrl}/test2.js`);
-  assert.equal(resp.status, 502);
+  assert.equal(resp.status, 401);
+  assert.equal(await resp.text(), bodyMustBe);
+  assert.equal(resp.headers.get('content-length'), bodyMustBe.length);
 }
 
 async function test3({ deploymentUrl, fetch, randomness }) {
   const bodyMustBe = `${randomness}:content-length`;
   const resp = await fetch(`https://${deploymentUrl}/test3.js`);
   assert.equal(resp.status, 401);
-  assert.equal(resp.headers.get('content-length'), null);
   assert.equal(await resp.text(), bodyMustBe);
+  assert.equal(resp.headers.get('content-length'), null);
 }
 
 module.exports = async ({ deploymentUrl, fetch, randomness }) => {
--- packages/now-node/test/fixtures/07-content-length/test2.js
+++ packages/now-node/test/fixtures/07-content-length/test2.js
@@ -1,4 +1,4 @@
 module.exports = (_, resp) => {
   resp.writeHead(401, { 'content-length': 2 });
-  resp.end(`${process.env.RANDOMNESS_ENV_VAR}:content-length`);
+  resp.end();
 };
