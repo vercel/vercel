ea7d8f0f6c494353c6b8cc7a6d21cc1fc0e3ce69
Nathan Rajlich <n@n8.io>
2019-01-19 08:30:20 -0800

    Update `@zeit/ncc` to v0.11.0 (#172)

--- packages/now-node/index.js
+++ packages/now-node/index.js
@@ -45,7 +45,7 @@ async function downloadInstallAndBundle(
       'package.json': new FileBlob({
         data: JSON.stringify({
           dependencies: {
-            '@zeit/ncc': '0.9.0',
+            '@zeit/ncc': '0.11.0',
           },
         }),
       }),
