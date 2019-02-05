fcb8eacec0642094618ab1b47ce6be75f8d2fb21
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2019-02-05 07:18:30 +0530

    Check for distDir existence inside `now-static-build` (#197)
    * Throw in case we don't see the distDir
    
    * Add test cases.

--- packages/now-static-build/index.js
+++ packages/now-static-build/index.js
@@ -1,11 +1,21 @@
 const download = require('@now/build-utils/fs/download.js'); // eslint-disable-line import/no-extraneous-dependencies
 const glob = require('@now/build-utils/fs/glob.js'); // eslint-disable-line import/no-extraneous-dependencies
 const path = require('path');
+const { existsSync } = require('fs');
 const {
   runNpmInstall,
   runPackageJsonScript,
   runShellScript,
 } = require('@now/build-utils/fs/run-user-scripts.js'); // eslint-disable-line import/no-extraneous-dependencies
+
+function validateDistDir(distDir) {
+  const distDirName = path.basename(distDir);
+  if (!existsSync(distDir)) {
+    const message = `Build was unable to create the distDir: ${distDirName}.`
+      + 'Make sure you mentioned the correct dist directory: https://zeit.co/docs/v2/deployments/official-builders/static-build-now-static-build/#configuring-the-dist-directory';
+    throw new Error(message);
+  }
+}
 
 exports.build = async ({
   files, entrypoint, workPath, config,
@@ -24,6 +34,7 @@ exports.build = async ({
   if (path.basename(entrypoint) === 'package.json') {
     await runNpmInstall(entrypointFsDirname, ['--prefer-offline']);
     if (await runPackageJsonScript(entrypointFsDirname, 'now-build')) {
+      validateDistDir(distPath);
       return glob('**', distPath, mountpoint);
     }
     throw new Error(`An error running "now-build" script in "${entrypoint}"`);
@@ -31,6 +42,7 @@ exports.build = async ({
 
   if (path.extname(entrypoint) === '.sh') {
     await runShellScript(path.join(workPath, entrypoint));
+    validateDistDir(distPath);
     return glob('**', distPath, mountpoint);
   }
 
--- packages/now-static-build/test/test.js
+++ packages/now-static-build/test/test.js
@@ -21,6 +21,21 @@ const fixturesPath = path.resolve(__dirname, 'fixtures');
 
 // eslint-disable-next-line no-restricted-syntax
 for (const fixture of fs.readdirSync(fixturesPath)) {
+  if (fixture === '04-wrong-dist-dir') {
+    // eslint-disable-next-line no-loop-func
+    it(`should not build ${fixture}`, async () => {
+      try {
+        await testDeployment(
+          { builderUrl, buildUtilsUrl },
+          path.join(fixturesPath, fixture),
+        );
+      } catch (err) {
+        expect(err.message).toMatch(/is ERROR/);
+      }
+    });
+    continue; //eslint-disable-line
+  }
+
   // eslint-disable-next-line no-loop-func
   it(`should build ${fixture}`, async () => {
     await expect(
