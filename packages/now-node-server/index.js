4742cd32f29c899fb142c8204071b56e964cc444
Steven <steven@ceriously.com>
2019-02-17 19:08:19 -0500

    [now-node] add sourceMap flag to ncc (#222)
    This adds a {sourceMap:true} flag to ncc builds

--- packages/now-node-server/index.js
+++ packages/now-node-server/index.js
@@ -64,7 +64,7 @@ async function downloadInstallAndBundle(
 async function compile(workNccPath, downloadedFiles, entrypoint) {
   const input = downloadedFiles[entrypoint].fsPath;
   const ncc = require(path.join(workNccPath, 'node_modules/@zeit/ncc'));
-  const { code, assets } = await ncc(input);
+  const { code, assets } = await ncc(input, { sourceMap: true });
 
   const preparedFiles = {};
   const blob = new FileBlob({ data: code });
