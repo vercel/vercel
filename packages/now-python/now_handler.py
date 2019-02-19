702f56b9b53555268d41936225abab08625c4cad
Honza Javorek <mail@honzajavorek.cz>
2019-02-19 21:11:12 +0100

    [now-python] Add missing `base64` import and fix coding style (#226)

--- packages/now-python/now_handler.py
+++ packages/now-python/now_handler.py
@@ -1,13 +1,17 @@
+import base64
 from http.server import HTTPServer
 import json
 import requests
 from __NOW_HANDLER_FILENAME import handler
 import _thread
 
+
 server = HTTPServer(('', 3000), handler)
+
 
 def now_handler(event, context):
     _thread.start_new_thread(server.handle_request, ())
+
     payload = json.loads(event['body'])
     path = payload['path']
     headers = payload['headers']
@@ -22,10 +26,10 @@ def now_handler(event, context):
         body = base64.b64decode(body)
 
     res = requests.request(method, 'http://0.0.0.0:3000' + path,
-            headers=headers, data=body, allow_redirects=False)
+                           headers=headers, data=body, allow_redirects=False)
 
     return {
         'statusCode': res.status_code,
         'headers': dict(res.headers),
-        'body': res.text
+        'body': res.text,
     }
