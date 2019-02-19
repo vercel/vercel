a09d5fb355dcb8eb2acbd00b2ced3890cb4c68ca
Nathan Rajlich <n@n8.io>
2019-02-19 14:45:04 -0800

    Add `publish.sh` script for CircleCI (#228)
    * Add `publish.sh` script for CircleCI
    
    Centralize the publishing logic for CircleCI to use.
    
    There was a bug in the previous "Potentially publish stable release"
    branch, such that it would be executed for non-tagged commits. The new
    publish script checks if there are indeed any tags for the commit, and
    bails if there are none.
    
    As an additional bonus, there's now only one publish step in the
    CircleCI config, because the publish script determines stable vs. canary
    based on the tag name.
    
    * Remove `echo`

---
+++ .circleci/publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ ! -e ~/.npmrc ]; then
+  echo "~/.npmrc file does not exist, skipping publish"
+  exit 0
+fi
+
+npm_tag=""
+tag="$(git describe --exact-match 2> /dev/null || :)"
+
+if [ -z "$tag" ]; then
+  echo "Not a tagged commit, skipping publish"
+  exit 0
+fi
+
+if [[ "$tag" =~ -canary ]]; then
+  echo "Publishing canary release"
+  npm_tag="--npm-tag canary"
+else
+  echo "Publishing stable release"
+fi
+
+yarn run lerna publish from-git $npm_tag --yes
