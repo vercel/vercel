75b3fb49811910be7d060ceeb1e42d1df2b2cd73
Nathaniel Hill <nata@goguna.com>
2019-02-19 14:28:21 -0500

    [now-python] Pass the request body to the HTTP request (#99)
    * pass body to lambda
    
    * reformat
    
    * look for requirements.txt in lambda root
    
    * single issue PR
    
    * support base64 encoded request body
    
    * support local requirements.txt
    
    * formatting cleanup
    
    * Update packages/now-python/now_handler.py
    
    Co-Authored-By: NathanielHill <nata@goguna.com>
    
    * Avoid error on empty POST body
    
    Just realized I forgot to address the empty POST body error. This should do it
    
    * quick formatting fix
    
    remove double quotes for consistency

--- packages/now-python/index.js
+++ packages/now-python/index.js
@@ -40,8 +40,16 @@ exports.build = async ({ files, entrypoint }) => {
 
   await pipInstall(pipPath, srcDir, 'requests');
 
-  if (files['requirements.txt']) {
-    console.log('found "requirements.txt"');
+  const entryDirectory = path.dirname(entrypoint);
+  const requirementsTxt = path.join(entryDirectory, 'requirements.txt');
+
+  if (files[requirementsTxt]) {
+    console.log('found local "requirements.txt"');
+
+    const requirementsTxtPath = files[requirementsTxt].fsPath;
+    await pipInstall(pipPath, srcDir, '-r', requirementsTxtPath);
+  } else if (files['requirements.txt']) {
+    console.log('found global "requirements.txt"');
 
     const requirementsTxtPath = files['requirements.txt'].fsPath;
     await pipInstall(pipPath, srcDir, '-r', requirementsTxtPath);
--- packages/now-python/now_handler.py
+++ packages/now-python/now_handler.py
@@ -12,12 +12,20 @@ def now_handler(event, context):
     path = payload['path']
     headers = payload['headers']
     method = payload['method']
-    
-    res = requests.request(method, 'http://0.0.0.0:3000' + path, headers=headers)
+    encoding = payload.get('encoding')
+    body = payload.get('body')
+
+    if (
+        (body is not None and len(body) > 0) and
+        (encoding is not None and encoding == 'base64')
+    ):
+        body = base64.b64decode(body)
+
+    res = requests.request(method, 'http://0.0.0.0:3000' + path,
+            headers=headers, data=body, allow_redirects=False)
 
     return {
         'statusCode': res.status_code,
         'headers': dict(res.headers),
         'body': res.text
     }
-
