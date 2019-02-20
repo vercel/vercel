6101ba9d9505b0dacd6af49c1ee1e804313f9d17
Nathan Rajlich <n@n8.io>
2019-02-20 13:47:17 -0800

    [now-node-server] Update `@now/node-bridge` to v1.0.0-canary.2 (#231)
    Update `@now/node-server` to use the latest `@now/node-bridge`, which
    removes the hard-coded port 3000 that was previously in-place,
    which was problematic for `now dev`. Now the `http.Server`
    instance listens on an ephemeral port which is detected by the
    Now node runtime.

--- packages/now-node-server/launcher.js
+++ packages/now-node-server/launcher.js
@@ -4,22 +4,16 @@ const { Bridge } = require('./bridge.js');
 const bridge = new Bridge();
 
 const saveListen = Server.prototype.listen;
-Server.prototype.listen = function listen(...args) {
-  this.on('listening', function listening() {
-    bridge.port = this.address().port;
-  });
-  saveListen.apply(this, args);
+Server.prototype.listen = function listen() {
+  bridge.setServer(this);
+  Server.prototype.listen = saveListen;
+  return bridge.listen();
 };
 
-try {
-  if (!process.env.NODE_ENV) {
-    process.env.NODE_ENV = 'production';
-  }
-
-  // PLACEHOLDER
-} catch (error) {
-  console.error(error);
-  bridge.userError = error;
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'production';
 }
+
+// PLACEHOLDER
 
 exports.launcher = bridge.launcher;
