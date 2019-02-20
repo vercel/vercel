aeafeb54412cb8a1b467972e435ca596f2dd6069
Nathan Rajlich <n@n8.io>
2019-02-19 16:17:58 -0800

    [now-node-bridge] Refactor API to be `http.Server` focused (#224)
    * [now-node-bridge] Refactor API to be `http.Server` focused
    
    This commit refactors the `@now/node-bridge` helper module to work with
    `http.Server` instances directly, instead of expecting the `port` to be
    set.
    
    The `Bridge` instance calls the `listen()` function on the server instance
    which binds to an ephemeral port. This is especially important for
    `now dev`, where using a hard-coded port will cause port conflicts for
    multiple lambdas using the same builder.
    
    Also converts to TypeScript and adds some basic unit tests.
    
    Example usage:
    
    ```js
    const server = new Server(() => {});
    const bridge = new Bridge(server);
    bridge.listen();
    
    const info = await bridge.listening;
    assert.equal(info.address, '127.0.0.1');
    assert.equal(typeof info.port, 'number');
    ```
    
    * Update `yarn.lock`
    
    * Add `pretest` script
    
    * Enable TypeScript `strict` mode
    
    * Throw if a string is returned from `server.address()`
    
    Defensive programming ftw
    
    Co-Authored-By: TooTallNate <n@n8.io>
    
    * Prettier
    
    * Prettier
    
    * Fixes
    
    * Attempt to fix CI
    
    * Add `files` array to package.json
    
    * Check for the `Action` property to avoid type casting
    
    Co-Authored-By: TooTallNate <n@n8.io>
    
    * Split the normalizing functions into separate ones
    
    Also adds additional unit tests.
    
    * export the `NowProxyRequest` and `NowProxyResponse`
    
    * Remove last `as` casting
    
    * Move some up
    
    * Debug CircleCI tests :/
    
    * Fix "Invoke" check
    
    * Attempt to fix CircleCI again
    
    * Fix bad
    
    * Convert tests to use `jest`

---
+++ packages/now-node-bridge/src/bridge.ts
@@ -0,0 +1,183 @@
+import { AddressInfo } from 'net';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import {
+  Server,
+  IncomingHttpHeaders,
+  OutgoingHttpHeaders,
+  request
+} from 'http';
+
+interface NowProxyEvent {
+  Action: string;
+  body: string;
+}
+
+export interface NowProxyRequest {
+  isApiGateway?: boolean;
+  method: string;
+  path: string;
+  headers: IncomingHttpHeaders;
+  body: Buffer;
+}
+
+export interface NowProxyResponse {
+  statusCode: number;
+  headers: OutgoingHttpHeaders;
+  body: string;
+  encoding: string;
+}
+
+function normalizeNowProxyEvent(event: NowProxyEvent): NowProxyRequest {
+  let bodyBuffer: Buffer | null;
+  const { method, path, headers, encoding, body } = JSON.parse(event.body);
+
+  if (body) {
+    if (encoding === 'base64') {
+      bodyBuffer = Buffer.from(body, encoding);
+    } else if (encoding === undefined) {
+      bodyBuffer = Buffer.from(body);
+    } else {
+      throw new Error(`Unsupported encoding: ${encoding}`);
+    }
+  } else {
+    bodyBuffer = Buffer.alloc(0);
+  }
+
+  return { isApiGateway: false, method, path, headers, body: bodyBuffer };
+}
+
+function normalizeAPIGatewayProxyEvent(
+  event: APIGatewayProxyEvent
+): NowProxyRequest {
+  let bodyBuffer: Buffer | null;
+  const { httpMethod: method, path, headers, body } = event;
+
+  if (body) {
+    if (event.isBase64Encoded) {
+      bodyBuffer = Buffer.from(body, 'base64');
+    } else {
+      bodyBuffer = Buffer.from(body);
+    }
+  } else {
+    bodyBuffer = Buffer.alloc(0);
+  }
+
+  return { isApiGateway: true, method, path, headers, body: bodyBuffer };
+}
+
+function normalizeEvent(
+  event: NowProxyEvent | APIGatewayProxyEvent
+): NowProxyRequest {
+  if ('Action' in event) {
+    if (event.Action === 'Invoke') {
+      return normalizeNowProxyEvent(event);
+    } else {
+      throw new Error(`Unexpected event.Action: ${event.Action}`);
+    }
+  } else {
+    return normalizeAPIGatewayProxyEvent(event);
+  }
+}
+
+export class Bridge {
+  private server: Server | null;
+  private listening: Promise<AddressInfo>;
+  private resolveListening: (info: AddressInfo) => void;
+
+  constructor(server?: Server) {
+    this.server = null;
+    if (server) {
+      this.setServer(server);
+    }
+    this.launcher = this.launcher.bind(this);
+
+    // This is just to appease TypeScript strict mode, since it doesn't
+    // understand that the Promise constructor is synchronous
+    this.resolveListening = (info: AddressInfo) => {};
+
+    this.listening = new Promise(resolve => {
+      this.resolveListening = resolve;
+    });
+  }
+
+  setServer(server: Server) {
+    this.server = server;
+    server.once('listening', () => {
+      const addr = server.address();
+      if (typeof addr === 'string') {
+        throw new Error(`Unexpected string for \`server.address()\`: ${addr}`);
+      } else if (!addr) {
+        throw new Error('`server.address()` returned `null`');
+      } else {
+        this.resolveListening(addr);
+      }
+    });
+  }
+
+  listen() {
+    if (!this.server) {
+      throw new Error('Server has not been set!');
+    }
+
+    return this.server.listen({
+      host: '127.0.0.1',
+      port: 0
+    });
+  }
+
+  async launcher(
+    event: NowProxyEvent | APIGatewayProxyEvent
+  ): Promise<NowProxyResponse> {
+    const { port } = await this.listening;
+
+    const { isApiGateway, method, path, headers, body } = normalizeEvent(
+      event
+    );
+
+    const opts = {
+      hostname: '127.0.0.1',
+      port,
+      path,
+      method,
+      headers
+    };
+
+    // eslint-disable-next-line consistent-return
+    return new Promise((resolve, reject) => {
+      const req = request(opts, res => {
+        const response = res;
+        const respBodyChunks: Buffer[] = [];
+        response.on('data', chunk => respBodyChunks.push(Buffer.from(chunk)));
+        response.on('error', reject);
+        response.on('end', () => {
+          const bodyBuffer = Buffer.concat(respBodyChunks);
+          delete response.headers.connection;
+
+          if (isApiGateway) {
+            delete response.headers['content-length'];
+          } else if (response.headers['content-length']) {
+            response.headers['content-length'] = String(bodyBuffer.length);
+          }
+
+          resolve({
+            statusCode: response.statusCode || 200,
+            headers: response.headers,
+            body: bodyBuffer.toString('base64'),
+            encoding: 'base64'
+          });
+        });
+      });
+
+      req.on('error', error => {
+        setTimeout(() => {
+          // this lets express print the true error of why the connection was closed.
+          // it is probably 'Cannot set headers after they are sent to the client'
+          reject(error);
+        }, 2);
+      });
+
+      if (body) req.write(body);
+      req.end();
+    });
+  }
+}
---
+++ packages/now-node-bridge/test/bridge.test.js
@@ -0,0 +1,71 @@
+const assert = require('assert');
+const { Server } = require('http');
+const { Bridge } = require('../bridge');
+
+test('port binding', async () => {
+  const server = new Server();
+  const bridge = new Bridge(server);
+  bridge.listen();
+
+  // Test port binding
+  const info = await bridge.listening;
+  assert.equal(info.address, '127.0.0.1');
+  assert.equal(typeof info.port, 'number');
+
+  server.close();
+});
+
+test('`APIGatewayProxyEvent` normalizing', async () => {
+  const server = new Server((req, res) => res.end(
+    JSON.stringify({
+      method: req.method,
+      path: req.url,
+      headers: req.headers,
+    }),
+  ));
+  const bridge = new Bridge(server);
+  bridge.listen();
+  const result = await bridge.launcher({
+    httpMethod: 'GET',
+    headers: { foo: 'bar' },
+    path: '/apigateway',
+    body: null,
+  });
+  assert.equal(result.encoding, 'base64');
+  assert.equal(result.statusCode, 200);
+  const body = JSON.parse(Buffer.from(result.body, 'base64').toString());
+  assert.equal(body.method, 'GET');
+  assert.equal(body.path, '/apigateway');
+  assert.equal(body.headers.foo, 'bar');
+
+  server.close();
+});
+
+test('`NowProxyEvent` normalizing', async () => {
+  const server = new Server((req, res) => res.end(
+    JSON.stringify({
+      method: req.method,
+      path: req.url,
+      headers: req.headers,
+    }),
+  ));
+  const bridge = new Bridge(server);
+  bridge.listen();
+  const result = await bridge.launcher({
+    Action: 'Invoke',
+    body: JSON.stringify({
+      method: 'POST',
+      headers: { foo: 'baz' },
+      path: '/nowproxy',
+      body: 'body=1',
+    }),
+  });
+  assert.equal(result.encoding, 'base64');
+  assert.equal(result.statusCode, 200);
+  const body = JSON.parse(Buffer.from(result.body, 'base64').toString());
+  assert.equal(body.method, 'POST');
+  assert.equal(body.path, '/nowproxy');
+  assert.equal(body.headers.foo, 'baz');
+
+  server.close();
+});
