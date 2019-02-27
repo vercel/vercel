425fb8a58420cdc7f401d12610947731bb6d9010
Mark Glagola <mark.glagola@gmail.com>
2019-02-27 11:54:25 -0600

    root domain func fix

--- src/util/is-root-domain.ts
+++ src/util/is-root-domain.ts
@@ -1,6 +1,10 @@
 import psl from 'psl';
 
 export default function isRootDomain(domainName: string) {
-  const { domain: rootDomain, subdomain } = psl.parse(domainName);
+  const parsedDomain = psl.parse(domainName);
+  if (parsedDomain.error) {
+    return false;
+  }
+  const { domain: rootDomain, subdomain } = parsedDomain;
   return Boolean(!subdomain && rootDomain);
 }
