6b37797de9d34d34a28101b745930e3dbbd6ad51
Javi Velasco <javier.velasco86@gmail.com>
2019-02-28 20:34:44 +0100

    Add --no-scale option (#1918)
    * Add no-scale option
    
    * Reword --no-scale warning

--- src/commands/deploy/latest.js
+++ src/commands/deploy/latest.js
@@ -181,7 +181,7 @@ export default async function main(
   }
 
   const { apiUrl, authConfig: { token }, config: { currentTeam } } = ctx;
-  const { log, debug, error, print } = output;
+  const { log, debug, error, print, warn } = output;
   const paths = Object.keys(stats);
   const debugEnabled = argv['--debug'];
 
@@ -214,6 +214,9 @@ export default async function main(
   let deployStamp = stamp();
   let deployment = null;
 
+  if (argv['--no-scale']) {
+    warn(`The option --no-scale is only supported on Now 1.0 deployments`);
+  }
 
   const isObject = item =>
     Object.prototype.toString.call(item) === '[object Object]';
