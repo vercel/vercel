7f6d900aec0625d804bd7faf0ddd6272c48c6b81
Mark Glagola <mark.glagola@gmail.com>
2019-03-12 10:50:11 -0500

    smaller domain removal conflict construction

--- src/util/domains/remove-domain-by-name.ts
+++ src/util/domains/remove-domain-by-name.ts
@@ -16,23 +16,14 @@ export default async function removeDomainByName(
       return new ERRORS.DomainPermissionDenied(domain, contextName);
     }
     if (error.code === 'domain_removal_conflict') {
-      const {
-        aliases,
-        certs,
-        message,
-        pendingAsyncPurchase,
-        resolvable,
-        suffix,
-        transferring
-      } = error;
       return new ERRORS.DomainRemovalConflict({
-        aliases,
-        certs,
-        message,
-        pendingAsyncPurchase,
-        resolvable,
-        suffix,
-        transferring
+        aliases: error.aliases,
+        certs: error.certs,
+        message: error.message,
+        pendingAsyncPurchase: error.pendingAsyncPurchase,
+        resolvable: error.resolvable,
+        suffix: error.suffix,
+        transferring: error.transferring
       });
     }
     throw error;
