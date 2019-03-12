d6f9970d50d78f1ebc7aee50ee7bd209674f1308
Leo Lamprecht <mindrun@icloud.com>
2019-03-12 15:43:17 +0000

    Handle Now errors too when loading local config

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ import { handleError } from './util/error';
 import reportError from './util/report-error';
 import getConfig from './util/get-config';
 import * as ERRORS from './util/errors-ts';
+import { NowError } from './util/now-error';
 
 const NOW_DIR = getNowDir();
 const NOW_CONFIG_PATH = configFiles.getConfigFilePath();
@@ -84,6 +85,11 @@ const main = async argv_ => {
   debug = output.debug;
 
   const localConfig = await getConfig(output, argv['--local-config']);
+
+  if (localConfig instanceof NowError) {
+    output.error(`Failed to load local config file: ${localConfig.message}`);
+    return 1;
+  }
 
   if (localConfig instanceof ERRORS.CantParseJSONFile) {
     output.error(`Couldn't parse JSON file ${localConfig.meta.file}.`);
