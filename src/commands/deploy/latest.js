0e2d23f01c9bfd504a432521b21feb63ca118db3
Leo Lamprecht <mindrun@icloud.com>
2019-03-14 20:39:32 +0100

    Improved output for `deploy` command (#1962)
    * Make alias links clickable
    
    * Make output print Ready instead of Success
    
    * Fixed missing export

--- src/commands/deploy/latest.js
+++ src/commands/deploy/latest.js
@@ -65,6 +65,7 @@ const addProcessEnv = async (log, env) => {
 };
 
 const deploymentErrorMsg = `Your deployment failed. Please retry later. More: https://err.sh/now-cli/deployment-error`;
+const prepareAlias = input => `https://${input}`;
 
 const printDeploymentStatus = async (
   output,
@@ -79,14 +80,14 @@ const printDeploymentStatus = async (
         if (clipboardEnabled) {
           try {
             await copy(aliasFinal[0]);
-            output.success(`Aliased to ${chalk.bold(chalk.cyan(aliasFinal[0]))} ${chalk.gray('[in clipboard]')} ${deployStamp()}`);
+            output.ready(`Aliased to ${chalk.bold(chalk.cyan(prepareAlias(aliasFinal[0])))} ${chalk.gray('[in clipboard]')} ${deployStamp()}`);
           } catch (err) {
             output.debug(`Error copying to clipboard: ${err}`);
-            output.success(`Aliased to ${chalk.bold(chalk.cyan(aliasFinal[0]))} ${deployStamp()}`);
+            output.ready(`Aliased to ${chalk.bold(chalk.cyan(prepareAlias(aliasFinal[0])))} ${deployStamp()}`);
           }
         }
       } else {
-        output.success(`Aliases assigned ${deployStamp()}`);
+        output.ready(`Aliases assigned ${deployStamp()}`);
 
         for (const alias of aliasFinal) {
           const index = aliasFinal.indexOf(alias);
@@ -95,7 +96,7 @@ const printDeploymentStatus = async (
           if (last && clipboardEnabled) {
             try {
               await copy(alias);
-              output.print(`- ${chalk.bold(chalk.cyan(alias))} ${chalk.gray('[in clipboard]')}\n`);
+              output.print(`- ${chalk.bold(chalk.cyan(prepareAlias(alias)))} ${chalk.gray('[in clipboard]')}\n`);
 
               return 0;
             } catch (err) {
