97cbe0b894013162ed15b6ea60c009184f3dbc63
Nathan Rajlich <n@n8.io>
2019-03-17 13:57:06 -0700

    [now-python] Bind HTTP server to an ephemeral port (#296)
    * [now-python] Bind HTTP server to an ephemeral port
    
    Similar to #224, but for Python.
    
    * Bind to port 0

--- packages/now-python/now_handler.py
+++ packages/now-python/now_handler.py
@@ -6,8 +6,8 @@ from __NOW_HANDLER_FILENAME import handler
 import _thread
 
 
-server = HTTPServer(('', 3000), handler)
-
+server = HTTPServer(('', 0), handler)
+port = server.server_address[1]
 
 def now_handler(event, context):
     _thread.start_new_thread(server.handle_request, ())
@@ -25,7 +25,7 @@ def now_handler(event, context):
     ):
         body = base64.b64decode(body)
 
-    res = requests.request(method, 'http://0.0.0.0:3000' + path,
+    res = requests.request(method, 'http://0.0.0.0:' + str(port) + path,
                            headers=headers, data=body, allow_redirects=False)
 
     return {
