93d0019153935f19d4725568414414515920dcf7
Julian Gruber <julian@juliangruber.com>
2019-03-18 18:15:45 +0100

    Fix the handling of user errors (#1984)
    * fix `ms(null)` in `certs rm`
    
    * fix uncaught `InvalidDomain`

--- src/util/deploy/generate-cert-for-deploy.ts
+++ src/util/deploy/generate-cert-for-deploy.ts
@@ -15,12 +15,12 @@ export default async function generateCertForDeploy(
 ) {
   const parsedDomain = psl.parse(deployURL);
   if (parsedDomain.error) {
-    throw new InvalidDomain(deployURL, parsedDomain.error.message);
+    return new InvalidDomain(deployURL, parsedDomain.error.message);
   }
 
   const { domain } = parsedDomain;
   if (!domain) {
-    throw new InvalidDomain(deployURL);
+    return new InvalidDomain(deployURL);
   }
 
   const cancelSetupWait = wait(`Setting custom suffix domain ${domain}`);
