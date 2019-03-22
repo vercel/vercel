950a4e98e91081c6fed9929e686e8ed4f7986d00
Nathan Rajlich <n@n8.io>
2019-03-22 14:32:46 -0700

    [now-node-server] Print a more helpful message when a module is missing (#313)
    Similar to #312, but for `@now/node-server`.

--- packages/now-node-server/launcher.js
+++ packages/now-node-server/launcher.js
@@ -14,6 +14,18 @@ if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = 'production';
 }
 
-// PLACEHOLDER
+try {
+  // PLACEHOLDER
+} catch (err) {
+  if (err.code === 'MODULE_NOT_FOUND') {
+    console.error(err.message);
+    console.error(
+      'Did you forget to add it to "dependencies" in `package.json`?',
+    );
+    process.exit(1);
+  } else {
+    throw err;
+  }
+}
 
 exports.launcher = bridge.launcher;
