8258ede23fa7a37239f2d9a1b4dbcea1996878a8
Nathan Rajlich <n@n8.io>
2019-03-22 14:32:17 -0700

    [now-node] Print a more helpful message when a module is missing (#312)

--- packages/now-node/src/launcher.ts
+++ packages/now-node/src/launcher.ts
@@ -7,7 +7,17 @@ if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = 'production';
 }
 
+try {
 // PLACEHOLDER
+} catch (err) {
+  if (err.code === 'MODULE_NOT_FOUND') {
+    console.error(err.message);
+    console.error('Did you forget to add it to "dependencies" in `package.json`?');
+    process.exit(1);
+  } else {
+    throw err;
+  }
+}
 
 const server = new Server(listener);
 const bridge = new Bridge(server);
