a5507ba59deddeb551913d80d3600ea0ad90b7c8
Nathan Rajlich <nathan@tootallnate.net>
2019-03-21 18:10:09 -0700

    Use `rsync` to copy over all of `fun`'s runtime files

--- build.sh
+++ build.sh
@@ -9,11 +9,16 @@ fi
 # Do the initial `ncc` build
 ncc build $NCC_OPTS ./src
 
-# `ncc` isn't copying over permissions of the emitted assets.
-# PR exists, but has not been merged.
-# https://github.com/zeit/ncc/pull/182
-find dist/runtimes -name bootstrap -print0 | xargs -0 chmod -vv +x
-
-# `ncc` doesn't seem to copy over assets with `.js` file extension.
-# https://github.com/zeit/ncc/issues/278
-cp -v node_modules/@zeit/fun/dist/src/runtimes/nodejs/bootstrap.js dist/runtimes/nodejs
+# `ncc` has some issues with `@zeit/fun`'s runtime files:
+#   - Executable bits on the `bootstrap` files appear to be lost:
+#       https://github.com/zeit/ncc/pull/182
+#   - The `bootstrap.js` asset does not get copied into the output dir:
+#       https://github.com/zeit/ncc/issues/278
+#
+# Aside from those issues, all the same files from the `runtimes` directory
+# should be copied into the output runtimes dir, specifically the `index.js`
+# files (correctly) do not get copied into the output bundle because they
+# get compiled into the final ncc bundle file, however, we want them to be
+# present on pkg's snapshot fs because the contents of those files are involved
+# with `fun`'s cache invalidation mechanism and they need to be shasum'd.
+rsync -av "node_modules/@zeit/fun/dist/src/runtimes/" "dist/runtimes"
