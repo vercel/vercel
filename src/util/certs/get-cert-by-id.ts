a713517ac2a1b54a312fbba583ae6859408ed393
Mark Glagola <mark.glagola@gmail.com>
2019-04-08 22:46:29 -0500

    Converted rm.js -> rm.ts

--- src/util/certs/get-cert-by-id.ts
+++ src/util/certs/get-cert-by-id.ts
@@ -1,18 +1,19 @@
 import { Cert } from '../../types';
 import { Output } from '../output/create-output';
 import Client from '../client';
-
-type CertDetails = Cert & {
-  key: string;
-  ca: string;
-  crt: string;
-};
+import * as ERRORS from '../errors-ts';
 
 export default async function getCertById(
   output: Output,
   client: Client,
   id: string
 ) {
-  const cert = await client.fetch<CertDetails>(`/v3/now/certs/${id}?limit=1`);
-  return cert || null;
+  try {
+    return await client.fetch<Cert>(`/v3/now/certs/${id}`);
+  } catch (error) {
+    if (error.code === 'cert_not_found') {
+      return new ERRORS.CertNotFound(id);
+    }
+    throw error;
+  }
 }
