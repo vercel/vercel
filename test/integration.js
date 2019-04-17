a9d4e2782fc797479a941ff981af0f81852f212f
Andy Bitz <artzbitz@gmail.com>
2019-04-17 18:43:06 +0200

    Add sleep to test

--- test/integration.js
+++ test/integration.js
@@ -1298,6 +1298,7 @@ test('try to revert a deployment and assign the automatic aliases', async t => {
     const { stdout: deploymentUrl, code } = await execute([firstDeployment]);
     t.is(code, 0);
 
+    await sleep(70000);
     await waitForDeployment(deploymentUrl);
 
     const result = await fetch(url).then(r => r.json());
@@ -1313,6 +1314,7 @@ test('try to revert a deployment and assign the automatic aliases', async t => {
     const { stdout: deploymentUrl, code } = await execute([secondDeployment]);
     t.is(code, 0);
 
+    await sleep(70000);
     await waitForDeployment(deploymentUrl);
 
     const result = await fetch(url).then(r => r.json());
@@ -1328,6 +1330,7 @@ test('try to revert a deployment and assign the automatic aliases', async t => {
     const { stdout: deploymentUrl, code } = await execute([firstDeployment]);
     t.is(code, 0);
 
+    await sleep(70000);
     await waitForDeployment(deploymentUrl);
 
     const result = await fetch(url).then(r => r.json());
