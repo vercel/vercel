ee158a7862ba2fa7c9175e1b4dffaa3932c091ac
Nathan Rajlich <nathan@tootallnate.net>
2019-04-27 18:55:14 -0700

    [now dev] Don't render building/built logging for `@now/static`
    Since it's a built-in builder.

--- src/commands/dev/lib/dev-builder.ts
+++ src/commands/dev/lib/dev-builder.ts
@@ -129,8 +129,12 @@ export async function executeBuild(
   await mkdirp(workPath);
 
   const startTime = Date.now();
-  devServer.output.log(`Building ${match.use}:${entrypoint}`);
-  devServer.output.debug(`${pkg.version ? `v${pkg.version} ` : ''}(workPath = ${workPath})`);
+  if (match.use !== '@now/static') {
+    devServer.output.log(`Building ${match.use}:${entrypoint}`);
+    devServer.output.debug(
+      `${pkg.version ? `v${pkg.version} ` : ''}(workPath = ${workPath})`
+    );
+  }
 
   const builderConfig = builder.config || {};
   const config = match.config || {};
@@ -345,8 +349,12 @@ export async function executeBuild(
   match.buildResults.set(requestPath, result);
   Object.assign(match.buildOutput, result.output);
 
-  const endTime = Date.now();
-  devServer.output.log(`Built ${match.use}:${entrypoint} [${ms(endTime - startTime)}]`);
+  if (match.use !== '@now/static') {
+    const endTime = Date.now();
+    devServer.output.log(
+      `Built ${match.use}:${entrypoint} [${ms(endTime - startTime)}]`
+    );
+  }
 }
 
 export async function getBuildMatches(
