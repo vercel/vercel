6c8f946a48638df7c9694696b7a749947d7ed5ae
Joe Haddad <joe.haddad@zeit.co>
2019-05-06 19:12:39 -0400

    [now-build-utils] Remove `mutable` option and add `sha+ephemeral` scheme (#466)
    Co-Authored-By: Steven <steven@ceriously.com>
    
    * Add an ephemeral option for files
    
    * Use array destructuring
    
    Co-Authored-By: Timer <joe.haddad@zeit.co>
    
    * Remove mutable option
    
    * Remove code all together
    
    * Introduce `sha+ephemeral` handling
    
    * http => https
    
    * Elaborate more on the cloudfront url
    
    * Update comment a bit more
    
    * Add comment explaining other url

--- packages/now-build-utils/src/file-ref.ts
+++ packages/now-build-utils/src/file-ref.ts
@@ -8,7 +8,6 @@ import { File } from './types';
 interface FileRefOptions {
   mode?: number;
   digest: string;
-  mutable?: boolean;
 }
 
 const semaToDownloadFromS3 = new Sema(5);
@@ -26,26 +25,29 @@ export default class FileRef implements File {
   public type: 'FileRef';
   public mode: number;
   public digest: string;
-  public mutable: boolean;
 
-  constructor({ mode = 0o100644, digest, mutable = false }: FileRefOptions) {
+  constructor({ mode = 0o100644, digest }: FileRefOptions) {
     assert(typeof mode === 'number');
     assert(typeof digest === 'string');
-    assert(typeof mutable === 'boolean');
     this.type = 'FileRef';
     this.mode = mode;
     this.digest = digest;
-    this.mutable = mutable;
   }
 
   async toStreamAsync(): Promise<NodeJS.ReadableStream> {
     let url = '';
     // sha:24be087eef9fac01d61b30a725c1a10d7b45a256
-    const digestParts = this.digest.split(':');
-    if (digestParts[0] === 'sha') {
-      url = this.mutable
-        ? `https://s3.amazonaws.com/now-files/${digestParts[1]}`
-        : `https://dmmcy0pwk6bqi.cloudfront.net/${digestParts[1]}`;
+    const [digestType, digestHash] = this.digest.split(':');
+    if (digestType === 'sha') {
+      // This CloudFront URL edge caches the `now-files` S3 bucket to prevent
+      // overloading it
+      // `https://now-files.s3.amazonaws.com/${digestHash}`
+      url = `https://dmmcy0pwk6bqi.cloudfront.net/${digestHash}`;
+    } else if (digestType === 'sha+ephemeral') {
+      // This URL is currently only used for cache files that constantly
+      // change. We shouldn't cache it on CloudFront because it'd always be a
+      // MISS.
+      url = `https://now-ephemeral-files.s3.amazonaws.com/${digestHash}`;
     } else {
       throw new Error('Expected digest to be sha');
     }
@@ -58,14 +60,14 @@ export default class FileRef implements File {
           const resp = await fetch(url);
           if (!resp.ok) {
             const error = new BailableError(
-              `download: ${resp.status} ${resp.statusText} for ${url}`,
+              `download: ${resp.status} ${resp.statusText} for ${url}`
             );
             if (resp.status === 403) error.bail = true;
             throw error;
           }
           return resp.body;
         },
-        { factor: 1, retries: 3 },
+        { factor: 1, retries: 3 }
       );
     } finally {
       // console.timeEnd(`downloading ${url}`);
@@ -77,15 +79,15 @@ export default class FileRef implements File {
     let flag = false;
 
     // eslint-disable-next-line consistent-return
-    return multiStream((cb) => {
+    return multiStream(cb => {
       if (flag) return cb(null, null);
       flag = true;
 
       this.toStreamAsync()
-        .then((stream) => {
+        .then(stream => {
           cb(null, stream);
         })
-        .catch((error) => {
+        .catch(error => {
           cb(error, null);
         });
     });
