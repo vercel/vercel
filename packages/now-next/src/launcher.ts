8a021c94173c503959706315a941e6c98d8dca7a
Tim Neutkens <timneutkens@me.com>
2019-05-11 16:18:23 +0200

    [now-next] Add support for /api routes (#494)

--- packages/now-next/src/launcher.ts
+++ packages/now-next/src/launcher.ts
@@ -1,12 +1,16 @@
 if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = process.env.NOW_REGION === 'dev1' ? 'development' : 'production';
+  process.env.NODE_ENV =
+    process.env.NOW_REGION === 'dev1' ? 'development' : 'production';
 }
 
 const { Server } = require('http');
 const { Bridge } = require('./now__bridge');
 const page = require('./page');
 
-const server = new Server(page.render);
+// page.render is for React rendering
+// page.default is for /api rendering
+// page is for module.exports in /api
+const server = new Server(page.render || page.default || page);
 const bridge = new Bridge(server);
 bridge.listen();
 
