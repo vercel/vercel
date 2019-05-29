efe4398e1cade44b92165173d2124b66ecd25f5b
Nathan Rajlich <nathan@tootallnate.net>
2019-05-28 18:46:45 -0700

    Add `now update` command
    Updates the `now` binary in-place.

---
+++ src/commands/update.ts
@@ -0,0 +1,198 @@
+import chalk from 'chalk';
+import { tmpdir } from 'os';
+import pipe from 'promisepipe';
+import fetch from 'node-fetch';
+import Progress from 'progress';
+import { createGunzip } from 'zlib';
+import { parse, format } from 'url';
+import { basename, join } from 'path';
+import { spawnSync } from 'child_process';
+import { createReadStream, createWriteStream, copyFile, remove } from 'fs-extra';
+
+import pkg from '../util/pkg';
+import cmd from '../util/output/cmd.ts';
+import logo from '../util/output/logo';
+import handleError from '../util/handle-error';
+import getArgs from '../util/get-args';
+import wait from '../util/output/wait';
+import { NowContext } from '../types';
+import createOutput, { Output } from '../util/output';
+
+const versionEndpoint = 'https://install-now-cli.zeit.sh/version';
+
+const platformMap: Map<string, string> = new Map([
+  ['darwin', 'macos'],
+  ['linux', 'linux'],
+  ['win32', 'win']
+]);
+
+const help = () => {
+  console.log(`
+  ${chalk.bold(`${logo} now update`)} [options]
+
+  ${chalk.dim('Options:')}
+
+    -h, --help                     Output usage information
+    -d, --debug                    Debug mode [off]
+    -c ${chalk.bold.underline('NAME')}, --channel=${chalk.bold.underline(
+    'NAME'
+  )}        Specify which release channel to install [stable]
+    -V ${chalk.bold.underline('VERSION')}, --version=${chalk.bold.underline(
+    'VERSION'
+  )}  Specfic version to install (overrides \`--channel\`)
+    -y, --yes                      Skip the confirmation prompt
+
+  ${chalk.dim('Examples:')}
+
+  ${chalk.gray('–')} Update Now CLI to the latest "canary" version
+
+      ${chalk.cyan(`$ now update --channel=canary`)}
+  `);
+};
+
+function detectAlpine() {
+  // https://github.com/sass/node-sass/issues/1589#issuecomment-265292579
+  const ldd = spawnSync('ldd', [process.execPath]).stdout.toString();
+  return /\bmusl\b/.test(ldd);
+}
+
+function getDefaultChannel(): string {
+  const m = pkg.version.match(/-(.+)\./);
+  return m ? m[1] : 'stable';
+}
+
+function getPlatform(nodePlatform: string): string {
+  return 'macos';
+}
+
+function getReleaseUrl(version: string, platform: string): string {
+  const ext = platform === 'win' ? '.exe' : '';
+  return `https://github.com/zeit/now-cli/releases/download/${version}/now-${platform}${ext}.gz`;
+}
+
+async function isNpmInstall() {
+  // TODO: check if `now` is a npm/yarn installed binary
+  return false;
+}
+
+async function getLatestVersion(
+  { debug }: Output,
+  channel: string
+): Promise<string> {
+  const parsed = parse(versionEndpoint, true);
+  parsed.query.channel = channel;
+  const url = format(parsed);
+  debug(`GET ${url}`);
+  const res = await fetch(url);
+  const body = await res.text();
+  debug(`Resolved latest version "${body}" from channel "${channel}"`);
+  return body.trim();
+}
+
+async function downloadNowCli({ debug, print }: Output, url: string, dest: string) {
+  debug(`GET ${url}`);
+  const res = await fetch(url, { redirect: 'follow' });
+  const total = parseInt(res.headers.get('content-length') || '0', 10);
+  const bar = new Progress(':bar :percent', {
+    complete: '#',
+    incomplete: ' ',
+    width: 80,
+    clear: true,
+    total
+  });
+  const pipePromise = pipe(
+    res.body,
+    createGunzip(),
+    createWriteStream(dest)
+  );
+  res.body.on('data', (buf: Buffer) => {
+    bar.tick(buf.length);
+  });
+  await pipePromise;
+}
+
+async function updateNowCli(src: string, dest: string) {
+  await pipe(
+    createReadStream(src),
+    createWriteStream(dest)
+  );
+}
+
+export default async function main(ctx: NowContext): Promise<number> {
+  let argv;
+
+  try {
+    argv = getArgs(ctx.argv.slice(2), {
+      '--channel': String,
+      '-c': '--channel',
+      '--version': String,
+      '-V': '--version',
+      '--yes': Boolean,
+      '-y': '--yes'
+    });
+  } catch (err) {
+    handleError(err);
+    return 1;
+  }
+
+  const skipConfirmation: boolean = argv['--yes'] || false;
+
+  if (argv['--help']) {
+    help();
+    return 2;
+  }
+
+  const location = process.execPath;
+  const debugEnabled = argv['--debug'];
+  let version: string = argv['--version'];
+  const channel: string = argv['--channel'] || getDefaultChannel();
+  const output = createOutput({ debug: debugEnabled });
+  const { log, note, success, print, debug } = output;
+
+  // Don't update if executing with `node` (i.e. during development)
+  if (!basename(location).startsWith('now')) {
+    note(`Refusing to update file "${location}" because it does not appear to be a \`now\` binary`);
+    return 1;
+  }
+
+  log('Updating Now CLI');
+
+  if (!version) {
+    version = await getLatestVersion(output, channel);
+  }
+
+  if (version === pkg.version) {
+    note(`Now CLI is already the latest version (${chalk.green(version)})`);
+    return 0;
+  }
+
+  const platform = getPlatform(process.platform);
+  const url = getReleaseUrl(version, platform);
+
+  const config: { [name: string]: string } = {
+    platform,
+    arch: 'x64',
+    location,
+    version
+  };
+
+  print(`  ${chalk.bold('Configuration')}\n\n`);
+  for (const name of Object.keys(config)) {
+    print(`    ${chalk.cyan(name)}\t${config[name]}\n`);
+  }
+
+  print('\n');
+  log('Downloading `now` binary...');
+  log(`Binary URL: ${chalk.underline.blue(url)}`);
+
+  const tmpBin: string = join(tmpdir(), Math.random().toString(32).slice(-10));
+  try {
+    await downloadNowCli(output, url, tmpBin);
+    await updateNowCli(tmpBin, location);
+    success(`Updated Now CLI to ${chalk.green(version)}`);
+  } finally {
+    await remove(tmpBin);
+  }
+
+  return 0;
+}
