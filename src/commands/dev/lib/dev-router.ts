9786a67e0ace8100a37a784d37216c634cf5ba1f
JJ Kasper <jj@jjsweb.site>
2019-06-04 09:39:56 -0700

    Move slash no slash check to same line

--- src/commands/dev/lib/dev-router.ts
+++ src/commands/dev/lib/dev-router.ts
@@ -71,8 +71,7 @@ export default async function(
 
       const keys: string[] = [];
       const matcher = PCRE(`%${src}%i`, keys);
-      let match = matcher.exec(reqPathname);
-      match = match || matcher.exec(`/${reqPathname}`)
+      const match = matcher.exec(reqPathname) || matcher.exec(`/${reqPathname}`);
 
       if (match) {
         let destPath: string = `/${reqPathname}`;
