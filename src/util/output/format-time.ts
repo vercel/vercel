1302d0cacc443bf95182aad4df971b7033ddc008
Andy Bitz <artzbitz@gmail.com>
2019-06-25 20:45:03 +0200

    Use en-US locale

--- src/util/output/format-time.ts
+++ src/util/output/format-time.ts
@@ -5,7 +5,7 @@ function prefix(number: number | string) {
 
 export function shortTimeFormat(date: number | Date) {
   date = typeof date === 'number' ? new Date(date) : date;
-  const string = date.toLocaleTimeString();
+  const string = date.toLocaleTimeString('en-US');
   const [time, suffix] = string.split(' ');
   const [hour, minute, seconds] = time.split(':');
   return `${prefix(hour)}:${prefix(minute)}:${prefix(seconds)} ${suffix}`;
