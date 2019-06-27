72758b6e0d8863f4f9aa95d3fc431bcb41d2f124
Luc <luc.leray@gmail.com>
2019-06-27 17:16:37 +0200

    [tests] Improve git diff in jest config and refactor (#663)
    * improve git diff in jest config
    
    * refactor jest.config.js
    
    * try with empty testMatch
    
    * Revert "try with empty testMatch"
    
    This reverts commit ec69a03cc7953a8e6e2d7b2f3ba2bb08d2fcbfa5.
    
    * Update jest.config.js
    
    Co-Authored-By: Steven <steven@ceriously.com>
    
    * trim branch name
    
    * move trim up
    
    * add empty files to test behaviour
    
    * Remove empty files

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,29 @@
 const { execSync } = require('child_process');
 const { relative } = require('path');
 
-const branch = execSync('git branch | grep "*" | cut -d " " -f2').toString();
+const branch = execSync('git branch | grep "*" | cut -d " " -f2')
+  .toString()
+  .trim();
 console.log(`Running tests on branch "${branch}"`);
-const base = branch === 'master' ? 'HEAD~1' : 'origin/canary';
-const diff = execSync(`git diff ${base} --name-only`).toString();
+const gitPath = branch === 'master' ? 'HEAD~1' : 'origin/canary...HEAD';
+const diff = execSync(`git diff ${gitPath} --name-only`).toString();
 
 const changed = diff
   .split('\n')
   .filter(item => Boolean(item) && item.includes('packages/'))
   .map(item => relative('packages', item).split('/')[0]);
 
-const matches = [];
+const matches = Array.from(new Set(changed));
 
-if (changed.length > 0) {
-  console.log('The following packages have changed:');
-
-  changed.map((item) => {
-    matches.push(item);
-    console.log(item);
-
-    return null;
-  });
-} else {
+if (matches.length === 0) {
   matches.push('now-node');
   console.log(`No packages changed, defaulting to ${matches[0]}`);
+} else {
+  console.log('The following packages have changed:');
+  console.log(matches.join('\n'));
 }
 
-const testMatch = Array.from(new Set(matches)).map(
+const testMatch = matches.map(
   item => `**/${item}/**/?(*.)+(spec|test).[jt]s?(x)`,
 );
 
