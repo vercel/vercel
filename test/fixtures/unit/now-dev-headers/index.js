13c88c0a6bf99ffa54da78f8a78fe6963f564598
Nathan Rajlich <nathan@tootallnate.net>
2019-07-05 19:08:16 -0700

    Remove `console.error()` call

--- test/fixtures/unit/now-dev-headers/index.js
+++ test/fixtures/unit/now-dev-headers/index.js
@@ -2,7 +2,6 @@ const { parse } = require('url');
 
 module.exports = (req, res) => {
   const { query } = parse(req.url, true);
-  console.error({ query });
   res.setHeader(query.name, query.value);
   res.end();
 };
