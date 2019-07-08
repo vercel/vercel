a55ce5da8fae6ef355e717849f75ae51791679a9
Steven <steven@ceriously.com>
2019-07-08 14:24:52 -0400

    [now-node-server] Remove package source code (#721)
    * [now-node-server] Remove package
    
    * Update comment so tests run

--- packages/now-node-server/test/fixtures/06-content-type/probe.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert');
-
-module.exports = async ({ deploymentUrl, fetch, randomness }) => {
-  const resp = await fetch(`https://${deploymentUrl}/`);
-  assert.equal(resp.headers.get('content-type'), null);
-  assert.equal(await resp.text(), randomness);
-};
