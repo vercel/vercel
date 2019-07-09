fb863de6ff984b9d01643d949d50c66a8e8b37a3
Nathan Rajlich <nathan@tootallnate.net>
2019-07-09 14:20:42 -0700

    [now dev] Revert "Add `etag` response header for Lambda invocations"
    This reverts commit f80f1f79a6c27767590df4733c730e7f54b3b77c (#2502).
    
    `ETag` header is only sent in production when the lambda function sets
    the "stale-while-revalidate" cache-control header, which will be
    implemented in a separate PR.

--- src/util/sha.ts
+++
@@ -1,7 +0,0 @@
-import { createHash } from 'crypto';
-
-export function getSha(buffer: Buffer, cypher: string = 'sha256'): string {
-  const hash = createHash(cypher);
-  hash.update(buffer);
-  return hash.digest('hex');
-}
