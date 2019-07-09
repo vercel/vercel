2879041a6551e2f9785af38cf682375102c55717
Steven <steven@ceriously.com>
2019-07-09 18:02:45 -0400

    Remove deprecated builders (#733)
    * Remove deprecated builders
    
    * Update comment so that tests  run

--- packages/now-node/src/example-import.ts
+++ packages/now-node/src/example-import.ts
@@ -1,3 +1,4 @@
-// we intentionally import these types here
-// to test that they are exported from index
+// We intentionally import these types here
+// which will fail at compile time if exports
+// are not found in the index file
 import { NowRequest, NowResponse } from './index';
