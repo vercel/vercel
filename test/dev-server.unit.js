855940c6fc5f429ad26134385e8833fcf631d6d4
Nathan Rajlich <n@n8.io>
2019-07-09 18:49:38 -0700

    [now dev] Update `serve-handler` to v6.1.0 and enable `etag` option (#2525)
    * [now dev] Update `serve-handler` to v6.1.0 and enable `etag` option
    
    Sending the `ETag` response header matches how Now in production works.
    
    * Test file contents as well

--- test/dev-server.unit.js
+++ test/dev-server.unit.js
@@ -124,6 +124,15 @@ test(
   })
 );
 
+test(
+  '[DevServer] Sends `etag` header for static files',
+  testFixture('now-dev-headers', async (t, server) => {
+    const res = await fetch(`${server.address}/foo.txt`);
+    t.is(res.headers.get('etag'), '"55ca6286e3e4f4fba5d0448333fa99fc5a404a73"');
+    t.is(await res.text(), 'hi\n');
+  })
+);
+
 test('[DevServer] Does not install builders if there are no builds', async t => {
   const handler = data => {
     if (data.includes('installing')) {
