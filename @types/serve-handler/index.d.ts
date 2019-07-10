926c14e0ef05d9b29e016e1f60cf365cba557050
Nathan Rajlich <n@n8.io>
2019-07-09 18:49:38 -0700

    [now dev] Update `serve-handler` to v6.1.0 and enable `etag` option (#2525)
    * [now dev] Update `serve-handler` to v6.1.0 and enable `etag` option
    
    Sending the `ETag` response header matches how Now in production works.
    
    * Test file contents as well

--- @types/serve-handler/index.d.ts
+++ @types/serve-handler/index.d.ts
@@ -1,39 +1,40 @@
 declare module 'serve-handler' {
-  import http from 'http'
+  import http from 'http';
 
-  export default function (
+  export default function(
     request: http.IncomingMessage,
     response: http.ServerResponse,
     options: serveHandler.Options
-  ): void
+  ): void;
 }
 
 declare namespace serveHandler {
   interface Options {
-    public?: string,
-    cleanUrls?: boolean,
-    rewrites?: RewriteConfig[],
-    redirects?: RedirectConfig[],
-    headers?: HeaderConfig[],
-    directoryListing?: boolean | string[],
-    unlisted?: string[],
-    trailingSlash?: boolean,
-    renderSingle?: boolean
+    public?: string;
+    cleanUrls?: boolean;
+    rewrites?: RewriteConfig[];
+    redirects?: RedirectConfig[];
+    headers?: HeaderConfig[];
+    directoryListing?: boolean | string[];
+    unlisted?: string[];
+    trailingSlash?: boolean;
+    renderSingle?: boolean;
+    etag?: boolean;
   }
 
   interface RewriteConfig {
-    source: string,
-    destination: string
+    source: string;
+    destination: string;
   }
 
   interface RedirectConfig extends RewriteConfig {
-    type: number
+    type: number;
   }
 
   interface HeaderConfig {
-    source: string,
+    source: string;
     headers: {
-      [key: string]: string
-    }[]
+      [key: string]: string;
+    }[];
   }
 }
