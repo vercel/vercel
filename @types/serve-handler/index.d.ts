c17db3fbde686d3023cd26cac00f9cb21cd95eb3
Nathan Rajlich <nathan@tootallnate.net>
2019-07-10 17:00:25 -0700

    Move `serve-handler/src/directory` types to root

--- @types/serve-handler/index.d.ts
+++ @types/serve-handler/index.d.ts
@@ -38,3 +38,26 @@ declare namespace serveHandler {
     }[];
   }
 }
+
+declare module 'serve-handler/src/directory' {
+  interface File {
+    relative: string;
+    title: string;
+    type: string;
+    ext: string;
+    base: string;
+  }
+
+  interface Path {
+    name: string;
+    url: string;
+  }
+
+  interface Spec {
+    files?: File[];
+    paths?: Path[];
+    directory: string;
+  }
+
+  export default function(spec: Spec): string;
+}
