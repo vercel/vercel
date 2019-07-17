a0943f1b36641abe9a7e8c560f7d4b6d79053ee6
Andy Bitz <artzbitz@gmail.com>
2019-07-17 17:18:52 +0200

    Fix build

--- src/util/dev/server.ts
+++ src/util/dev/server.ts
@@ -596,7 +596,7 @@ export default class DevServer {
     this.files = results;
 
     const builders: Set<string> = new Set(
-      (nowConfig.builds || []).filter((b: BuildConfig) => b.use).map((b: BuildConfig) => b.use)
+      (nowConfig.builds || []).filter((b: BuildConfig) => b.use).map((b: BuildConfig) => b.use as string)
     );
 
     await installBuilders(builders, this.yarnPath, this.output);
