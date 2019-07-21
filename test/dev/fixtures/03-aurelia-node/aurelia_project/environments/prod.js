7674a4af4ac59a2f932aa5627b1716906953733e
Sophearak Tha <t.sophearak@gmail.com>
2019-07-22 06:48:23 +0700

    Add configuration and basic integration tests for `now dev` (#2553)
    * Add configuration and basic integration tests for `now dev`
    
    * Add test fixtures from `now-examples`
    
    * Add `include` to `tsconfig.json`
    
    * Increase retries for 00-list-directory
    
    * Increase retries for `03-aurelia-node`
    
    * Enable test fixtures for hexo, hugo, next, polymer, preact, svelte, vue, and vuepress
    
    * Increase retries for `08-hugo-node`
    
    * Disable `08-hugo-node` fixture
    
    * Enable `04-create-react-app-node` test
    
    * Disable `04-create-react-app-node` test
    
    * Better name for now dev test job

---
+++ test/dev/fixtures/03-aurelia-node/aurelia_project/environments/prod.js
@@ -0,0 +1,4 @@
+export default {
+  debug: false,
+  testing: false
+};
---
+++ test/dev/fixtures/03-aurelia-node/src/main.js
@@ -0,0 +1,19 @@
+// regenerator-runtime is to support async/await syntax in ESNext.
+// If you don't use async/await, you can remove regenerator-runtime.
+import 'regenerator-runtime/runtime';
+import environment from './environment';
+import {PLATFORM} from 'aurelia-pal';
+
+export function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .feature(PLATFORM.moduleName('resources/index'));
+
+  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
+
+  if (environment.testing) {
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+  }
+
+  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+}
