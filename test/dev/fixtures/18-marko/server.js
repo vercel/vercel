3d2929b345958a17eaa6797fd0b798f15359e629
Sophearak Tha <t.sophearak@gmail.com>
2019-07-29 09:44:30 +0700

    [now dev] Improve integration tests for `now dev` (#2597)
    * Using `stdio` to resolve `now dev` Ready
    
    * Update test/dev/integration.js
    
    Co-Authored-By: Andy <AndyBitz@users.noreply.github.com>
    
    * Improve `testFixtureStdio` and update aurelia fixture
    
    * Remove duplicate test
    
    * Update `create-react-app` and `gatsby` test fixture
    
    * Update `gridsome` test fixture
    
    * Update `hugo` test fixture
    
    * Update `jekyll` test fixture
    
    * Add `marko` test fixture
    
    * Add `mithril` test fixture
    
    * Add `riot` test fixture
    
    * Add `charge` test fixture
    
    * Add `brunch` test fixture
    
    * Add `docusaurus` and `ember` test fixtures
    
    * Install ruby for `jekyll`
    
    * Update base on platform for `jekyll` and `hugo`
    
    * Use `sudo` to when installing `gem`
    
    * Remove `09-jekyll`
    
    * Extract the right filename
    
    * Remove `-L` flag from `curl` for macOS
    
    * Use different version of `hugo`
    
    * Remove `min_version` from `08-hugo` theme

---
+++ test/dev/fixtures/18-marko/server.js
@@ -0,0 +1,3 @@
+require("./project").server({
+  httpPort: process.env.PORT || 8080 // Optional, but added here for demo purposes
+});
---
+++ test/dev/fixtures/21-charge/source/index.html.jsx
@@ -0,0 +1,10 @@
+import About from './pages/about.html.mdx';
+
+export default () => {
+  return (
+    <>
+      <h1>Welcome to my new Charge site!</h1>
+      <About />
+    </>
+  );
+};
