612607f09c2349abfb28cb3898dfeb0f37316b48
Andy Bitz <artzbitz@gmail.com>
2019-07-30 23:14:55 +0200

    Handle status code

--- scripts/start.js
+++ scripts/start.js
@@ -6,12 +6,16 @@ const match = process.version.match(/^v(\d+)\.(\d+)/);
 const major = match && parseInt(match[1], 10);
 const minor = match && parseInt(match[2], 10);
 
+// Must be above or equal to 10.10.0
 if (major > 10 || (major === 10 && minor >= 10)) {
   require('../dist/index.js');
 } else {
   const [node, _, ...args] = process.argv;
   const script = join(__dirname, '../dist/index.js');
-  spawnSync(node, ['--no-warnings', script, ...args], {
+
+  const { status } = spawnSync(node, ['--no-warnings', script, ...args], {
     stdio: 'inherit'
   });
+
+  process.exit(status);
 }
