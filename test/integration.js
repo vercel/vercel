71ca9189e42c897545029218083b824c17c1f0ef
Andy Bitz <artzbitz@gmail.com>
2019-07-30 23:03:00 +0200

    Make tests use custom start script

--- test/integration.js
+++ test/integration.js
@@ -15,7 +15,7 @@ import pkg from '../package';
 import parseList from './helpers/parse-list';
 import prepareFixtures from './helpers/prepare';
 
-const binaryPath = path.resolve(__dirname, `../dist/index.js`);
+const binaryPath = path.resolve(__dirname, `../scripts/start.js`);
 const fixture = name => path.join(__dirname, 'fixtures', 'integration', name);
 const deployHelpMessage = `${logo} now [options] <command | path>`;
 const session = Math.random()
