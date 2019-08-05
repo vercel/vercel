b4f9723f18dec7aed2ddfd6a14c27ddb633794ab
Nathan Rajlich <nathan@tootallnate.net>
2019-08-05 15:59:54 -0700

    Add `unhandledRejection` and `uncaughtException` handlers to scripts
    This is to prevent false-positives like this from occurring,
    and fixes the warning from `node`:
    
    ```
    $ ts-node ./scripts/build.ts
    Creating builders tarball with: @now/build-utils@canary, @now/go@canary, @now/next@canary, @now/node@canary, @now/php@canary, @now/static-build@canary
    (node:156) UnhandledPromiseRejectionWarning: Error: ENOENT: no such file or directory, open '/home/circleci/repo/assets/builders.tar.gz'
        at ReadStream.evt.error.err (/home/circleci/repo/node_modules/promisepipe/index.js:30:23)
        at ReadStream.emit (events.js:198:13)
        at ReadStream.EventEmitter.emit (domain.js:448:20)
        at /home/circleci/repo/node_modules/graceful-fs/graceful-fs.js:207:14
        at /home/circleci/repo/node_modules/graceful-fs/graceful-fs.js:258:16
        at FSReqWrap.args [as oncomplete] (fs.js:140:20)
    (node:156) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). (rejection id: 1)
    (node:156) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
    ```

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -15,7 +15,10 @@ function error(command) {
 }
 
 function debug(str) {
-  if (process.argv.find(str => str === '--debug') || process.env.PREINSTALL_DEBUG) {
+  if (
+    process.argv.find(str => str === '--debug') ||
+    process.env.PREINSTALL_DEBUG
+  ) {
     console.log(`[debug] [${new Date().toISOString()}]`, str);
   }
 }
@@ -113,6 +116,23 @@ async function main() {
 
     process.exit(1);
   }
+
+  process.exit(0);
 }
 
-main().then(() => process.exit(0));
+process.on('unhandledRejection', err => {
+  console.error('Unhandled Rejection:');
+  console.error(err);
+  process.exit(1);
+});
+
+process.on('uncaughtException', err => {
+  console.error('Uncaught Exception:');
+  console.error(err);
+  process.exit(1);
+});
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
