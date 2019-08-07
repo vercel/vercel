4c6c17af8ad19fb1376bf222866f4cac4a8b762d
Leo Lamprecht <mindrun@icloud.com>
2019-08-08 01:08:08 +0200

    [now-static-build] Capture file system routes for CRA apps (#891)
    * Capture file system routes for CRA apps
    
    * Added integration test
    
    * Fixed defaults

--- packages/now-static-build/src/frameworks.ts
+++ packages/now-static-build/src/frameworks.ts
@@ -158,32 +158,19 @@ export default [
       {
         src: '/static/(.*)',
         headers: { 'cache-control': 's-maxage=31536000, immutable' },
-        dest: '/static/$1',
-      },
-      {
-        src: '/favicon.ico',
-        dest: '/favicon.ico',
-      },
-      {
-        src: '/asset-manifest.json',
-        dest: '/asset-manifest.json',
-      },
-      {
-        src: '/manifest.json',
-        dest: '/manifest.json',
-      },
-      {
-        src: '/precache-manifest.(.*)',
-        dest: '/precache-manifest.$1',
+        continue: true,
       },
       {
         src: '/service-worker.js',
         headers: { 'cache-control': 's-maxage=0' },
-        dest: '/service-worker.js',
+        continue: true,
       },
       {
         src: '/sockjs-node/(.*)',
         dest: '/sockjs-node/$1',
+      },
+      {
+        handle: 'filesystem',
       },
       {
         src: '/(.*)',
