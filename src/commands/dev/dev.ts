38bc7cc6d65c7af1f538f0d86ae3ed97b0af42f8
Leo Lamprecht <mindrun@icloud.com>
2019-08-09 20:16:21 +0200

    [now dev] Add `--listen` / `-l` flag, deprecate `--port` / `-p` (#2720)
    * [now dev] Add `--bind` / `-b` flag
    
    This changes the default network interface that `now dev` binds to, in
    order to prevent operating system firewalls from showing a confirmation
    prompt in order to run.
    
    Fixes #2704.
    
    * Change to `--listen`
    
    * Default port 3000
    
    * Fix unit
    
    * Fix `--port`
    
    * Use `-l` for integration tests
    
    * Add unit tests

--- src/commands/dev/dev.ts
+++ src/commands/dev/dev.ts
@@ -2,14 +2,13 @@ import path from 'path';
 
 import pkg from '../../../package.json';
 import DevServer from '../../util/dev/server';
+import parseListen from '../../util/dev/parse-listen';
 import { Output } from '../../util/output';
 import { NowContext } from '../../types';
 
 type Options = {
-  '--debug': boolean;
-  '-d': boolean;
-  '--port': number;
-  '-p': number;
+  '--debug'?: boolean;
+  '--listen'?: string;
 };
 
 export default async function dev(
@@ -24,11 +23,11 @@ export default async function dev(
 
   const [dir = '.'] = args;
   const cwd = path.resolve(dir);
-  const port = opts['-p'] || opts['--port'];
-  const debug = opts['-d'] || opts['--debug'];
+  const listen = parseListen(opts['--listen'] || '3000');
+  const debug = opts['--debug'] || false;
   const devServer = new DevServer(cwd, { output, debug });
 
   process.once('SIGINT', devServer.stop.bind(devServer));
 
-  await devServer.start(port);
+  await devServer.start(...listen);
 }
