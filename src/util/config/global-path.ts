532c3eff18573ca48185f048e71ebe5e71c7dd88
Max Strübing <mxstrbng@gmail.com>
2019-08-16 19:46:11 +0200

    Use `xdg-portable` to determine the data dir if no env variable is set
    and use the old location as a last resort.

--- src/util/config/global-path.ts
+++ src/util/config/global-path.ts
@@ -1,18 +1,20 @@
 // Native
 import { homedir } from 'os';
 import fs from 'fs';
-
 import path from 'path';
 
 // Packages
 import mri from 'mri';
+import xdg from 'xdg-portable';
+import head from 'lodash.head';
 
 // The `homeConfigPath` is the legacy configuration path located in the users home directory.
 const homeConfigPath: string = path.join(homedir(), '.now');
 
 // The `xdgConfigPath` is the configuration path which is based on the XDG standard.
+// the old legacy path is used as a fallback
 const xdgConfigPath: string = path.join(
-  process.env.XDG_DATA_HOME || path.join(homedir(), '.local', 'share'),
+  process.env.XDG_DATA_HOME || (head(xdg.dataDirs()) || homeConfigPath),
   'now'
 );
 
