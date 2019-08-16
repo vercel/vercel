a322b78721fa8fb109aabd7f308c9acec79eabb8
Max Strübing <mxstrbng@gmail.com>
2019-08-16 19:16:09 +0200

    Use XDG standard instead of writing to home directory

--- src/util/config/global-path.ts
+++ src/util/config/global-path.ts
@@ -1,12 +1,31 @@
 // Native
 import { homedir } from 'os';
+import fs from 'fs';
 
 import path from 'path';
 
 // Packages
 import mri from 'mri';
 
-const getNowDir = () => {
+// The `homeConfigPath` is the legacy configuration path located in the users home directory.
+const homeConfigPath: string = path.join(homedir(), '.now');
+
+// The `xdgConfigPath` is the configuration path which is based on the XDG standard.
+const xdgConfigPath: string = path.join(
+  process.env.XDG_DATA_HOME || path.join(homedir(), '.local', 'share'),
+  'now'
+);
+
+// Returns wether a directory exists
+const isDirectory = (path: string): boolean => {
+  try {
+    return fs.lstatSync(path).isDirectory();
+  } catch (e) {
+    return false;
+  }
+};
+
+const getNowDir = (): string => {
   const args = mri(process.argv.slice(2), {
     string: ['global-config'],
     alias: {
@@ -15,12 +34,16 @@ const getNowDir = () => {
   });
 
   const customPath = args['global-config'];
-
-  if (!customPath) {
-    return path.join(homedir(), '.now');
+  if (customPath) {
+    return path.resolve(customPath);
   }
 
-  return path.resolve(customPath);
+  // legacy config exists
+  if (isDirectory(homeConfigPath)) {
+    return homeConfigPath;
+  }
+
+  return xdgConfigPath;
 };
 
 export default getNowDir;
