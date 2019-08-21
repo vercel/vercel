d62770f1aefc19b29cb0bb99ce0934e5ebdb2826
Steven <steven@ceriously.com>
2019-08-21 16:34:50 -0400

    Fix string match in shell scripts

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 circleci_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-found="$(grep -rn '"build"' packages/*/package.json | cut -d: -f1)"
+found="$(grep -rn '"build":' packages/*/package.json | cut -d: -f1)"
 
 for pkg in $found; do
   dir="$(dirname "$pkg")"
