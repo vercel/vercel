55dd5bc8d1220e01721ec3fd8a37beb7605279c3
Steven <steven@ceriously.com>
2019-08-21 15:46:44 -0400

    Add scripts

---
+++ .circleci/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+circleci_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+found="$(grep -rn '"build"' packages/*/package.json | cut -d: -f1)"
+
+for pkg in $found; do
+  dir="$(dirname "$pkg")"
+  cd "$circleci_dir/../$dir"
+  echo "yarn build \`$dir\`"
+  yarn build
+done
---
+++ .circleci/test-unit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+circleci_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+found="$(grep -rn '"test-unit"' packages/*/package.json | cut -d: -f1)"
+
+for pkg in $found; do
+  dir="$(dirname "$pkg")"
+  cd "$circleci_dir/../$dir"
+  echo "yarn test-unit \`$dir\`"
+  yarn test-unit
+done
