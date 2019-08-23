e8a9efcba452c5bf3cdeeb8690bd2754298d4acf
Steven <steven@ceriously.com>
2019-08-23 16:02:31 -0400

    Only run script for modified packages

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -7,11 +7,16 @@ if [ "$1" == "" ]; then
 fi
 
 circleci_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-found="$(grep -rn \"""$1"\"": packages/*/package.json | cut -d: -f1 | uniq)"
+found="$(grep -rn \"""$1"\"": packages/*/package.json | cut -d '/' -f2)"
+modified="$(git diff origin/canary...HEAD --name-only | grep '^packages/' | cut -d '/' -f2)"
+both="${found}"$'\n'"${modified}"
+pkgs="$(echo -e "${both// /\\n}" | sort -u)"
+echo "The following packages were modified: "
+echo "$pkgs" | wc -l
+echo "$pkgs"
 
-for pkg in $found; do
-  dir="$(dirname "$pkg")"
-  cd "$circleci_dir/../$dir"
-  echo "yarn $1 \`$dir\`"
+for dir in $pkgs; do
+  echo "cd packages/$dir && yarn $1"
+  cd "$circleci_dir/../packages/$dir"
   yarn "$1"
 done
