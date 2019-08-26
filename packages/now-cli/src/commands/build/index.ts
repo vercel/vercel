10130d477e057d088ac49ae42dbbe1c53c14eee7
JJ Kasper <jj@jjsweb.site>
2019-08-26 15:30:17 -0500

    Run prettier on new files

--- packages/now-cli/src/commands/build/index.ts
+++ packages/now-cli/src/commands/build/index.ts
@@ -1,69 +1,71 @@
-import _glob from 'glob'
-import path from 'path'
-import fs from 'fs-extra'
-import execa from 'execa'
-import {promisify} from 'util'
-import plural from 'pluralize'
-import npa from 'npm-package-arg'
-import { detectBuilders, Builder, FileFsRef } from '@now/build-utils'
-import { NowContext } from '../../types'
-import createOutput, { Output } from '../../util/output'
-import { getAllProjectFiles } from '../../util/get-files'
-import { getYarnPath } from '../../util/dev/yarn-installer'
-import runBuild from './run-build';
+import _glob from "glob";
+import path from "path";
+import fs from "fs-extra";
+import execa from "execa";
+import { promisify } from "util";
+import plural from "pluralize";
+import npa from "npm-package-arg";
+import { detectBuilders, Builder, FileFsRef } from "@now/build-utils";
+import { NowContext } from "../../types";
+import createOutput, { Output } from "../../util/output";
+import { getAllProjectFiles } from "../../util/get-files";
+import { getYarnPath } from "../../util/dev/yarn-installer";
+import runBuild from "./run-build";
 
-const glob = promisify(_glob)
+const glob = promisify(_glob);
 
-const cwd = process.cwd()
-const outputDir = path.join(cwd, '.now')
-const workDir = path.join(outputDir, 'workDir')
-const buildersDir = path.join(outputDir, '/builders')
-const buildsOutputDir = path.join(outputDir, 'builds')
+const cwd = process.cwd();
+const outputDir = path.join(cwd, ".now");
+const workDir = path.join(outputDir, "workDir");
+const buildersDir = path.join(outputDir, "/builders");
+const buildsOutputDir = path.join(outputDir, "builds");
 
-let output: Output
+let output: Output;
 
-async function getBuilds (files: string[]): Promise<Builder[] | null> {
-  let nowJson
-  let builds
+async function getBuilds(files: string[]): Promise<Builder[] | null> {
+  let nowJson;
+  let builds;
 
   try {
-    nowJson = await fs.readJson(path.join(cwd, 'now.json'))
-    builds = nowJson.builds
+    nowJson = await fs.readJson(path.join(cwd, "now.json"));
+    builds = nowJson.builds;
   } catch (err) {
-    output.debug('No now.json found, assuming zero-config')
+    output.debug("No now.json found, assuming zero-config");
   }
 
   // if no builds defined, should be zero-config
   if (!Array.isArray(builds) || builds.length === 0) {
-    let pkg
+    let pkg;
     try {
-      pkg = await fs.readJson(path.join(cwd, 'package.json'))
+      pkg = await fs.readJson(path.join(cwd, "package.json"));
     } catch (err) {
-      output.debug('No package.json found')
+      output.debug("No package.json found");
     }
-    builds = (await detectBuilders(files, pkg)).builders
+    builds = (await detectBuilders(files, pkg)).builders;
   }
 
-  return builds
+  return builds;
 }
 
 export default async function main(ctx: NowContext) {
-  const debug = ctx.argv.some(arg => arg === '-d' || arg === '--debug')
-  const onlyArgIdx = ctx.argv.indexOf('--only')
-  const onlyBuild = onlyArgIdx !== -1 && ctx.argv[ctx.argv.indexOf('--only') + 1]
+  const debug = ctx.argv.some(arg => arg === "-d" || arg === "--debug");
+  const onlyArgIdx = ctx.argv.indexOf("--only");
+  const onlyBuild =
+    onlyArgIdx !== -1 && ctx.argv[ctx.argv.indexOf("--only") + 1];
 
-  output = createOutput({ debug })
-  output.log('Setting up builds...')
+  output = createOutput({ debug });
+  output.log("Setting up builds...");
 
   const files = (await getAllProjectFiles(cwd, output))
     .concat(
       // getAllProjectFiles doesn't include dotfiles (.babelrc) so grab those
-      (await glob('**/.*', { cwd, nodir: true }))
-        .map(f => f.replace(/\\/g, '/'))
+      (await glob("**/.*", { cwd, nodir: true })).map(f =>
+        f.replace(/\\/g, "/")
+      )
     )
     .filter(file => {
-      return !file.startsWith('node_modules') && !file.startsWith('.now')
-    })
+      return !file.startsWith("node_modules") && !file.startsWith(".now");
+    });
 
   const fileRefs: { [filePath: string]: FileFsRef } = {};
 
@@ -72,65 +74,61 @@ export default async function main(ctx: NowContext) {
     const { mode } = await fs.stat(fsPath);
     fileRefs[relPath] = new FileFsRef({ mode, fsPath });
   }
-  let builds = await getBuilds(files)
+  let builds = await getBuilds(files);
 
   if (builds && onlyBuild) {
-    output.debug(`Filtering by src: ${onlyBuild}`)
-    builds = builds.filter(build => build.src === onlyBuild)
+    output.debug(`Filtering by src: ${onlyBuild}`);
+    builds = builds.filter(build => build.src === onlyBuild);
   }
   if (!builds || builds.length === 0) {
-    return output.warn('No builds found')
+    return output.warn("No builds found");
   }
-  output.log(`Found ${builds.length} ${plural('build', builds.length)}`)
-  output.debug(`builds: ${JSON.stringify(builds)}`)
+  output.log(`Found ${builds.length} ${plural("build", builds.length)}`);
+  output.debug(`builds: ${JSON.stringify(builds)}`);
 
-  await fs.ensureDir(buildersDir)
-  const builders = [...new Set(builds.map(build => build.use))]
+  await fs.ensureDir(buildersDir);
+  const builders = [...new Set(builds.map(build => build.use))];
 
   const buildersPkg = {
-    name: 'builders',
+    name: "builders",
     dependencies: {} as { [name: string]: string }
-  }
+  };
 
   for (const builder of builders) {
-    const info = npa(builder)
-    buildersPkg.dependencies[info.name || ''] = info.fetchSpec || 'latest'
+    const info = npa(builder);
+    buildersPkg.dependencies[info.name || ""] = info.fetchSpec || "latest";
   }
 
   await fs.writeFile(
-    path.join(buildersDir, 'package.json'),
+    path.join(buildersDir, "package.json"),
     JSON.stringify(buildersPkg),
-    'utf8'
-  )
-  const yarnDir = await getYarnPath(output)
-  const yarnPath = path.join(yarnDir, 'yarn')
+    "utf8"
+  );
+  const yarnDir = await getYarnPath(output);
+  const yarnPath = path.join(yarnDir, "yarn");
 
-  output.log('Installing builders')
+  output.log("Installing builders");
 
-  await execa(
-    process.execPath,
-    [
-      yarnPath,
-      'install'
-    ],
-    {
-      cwd: buildersDir,
-      stdio: 'inherit'
-    }
-  )
+  await execa(process.execPath, [yarnPath, "install"], {
+    cwd: buildersDir,
+    stdio: "inherit"
+  });
 
-  await fs.ensureDir(workDir)
-  await fs.ensureDir(buildsOutputDir)
+  await fs.ensureDir(workDir);
+  await fs.ensureDir(buildsOutputDir);
 
   const filesToPersist = new Set([
-    'node_modules', 'package.json', 'yarn.lock', 'package-log.json'
-  ])
+    "node_modules",
+    "package.json",
+    "yarn.lock",
+    "package-log.json"
+  ]);
 
   // Run the builds
   for (const build of builds) {
     for (const file of await fs.readdir(workDir)) {
-      if (filesToPersist.has(file)) continue
-      await fs.remove(path.join(workDir, file))
+      if (filesToPersist.has(file)) continue;
+      await fs.remove(path.join(workDir, file));
     }
 
     await runBuild({
@@ -139,7 +137,7 @@ export default async function main(ctx: NowContext) {
       buildersDir,
       buildsOutputDir,
       build,
-      files: fileRefs,
-    })
+      files: fileRefs
+    });
   }
 }
--- packages/now-cli/src/commands/build/run-build.ts
+++ packages/now-cli/src/commands/build/run-build.ts
@@ -1,11 +1,11 @@
-import path from 'path'
-import fs from 'fs-extra'
-import npa from 'npm-package-arg'
-import resolveFrom from 'resolve-from'
-import { Builder, FileFsRef } from '@now/build-utils'
-import { Output } from '../../util/output'
+import path from "path";
+import fs from "fs-extra";
+import npa from "npm-package-arg";
+import resolveFrom from "resolve-from";
+import { Builder, FileFsRef } from "@now/build-utils";
+import { Output } from "../../util/output";
 
-const cleanDirPart = (part: string) => part.replace(/(\/|\\)/g, '_')
+const cleanDirPart = (part: string) => part.replace(/(\/|\\)/g, "_");
 
 export default async function runBuild({
   workDir,
@@ -15,58 +15,57 @@ export default async function runBuild({
   files,
   output
 }: {
-  workDir: string,
-  buildersDir: string,
-  buildsOutputDir: string,
-  build: Builder,
-  files: { [filePath: string]: FileFsRef },
-  output: Output
+  workDir: string;
+  buildersDir: string;
+  buildsOutputDir: string;
+  build: Builder;
+  files: { [filePath: string]: FileFsRef };
+  output: Output;
 }): Promise<void> {
-  output.log(`Running build: ${JSON.stringify(build)}`)
-  const builderInfo = npa(build.use)
-  const builderName = builderInfo.name || ''
-  const builderPath = resolveFrom(buildersDir, builderName)
-  const builder = require(builderPath)
+  output.log(`Running build: ${JSON.stringify(build)}`);
+  const builderInfo = npa(build.use);
+  const builderName = builderInfo.name || "";
+  const builderPath = resolveFrom(buildersDir, builderName);
+  const builder = require(builderPath);
 
-  const outputDir = path.join(buildsOutputDir, `${
-    cleanDirPart(build.src)
-  }-${
-    cleanDirPart(builderName)
-  }`)
-  await fs.remove(outputDir)
-  await fs.ensureDir(outputDir)
+  const outputDir = path.join(
+    buildsOutputDir,
+    `${cleanDirPart(build.src)}-${cleanDirPart(builderName)}`
+  );
+  await fs.remove(outputDir);
+  await fs.ensureDir(outputDir);
 
   const buildOutput = await builder.build({
     files,
     entrypoint: build.src,
     workPath: workDir,
     config: build.config || {}
-  })
+  });
 
-  const outputKeys = Object.keys(buildOutput.output)
-  const dirNames = new Set(outputKeys.map(p => path.dirname(p)))
+  const outputKeys = Object.keys(buildOutput.output);
+  const dirNames = new Set(outputKeys.map(p => path.dirname(p)));
 
   for (const outputKey of outputKeys) {
-    const item = buildOutput.output[outputKey]
-    let itemPath = path.join(outputDir, outputKey)
+    const item = buildOutput.output[outputKey];
+    let itemPath = path.join(outputDir, outputKey);
 
     if (dirNames.has(outputKey)) {
-      itemPath = path.join(itemPath, 'index')
+      itemPath = path.join(itemPath, "index");
     }
 
-    await fs.ensureDir(path.dirname(itemPath))
+    await fs.ensureDir(path.dirname(itemPath));
 
-    if (item.type === 'Lambda') {
-      await fs.writeFile(itemPath, item.zipBuffer)
-      item.zipBuffer = 'OMITTED'
-    } else if (item.type === 'FileFsRef') {
-      const writeStream = fs.createWriteStream(itemPath)
-      item.toStream().pipe(writeStream)
+    if (item.type === "Lambda") {
+      await fs.writeFile(itemPath, item.zipBuffer);
+      item.zipBuffer = "OMITTED";
+    } else if (item.type === "FileFsRef") {
+      const writeStream = fs.createWriteStream(itemPath);
+      item.toStream().pipe(writeStream);
     }
   }
 
   await fs.writeFile(
-    path.join(outputDir, 'output.json'),
-    JSON.stringify(buildOutput, null, 2),
-  )
+    path.join(outputDir, "output.json"),
+    JSON.stringify(buildOutput, null, 2)
+  );
 }
