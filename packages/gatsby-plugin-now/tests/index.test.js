8eb9d8c52f8328b1bb32520ce5922b375890d370
luc <luc.leray@gmail.com>
2019-08-28 01:27:46 +0200

    add test

---
+++ packages/gatsby-plugin-now/tests/index.test.js
@@ -0,0 +1,15 @@
+const execa = require('execa')
+const { join } = require('path')
+
+jest.setTimeout(15000)
+
+test('test generated now routes', async () => {
+  await execa('gatsby', ['build'], {
+    preferLocal: true,
+    cwd: join(__dirname, 'fixtures')
+  })
+
+  const nowRoutes = require('./fixtures/public/__now_routes.json')
+
+  expect(nowRoutes).toMatchSnapshot()
+})
