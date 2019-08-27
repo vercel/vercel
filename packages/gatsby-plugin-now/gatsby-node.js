4d64b03b8f53c805e338cb446daa453b733376aa
luc <luc.leray@gmail.com>
2019-08-28 01:27:38 +0200

    add gatsby-plugin-now

---
+++ packages/gatsby-plugin-now/gatsby-node.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const writeFile = require('util').promisify(require('fs').writeFile)
+
+const REDIRECT_FILE_NAME = '__now_routes.json'
+
+exports.onPostBuild = async ({ store }) => {
+  const { redirects, program } = store.getState()
+
+  if (!redirects.length === 0) {
+    return
+  }
+
+  const routes = redirects.map(redirect => {
+    return {
+      src: redirect.fromPath,
+      status: redirect.statusCode || (redirect.isPermanent ? 301 : 302),
+      headers: { Location: redirect.toPath },
+      force: redirect.force
+    }
+  })
+
+  await writeFile(
+    path.join(program.directory, 'public', REDIRECT_FILE_NAME),
+    JSON.stringify(routes)
+  )
+}
