130e634a0eb0e72f53366e66a836bbd908ae1085
Max Strübing <mxstrbng@gmail.com>
2019-08-27 08:55:20 +0200

    Use XDG standard instead of writing to home directory

--- packages/now-cli/src/util/config/global-path.ts
+++ packages/now-cli/src/util/config/global-path.ts
@@ -1,12 +1,36 @@
 // Native
 import { homedir } from 'os';
 
+import fs from 'fs';
 import path from 'path';
 
 // Packages
 import mri from 'mri';
+import xdg from 'xdg-portable';
+import head from 'lodash.head';
 
-const getNowDir = () => {
+// The `homeConfigPath` is the legacy configuration path located in the users home directory.
+const homeConfigPath: string = path.join(homedir(), '.now');
+
+// The `xdgConfigPath` is the configuration path which is based on the XDG standard.
+// the old legacy path is used as a fallback
+const xdgConfigPath: string = path.join(
+  head(xdg.dataDirs()) || homeConfigPath,
+  'now'
+);
+
+// Returns wether a directory exists
+const isDirectory = (path: string): boolean => {
+  try {
+    return fs.lstatSync(path).isDirectory();
+  } catch (_) {
+    // We don't care which kind of error occured, it isn't a directory anyway.
+    return false;
+  }
+};
+
+// Returns in which directory the cofig should be present
+const getNowDir = (): string => {
   const args = mri(process.argv.slice(2), {
     string: ['global-config'],
     alias: {
@@ -16,11 +40,14 @@ const getNowDir = () => {
 
   const customPath = args['global-config'];
 
-  if (!customPath) {
-    return path.join(homedir(), '.now');
-  }
-
-  return path.resolve(customPath);
+  // We use the customPath if it is available,
+  // otherwise the config at the users homedirectory if it is present
+  // and if nothing of this is there we use the XDG-standard
+  return (
+    (customPath && path.resolve(customPath)) ||
+    (isDirectory(homeConfigPath) && homeConfigPath) ||
+    xdgConfigPath
+  );
 };
 
 export default getNowDir;
