ca049467252e30fd92912ac74ca2dba28bd37e4b
luc <luc.leray@gmail.com>
2019-08-28 16:39:59 +0200

    copy gatsby plugin files in test case

--- packages/now-static-build/build.sh
+++ packages/now-static-build/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-ncc build src/index.ts -o dist
+# ncc build src/index.ts -o dist
+
+# copy gatsby plugin files in the gatsby redirect test case
+gatsby_plugin="$(dirname $(pwd))/gatsby-plugin-now"
+gatsby_local="test/fixtures/10a-gatsby-redirects/plugins/gatsby-plugin-now"
+mkdir -p $gatsby_local
+cp -v "$gatsby_plugin/gatsby-node.js" "$gatsby_plugin/package.json" "$gatsby_local"
