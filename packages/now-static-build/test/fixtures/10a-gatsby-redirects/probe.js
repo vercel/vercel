69c617cba71074d321365eabf9f3199c20e072a0
luc <luc.leray@gmail.com>
2019-08-28 22:05:51 +0200

    fetch location is not raw location

--- packages/now-static-build/test/fixtures/10a-gatsby-redirects/probe.js
+++ packages/now-static-build/test/fixtures/10a-gatsby-redirects/probe.js
@@ -1,21 +1,28 @@
 const assert = require("assert");
 
+// we need this due to https://github.com/bitinn/node-fetch/issues/417
+const getRawLocation = res => {
+  const location = resp1.headers.get("location");
+  const parsed = new URL(location);
+  return parsed.pathname;
+};
+
 module.exports = async ({ deploymentUrl, fetch }) => {
   const resp1 = await fetch(`https://${deploymentUrl}/permanent`, {
     redirect: "manual"
   });
   assert.equal(resp1.status, 301);
-  assert.equal(resp1.headers.get("location"), "/");
+  assert.equal(getRawLocation(resp1), "/");
 
   const resp2 = await fetch(`https://${deploymentUrl}/not-permanent`, {
     redirect: "manual"
   });
   assert.equal(resp2.status, 302);
-  assert.equal(resp2.headers.get("location"), "/");
+  assert.equal(getRawLocation(resp2), "/");
 
   const resp3 = await fetch(`https://${deploymentUrl}/custom-status`, {
     redirect: "manual"
   });
   assert.equal(resp3.status, 404);
-  assert.equal(resp3.headers.get("location"), "/");
+  assert.equal(getRawLocation(resp3), "/");
 };
