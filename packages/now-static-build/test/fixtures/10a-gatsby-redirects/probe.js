4524a36eda006d1857f905ce57bdb46707719e21
luc <luc.leray@gmail.com>
2019-08-29 00:20:00 +0200

    fix type error

--- packages/now-static-build/test/fixtures/10a-gatsby-redirects/probe.js
+++ packages/now-static-build/test/fixtures/10a-gatsby-redirects/probe.js
@@ -1,28 +1,23 @@
 const assert = require("assert");
 
-// we need this due to https://github.com/bitinn/node-fetch/issues/417
-const getRawLocation = res => {
-  const location = res.headers.get("location");
-  const parsed = new URL(location);
-  return parsed.pathname;
-};
-
 module.exports = async ({ deploymentUrl, fetch }) => {
   const resp1 = await fetch(`https://${deploymentUrl}/permanent`, {
     redirect: "manual"
   });
   assert.equal(resp1.status, 301);
-  assert.equal(getRawLocation(resp1), "/");
+  // we need to put the full url
+  // due to https://github.com/bitinn/node-fetch/issues/417
+  assert.equal(resp1.headers.get("location"), `https://${deploymentUrl}/`);
 
   const resp2 = await fetch(`https://${deploymentUrl}/not-permanent`, {
     redirect: "manual"
   });
   assert.equal(resp2.status, 302);
-  assert.equal(getRawLocation(resp2), "/");
+  assert.equal(resp2.headers.get("location"), `https://${deploymentUrl}/`);
 
   const resp3 = await fetch(`https://${deploymentUrl}/custom-status`, {
     redirect: "manual"
   });
   assert.equal(resp3.status, 404);
-  assert.equal(getRawLocation(resp3), "/");
+  assert.equal(resp3.headers.get("location"), `https://${deploymentUrl}/`);
 };
