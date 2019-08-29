ef86bb0bd9286d8dba600f439cf7cc15bddf0b72
Luc <luc.leray@gmail.com>
2019-08-30 01:56:10 +0200

    [tests] Unify `.editorconfig` (#2920)

--- packages/now-cgi/index.js
+++ packages/now-cgi/index.js
@@ -10,35 +10,35 @@ const { shouldServe } = require('@now/build-utils');
 exports.analyze = ({ files, entrypoint }) => files[entrypoint].digest;
 
 exports.build = async ({ workPath, files, entrypoint, meta }) => {
-	console.log('downloading files...');
-	const outDir = await getWritableDirectory();
+  console.log('downloading files...');
+  const outDir = await getWritableDirectory();
 
-	await download(files, workPath, meta);
+  await download(files, workPath, meta);
 
-	const handlerPath = path.join(__dirname, 'handler');
-	await copyFile(handlerPath, path.join(outDir, 'handler'));
+  const handlerPath = path.join(__dirname, 'handler');
+  await copyFile(handlerPath, path.join(outDir, 'handler'));
 
-	const entrypointOutDir = path.join(outDir, path.dirname(entrypoint));
-	await mkdirp(entrypointOutDir);
+  const entrypointOutDir = path.join(outDir, path.dirname(entrypoint));
+  await mkdirp(entrypointOutDir);
 
-	// For now only the entrypoint file is copied into the lambda
-	await copyFile(
-		path.join(workPath, entrypoint),
-		path.join(outDir, entrypoint)
-	);
+  // For now only the entrypoint file is copied into the lambda
+  await copyFile(
+    path.join(workPath, entrypoint),
+    path.join(outDir, entrypoint)
+  );
 
-	const lambda = await createLambda({
-		files: await glob('**', outDir),
-		handler: 'handler',
-		runtime: 'go1.x',
-		environment: {
-			SCRIPT_FILENAME: entrypoint
-		}
-	});
+  const lambda = await createLambda({
+    files: await glob('**', outDir),
+    handler: 'handler',
+    runtime: 'go1.x',
+    environment: {
+      SCRIPT_FILENAME: entrypoint
+    }
+  });
 
-	return {
-		[entrypoint]: lambda
-	};
+  return {
+    [entrypoint]: lambda
+  };
 };
 
 exports.shouldServe = shouldServe;
