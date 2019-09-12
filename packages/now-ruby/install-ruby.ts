9956e85f129bf12c1b7d462f56683bf88c9af4af
Steven <steven@ceriously.com>
2019-09-12 09:34:54 -0400

    [now-ruby] Use pre-installed ruby 2.5 (#2991)
    * [now-ruby] Use pre-installed ruby 2.5
    
    * Change GEM_HOME
    
    * Add polyfill for Node 8

--- packages/now-ruby/install-ruby.ts
+++ packages/now-ruby/install-ruby.ts
@@ -1,40 +1,9 @@
 import { join } from 'path';
 import execa from 'execa';
-import { getWriteableDirectory, debug } from '@now/build-utils';
+import { debug } from '@now/build-utils';
 
-const RUBY_VERSION = '2.5.5';
-
-async function installRuby(version: string = RUBY_VERSION) {
-  const baseDir = await getWriteableDirectory();
-  const rubyDir = join(baseDir, 'ruby');
-  const rubyBuildDir = join(baseDir, 'ruby-build');
-
-  await execa(
-    'yum',
-    [
-      'install',
-      '-y',
-      'git',
-      'gcc',
-      'make',
-      'tar',
-      'bzip2',
-      'readline-devel',
-      'openssl-devel',
-      'ruby-devel',
-      'zlib-devel',
-    ],
-    { stdio: 'pipe' }
-  );
-  await execa(
-    'git',
-    ['clone', 'git://github.com/rbenv/ruby-build.git', rubyBuildDir],
-    { stdio: 'pipe' }
-  );
-  await execa(join(rubyBuildDir, 'bin', 'ruby-build'), [version, rubyDir], {
-    stdio: 'pipe',
-  });
-
+async function installRuby() {
+  const rubyDir = '/ruby25';
   return {
     gemHome: rubyDir,
     rubyPath: join(rubyDir, 'bin', 'ruby'),
