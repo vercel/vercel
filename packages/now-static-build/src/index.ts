b773745bc827f9ac37547b9f837fda07150c215b
Steven <steven@ceriously.com>
2019-09-17 14:30:55 -0400

    Change spawnAsync to remove cwd

--- packages/now-static-build/src/index.ts
+++ packages/now-static-build/src/index.ts
@@ -167,17 +167,17 @@ export async function build({
         */
         const prefix = `hugo_`;
         const url = `https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${prefix}${HUGO_VERSION}_Linux-64bit.tar.gz`;
-        await spawnAsync(`curl -L ${url} | tar -zx -C /usr/local/bin`, [], '/tmp');
+        await spawnAsync(`curl -L ${url} | tar -zx -C /usr/local/bin`, [], { shell: true });
       }
 
       if (ZOLA_VERSION && !meta.isDev) {
         const url = `https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz`;
-        await spawnAsync(`curl -L ${url} | tar -zx -C /usr/local/bin`, [], '/tmp');
+        await spawnAsync(`curl -L ${url} | tar -zx -C /usr/local/bin`, [], { shell: true });
       }
 
       if (GUTENBERG_VERSION && !meta.isDev) {
         const url = `https://github.com/getzola/zola/releases/download/v${GUTENBERG_VERSION}/gutenberg-v${GUTENBERG_VERSION}-x86_64-unknown-linux-gnu.tar.gz`;
-        await spawnAsync(`curl -L ${url} | tar -zx -C /usr/local/bin`, [], '/tmp');
+        await spawnAsync(`curl -L ${url} | tar -zx -C /usr/local/bin`, [], { shell: true });
       }
 
       // `public` is the default for zero config
