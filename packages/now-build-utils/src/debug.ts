9e66f9bb08e613323c68d080e3a8f06faacf8aaa
Sophearak Tha <t.sophearak@gmail.com>
2019-09-20 16:07:36 +0700

    [now-build-utils] Remove `NOW_BUILDER_ANNOTATE` (#3027)

--- packages/now-build-utils/src/debug.ts
+++ packages/now-build-utils/src/debug.ts
@@ -1,7 +1,5 @@
 export default function debug(message: string, ...additional: any[]) {
   if (process.env.NOW_BUILDER_DEBUG) {
     console.log(message, ...additional);
-  } else if (process.env.NOW_BUILDER_ANNOTATE) {
-    console.log(`[now-builder-debug] ${message}`, ...additional);
   }
 }
