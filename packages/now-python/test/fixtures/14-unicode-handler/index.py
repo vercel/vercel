f045705d8891b62e99fe0687050f54667f9e892f
Christopher <offero@gmail.com>
2019-09-28 21:04:57 -0400

    removed prints. minor.

--- packages/now-python/test/fixtures/14-unicode-handler/index.py
+++ packages/now-python/test/fixtures/14-unicode-handler/index.py
@@ -4,7 +4,6 @@ import json
 class handler(BaseHTTPRequestHandler):
 
     def do_POST(self):
-        print('handling POST request')
         post_body = json.loads(self.rfile.read(int(self.headers['content-length'])).decode('utf-8'))
         name = post_body.get('name', 'someone')
         self.send_response(200)
@@ -12,13 +11,10 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
         response_data = {'greeting': f'hello, {name}'}
         self.wfile.write(json.dumps(response_data).encode('utf-8'))
-        print('done handling POST request')
         return
 
     def do_GET(self):
-        print('handling GET request')
         self.send_response(200)
         self.end_headers()
         self.wfile.write('ok'.encode('utf-8'))
-        print('done handling GET request')
         return
