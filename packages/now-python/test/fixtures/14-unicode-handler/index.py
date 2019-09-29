1b919e88c43df7a2a5c1dce3f674309048f9b2d8
Christopher <offero@gmail.com>
2019-09-28 20:43:20 -0400

    test updates

--- packages/now-python/test/fixtures/14-unicode-handler/index.py
+++ packages/now-python/test/fixtures/14-unicode-handler/index.py
@@ -4,11 +4,21 @@ import json
 class handler(BaseHTTPRequestHandler):
 
     def do_POST(self):
-        post_body = json.loads(self.rfile.read().decode('utf-8'))
+        print('handling POST request')
+        post_body = json.loads(self.rfile.read(int(self.headers['content-length'])).decode('utf-8'))
         name = post_body.get('name', 'someone')
         self.send_response(200)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
         response_data = {'greeting': f'hello, {name}'}
         self.wfile.write(json.dumps(response_data).encode('utf-8'))
+        print('done handling POST request')
+        return
+
+    def do_GET(self):
+        print('handling GET request')
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write('ok'.encode('utf-8'))
+        print('done handling GET request')
         return
