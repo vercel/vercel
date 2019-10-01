b9902c9204b14ae90b43a33f49974dfeddafdf51
Christopher <offero@gmail.com>
2019-10-01 17:24:02 -0400

    update test to use passed in fetch. minor.

--- packages/now-python/test/fixtures/14-unicode-handler/probe.js
+++ packages/now-python/test/fixtures/14-unicode-handler/probe.js
@@ -1,24 +1,8 @@
-const fetch = require('../../../../../test/lib/deployment/fetch-retry');
-
-async function fetchDeploymentUrl(url, opts) {
-  for (let i = 0; i < 50; i += 1) {
-    const resp = await fetch(url, opts);
-    const text = await resp.text();
-    if (text && !text.includes('Join Free')) {
-      return { resp, text };
-    }
-
-    await new Promise(r => setTimeout(r, 1000));
-  }
-
-  throw new Error(`Failed to wait for deployment READY. Url is ${url}`);
-}
-
 module.exports = async function({ deploymentUrl, fetch, randomness }) {
   const nowjson = require('./now.json');
   const probe = nowjson.probes[0];
   const probeUrl = `https://${deploymentUrl}${probe.path}`;
-  const { resp, text } = await fetchDeploymentUrl(probeUrl, {
+  const resp = await fetch(probeUrl, {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
@@ -26,6 +10,7 @@ module.exports = async function({ deploymentUrl, fetch, randomness }) {
     body: JSON.stringify(probe.body),
   });
 
+  const text = await resp.text();
   const respBody = JSON.parse(text);
 
   if (respBody.greeting !== 'hello, Χριστοφορε') {
