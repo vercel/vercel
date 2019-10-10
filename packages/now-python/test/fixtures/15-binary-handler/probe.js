73291ee452a9ab7edf9b0b60664b98f5ceed107b
Jacob Mischka <jacob@mischka.me>
2019-10-10 09:36:43 -0500

    Add test for binary handler #3148

---
+++ packages/now-python/test/fixtures/15-binary-handler/probe.js
@@ -0,0 +1,15 @@
+module.exports = async function({ deploymentUrl, fetch, randomness }) {
+  const fs = require('fs');
+  const nowjson = require('./now.json');
+  const probe = nowjson.probes[0];
+  const probeUrl = `${deploymentUrl}${probe.path}`;
+  const resp = await fetch(probeUrl);
+
+  const bytes = await resp.arrayBuffer();
+
+  const image = fs.readFileSync('./zeit-white-triangle.png');
+
+  if (!image.equals(new Uint8Array(bytes))) {
+    throw new Error(`unexpected response: ${bytes}`);
+  }
+};
