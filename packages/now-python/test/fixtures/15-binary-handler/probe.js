781c40b038042c731868e527f2cf8000def1ae4e
Jacob Mischka <jacob@mischka.me>
2019-10-10 09:43:30 -0500

    Re-add protocol to probeUrl
    Removed it for local `now dev` testing, oops

--- packages/now-python/test/fixtures/15-binary-handler/probe.js
+++ packages/now-python/test/fixtures/15-binary-handler/probe.js
@@ -2,7 +2,7 @@ module.exports = async function({ deploymentUrl, fetch, randomness }) {
   const fs = require('fs');
   const nowjson = require('./now.json');
   const probe = nowjson.probes[0];
-  const probeUrl = `${deploymentUrl}${probe.path}`;
+  const probeUrl = `https://${deploymentUrl}${probe.path}`;
   const resp = await fetch(probeUrl);
 
   const bytes = await resp.arrayBuffer();
