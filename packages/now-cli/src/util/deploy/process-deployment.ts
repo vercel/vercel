c36d0f4c6e8397bb4b5aecad25cfad901cda0a9b
Max Rovensky <hey@fivepointseven.com>
2019-10-23 22:34:43 +0800

    Handle alias-assigned for v1 deployments

--- packages/now-cli/src/util/deploy/process-deployment.ts
+++ packages/now-cli/src/util/deploy/process-deployment.ts
@@ -214,7 +214,7 @@ export default async function processDeployment({
       }
 
       // Handle ready event
-      if (event.type === 'ready') {
+      if (event.type === 'alias-assigned') {
         log(`Build completed`);
         return event.payload;
       }
