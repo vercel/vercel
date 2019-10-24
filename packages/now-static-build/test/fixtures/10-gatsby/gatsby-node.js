d93178e13c043c2e9bef476770b25a38ae96f5ca
luc <luc.leray@gmail.com>
2019-10-25 01:32:22 +0200

    Revert "add redirect to gatsby test case"
    This reverts commit 343c04deb6c5cf7890f245d1fc9c80cb9c0452df.

--- packages/now-static-build/test/fixtures/10-gatsby/gatsby-node.js
+++ packages/now-static-build/test/fixtures/10-gatsby/gatsby-node.js
@@ -5,12 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-
-exports.createPages = ({ actions }) => {
-  const { createRedirect } = actions
-
-  createRedirect({
-    fromPath: "/redirect",
-    toPath: "/",
-  })
-}
