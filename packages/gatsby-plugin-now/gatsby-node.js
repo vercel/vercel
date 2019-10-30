6a5a964b55ab6fe860ebb7304576f9967646d88b
Luc <luc.leray@gmail.com>
2019-10-30 12:12:06 +0800

    [now-static-build] Improve Gatsby default routes (#3212)
    This PR does 3 things:
    - add default caching headers
    - redirect all missed requests to Gatsby's own 404.html page
    - add deprecation message to `gatsby-plugin-now` since we're auto injecting it and it's not needed anymore
    
    Fixes #2859

--- packages/gatsby-plugin-now/gatsby-node.js
+++ packages/gatsby-plugin-now/gatsby-node.js
@@ -6,10 +6,6 @@ const REDIRECT_FILE_NAME = '__now_routes_g4t5bY.json';
 exports.onPostBuild = async ({ store }) => {
   const { redirects, program } = store.getState();
 
-  if (!redirects.length === 0) {
-    return;
-  }
-
   const routes = [{ handle: 'filesystem' }];
 
   for (const redirect of redirects) {
@@ -26,8 +22,30 @@ exports.onPostBuild = async ({ store }) => {
     }
   }
 
+  // we implement gatsby's recommendations
+  // https://www.gatsbyjs.org/docs/caching/
+  const finalRoutes = [
+    {
+      src: '^/static/(.*)$',
+      headers: { 'cache-control': 'public,max-age=31536000,immutable' },
+      continue: true,
+    },
+    {
+      src: '^/.*\\.(js|css)$',
+      headers: { 'cache-control': 'public,max-age=31536000,immutable' },
+      continue: true,
+    },
+    {
+      src: '^/(sw\\.js|app-data\\.json|.*\\.html|page-data/.*)$',
+      headers: { 'cache-control': 'public,max-age=0,must-revalidate' },
+      continue: true,
+    },
+    ...routes,
+    { src: '.*', status: 404, dest: '/404.html' },
+  ];
+
   await writeFile(
     path.join(program.directory, 'public', REDIRECT_FILE_NAME),
-    JSON.stringify(routes)
+    JSON.stringify(finalRoutes)
   );
 };
