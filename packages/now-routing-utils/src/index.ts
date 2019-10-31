2c867295e61442153d8aeece5fd8ab604fc1575d
Leo Lamprecht <mindrun@icloud.com>
2019-10-31 16:22:34 +0100

    [now-routing-utils] Improve error message for invalid regexes (#3230)
    Currently, this is what you see if you enter invalid regexes with any of the new routing properties:
    
    ![image](https://user-images.githubusercontent.com/6170607/67955351-c0fd6480-fbf2-11e9-999c-b36509b5c405.png)
    
    Instead of printing JSON, we want to print a list of all error messages.

--- packages/now-routing-utils/src/index.ts
+++ packages/now-routing-utils/src/index.ts
@@ -112,7 +112,7 @@ function createNowError(
     errors.length > 0
       ? {
           code,
-          message: `${msg}: \n${JSON.stringify(errors, null, 2)}`,
+          message: `${msg}: \n${errors.map(item => `- ${item.message}\n`)}`,
           errors,
         }
       : null;
