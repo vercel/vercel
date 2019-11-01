vercel/vercel

commit dc9b9c240a2418b127fb1152527043a0c75c926e

[now-cli] Add support for new keys in `now dev` (#3174)
This PR is a followup to #3138 so that `now dev` will validate and transform the following `now.json` config keys to routes:

- cleanUrls
- rewrites
- redirects
- headers
- trailingSlash

[PRODUCT-341] #close

[PRODUCT-341]: https://zeit.atlassian.net/browse/PRODUCT-341

packages/now-cli/test/dev/integration.js
@@ -19,20 +19,19 @@ function fetchWithRetry(url, retries = 3, opts = {}) {
   return new Promise(async (resolve, reject) => {
     try {
       const res = await fetch(url, opts);
-
-      if (res.ok) {
-        resolve(res);
+      if (!res.ok) {
+        throw new Error('Responded with status ' + res.status);
       }
+      resolve(res);
     } catch (error) {
       if (retries === 0) {
         reject(error);
         return;
       }
-      setTimeout(() => {
-        fetchWithRetry(url, retries - 1, opts)
-          .then(resolve)
-          .catch(reject);
-      }, 1000);
+      await sleep(1000);
+      fetchWithRetry(url, retries - 1, opts)
+        .then(resolve)
+        .catch(reject);
     }
   });
 }
@@ -155,6 +154,66 @@ test('[now dev] validate routes', async t => {
   );
 });
 
+test('[now dev] validate cleanUrls', async t => {
+  const directory = fixture('invalid-clean-urls');
+  const output = await exec(directory);
+
+  t.is(output.code, 1, formatOutput(output));
+  t.regex(output.stderr, /Invalid `cleanUrls` property:\s+should be boolean/gm);
+});
+
+test('[now dev] validate trailingSlash', async t => {
+  const directory = fixture('invalid-trailing-slash');
+  const output = await exec(directory);
+
+  t.is(output.code, 1, formatOutput(output));
+  t.regex(
+    output.stderr,
+    /Invalid `trailingSlash` property:\s+should be boolean/gm
+  );
+});
+
+test('[now dev] validate rewrites', async t => {
+  const directory = fixture('invalid-rewrites');
+  const output = await exec(directory);
+
+  t.is(output.code, 1, formatOutput(output));
+  t.regex(
+    output.stderr,
+    /Invalid `rewrites` property: \[0\]\.destination should be string/gm
+  );
+});
+
+test('[now dev] validate redirects', async t => {
+  const directory = fixture('invalid-redirects');
+  const output = await exec(directory);
+
+  t.is(output.code, 1, formatOutput(output));
+  t.regex(
+    output.stderr,
+    /Invalid `redirects` property: \[0\]\.statusCode should be integer/gm
+  );
+});
+
+test('[now dev] validate headers', async t => {
+  const directory = fixture('invalid-headers');
+  const output = await exec(directory);
+
+  t.is(output.code, 1, formatOutput(output));
+  t.regex(
+    output.stderr,
+    /Invalid `headers` property: \[0\]\.headers\[0\]\.value should be string/gm
+  );
+});
+
+test('[now dev] validate mixed routes and rewrites', async t => {
+  const directory = fixture('invalid-mixed-routes-rewrites');
+  const output = await exec(directory);
+
+  t.is(output.code, 1, formatOutput(output));
+  t.regex(output.stderr, /Cannot define both `routes` and `rewrites`/gm);
+});
+
 test('[now dev] validate env var names', async t => {
   const directory = fixture('invalid-env-var-name');
   const { dev } = await testFixture(directory, { stdio: 'pipe' });
@@ -188,6 +247,19 @@ test('[now dev] validate env var names', async t => {
     dev.kill('SIGTERM');
   }
 });
+
+test(
+  '[now dev] test rewrites serve correct content',
+  testFixtureStdio('test-rewrites', async (t, port) => {
+    const result = await fetchWithRetry(`http://localhost:${port}/hello`, 3);
+    const response = await result;
+
+    validateResponseHeaders(t, response);
+
+    const body = await response.text();
+    t.regex(body, /Hello World/gm);
+  })
+);
 
 test(
   '[now dev] 00-list-directory',

packages/now-cli/test/dev/fixtures/invalid-clean-urls/index.html
@@ -1,0 +1,1 @@
+index.html

packages/now-cli/test/dev/fixtures/invalid-headers/index.html
@@ -1,0 +1,1 @@
+index.html

packages/now-cli/test/dev/fixtures/invalid-mixed-routes-rewrites/index.html
@@ -1,0 +1,1 @@
+index.html

packages/now-cli/test/dev/fixtures/invalid-redirects/index.html
@@ -1,0 +1,1 @@
+index.html

packages/now-cli/test/dev/fixtures/invalid-rewrites/index.html
@@ -1,0 +1,1 @@
+index.html

packages/now-cli/test/dev/fixtures/invalid-trailing-slash/index.html
@@ -1,0 +1,1 @@
+index.html

packages/now-cli/test/dev/fixtures/test-rewrites/index.html
@@ -1,0 +1,1 @@
+Hello World

commit 2dee810e74ff656cb36184656949268d2e4e038b

[now-dev] Fix route normalization (#3225)
This PR fixes a regression introduced in #3174 when removing the `^` and `$` normalization.

The previous PR was normalizing user-defined routes but forgot to normalize builder routes.

This PR normalizes builder routes 👍

packages/now-cli/src/util/dev/builder.ts
@@ -30,6 +30,7 @@ import {
   BuilderOutput,
   BuilderOutputs,
 } from './types';
+import { normalizeRoutes } from '@now/routing-utils';
 
 interface BuildMessage {
   type: string;
@@ -225,6 +226,14 @@ export async function executeBuild(
     };
   } else {
     result = buildResultOrOutputs as BuildResult;
+  }
+
+  // Normalize Builder Routes
+  const normalized = normalizeRoutes(result.routes);
+  if (normalized.error) {
+    throw new Error(normalized.error.message);
+  } else {
+    result.routes = normalized.routes || [];
   }
 
   // Convert the JSON-ified output map back into their corresponding `File`

packages/now-cli/test/dev/fixtures/invalid-builder-routes/index.html
@@ -1,0 +1,1 @@
+Home Page

packages/now-cli/test/dev/integration.js
@@ -266,6 +266,15 @@ test(
 );
 
 test(
+  '[now dev] throw when invalid builder routes detected',
+  testFixtureStdio('invalid-builder-routes', async (t, port) => {
+    const response = await fetch(`http://localhost:${port}`);
+    const body = await response.text();
+    t.regex(body, /Invalid regular expression/gm);
+  })
+);
+
+test(
   '[now dev] 00-list-directory',
   testFixtureStdio('00-list-directory', async (t, port) => {
     const result = await fetchWithRetry(`http://localhost:${port}`, 60);