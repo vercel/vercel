afdad260cd488bc58f2bbdcc3080332369ff2152
Nathan Rajlich <nathan@tootallnate.net>
2019-11-04 12:47:49 -0800

    Optimize "gatsby" detector
    Check the `package.json` first

--- packages/now-build-utils/src/detectors/gatsby.ts
+++ packages/now-build-utils/src/detectors/gatsby.ts
@@ -1,18 +1,19 @@
 import { DetectorParameters, DetectorResult } from '../types';
 
 export default async function detectGatsby({
-  fs: { exists, hasDependency, readPackageJson },
+  fs: { exists, hasDependency },
 }: DetectorParameters): Promise<DetectorResult> {
-  const [pkg, hasGatsby, hasConfig] = await Promise.all([
-    readPackageJson(),
-    hasDependency('gatsby'),
-    exists('gatsby-config.js'),
-  ]);
-  if (!pkg || !hasGatsby || !hasConfig) {
+  const hasGatsby = await hasDependency('gatsby');
+  if (!hasGatsby) {
+    return false;
+  }
+  const hasConfig = await exists('gatsby-config.js');
+  if (!hasConfig) {
     return false;
   }
   return {
     buildCommand: ['gatsby', 'build'],
     buildDirectory: 'public',
+    devCommand: ['gatsby', 'develop', '-p', '$PORT'],
   };
 }
