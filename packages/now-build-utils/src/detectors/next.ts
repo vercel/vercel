c7d611eec699aaff098c200d4f514b546b4cf276
Nathan Rajlich <nathan@tootallnate.net>
2019-11-04 12:47:27 -0800

    Add "next" detector

---
+++ packages/now-build-utils/src/detectors/next.ts
@@ -0,0 +1,22 @@
+import { DetectorParameters, DetectorResult } from '../types';
+
+export default async function detectNext({
+  fs: { hasDependency },
+}: DetectorParameters): Promise<DetectorResult> {
+  const hasNext = await hasDependency('next');
+  if (!hasNext) return false;
+  return {
+    buildCommand: ['next', 'build'],
+    buildDirectory: 'build',
+    devCommand: ['next', 'start', '-p', '$PORT'],
+    routes: [
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '/(.*)',
+        dest: '/index.html',
+      },
+    ],
+  };
+}
