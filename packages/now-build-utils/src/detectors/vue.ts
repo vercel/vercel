7c7cf2fec9dd9f49c1345befddd67fe476572203
Nathan Rajlich <nathan@tootallnate.net>
2019-11-04 13:18:57 -0800

    Add "vue" detector

---
+++ packages/now-build-utils/src/detectors/vue.ts
@@ -0,0 +1,30 @@
+import { DetectorParameters, DetectorResult } from '../types';
+
+export default async function detectVue({
+  fs: { hasDependency },
+}: DetectorParameters): Promise<DetectorResult> {
+  const hasVue = await hasDependency('@vue/cli-service');
+  if (!hasVue) return false;
+  return {
+    buildDirectory: 'dist',
+    routes: [
+      {
+        src: '^/[^/]*\\.(js|txt|ico|json)',
+        headers: { 'cache-control': 'max-age=300' },
+        continue: true,
+      },
+      {
+        src: '^/(img|js|css|fonts|media)/.*',
+        headers: { 'cache-control': 'max-age=31536000, immutable' },
+        continue: true,
+      },
+      {
+        handle: 'filesystem',
+      },
+      {
+        src: '^.*',
+        dest: '/index.html',
+      },
+    ],
+  };
+}
