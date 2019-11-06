53d341be62b591c1aa0be6f45326a687879d35f8
Nathan Rajlich <nathan@tootallnate.net>
2019-11-06 10:45:40 -0800

    Add missing commands

--- packages/now-build-utils/src/detectors/eleventy.ts
+++ packages/now-build-utils/src/detectors/eleventy.ts
@@ -6,6 +6,15 @@ export default async function detectEleventy({
   const hasEleventy = await hasDependency('@11ty/eleventy');
   if (!hasEleventy) return false;
   return {
+    buildCommand: ['npx', '@11ty/eleventy'],
     buildDirectory: '_site',
+    devCommand: [
+      'npx',
+      '@11ty/eleventy',
+      '--serve',
+      '--watch',
+      '--port',
+      '$PORT',
+    ],
   };
 }
