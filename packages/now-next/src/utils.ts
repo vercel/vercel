8c370eb88e52ab97b36506a8eafb0399d3a3fac4
Joe Haddad <joe.haddad@zeit.co>
2019-11-09 13:01:38 -0800

    Update behavior

--- packages/now-next/src/utils.ts
+++ packages/now-next/src/utils.ts
@@ -290,44 +290,55 @@ function getRoutes(
   return routes;
 }
 
-export function getDynamicRoutes(
+export async function getDynamicRoutes(
   entryPath: string,
   entryDirectory: string,
   dynamicPages: string[],
   isDev?: boolean
-): { src: string; dest: string }[] {
+): Promise<{ src: string; dest: string }[]> {
   if (!dynamicPages.length) {
     return [];
   }
 
-  try {
-    // Use routes-manifest if available so that we don't have
-    // to rely on inner utils of Next.js to generate the dynamic routes
-    const manifestStr = fs.readFileSync(path.join(entryPath, '.next/routes-manifest.json'), 'utf8')
-    const routesManifest = JSON.parse(manifestStr)
+  const pathRoutesManifest = path.join(
+    entryPath,
+    '.next',
+    'routes-manifest.json'
+  );
+  const hasRoutesManifest = await fs
+    .access(pathRoutesManifest)
+    .then(() => true)
+    .catch(() => false);
+
+  if (hasRoutesManifest) {
+    const routesManifest = await fs.readJSON(pathRoutesManifest);
 
     switch (routesManifest.version) {
-      case 0:
-        return routesManifest.dynamicRoutes.map((
-          { page, regex }: { page: string, regex: string}
-        ) => {
-          return {
-            src: regex,
-            dest: !isDev
-              ? path.join('/', entryDirectory, page)
-              : page
+      case 0: {
+        return routesManifest.dynamicRoutes.map(
+          ({ page, regex }: { page: string; regex: string }) => {
+            return {
+              src: regex,
+              dest: !isDev ? path.join('/', entryDirectory, page) : page,
+            };
           }
-        })
-    }
-  } catch (err) {
-    if (err.code !== 'ENOENT') {
-      // if the routes-manifest doesn't exist they probably aren't
-      // on a new enough Next.js version if an error occurs accessing
-      // it rethrow though
-      throw err
+        );
+      }
+      default: {
+        throw new Error(
+          'This version of `@now/next` does not support the version of Next.js you are trying to deploy.\n' +
+            'Please upgrade your `@now/next` builder and try again. Contact support if this continues to happen.'
+        );
+      }
     }
   }
 
+  // FALLBACK:
+  // When `routes-manifest.json` does not exist (old Next.js versions), we'll try to
+  // require the methods we need from Next.js' internals.
+  //
+  // TODO: implement this branch behind a Next.js version check so we don't "fallback"
+  // to this behavior blindly.
   let getRouteRegex:
     | ((pageName: string) => { re: RegExp })
     | undefined = undefined;
