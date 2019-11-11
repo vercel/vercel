cf42a737b473ad6f66426fff16b229e76b6e3a50
Andy Bitz <artzbitz@gmail.com>
2019-11-11 14:20:53 +0100

    Typescript

--- packages/now-node/src/index.ts
+++ packages/now-node/src/index.ts
@@ -290,13 +290,8 @@ export async function build({
   config = {},
   meta = {},
 }: BuildOptions) {
-  const shouldAddHelpers = config.helpers
-    ? config.helpers !== false
-    : process.env.NODEJS_HELPERS !== '0';
-
-  const awsLambdaHandler = config.awsLambdaHandler
-    ? config.awsLambdaHandler as string
-    : process.env.NODEJS_AWS_HANDLER_NAME;
+  const shouldAddHelpers = config.helpers !== false || process.env.NODEJS_HELPERS !== '0';
+  const awsLambdaHandler = (config.awsLambdaHandler || process.env.NODEJS_AWS_HANDLER_NAME) as string;
 
   const {
     entrypointPath,
