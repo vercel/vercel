28b1657653119264035305c5ca3244d7672ee12c
luc <luc.leray@gmail.com>
2019-11-21 14:38:04 +0800

    try with env variables for feature flag

--- packages/now-static-build/src/utils/is-canary.ts
+++ packages/now-static-build/src/utils/is-canary.ts
@@ -1,6 +1,3 @@
-//eslint-disable-next-line @typescript-eslint/no-var-requires
-const pkg = require('../../package.json');
-
 export function isCanary() {
-  return pkg.version && pkg.version.includes('canary');
+  return process.env.__NOW_CANARY === '1';
 }
