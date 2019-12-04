cb65b587a740efb500f3b7b76784513f64d188b1
paulogdm <paulogdemitri@gmail.com>
2019-12-04 18:06:51 -0300

    only thing left to fight is TS

--- packages/now-cli/src/commands/dns/add.ts
+++ packages/now-cli/src/commands/dns/add.ts
@@ -15,6 +15,7 @@ import stamp from '../../util/output/stamp';
 import getDNSData from '../../util/dns/get-dns-data';
 import { DNSRecordData } from '../../types'
 import table from 'text-table';
+import promptBool from '../../util/input/prompt-bool';
 
 type Options = {
   '--debug': boolean;
@@ -58,17 +59,28 @@ export default async function add(
   }
  
   const { domain, data: argData } = parsedParams;
-  if (argData && domain === argData.name.substr(argData.name.length - domain.length, argData.name.length)) {
-    const yes = await readConfirmation(
-      output,
-      'Domain identified inside the subdomain argument. The following record will be created',
-      domain,
-      argData
-    );
-    if (!yes) {
-      output.error(`User aborted.`);
-      return 0;
+
+
+  if (argData && argData.name.endsWith(domain)) {
+    output.log(`'Domain identified inside the subdomain argument. The following record will be created':`);
+    switch (argData.type) {
+      case 'MX':
+        output.log(
+          chalk.bold(`${argData.type} ${argData.value} ${argData.mxPriority} || ''}`)
+        );
+        break;
+      case 'SRV':
+        output.log(
+          chalk.bold(`${argData.type} ${argData.srv.priority} ${argData.srv.port} ${argData.srv.weight} ${argData.srv.target}`)
+        );
+        break;
+      default:
+        output.log(
+          chalk.bold(`${argData.type} ${argData.value || ''}`)
+        );
+        break;
     }
+    await promptBool(`Do you want to create this record on ${argData.name}.${domain}?`)
   }
 
   const addStamp = stamp();
@@ -122,7 +134,7 @@ export default async function add(
     return 1;
   }
 
-  console.log(
+  output.log(
     `${chalk.cyan('> Success!')} DNS record for domain ${chalk.bold(
       domain
     )} ${chalk.gray(`(${record.uid})`)} created under ${chalk.bold(
@@ -131,52 +143,4 @@ export default async function add(
   );
 
   return 0;
-}
-
-function readConfirmation(
-  output: Output,
-  msg: string,
-  domain: string,
-  argData: any
-) {
-  return new Promise(resolve => {
-    output.log(msg);
-    output.print(
-      `${table([getAddebleTableRow(domain, argData)], {
-        align: ['l', 'r', 'l'],
-        hsep: ' '.repeat(6)
-      }).replace(/^(.*)/gm, '  $1')}\n`
-    );
-    output.print(
-      `${chalk.bold.red('> Are you sure?')} ${chalk.gray('[y/N] ')}`
-    );
-    process.stdin
-      .on('data', d => {
-        process.stdin.pause();
-        resolve(
-          d
-            .toString()
-            .trim()
-            .toLowerCase() === 'y'
-        );
-      })
-      .resume();
-  });
-}
-
-function getAddebleTableRow(domain: string, argData: any) {
-  switch (argData.type) {
-    case 'MX':
-      return [
-        chalk.bold(`${argData.name}.${domain} ${argData.type} ${argData.value} ${argData.mxPriority} || ''}`)
-      ];
-    case 'SRV':
-      return [
-        chalk.bold(`${argData.name}.${domain} ${argData.type} ${argData.srv.priority} ${argData.srv.port} ${argData.srv.weight} ${argData.srv.target}`)
-      ];
-    default:
-      return [
-        chalk.bold(`${argData.name}.${domain} ${argData.type} ${argData.value || ''}`)
-      ];
-  }
 }
