fa11ad2f73c0a8c10a3fb4f84ce115c70c333a55
Steven <steven@ceriously.com>
2019-12-10 18:53:45 -0500

    [now-build-utils] Discontinue Node 8

--- packages/now-build-utils/src/fs/node-version.ts
+++ packages/now-build-utils/src/fs/node-version.ts
@@ -5,8 +5,13 @@ import debug from '../debug';
 const supportedOptions: NodeVersion[] = [
   { major: 12, range: '12.x', runtime: 'nodejs12.x' },
   { major: 10, range: '10.x', runtime: 'nodejs10.x' },
-  { major: 8, range: '8.10.x', runtime: 'nodejs8.10' },
-];
+  {
+    major: 8,
+    range: '8.10.x',
+    runtime: 'nodejs8.10',
+    discontinueDate: new Date('2020-01-01'),
+  },
+].filter(o => !isDiscontinued(o));
 
 // This version should match Fargate's default in the PATH
 // Today that is Node 8
@@ -42,15 +47,33 @@ export async function getSupportedNodeVersion(
         );
       }
     } else {
-      if (!silent) {
-        throw new Error(
-          'Found `engines` in `package.json` with an unsupported node range: ' +
-            engineRange +
-            '\nPlease use one of the following supported ranges: ' +
-            JSON.stringify(supportedOptions.map(o => o.range))
-        );
-      }
+      throw new Error(
+        'Found `engines` in `package.json` with an unsupported node range: ' +
+          engineRange +
+          '\nPlease use one of the following supported ranges: ' +
+          JSON.stringify(supportedOptions.map(o => o.range))
+      );
     }
   }
+
+  const { range, discontinueDate } = selection;
+  if (discontinueDate && !isDiscontinued(selection)) {
+    const d = discontinueDate.toISOString().split('T')[0];
+    const validRanges = supportedOptions
+      .filter(o => !o.discontinueDate)
+      .map(o => o.range);
+    console.warn(
+      `Warning: Node ${range} will be discontinued on ${d}.\n` +
+        `Deployments created on or after ${d} will fail to build.\n` +
+        'Please use one of the following supported `engines` in `package.json`: ' +
+        JSON.stringify(validRanges)
+    );
+  }
+
   return selection;
+}
+
+function isDiscontinued({ discontinueDate }: NodeVersion): boolean {
+  const today = new Date();
+  return typeof discontinueDate !== 'undefined' && discontinueDate <= today;
 }
