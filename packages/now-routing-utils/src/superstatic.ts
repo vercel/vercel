37905721c86b0ca0255be54bb4dd68acab698e22
JJ Kasper <jj@jjsweb.site>
2019-12-17 16:24:14 -0600

    Revert sugguestion to make TypeScript happy

--- packages/now-routing-utils/src/superstatic.ts
+++ packages/now-routing-utils/src/superstatic.ts
@@ -109,7 +109,7 @@ function isString(key: any): key is string {
 
 function replaceSegments(segments: string[], destination: string): string {
   const parsedDestination = parseUrl(destination, true);
-  let { pathname='' } = parsedDestination;
+  let { pathname } = parsedDestination;
   pathname = pathname || '';
 
   if (pathname.includes(':') && segments.length > 0) {
