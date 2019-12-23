86b2169caf145200b164b0c2f860c22d50b495cb
Andy Bitz <artzbitz@gmail.com>
2019-12-23 15:11:41 +0100

    Update test

--- packages/now-cli/test/integration.js
+++ packages/now-cli/test/integration.js
@@ -2010,7 +2010,7 @@ test('fail to deploy a Lambda with an incorrect value for of memory', async t =>
   t.is(output.exitCode, 1, formatOutput(output));
   t.regex(
     output.stderr,
-    /Functions must have a memory value between 128 and 3008 in steps of 64\./gm,
+    /memory should be equal to one of the allowed values/gm,
     formatOutput(output)
   );
 });
