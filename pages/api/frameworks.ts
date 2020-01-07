f5e25ca2f59aecc613809be135dede717564064b
Andy Bitz <artzbitz@gmail.com>
2020-01-07 14:31:45 +0100

    Update frameworks list

--- pages/api/frameworks.ts
+++ pages/api/frameworks.ts
@@ -2,12 +2,12 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import frameworkList, { Framework } from '@now/frameworks';
 import { withApiHandler } from '../../lib/util/with-api-handler';
 
-const frameworks: Framework[] = frameworkList as Framework[];
-
-frameworks.forEach(framework => {
+const frameworks: Framework[] = frameworkList.map(framework => {
   if (framework.logo) {
     framework.logo = `https://res.cloudinary.com/zeit-inc/image/fetch/${framework.logo}`;
   }
+
+  return framework;
 });
 
 export default withApiHandler(async function(
