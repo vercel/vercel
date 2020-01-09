c65336e63b6749b8a1e8088d2e02c0e226d8bbd3
Andy Bitz <artzbitz@gmail.com>
2020-01-09 17:28:53 +0100

    Revert "[examples] Add manifest.json"
    This reverts commit b7eca1d069eec3c61b2c3e467b8298a2c06c6063.

--- api/_lib/examples/example-list.ts
+++ api/_lib/examples/example-list.ts
@@ -1,10 +1,21 @@
-import { join } from 'path';
-import { readFileSync } from 'fs';
+// Currently we read & parse the README file from zeit/now-examples
+// TODO: create a `manifest.json` for zeit/now-examples
 
-const manifest = readFileSync(
-  join('..', '..', '..', 'examples', 'manifest.json')
-);
+import fetch from 'node-fetch';
 
+/**
+ * Fetch and parse the `Frameworks and Libraries` table
+ * in the README file of zeit/now-examples
+ */
 export async function getExampleList() {
-  return manifest;
+  const response = await fetch(
+    `https://raw.githubusercontent.com/zeit/now-examples/master/manifest.json`
+  );
+
+  if (response.status !== 200) {
+    console.log('manifest.json missing in zeit/now-examples');
+    return null;
+  }
+
+  return response.json();
 }
