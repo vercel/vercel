b7eca1d069eec3c61b2c3e467b8298a2c06c6063
Andy Bitz <artzbitz@gmail.com>
2020-01-09 17:24:54 +0100

    [examples] Add manifest.json

--- api/_lib/examples/example-list.ts
+++ api/_lib/examples/example-list.ts
@@ -1,21 +1,10 @@
-// Currently we read & parse the README file from zeit/now-examples
-// TODO: create a `manifest.json` for zeit/now-examples
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
-import fetch from 'node-fetch';
+const manifest = readFileSync(
+  join('..', '..', '..', 'examples', 'manifest.json')
+);
 
-/**
- * Fetch and parse the `Frameworks and Libraries` table
- * in the README file of zeit/now-examples
- */
 export async function getExampleList() {
-  const response = await fetch(
-    `https://raw.githubusercontent.com/zeit/now-examples/master/manifest.json`
-  );
-
-  if (response.status !== 200) {
-    console.log('manifest.json missing in zeit/now-examples');
-    return null;
-  }
-
-  return response.json();
+  return manifest;
 }
