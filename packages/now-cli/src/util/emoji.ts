a015003cb023424059903c9025555cbe90bb1b2a
luc <luc.leray@gmail.com>
2020-01-15 12:46:53 +0100

    always set double spaces after emojis

---
+++ packages/now-cli/src/util/emoji.ts
@@ -0,0 +1,26 @@
+export function emoji(label: string): string | undefined {
+  switch (label) {
+    case 'notice':
+      return 'ℹ️';
+    case 'tip':
+      return '💡';
+    case 'warning':
+      return '⚠️';
+    case 'link':
+      return '☑️';
+    case 'inspect':
+      return '🔍';
+    case 'success':
+      return '✅';
+    default:
+      return undefined;
+  }
+}
+
+export function prependEmoji(message: string, emoji?: string): string {
+  if (emoji && process.stdout.isTTY) {
+    return `${emoji}  ${message}`;
+  }
+
+  return message;
+}
