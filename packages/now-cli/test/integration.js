27934ce6bb69ba1edd96ceab2812137800ea3d44
Luc <luc.leray@gmail.com>
2020-01-16 16:39:16 +0100

    Update packages/now-cli/test/integration.js

--- packages/now-cli/test/integration.js
+++ packages/now-cli/test/integration.js
@@ -2317,7 +2317,7 @@ test('should show prompts to set up project', async t => {
   t.is(output.exitCode, 0, formatOutput(output));
 
   // Ensure .gitignore is created
-  t.is(await readFile(path.join(directory, '.gitignore')), '.now');
+  t.is((await readFile(path.join(directory, '.gitignore'))).toString(), '.now');
 
   // Send a test request to the deployment
   const response = await fetch(new URL(output.stdout).href);
