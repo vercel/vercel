05c0d8d0a120ea4a0980b02524b203503a6942bb
Steven <steven@ceriously.com>
2020-01-30 17:45:01 -0500

    Update with env vars

--- utils/cancel.js
+++ utils/cancel.js
@@ -1,21 +1,20 @@
 const fetch = require('node-fetch');
 
-const github = process.env.github || {
-  workflow: 'continuous-integration.yml',
-  ref: 'master',
-};
+const ref = process.env.GITHUB_REF; // 'refs/heads/ci-cancel-previous',
+const sha = process.env.GITHUB_SHA; // 'a5d18518ea755ddc4212f47ec3448f59e0e7e3a5',
+const run = process.env.GITHUB_RUN_ID; // '33175268',
+const name = process.env.GITHUB_WORKFLOW; // 'CI';
+const workflow = 'continuous-integration.yml';
 
-console.log({ github });
-
-console.log({ env: process.env });
+console.log(process.env);
 
 fetch(
-  `https://api.github.com/repos/zeit/now/actions/workflows/${github.workflow}/runs`
+  `https://api.github.com/repos/zeit/now/actions/workflows/${workflow}/runs`
 )
   .then(res => res.json())
   .then(data => {
     data.workflow_runs
-      .filter(o => o.head_branch === github.ref && o.status === 'in_progress')
+      .filter(o => o.head_branch === ref && o.status === 'in_progress')
       .forEach(o => {
         console.log(o);
         // TODO: send POST to `o.cancel_url` but we need to exclude the current run
