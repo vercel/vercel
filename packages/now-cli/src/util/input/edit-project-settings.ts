9ae55432b769b069f7736da8cc1438f57344db77
Luc <luc.leray@gmail.com>
2020-01-30 17:02:25 +0100

    Update packages/now-cli/src/util/input/edit-project-settings.ts
    Co-Authored-By: Steven <steven@ceriously.com>

--- packages/now-cli/src/util/input/edit-project-settings.ts
+++ packages/now-cli/src/util/input/edit-project-settings.ts
@@ -11,7 +11,7 @@ export interface ProjectSettings {
   devCommand: string | null;
 }
 
-export type ProjectSettingsWithFramework = ProjectSettings & {
+export interface ProjectSettingsWithFramework extends ProjectSettings {
   framework: string | null;
 };
 
