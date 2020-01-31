58f15d67548a13c6b774ce1d771570d141be9d08
Steven <steven@ceriously.com>
2020-01-31 06:36:08 -0500

    Update token

--- utils/cancel.js
+++ utils/cancel.js
@@ -4,9 +4,10 @@ const ref = process.env.GITHUB_REF.slice(11); // 'refs/heads/ci-cancel-previous'
 const sha = process.env.GITHUB_SHA; // 'a5d18518ea755ddc4212f47ec3448f59e0e7e3a5',
 const run = process.env.GITHUB_RUN_ID; // '33175268',
 const name = process.env.GITHUB_WORKFLOW; // 'CI';
-const token = process.env.GITHUB_TOKEN;
+const token = process.env.GITHUB_WORKFLOW_TOKEN;
 const workflow = 'continuous-integration.yml';
-
+console.log('token type ' + typeof token);
+console.log('token length ' + token.length);
 console.log({ ref, sha, run, name, workflow });
 
 const url = `https://api.github.com/repos/zeit/now/actions/workflows/${workflow}/runs`;
@@ -26,7 +27,7 @@ fetch(url, opts)
         o.head_sha !== sha &&
         o.status === 'in_progress'
     );
-    console.log(`Found ${inProgress} checks in progress.`);
+    console.log(`Found ${inProgress.length} checks in progress.`);
     inProgress.forEach(o => {
       const { id, cancel_url, head_branch, head_sha, status } = o;
       console.log('Cancelling another check: ', {
