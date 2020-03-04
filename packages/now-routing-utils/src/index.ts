52d6274fbbd697e1afcdbeab1e1fe9136bb9db2c
JJ Kasper <jj@jjsweb.site>
2020-03-04 16:14:27 -0600

    Apply suggestions from code review
    Co-Authored-By: Connor Davis <mail@connordav.is>

--- packages/now-routing-utils/src/index.ts
+++ packages/now-routing-utils/src/index.ts
@@ -27,6 +27,7 @@ const VALID_HANDLE_VALUES = [
   'miss',
   'rewrite',
   'error',
+  'resource',
 ] as const;
 const validHandleValues = new Set<string>(VALID_HANDLE_VALUES);
 export type HandleValue = typeof VALID_HANDLE_VALUES[number];
