9c5260455cec6109fe978dd129dfa609c8e3fa1c
luc <luc.leray@gmail.com>
2020-04-15 18:45:44 +0200

    simplify

--- packages/now-cli/src/util/get-frameworks.ts
+++ packages/now-cli/src/util/get-frameworks.ts
@@ -1,10 +1,6 @@
 import { Framework } from '@now/frameworks';
 import Client from './client';
 
-export async function getFrameworks(): Promise<Framework[]> {
-  const client = new Client({
-    apiUrl: 'https://api.zeit.co',
-  });
-
+export async function getFrameworks(client: Client) {
   return await client.fetch<Framework[]>('/v1/frameworks');
 }
