62546350f2952885318bebe22bfebf9e312bd7e0
Andy Bitz <artzbitz@gmail.com>
2020-04-15 20:55:43 +0200

    Add cache

--- packages/now-cli/src/util/get-team-by-id.ts
+++ packages/now-cli/src/util/get-team-by-id.ts
@@ -1,6 +1,18 @@
 import Client from './client';
+import { Team } from '../types';
 
-export default async function getTeamById(client: Client, teamId: string) {
-  const team = await client.fetch(`/teams/${teamId}`);
+const teamCache = new Map<string, Team>();
+
+export default async function getTeamById(
+  client: Client,
+  teamId: string
+): Promise<Team> {
+  if (teamCache.has(teamId)) {
+    return teamCache.get(teamId)!;
+  }
+
+  const team = await client.fetch<Team>(`/teams/${teamId}`);
+  teamCache.set(teamId, team);
+
   return team;
 }
