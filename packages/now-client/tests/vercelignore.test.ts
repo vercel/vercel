a75c196eba93b2a6d122ae016c3fb8b9e2cfefe5
Nathan Rajlich <nathan@tootallnate.net>
2020-05-08 15:24:45 -0700

    Add tests

---
+++ packages/now-client/tests/vercelignore.test.ts
@@ -0,0 +1,27 @@
+import assert from 'assert';
+import { join } from 'path';
+import { getVercelIgnore } from '../src/utils';
+
+describe('Test `getVercelIgnore()`', () => {
+  it('Should read `.nowignore`', async () => {
+    const fixture = join(__dirname, 'fixtures', 'nowignore');
+    const { ig } = await getVercelIgnore(fixture);
+    assert.equal(ig.ignores('ignore.txt'), true);
+    assert.equal(ig.ignores('keep.txt'), false);
+  });
+
+  it('Should throw an error if `.vercelignore` and `.nowignore` exist', async () => {
+    let err: Error | null = null;
+    const fixture = join(__dirname, 'fixtures', 'vercelignore-and-nowignore');
+    try {
+      await getVercelIgnore(fixture);
+    } catch (_err) {
+      err = _err;
+    }
+    assert(err);
+    assert.equal(
+      err!.message,
+      'Can not have both a `.vercelignore` and `.nowignore` file. Please delete the `.nowignore` file'
+    );
+  });
+});
