d6b74c9e0bc51ff71b7dad99eb6dcaf77c641935
Andy Bitz <artzbitz@gmail.com>
2020-05-13 22:57:32 +0200

    Add test to frameworks

---
+++ packages/frameworks/test/frameworks.unit.test.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import { existsSync } from 'fs';
+import { Framework } from '../';
+
+function isString(arg: any): arg is string {
+  return typeof arg === 'string';
+}
+
+describe('frameworks', () => {
+  it('ensure there is an example for every framework', async () => {
+    const root = path.join(__dirname, '..', '..', '..');
+    const getExample = (name: string) => path.join(root, 'examples', name);
+
+    const frameworks = require('../frameworks.json') as Framework[];
+
+    const result = frameworks
+      .map(f => f.slug)
+      .filter(isString)
+      .filter(f => existsSync(getExample(f)) === false);
+    expect(result).toEqual([]);
+  });
+});
