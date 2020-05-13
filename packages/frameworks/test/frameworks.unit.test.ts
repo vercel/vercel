a0770ab56308ce9524aa9102f86a81da6dbef8e9
Andy Bitz <artzbitz@gmail.com>
2020-05-14 00:12:11 +0200

    Add schema validation and add missing Scully.io logo

--- packages/frameworks/test/frameworks.unit.test.ts
+++ packages/frameworks/test/frameworks.unit.test.ts
@@ -1,3 +1,4 @@
+import Ajv from 'ajv';
 import path from 'path';
 import { existsSync } from 'fs';
 import { Framework } from '../';
@@ -5,6 +6,86 @@ import { Framework } from '../';
 function isString(arg: any): arg is string {
   return typeof arg === 'string';
 }
+
+const SchemaFrameworkDetectionItem = {
+  type: 'array',
+  items: [
+    {
+      type: 'object',
+      required: ['path'],
+      additionalProperties: false,
+      properties: {
+        path: {
+          type: 'string',
+        },
+        matchContent: {
+          type: 'string',
+        },
+      },
+    },
+  ],
+};
+
+const SchemaSettings = {
+  oneOf: [
+    {
+      type: 'object',
+      required: ['value'],
+      additionalProperties: false,
+      properties: {
+        value: {
+          type: 'string',
+        },
+      },
+    },
+    {
+      type: 'object',
+      required: ['placeholder'],
+      additionalProperties: false,
+      properties: {
+        placeholder: {
+          type: 'string',
+        },
+      },
+    },
+  ],
+};
+
+const Schema = {
+  type: 'array',
+  items: {
+    type: 'object',
+    required: ['name', 'slug', 'logo', 'description', 'settings'],
+    additionalProperties: false,
+    properties: {
+      name: { type: 'string' },
+      slug: { type: ['string', 'null'] },
+      logo: { type: 'string' },
+      demo: { type: 'string' },
+      tagline: { type: 'string' },
+      website: { type: 'string' },
+      description: { type: 'string' },
+      detectors: {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          every: SchemaFrameworkDetectionItem,
+          some: SchemaFrameworkDetectionItem,
+        },
+      },
+      settings: {
+        type: 'object',
+        required: ['buildCommand', 'devCommand', 'outputDirectory'],
+        additionalProperties: false,
+        properties: {
+          buildCommand: SchemaSettings,
+          devCommand: SchemaSettings,
+          outputDirectory: SchemaSettings,
+        },
+      },
+    },
+  },
+};
 
 describe('frameworks', () => {
   it('ensure there is an example for every framework', async () => {
@@ -19,5 +100,18 @@ describe('frameworks', () => {
       .filter(f => existsSync(getExample(f)) === false);
 
     expect(result).toEqual([]);
+  });
+
+  it('ensure schema', async () => {
+    const frameworks = require('../frameworks.json') as Framework[];
+
+    const ajv = new Ajv();
+    const result = ajv.validate(Schema, frameworks);
+
+    if (ajv.errors) {
+      console.error(ajv.errors);
+    }
+
+    expect(result).toBe(true);
   });
 });
