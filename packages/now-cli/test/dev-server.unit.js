52258c85fd0aee129d6368ebaeb4e50b43894b94
Nathan Rajlich <nathan@tootallnate.net>
2020-05-19 20:00:59 -0700

    Include both `req.body` and "data" events tests

--- packages/now-cli/test/dev-server.unit.js
+++ packages/now-cli/test/dev-server.unit.js
@@ -447,12 +447,29 @@ test(
   '[DevServer] Test request body',
   testFixture('now-dev-request-body', async (t, server) => {
     {
-      const res = await fetch(`${server.address}/`, {
+      // Test that `req.body` works in dev
+      const res = await fetch(`${server.address}/api/req-body`, {
         method: 'POST',
-        body: 'test1',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({ hello: 'world' }),
       });
       const body = await res.json();
-      t.is(body.data, 'test1');
+      t.is(body.hello, 'world');
+    }
+
+    {
+      // Test that `req` "data" events work in dev
+      const res = await fetch(`${server.address}/api/data-events`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({ hello: 'world' }),
+      });
+      const body = await res.json();
+      t.is(body.hello, 'world');
     }
   })
 );
--- packages/now-cli/test/fixtures/unit/now-dev-request-body/api/data-events.js
+++ packages/now-cli/test/fixtures/unit/now-dev-request-body/api/data-events.js
@@ -1,13 +1,10 @@
 module.exports = (req, res) => {
-  let data = '';
-
+  let body = '';
   req.setEncoding('utf8');
-
   req.on('data', d => {
-    data += d;
+    body += d;
   });
-
   req.on('end', () => {
-    res.send({ data });
+    res.end(body);
   });
 };
