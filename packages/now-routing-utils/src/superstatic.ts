vercel/vercel

commit e6c50e2efb913443e46de7139365a10e9f6e03e0

[next][routing-utils] Fix unnamed multi-match custom route (#4484)
This adds a failing test for the regex from https://github.com/zeit/next.js/discussions/13347#discussion-4546 

It appears the reason the regex does not work when deployed on Vercel is due to us not indexing the un-named segments and instead we filter them out as noticed by @dav-is. 

This PR adds one approach to resolve this by accounting for the un-named indexes while not allowing them to be used in the destination still.

packages/now-routing-utils/src/superstatic.ts
@@ -2,10 +2,11 @@
  * This converts Superstatic configuration to vercel.json Routes
  * See https://github.com/firebase/superstatic#configuration
  */
-import { isString } from 'util';
 import { parse as parseUrl, format as formatUrl } from 'url';
 import { pathToRegexp, compile, Key } from 'path-to-regexp';
 import { Route, NowRedirect, NowRewrite, NowHeader } from './types';
+
+const UN_NAMED_SEGMENT = '__UN_NAMED_SEGMENT__';
 
 export function getCleanUrls(
   filePaths: string[]
@@ -147,7 +148,14 @@ export function sourceToRegex(
     sensitive: true,
     delimiter: '/',
   });
-  const segments = keys.map(k => k.name).filter(isString);
+  const segments = keys
+    .map(k => k.name)
+    .map(name => {
+      if (typeof name !== 'string') {
+        return UN_NAMED_SEGMENT;
+      }
+      return name;
+    });
   return { src: r.source, segments };
 }
 
@@ -164,7 +172,10 @@ function replaceSegments(
   let { pathname, hash, query, ...rest } = parsedDestination;
   pathname = pathname || '';
   hash = hash || '';
-  if (segments.length > 0) {
+
+  const namedSegments = segments.filter(name => name !== UN_NAMED_SEGMENT);
+
+  if (namedSegments.length > 0) {
     const indexes: { [k: string]: string } = {};
     segments.forEach((name, index) => {
       indexes[name] = toSegmentDest(index);
@@ -187,7 +198,7 @@ function replaceSegments(
     // specified
     if (!isRedirect) {
       for (const [name, value] of Object.entries(indexes)) {
-        if (!(name in query)) {
+        if (!(name in query) && name !== UN_NAMED_SEGMENT) {
           query[name] = value;
         }
       }

packages/now-next/test/fixtures/23-custom-routes-verbose/next.config.js
@@ -38,6 +38,10 @@ module.exports = {
           destination: '/hello',
         },
         {
+          source: '/(.*)-:id(\\d+).html',
+          destination: '/blog/:id',
+        },
+        {
           source: '/blog/post-1',
           destination: '/blog/post-2',
         },

packages/now-routing-utils/test/superstatic.spec.js
@@ -349,6 +349,7 @@ test('convertRewrites', () => {
     { source: '/catchme/:id*', destination: '/api/user' },
     { source: '/:path', destination: '/test?path=:path' },
     { source: '/:path/:two', destination: '/test?path=:path' },
+    { source: '/(.*)-:id(\\d+).html', destination: '/blog/:id' },
   ]);
 
   const expected = [
@@ -424,6 +425,7 @@ test('convertRewrites', () => {
       dest: '/test?path=$1&two=$2',
       src: '^(?:\\/([^\\/]+?))(?:\\/([^\\/]+?))$',
     },
+    { check: true, dest: '/blog/$2?id=$2', src: '^(?:\\/(.*))-(\\d+)\\.html$' },
   ];
 
   deepEqual(actual, expected);
@@ -444,6 +446,7 @@ test('convertRewrites', () => {
     ['/catchme/id-1', '/catchme/id/2'],
     ['/first', '/another'],
     ['/first/second', '/one/two'],
+    ['/hello/post-123.html', '/post-123.html'],
   ];
 
   const mustNotMatch = [
@@ -462,6 +465,7 @@ test('convertRewrites', () => {
     ['/catchm', '/random'],
     ['/another/one'],
     ['/not', '/these'],
+    ['/hello/post.html'],
   ];
 
   assertRegexMatches(actual, mustMatch, mustNotMatch);

commit 0ca3189f797e39f42c1e9fd8803c5f1fe5a74785

[routing-utils] Fix headers with content-security-policy URL (#4550)
Fixes a case when the header value contains a URL which was mistaken for a named segment.

https://sentry.io/organizations/zeithq/issues/1702692084/?project=1351065

The regression was introduced in PR #4484 where unnamed segments were implemented for `redirects` and `rewrites` but not handled properly in `headers`.

packages/now-routing-utils/src/superstatic.ts
@@ -89,7 +89,7 @@ export function convertHeaders(headers: NowHeader[]): Route[] {
   return headers.map(h => {
     const obj: { [key: string]: string } = {};
     const { src, segments } = sourceToRegex(h.source);
-    const hasSegments = segments.length > 0;
+    const namedSegments = segments.filter(name => name !== UN_NAMED_SEGMENT);
     const indexes: { [k: string]: string } = {};
 
     segments.forEach((name, index) => {
@@ -97,7 +97,7 @@ export function convertHeaders(headers: NowHeader[]): Route[] {
     });
 
     h.headers.forEach(({ key, value }) => {
-      if (hasSegments) {
+      if (namedSegments.length > 0) {
         if (key.includes(':')) {
           key = safelyCompile(key, indexes);
         }

packages/now-routing-utils/test/index.spec.js
@@ -915,4 +915,65 @@ describe('getTransformedRoutes', () => {
       actual.error.message
     );
   });
+
+  test('should work with content-security-policy header containing URL', () => {
+    const nowConfig = {
+      headers: [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'content-security-policy',
+              value:
+                "default-src 'self'; script-src 'self'; img-src 'self' https://*.example.com; style-src 'self' 'unsafe-inline'; connect-src 'self' https://*.examplpe.com wss://gateway.example.com; form-action 'self'",
+            },
+            {
+              key: 'feature-policy',
+              value:
+                "accelerometer 'none'; camera 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment 'none'; usb 'none'",
+            },
+            {
+              key: 'referrer-policy',
+              value: 'strict-origin-when-cross-origin',
+            },
+            {
+              key: 'strict-transport-security',
+              value: 'max-age=31536000; includesubdomains; preload',
+            },
+            {
+              key: 'x-content-type-options',
+              value: 'nosniff',
+            },
+            {
+              key: 'x-frame-options',
+              value: 'sameorigin',
+            },
+            {
+              key: 'x-xss-protection',
+              value: '1; mode=block',
+            },
+          ],
+        },
+      ],
+    };
+    const actual = getTransformedRoutes({ nowConfig });
+    assert.deepEqual(actual.routes, [
+      {
+        continue: true,
+        headers: {
+          'content-security-policy':
+            "default-src 'self'; script-src 'self'; img-src 'self' https://*.example.com; style-src 'self' 'unsafe-inline'; connect-src 'self' https://*.examplpe.com wss://gateway.example.com; form-action 'self'",
+          'feature-policy':
+            "accelerometer 'none'; camera 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment 'none'; usb 'none'",
+          'referrer-policy': 'strict-origin-when-cross-origin',
+          'strict-transport-security':
+            'max-age=31536000; includesubdomains; preload',
+          'x-content-type-options': 'nosniff',
+          'x-frame-options': 'sameorigin',
+          'x-xss-protection': '1; mode=block',
+        },
+        src: '^(?:/(.*))$',
+      },
+    ]);
+  });
 });