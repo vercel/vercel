46d652bc56f354aeef2b52381b68caf9e46343f9
Nathan Rajlich <nathan@tootallnate.net>
2020-06-03 11:38:04 -0700

    Add logic to `getLocalPathConfig()` as well

--- packages/now-cli/src/util/config/local-path.ts
+++ packages/now-cli/src/util/config/local-path.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import mri from 'mri';
 import { InvalidLocalConfig } from '../errors';
+import { ConflictingConfigFiles } from './errors-ts';
 import { existsSync } from 'fs';
 
 export default function getLocalPathConfig(prefix: string) {
@@ -11,20 +12,30 @@ export default function getLocalPathConfig(prefix: string) {
     },
   });
 
+  // If `--local-config` flag was specified, then that takes priority
   const customPath = args['local-config'];
-
-  if (customPath && typeof customPath !== 'string') {
-    throw new InvalidLocalConfig(customPath);
+  if (customPath) {
+    if (typeof customPath !== 'string') {
+      throw new InvalidLocalConfig(customPath);
+    }
+    return path.resolve(prefix, customPath);
   }
 
-  const possibleConfigFiles = [
-    path.join(prefix, 'vercel.json'),
-    path.join(prefix, 'now.json'),
-  ];
+  // Otherwise check for either `vercel.json` or `now.json`.
+  // Throw an error if both exist.
+  const vercelConfigPath = path.join(prefix, 'vercel.json');
+  const nowConfigPath = path.join(prefix, 'now.json');
 
-  return (
-    (customPath && path.resolve(prefix, customPath)) ||
-    possibleConfigFiles.find(configFile => existsSync(configFile)) ||
-    possibleConfigFiles[0]
-  );
+  const vercelConfigExists = existsSync(vercelConfigPath);
+  const nowConfigExists = existsSync(nowConfigPath);
+
+  if (nowConfigExists && vercelConfigExists) {
+    throw new ConflictingConfigFiles([vercelConfigPath, nowConfigPath]);
+  }
+
+  if (nowConfigExists) {
+    return nowConfigPath;
+  }
+
+  return vercelConfigPath;
 }
