fd54cd9783d5e979893b57811e54c8ed481dc03f
Steven <steven@ceriously.com>
2020-06-11 10:22:19 -0400

    Wait 60s and move to bottom

--- packages/now-cli/test/integration.js
+++ packages/now-cli/test/integration.js
@@ -299,33 +299,6 @@ test('deploy using only now.json with `redirects` defined', async t => {
   t.is(location, 'https://example.com/foo/bar');
 });
 
-test('deploys gatsby and prints cached directories', async t => {
-  const directory = example('gatsby');
-  const packageJsonPath = path.join(directory, 'package.json');
-  const packageJsonOriginal = await readFile(packageJsonPath, 'utf8');
-  const pkg = JSON.parse(packageJsonOriginal);
-
-  async function tryDeploy(cwd) {
-    await execa(binaryPath, [...defaultArgs, '--public', '--confirm'], {
-      cwd,
-      stdio: 'inherit',
-      reject: true,
-    });
-
-    t.true(true);
-  }
-
-  await tryDeploy(directory);
-  await sleep(5000);
-  pkg.scripts.build = `ls -lA && ls .cache && ls public && ${pkg.scripts.build}`;
-  await writeFile(packageJsonPath, JSON.stringify(pkg));
-  try {
-    await tryDeploy(directory);
-  } finally {
-    await writeFile(packageJsonPath, packageJsonOriginal);
-  }
-});
-
 test('deploy using --local-config flag v2', async t => {
   const target = fixture('local-config-v2');
   const configPath = path.join(target, 'now-test.json');
@@ -2960,4 +2933,37 @@ test('`vc --debug project ls` should output the projects listing', async t => {
     stdout.includes('> Projects found under'),
     formatOutput({ stderr, stdout })
   );
+});
+
+test('deploy gatsby twice and print cached directories', async t => {
+  const directory = example('gatsby');
+  const packageJsonPath = path.join(directory, 'package.json');
+  const packageJsonOriginal = await readFile(packageJsonPath, 'utf8');
+  const pkg = JSON.parse(packageJsonOriginal);
+
+  async function tryDeploy(cwd) {
+    await execa(binaryPath, [...defaultArgs, '--public', '--confirm'], {
+      cwd,
+      stdio: 'inherit',
+      reject: true,
+    });
+
+    t.true(true);
+  }
+
+  // Deploy once too populate the cache
+  await tryDeploy(directory);
+
+  // Wait because the cache is not available right away
+  // See https://codeburst.io/quick-explanation-of-the-s3-consistency-model-6c9f325e3f82
+  await sleep(60000);
+
+  // Update build script to ensure cached files were restored in the next deploy
+  pkg.scripts.build = `ls -lA && ls .cache && ls public && ${pkg.scripts.build}`;
+  await writeFile(packageJsonPath, JSON.stringify(pkg));
+  try {
+    await tryDeploy(directory);
+  } finally {
+    await writeFile(packageJsonPath, packageJsonOriginal);
+  }
 });
