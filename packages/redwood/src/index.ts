aec16a5bf7f69adc14dea89609ba784dd8a2bd5d
Steven <steven@ceriously.com>
2020-06-17 15:07:56 -0400

    Change `node-file-trace` to `zip-it-and-ship-it`

--- packages/redwood/src/index.ts
+++ packages/redwood/src/index.ts
@@ -1,22 +1,20 @@
-import { join, dirname, relative } from 'path';
-import { readFileSync, lstatSync } from 'fs';
+import { join, dirname, parse } from 'path';
 import buildUtils from './build-utils';
-import { BuildOptions, Lambda, File } from '@vercel/build-utils';
+import { BuildOptions, Lambda as LambdaType } from '@vercel/build-utils';
 const {
   download,
   glob,
-  createLambda,
   shouldServe,
   debug,
   getNodeVersion,
   getSpawnOptions,
   runNpmInstall,
   spawnAsync,
-  isSymbolicLink,
-  FileFsRef,
-  FileBlob,
+  Lambda,
+  streamToBuffer,
 } = buildUtils;
-import nodeFileTrace from '@zeit/node-file-trace';
+// @ts-ignore
+import { zipFunctions } from '@netlify/zip-it-and-ship-it';
 
 export const version = 2;
 
@@ -52,65 +50,37 @@ export async function build({
     prettyCommand: 'yarn rw build',
   });
 
-  const lambdaOutputs: { [filePath: string]: Lambda } = {};
+  const lambdaOutputs: { [filePath: string]: LambdaType } = {};
 
   const staticOutputs = await glob('**', {
     cwd: join(workPath, 'web', 'dist'),
   });
 
+  /*
   const inputFsFiles = await glob('*.js', {
     cwd: join(workPath, 'api', 'dist', 'functions'),
   });
+  */
 
-  const sourceCache = new Map<string, Buffer | null>();
-  const fsCache = new Map<string, File>();
-  const inputs = Object.keys(inputFsFiles);
-  const { fileList, warnings } = await nodeFileTrace(inputs, {
-    base: workPath,
-    readFile(fsPath: string): string | null {
-      const relPath = relative(workPath, fsPath);
-      const cached = sourceCache.get(relPath);
-      if (cached) return cached.toString();
-      // null represents a not found
-      if (cached === null) return null;
-      try {
-        const source = readFileSync(fsPath);
-        const { mode } = lstatSync(fsPath);
-        const file = isSymbolicLink(mode)
-          ? new FileFsRef({ fsPath, mode })
-          : new FileBlob({ data: source, mode });
-        fsCache.set(relPath, file);
-        sourceCache.set(relPath, source);
-        return source.toString();
-      } catch (e) {
-        if (e.code === 'ENOENT' || e.code === 'EISDIR') {
-          sourceCache.set(relPath, null);
-          return null;
-        }
-        throw e;
-      }
-    },
+  const apiDistPath = join(workPath, 'api', 'dist', 'functions');
+  const zipBallPath = join(workPath, 'api', 'zipballs');
+  console.log(`zisi: zip ${apiDistPath} and ship ${zipBallPath}`);
+  await zipFunctions(apiDistPath, zipBallPath);
+
+  const zipballs = await glob('*.zip', {
+    cwd: zipBallPath,
   });
 
-  console.log({ warnings });
-
-  const tracedFiles: { [filename: string]: File } = {};
-  for (const filePath of fileList) {
-    const file = fsCache.get(filePath);
-    if (file) {
-      tracedFiles[filePath] = file;
-    }
-  }
-
-  for (const [filePath] of Object.entries(inputs)) {
-    const filename = filePath.slice(0, -3); // strip .js
-    const lambda = await createLambda({
-      files: tracedFiles,
-      handler: `${filename}.handler`,
+  for (const [filePath, zipFile] of Object.entries(zipballs)) {
+    const functionName = parse(filePath).name;
+    const zipBuffer = await streamToBuffer(zipFile.toStream());
+    const lambda = new Lambda({
+      zipBuffer,
+      handler: `${functionName}.handler`,
       runtime: nodeVersion.runtime,
       environment: {},
     });
-    lambdaOutputs[filename] = lambda;
+    lambdaOutputs[join('api', functionName)] = lambda;
   }
 
   return {
