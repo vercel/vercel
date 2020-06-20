06ee0bec65a3177953285cb68466ce217c116899
Brandon Bayer <b@bayer.ws>
2020-06-20 10:27:30 +0700

    revert files

--- examples/blitzjs/blitz.config.js
+++ examples/blitzjs/blitz.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  /*
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
@@ -10,4 +11,5 @@ module.exports = {
     // Important: return the modified config
     return config
   },
+  */
 }
