e1129d921fe92f732b00c8f71a4e659e3dde125c
Nathan Rajlich <nathan@tootallnate.net>
2020-06-23 11:18:27 -0700

    Test more paths

--- packages/now-cli/test/dev/integration.js
+++ packages/now-cli/test/dev/integration.js
@@ -1624,7 +1624,10 @@ test(
   '[vercel dev] Should support `*.go` API serverless functions',
   testFixtureStdio('go', async testPath => {
     await testPath(200, `/api`, 'This is the index page');
+    await testPath(200, `/api/index`, 'This is the index page');
+    await testPath(200, `/api/index.go`, 'This is the index page');
     await testPath(200, `/api/another`, 'This is another page');
+    await testPath(200, '/api/another.go', 'This is another page');
     await testPath(200, `/api/foo`, 'Req Path: /api/foo');
     await testPath(200, `/api/bar`, 'Req Path: /api/bar');
   })
