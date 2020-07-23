5eb14dcf8748ec067b3dd7c39951f1cb56bb64ba
Steven <steven@ceriously.com>
2020-07-23 11:43:02 -0400

    Add dev support

--- packages/redwood/src/index.ts
+++ packages/redwood/src/index.ts
@@ -1,4 +1,5 @@
 import { join, dirname, relative, parse as parsePath, sep } from 'path';
+import { ChildProcess, SpawnOptions } from 'child_process';
 import {
   BuildOptions,
   Lambda,
@@ -7,28 +8,58 @@ import {
   createLambda,
   download,
   glob,
-  shouldServe,
   debug,
   getNodeVersion,
   getSpawnOptions,
   runNpmInstall,
-  spawnAsync,
   execCommand,
+  spawnCommand,
+  readConfigFile,
   FileBlob,
   FileFsRef,
+  NowBuildError,
 } from '@vercel/build-utils';
 import { makeAwsLauncher } from './launcher';
 const {
   getDependencies,
   // eslint-disable-next-line @typescript-eslint/no-var-requires
 } = require('@netlify/zip-it-and-ship-it/src/dependencies.js');
+//@ts-ignore
+import isPortReachable from 'is-port-reachable';
+
+interface RedwoodConfig {
+  web?: {
+    port?: number;
+    apiProxyPath?: string;
+  };
+  api?: {
+    port?: number;
+  };
+  browser?: {
+    open?: boolean;
+  };
+}
 
 const LAUNCHER_FILENAME = '___vc_launcher';
 const BRIDGE_FILENAME = '___vc_bridge';
 const HELPERS_FILENAME = '___vc_helpers';
 const SOURCEMAP_SUPPORT_FILENAME = '__vc_sourcemap_support';
 
+const entrypointToPort = new Map<string, number>();
+const childProcesses = new Set<ChildProcess>();
 export const version = 2;
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function waitForPort(port: number): Promise<boolean> {
+  for (let i = 0; i < 500; i++) {
+    if (await isPortReachable(port)) {
+      return true;
+    }
+    await sleep(100);
+  }
+  return false;
+}
 
 export async function build({
   workPath,
@@ -39,7 +70,8 @@ export async function build({
 }: BuildOptions) {
   await download(files, workPath, meta);
 
-  const entrypointFsDirname = join(workPath, dirname(entrypoint));
+  const mountpoint = dirname(entrypoint);
+  const entrypointFsDirname = join(workPath, mountpoint);
   const nodeVersion = await getNodeVersion(
     entrypointFsDirname,
     undefined,
@@ -55,17 +87,72 @@ export async function build({
     meta
   );
 
-  debug('Running build script...');
-  await spawnAsync('yarn', ['rw', 'build'], {
-    ...spawnOpts,
-    cwd: workPath,
-    prettyCommand: 'yarn rw build',
-  });
-
   const {
     buildCommand = 'yarn rw db up --no-db-client --auto-approve && yarn rw build',
+    devCommand = 'yarn rw dev',
   } = config;
 
+  if (meta.isDev) {
+    const toml = await readConfigFile<RedwoodConfig>(
+      join(mountpoint, 'redwood.toml')
+    );
+    const webPort = toml?.web?.port || 8910;
+    const apiPort = toml?.web?.port || 8911;
+    let devPort = entrypointToPort.get(entrypoint);
+
+    if (typeof devPort === 'number') {
+      debug('`%s` server already running for %j', devCommand, entrypoint);
+    } else {
+      devPort = webPort;
+      entrypointToPort.set(entrypoint, devPort);
+
+      const opts: SpawnOptions = {
+        cwd: mountpoint,
+        stdio: 'inherit',
+        env: { ...spawnOpts.env, PORT: String(devPort) },
+      };
+
+      const child = spawnCommand(devCommand, opts);
+      child.on('exit', () => entrypointToPort.delete(entrypoint));
+      childProcesses.add(child);
+
+      const found = await waitForPort(devPort);
+      if (!found) {
+        throw new NowBuildError({
+          code: 'REDWOOD_PORT_UNAVAILABLE',
+          message: `Failed to detect a server running on port ${devPort}`,
+          action: 'More Details',
+          link:
+            'https://err.sh/vercel/vercel/now-static-build-failed-to-detect-a-server',
+        });
+      }
+
+      debug('Detected dev server for %j', entrypoint);
+    }
+
+    let srcBase = mountpoint.replace(/^\.\/?/, '');
+
+    if (srcBase.length > 0) {
+      srcBase = `/${srcBase}`;
+    }
+
+    return {
+      routes: [
+        {
+          src: `${srcBase}/api/(.*)`,
+          dest: `http://localhost:${apiPort}/$1`,
+        },
+        {
+          src: `${srcBase}/(.*)`,
+          dest: `http://localhost:${webPort}/$1`,
+        },
+      ],
+      watch: [join(srcBase, '**/*')],
+      output: {},
+    };
+  }
+
+  debug('Running build command...');
   await execCommand(buildCommand, {
     ...spawnOpts,
     cwd: workPath,
@@ -139,5 +226,3 @@ export async function prepareCache({
   const cache = await glob('node_modules/**', workPath);
   return cache;
 }
-
-export { shouldServe };
