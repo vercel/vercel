93ebd213def6dbb39bed86e972b30cb48e98c007
Torsten Dittmann <torsten.dittmann@googlemail.com>
2020-07-31 00:44:39 +0200

    [examples] Update deprecated and outdated Svelte example packages (#4706)

--- examples/svelte/rollup.config.js
+++ examples/svelte/rollup.config.js
@@ -1,6 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
