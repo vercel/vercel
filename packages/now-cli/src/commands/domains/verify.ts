d5e1de8cc86cf2f1a40b531392a53186f1fbe2a5
Andy Bitz <artzbitz@gmail.com>
2020-08-03 16:51:36 +0200

    Update error

--- packages/now-cli/src/commands/domains/verify.ts
+++ packages/now-cli/src/commands/domains/verify.ts
@@ -1,5 +1,6 @@
 import { NowContext } from '../../types';
 import { Output } from '../../util/output';
+import { NowBuildError } from '@vercel/build-utils';
 import { getCommandName } from '../../util/pkg-name';
 
 export default async function verify(
@@ -18,17 +19,15 @@ export default async function verify(
     return 1;
   }
 
-  const error = new Error(
-    `It's not necessary to verify Domains anymore. Instead, you can run ${getCommandName(
+  const error = new NowBuildError({
+    code: 'domains_verify_command_deprecated',
+    message: `It's not necessary to verify Domains anymore. Instead, you can run ${getCommandName(
       `domains inspect ${domainName}`
-    )} to see what you need to do in order to configure it properly.`
-  );
+    )} to see what you need to do in order to configure it properly.`,
+    link: 'https://vercel.link/domain-verification-via-cli',
+  });
 
-  output.prettyError(
-    Object.assign(error, {
-      link: 'https://vercel.link/domain-verification-via-cli',
-    })
-  );
+  output.prettyError(error);
 
   return 0;
 }
