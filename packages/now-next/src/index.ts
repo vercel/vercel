dc3a273f11ef5f109b97e23346a00ca9b01a45ba
JJ Kasper <jj@jjsweb.site>
2020-09-05 12:01:22 -0500

    Optimize page loading for shared lambdas

--- packages/now-next/src/index.ts
+++ packages/now-next/src/index.ts
@@ -1324,7 +1324,7 @@ export const build = async ({
                     ${groupPageKeys
                       .map(
                         page =>
-                          `'${page}': require('./${path.join(
+                          `'${page}': () => require('./${path.join(
                             './',
                             group.pages[page].pageFileName
                           )}')`
@@ -1333,7 +1333,7 @@ export const build = async ({
                     ${
                       '' /*
                       creates a mapping of the page and the page's module e.g.
-                      '/about': require('./.next/serverless/pages/about.js')
+                      '/about': () => require('./.next/serverless/pages/about.js')
                     */
                     }
                   }
@@ -1393,7 +1393,10 @@ export const build = async ({
                     res.statusCode = 500
                     return res.end('internal server error')
                   }
-                  const method = currentPage.render || currentPage.default || currentPage
+
+                  const mod = currentPage()
+                  const method = mod.render || mod.default || mod
+
                   return method(req, res)
                 } catch (err) {
                   console.error('Unhandled error during request:', err)
