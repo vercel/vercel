e01ec0f2fe1cab277d7eeb331efa8f223fb4b042
RAJPRAKASH <42652941+rajprakash00@users.noreply.github.com>
2020-09-18 16:00:25 +0530

    Fixed links of gatsby live example

--- examples/gatsby/src/pages/index.js
+++ examples/gatsby/src/pages/index.js
@@ -31,7 +31,7 @@ function Index() {
       </h2>
       <p>
         <a
-          href="https://github.com/vercel/vercel/blob/master/gatsby"
+          href="https://github.com/vercel/vercel/tree/master/examples/gatsby"
           target="_blank"
           rel="noreferrer noopener"
         >
