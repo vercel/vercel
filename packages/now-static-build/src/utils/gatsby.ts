24294d60649181155f7bb30807675e819a6d1b21
Joe Haddad <timer150@gmail.com>
2020-11-04 21:16:51 -0500

    [static-build] zero-config gatsby should support `export default`

--- packages/now-static-build/src/utils/gatsby.ts
+++ packages/now-static-build/src/utils/gatsby.ts
@@ -49,7 +49,15 @@ export async function injectVercelAnalyticsPlugin(dir: string) {
       gatsbyConfigPath,
       `const userConfig = require("./gatsby-config.js.__vercel_builder_backup__.js");
 
-const vercelConfig = Object.assign({}, userConfig);
+// https://github.com/gatsbyjs/gatsby/blob/354003fb2908e02ff12109ca3a02978a5a6e608c/packages/gatsby/src/bootstrap/prefer-default.ts
+const preferDefault = m => (m && m.default) || m;
+
+const vercelConfig = Object.assign(
+  {},
+
+  // https://github.com/gatsbyjs/gatsby/blob/a6ecfb2b01d761e8a3612b8ea132c698659923d9/packages/gatsby/src/services/initialize.ts#L113-L117
+  preferDefault(userConfig)
+);
 if (!vercelConfig.plugins) {
   vercelConfig.plugins = [];
 }
