049ffb268b39c3da2daa5393dbcf82f874242372
Steven <steven@ceriously.com>
2020-11-11 09:44:06 -0500

    [examples] Update svelte example with latest rollup (#5404)
    Updated the template using `npx degit sveltejs/template my-svelte-project`
    
    I also added a `yarn.lock` file so dependencies don't break for future deployments.
    
    Related to https://github.com/Samuel-Martineau/generator-svelte/issues/7

--- examples/svelte/src/main.js
+++ examples/svelte/src/main.js
@@ -1,10 +1,10 @@
 import App from './App.svelte';
 
 const app = new App({
-  target: document.body,
-  props: {
-    name: 'world',
-  },
+	target: document.body,
+	props: {
+		name: 'world'
+	}
 });
 
 export default app;
