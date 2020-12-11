bea38dc67ac7829f6f812238d82e5d81bcefb1d6
Andy Bitz <artzbitz@gmail.com>
2020-12-11 15:55:47 +0100

    Undo test changes

--- packages/now-cli/test/integration.js
+++ packages/now-cli/test/integration.js
@@ -1734,9 +1734,10 @@ test('try to create a builds deployments with wrong now.json', async t => {
   t.is(exitCode, 1);
   t.true(
     stderr.includes(
-      'Error! Invalid request: should NOT have additional property `builder`.'
+      'Error! Invalid now.json - should NOT have additional property `builder`. Did you mean `builds`?'
     )
   );
+  t.true(stderr.includes('https://vercel.com/docs/configuration'));
 });
 
 test('try to create a builds deployments with wrong vercel.json', async t => {
