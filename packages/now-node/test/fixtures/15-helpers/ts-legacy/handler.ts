1215964060df31463f1472c52fb169e7c950a77b
Nathan Rajlich <nathan@tootallnate.net>
2020-12-14 13:17:15 -0800

    Apply suggestions from code review
    Co-authored-by: Steven <steven@ceriously.com>

--- packages/now-node/test/fixtures/15-helpers/ts-legacy/handler.ts
+++ packages/now-node/test/fixtures/15-helpers/ts-legacy/handler.ts
@@ -2,7 +2,7 @@ import { NowApiHandler } from './types';
 
 const listener: NowApiHandler = (req, res) => {
   res.status(200);
-  res.send('hello:RANDOMNESS_PLACEHOLDER');
+  res.send('hello legacy:RANDOMNESS_PLACEHOLDER');
 };
 
 export default listener;
