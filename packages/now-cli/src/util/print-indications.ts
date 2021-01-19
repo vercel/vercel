842150a19b73fc95769523547cdd174aa3fad643
Ana Jovanova <ana.trajkovska2015@gmail.com>
2021-01-19 14:04:40 +0100

    Improvements

--- packages/now-cli/src/util/print-indications.ts
+++ packages/now-cli/src/util/print-indications.ts
@@ -2,6 +2,7 @@ import chalk from 'chalk';
 import { Response } from 'node-fetch';
 import { emoji, EmojiLabel, prependEmoji } from './emoji';
 import createOutput from './output';
+import linkStyle from './output/link';
 
 export default function printIndications(res: Response) {
   const _output = createOutput();
@@ -15,14 +16,15 @@ export default function printIndications(res: Response) {
       const action = res.headers.get(`x-vercel-action-${identifier}`);
       const link = res.headers.get(`x-vercel-link-${identifier}`);
       if (indications.has(type)) {
-        const indent = process.stdout.isTTY ? '    ' : ''; // if using emojis
         const newline = '\n';
         const message =
           prependEmoji(chalk.dim(payload), emoji(type as EmojiLabel)) + newline;
         let finalLink = '';
-        if (link)
+        if (link) {
           finalLink =
-            indent + chalk.dim(`${action || 'Learn More'}: ${link}`) + newline;
+            chalk.dim(`${action || 'Learn More'}: ${linkStyle(link)}`) +
+            newline;
+        }
         _output.print(message + finalLink);
       }
     }
