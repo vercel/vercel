d94a91672f793fc9af84a725669bbf602398fb48
Nathan Rajlich <n@n8.io>
2021-01-21 18:25:19 -0800

    Add test against real Python servers

--- packages/now-python/test/test.js
+++ packages/now-python/test/test.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const assert = require('assert');
+const fetch = require('node-fetch');
+const { spawn } = require('child_process');
 
+const { installRequirementsFile } = require('../');
 const {
   packAndDeploy,
-  testDeployment
+  testDeployment,
 } = require('../../../test/lib/deployment/test-deployment.js');
 
 jest.setTimeout(4 * 60 * 1000);
@@ -17,6 +21,54 @@ beforeAll(async () => {
 });
 
 const fixturesPath = path.resolve(__dirname, 'fixtures');
+
+it('should match the probes against Python dev servers', async () => {
+  const fixture = path.join(fixturesPath, '00-request-path');
+
+  await installRequirementsFile({
+    filePath: 'requirements.txt',
+    workPath: fixture,
+    meta: {},
+  });
+
+  const ports = new Map();
+  ports.set('/api/python.py', 8001);
+  ports.set('/api/wsgi.py', 8002);
+  ports.set('/api/asgi.py', 8003);
+
+  const { probes } = require(path.join(fixture, 'vercel.json'));
+
+  const pythonServer = spawn('python3', ['api/python.py'], {
+    cwd: fixture,
+  });
+
+  const wsgiServer = spawn('python3', ['api/wsgi.py'], {
+    cwd: fixture,
+  });
+
+  const asgiServer = spawn('python3', ['api/asgi.py'], {
+    cwd: fixture,
+  });
+
+  try {
+    // wait a few seconds for the dev servers to boot-up
+    await new Promise(r => setTimeout(r, 3000));
+
+    for (const { path, mustContain } of probes) {
+      const port = ports.get(path.substring(0, path.indexOf('?')));
+      const res = await fetch(`http://localhost:${port}${path}`);
+      const body = await res.text();
+      assert(
+        body.includes(mustContain),
+        `Expected to contain "${mustContain}" but got "${body}"`
+      );
+    }
+  } finally {
+    process.kill(pythonServer.pid);
+    process.kill(wsgiServer.pid);
+    process.kill(asgiServer.pid);
+  }
+});
 
 // eslint-disable-next-line no-restricted-syntax
 for (const fixture of fs.readdirSync(fixturesPath)) {
