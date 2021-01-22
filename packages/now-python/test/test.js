9fe7fc5015ee3789b69ff7284ce4e08076636a93
Nathan Rajlich <n@n8.io>
2021-01-21 18:59:43 -0800

    pip install --upgrade setuptools

--- packages/now-python/test/test.js
+++ packages/now-python/test/test.js
@@ -25,7 +25,11 @@ const fixturesPath = path.resolve(__dirname, 'fixtures');
 it('should match the probes against Python dev servers', async () => {
   const fixture = path.join(fixturesPath, '00-request-path');
 
-  await execa('pip3', ['install', '-r', 'requirements.txt', '--user'], {
+  await execa('pip3', ['install', '--user', '--upgrade', 'setuptools'], {
+    cwd: fixture,
+    stdio: 'inherit',
+  });
+  await execa('pip3', ['install', '--user', '-r', 'requirements.txt'], {
     cwd: fixture,
     stdio: 'inherit',
   });
