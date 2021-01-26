d3e6c2d335110f737c4714500cda6419ae541bd0
Nathan Rajlich <nathan@tootallnate.net>
2021-01-26 12:37:15 -0800

    [python] Fix request URL path encoding for basic, WSGI and ASGI interfaces (#5739)
    `@vercel/python` has been incorrectly "unquoting" the incoming request
    URL for all Python interfaces (basic, WSGI, ASGI). The proper behavior is
    to pass along the request URL to the serverless function handler unmodified.
    
    This is proven by the additional test that is added in this PR, which actually
    fires up the corresponding Python server via i.e. `python3 api/asgi.py`,
    and executes the test probes defined in the
    `test/fixtures/00-request-path/vercel.json` file. Adding this test ensures
    that the request URL behavior of `@vercel/python` matches the behavior
    as compared to executing via `python` directly.

--- packages/now-cli/test/integration.js
+++ packages/now-cli/test/integration.js
@@ -1600,7 +1600,8 @@ test('output logs with "short" output', async t => {
   t.truthy(
     stdout.match(
       /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/
-    )
+    ),
+    formatOutput({ stderr, stdout })
   );
 
   t.is(exitCode, 0);
--- packages/now-python/test/fixtures/13-wsgi-url-params-encoding/custom.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, Response, request, __version__
-app = Flask(__name__)
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    qs = request.args.to_dict()
-    return Response("path: %s query: %s" %(path, qs), mimetype='text/html')
