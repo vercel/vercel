c6955d5abda5eb9e1140d5d1d76ec7a4b193fe90
luc <luc.leray@gmail.com>
2021-02-11 14:18:36 +0100

    replace "ensure project" with "create project"

--- packages/now-cli/src/commands/projects.js
+++ packages/now-cli/src/commands/projects.js
@@ -248,7 +248,7 @@ async function run({ client, contextName }) {
     }
 
     const [name] = args;
-    await client.fetch('/projects/ensure-project', {
+    await client.fetch('/projects', {
       method: 'POST',
       body: { name },
     });
