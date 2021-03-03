3bf294b0e5d4322fd0663c9fca4253e41352ab15
Steven <steven@ceriously.com>
2021-03-03 16:15:28 -0500

    Fix strict equal

--- packages/now-node/test/fixtures/07-content-length/probe.js
+++ packages/now-node/test/fixtures/07-content-length/probe.js
@@ -5,7 +5,7 @@ async function test3({ deploymentUrl, fetch, randomness }) {
   const resp = await fetch(`https://${deploymentUrl}/test3.js`);
   strictEqual(resp.status, 401);
   strictEqual(await resp.text(), bodyMustBe);
-  strictEqual(resp.headers.get('content-length'), bodyMustBe.length);
+  strictEqual(resp.headers.get('content-length'), String(bodyMustBe.length));
 }
 
 module.exports = async ({ deploymentUrl, fetch, randomness }) => {
