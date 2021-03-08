175b08cbcec7ca092090159e5c76a4244eef208a
Nathan Rajlich <n@n8.io>
2021-03-08 12:50:35 -0800

    Debug

--- packages/cli/test/helpers/get-url.js
+++ packages/cli/test/helpers/get-url.js
@@ -1,8 +1,10 @@
 // Packages
 const listen = require('test-listen');
 const micro = require('micro');
+console.log(micro);
 
 module.exports = fn => {
   const srv = micro(fn);
+  console.log({ srv });
   return listen(srv);
 };
