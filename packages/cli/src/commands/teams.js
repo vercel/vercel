vercel/vercel

commit 2eb3d7ac5723cdbed72891488384354df53cc64c

[cli] Refactor to merge `NowContext` into `Client` (#6016)
This is a rather large refactor of the CLI codebase that merges the
`NowContext` object into the `Client` class.

There's a few reasons to do this:

  1. Nearly every sub-command was creating its own `Client` instance, leading to a lot of duplicated code. It makes more sense to create one instance and pass it to every sub-command, which deletes a lot of code as well.
  2. There's a lot of overlap between `NowContext` and the props on the `Client` class, so it was a relatively easy transition.
  3. The main reason is so the same `authConfig` is used throughout the program lifecycle. This paves the way for updating the token mid-flight when i.e. a SAML token has expired. In a follow-up PR, CLI will handle that scenario gracefully by re-triggering the SAML auth flow.

packages/cli/src/commands/teams.js
@@ -3,7 +3,6 @@ import mri from 'mri';
 import error from '../util/output/error';
 import NowTeams from '../util/teams';
 import logo from '../util/output/logo';
-import exit from '../util/exit';
 import { handleError } from '../util/error';
 import list from './teams/list';
 import add from './teams/add';
@@ -66,8 +65,8 @@ let debug;
 let apiUrl;
 let subcommand;
 
-const main = async ctx => {
-  argv = mri(ctx.argv.slice(2), {
+const main = async client => {
+  argv = mri(client.argv.slice(2), {
     boolean: ['help', 'debug'],
     alias: {
       help: 'h',
@@ -78,7 +77,7 @@ const main = async ctx => {
   });
 
   debug = argv.debug;
-  apiUrl = ctx.apiUrl;
+  apiUrl = client.apiUrl;
 
   const isSwitch = argv._[0] && argv._[0] === 'switch';
 
@@ -92,73 +91,37 @@ const main = async ctx => {
 
   if (argv.help || !subcommand) {
     help();
-    await exit(0);
+    return 2;
   }
 
   const {
     authConfig: { token },
-    output,
     config,
-  } = ctx;
+  } = client;
 
-  return run({ token, config, output });
-};
-
-export default async ctx => {
-  try {
-    return main(ctx);
-  } catch (err) {
-    handleError(err);
-    return 1;
-  }
-};
-
-async function run({ token, config, output }) {
   const { currentTeam } = config;
   const teams = new NowTeams({ apiUrl, token, debug, currentTeam });
-  const args = argv._;
 
   let exitCode;
   switch (subcommand) {
     case 'list':
     case 'ls': {
-      exitCode = await list({
-        teams,
-        config,
-        apiUrl,
-        token,
-        output,
-        argv,
-      });
+      exitCode = await list(client, argv, teams);
       break;
     }
     case 'switch':
     case 'change': {
-      exitCode = await change({
-        args,
-        config,
-        apiUrl,
-        token,
-        debug,
-        output,
-      });
+      exitCode = await change(client, argv);
       break;
     }
     case 'add':
     case 'create': {
-      exitCode = await add({ apiUrl, token, teams, config });
+      exitCode = await add(client, argv, teams);
       break;
     }
 
     case 'invite': {
-      exitCode = await invite({
-        teams,
-        args,
-        config,
-        apiUrl,
-        token,
-        output,
-      });
+      exitCode = await invite(client, argv, teams);
       break;
     }
     default: {
@@ -166,11 +129,21 @@ async function run({ token, config, output }) {
         console.error(
           error('Please specify a valid subcommand: add | ls | switch | invite')
         );
-        exitCode = 1;
       }
+      exitCode = 2;
       help();
     }
   }
   teams.close();
   return exitCode || 0;
-}
+};
+
+export default async client => {
+  try {
+    return await main(client);
+  } catch (err) {
+    console.error(err);
+    handleError(err);
+    return 1;
+  }
+};

packages/cli/src/index.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-import error from './util/output/error';
-import param from './util/output/param';
-import info from './util/output/info';
 try {
   // Test to see if cwd has been deleted before
   // importing 3rd party packages that might need cwd.
@@ -13,6 +10,7 @@ try {
     process.exit(1);
   }
 }
+
 import { join } from 'path';
 import { existsSync, lstatSync } from 'fs';
 import sourceMap from '@zeit/source-map-support';
@@ -23,25 +21,28 @@ import updateNotifier from 'update-notifier';
 import { URL } from 'url';
 import * as Sentry from '@sentry/node';
 import { NowBuildError } from '@vercel/build-utils';
+import hp from './util/humanize-path';
+import commands from './commands/index.ts';
+import pkg from './util/pkg.ts';
+import createOutput from './util/output';
+import cmd from './util/output/cmd';
+import info from './util/output/info';
+import error from './util/output/error';
+import param from './util/output/param';
+import highlight from './util/output/highlight';
+import getArgs from './util/get-args';
+import getUser from './util/get-user.ts';
+import Client from './util/client.ts';
+import NowTeams from './util/teams';
+import { handleError } from './util/error';
+import reportError from './util/report-error';
+import getConfig from './util/get-config';
+import * as configFiles from './util/config/files';
 import getGlobalPathConfig from './util/config/global-path';
 import {
   getDefaultConfig,
   getDefaultAuthConfig,
 } from './util/config/get-default';
-import hp from './util/humanize-path';
-import commands from './commands/index.ts';
-import * as configFiles from './util/config/files';
-import pkg from './util/pkg.ts';
-import createOutput from './util/output';
-import getArgs from './util/get-args';
-import getUser from './util/get-user.ts';
-import Client from './util/client.ts';
-import NowTeams from './util/teams';
-import cmd from './util/output/cmd';
-import { handleError } from './util/error';
-import highlight from './util/output/highlight';
-import reportError from './util/report-error';
-import getConfig from './util/get-config';
 import * as ERRORS from './util/errors-ts';
 import { NowError } from './util/now-error';
 import { APIError } from './util/errors-ts.ts';
@@ -75,17 +76,18 @@ Sentry.init({
   environment: isCanary ? 'canary' : 'stable',
 });
 
+let client;
 let debug = () => {};
 let apiUrl = 'https://api.vercel.com';
 
-const main = async argv_ => {
+const main = async () => {
   const { isTTY } = process.stdout;
 
-  let argv = null;
+  let argv;
 
   try {
     argv = getArgs(
-      argv_,
+      process.argv,
       {
         '--version': Boolean,
         '-v': '--version',
@@ -129,11 +131,7 @@ const main = async argv_ => {
     return 1;
   }
 
-  // the second argument to the command can be a path
-  // (as in: `vercel path/`) or a subcommand / provider
-  // (as in: `vercel ls`)
-  const targetOrSubcommand = argv._[2];
-
+  // Print update information, if available
   if (notifier.update && notifier.update.latest !== pkg.version && isTTY) {
     const { latest } = notifier.update;
     console.log(
@@ -152,6 +150,12 @@ const main = async argv_ => {
     );
   }
 
+  // The second argument to the command can be:
+  //
+  //  * a path to deploy (as in: `vercel path/`)
+  //  * a subcommand (as in: `vercel ls`)
+  const targetOrSubcommand = argv._[2];
+
   output.print(
     `${chalk.grey(
       `${getTitleName()} CLI ${pkg.version}${
@@ -164,41 +168,24 @@ const main = async argv_ => {
     )}\n`
   );
 
-  // we want to handle version or help directly only
-  if (!targetOrSubcommand) {
-    if (argv['--version']) {
-      console.log(pkg.version);
-      return 0;
-    }
+  // Handle `--version` directly
+  if (!targetOrSubcommand && argv['--version']) {
+    console.log(pkg.version);
+    return 0;
   }
 
-  let nowDirExists;
-
+  // Ensure that the Vercel global configuration directory exists
   try {
-    nowDirExists = existsSync(VERCEL_DIR);
+    await mkdirp(VERCEL_DIR);
   } catch (err) {
     console.error(
       error(
-        `An unexpected error occurred while trying to find the global directory: ${err.message}`
+        `${
+          'An unexpected error occurred while trying to create the ' +
+          `global directory "${hp(VERCEL_DIR)}" `
+        }${err.message}`
       )
     );
-
-    return 1;
-  }
-
-  if (!nowDirExists) {
-    try {
-      await mkdirp(VERCEL_DIR);
-    } catch (err) {
-      console.error(
-        error(
-          `${
-            'An unexpected error occurred while trying to create the ' +
-            `global directory "${hp(VERCEL_DIR)}" `
-          }${err.message}`
-        )
-      );
-    }
   }
 
   let migrated = false;
@@ -344,18 +331,33 @@ const main = async argv_ => {
     );
   }
 
-  // the context object to supply to the providers or the commands
-  const ctx = {
+  if (typeof argv['--api'] === 'string') {
+    apiUrl = argv['--api'];
+  } else if (config && config.api) {
+    apiUrl = config.api;
+  }
+
+  try {
+    // eslint-disable-next-line no-new
+    new URL(apiUrl);
+  } catch (err) {
+    output.error(`Please provide a valid URL instead of ${highlight(apiUrl)}.`);
+    return 1;
+  }
+
+  // Shared API `Client` instance for all sub-commands to utilize
+  client = new Client({
+    apiUrl,
     output,
     config,
     authConfig,
     localConfig,
-    argv: argv_,
-  };
+    argv: process.argv,
+  });
 
   let subcommand;
 
-  // we check if we are deploying something
+  // Check if we are deploying something
   if (targetOrSubcommand) {
     const targetPath = join(process.cwd(), targetOrSubcommand);
     const targetPathExists = existsSync(targetPath);
@@ -398,8 +400,6 @@ const main = async argv_ => {
       subcommand = targetOrSubcommand;
     } else {
       debug('user supplied a possible target for deployment');
-      // our default command is deployment
-      // at this point we're
       subcommand = 'deploy';
     }
   } else {
@@ -409,55 +409,33 @@ const main = async argv_ => {
 
   if (subcommand === 'help') {
     subcommand = argv._[3] || 'deploy';
-    ctx.argv.push('-h');
+    client.argv.push('-h');
   }
 
-  if (argv['--api'] && typeof argv['--api'] === 'string') {
-    apiUrl = argv['--api'];
-  } else if (ctx.config && ctx.config.api) {
-    apiUrl = ctx.config.api;
-  }
-
-  ctx.apiUrl = apiUrl;
-
-  try {
-    // eslint-disable-next-line no-new
-    new URL(ctx.apiUrl);
-  } catch (err) {
-    console.error(
-      error(`Please provide a valid URL instead of ${highlight(ctx.apiUrl)}.`)
-    );
-    return 1;
-  }
-
-  // If no credentials are set at all, prompt for
-  // login to the .sh provider
+  // Prompt for login if there is no current token
   if (
     (!authConfig || !authConfig.token) &&
-    !ctx.argv.includes('-h') &&
-    !ctx.argv.includes('--help') &&
+    !client.argv.includes('-h') &&
+    !client.argv.includes('--help') &&
     !argv['--token'] &&
     !subcommandsWithoutToken.includes(subcommand)
   ) {
     if (isTTY) {
-      console.log(info(`No existing credentials found. Please log in:`));
+      output.log(info(`No existing credentials found. Please log in:`));
 
       subcommand = 'login';
-      ctx.argv[2] = 'login';
+      client.argv[2] = 'login';
 
       // Ensure that subcommands lead to login as well, if
       // no credentials are defined
-      ctx.argv = ctx.argv.splice(0, 3);
+      client.argv = client.argv.splice(0, 3);
     } else {
-      console.error(
-        error({
-          message:
-            'No existing credentials found. Please run ' +
-            `${getCommandName('login')} or pass ${param('--token')}`,
-          slug: 'no-credentials-found',
-        })
-      );
-
+      output.prettyError({
+        message:
+          'No existing credentials found. Please run ' +
+          `${getCommandName('login')} or pass ${param('--token')}`,
+        link: 'https://err.sh/vercel/no-credentials-found',
+      });
       return 1;
     }
   }
@@ -506,11 +484,11 @@ const main = async argv_ => {
       return 1;
     }
 
-    ctx.authConfig.token = token;
+    client.authConfig.token = token;
 
     // Don't use team from config if `--token` was set
-    if (ctx.config && ctx.config.currentTeam) {
-      delete ctx.config.currentTeam;
+    if (client.config && client.config.currentTeam) {
+      delete client.config.currentTeam;
     }
   }
 
@@ -524,7 +502,7 @@ const main = async argv_ => {
 
   const {
     authConfig: { token },
-  } = ctx;
+  } = client;
 
   let scope = argv['--scope'] || argv['--team'] || localConfig.scope;
 
@@ -537,7 +515,6 @@ const main = async argv_ => {
     !(targetCommand === 'teams' && argv._[3] !== 'invite')
   ) {
     let user = null;
-    const client = new Client({ apiUrl, token, output });
 
     try {
       user = await getUser(client);
@@ -558,7 +535,7 @@ const main = async argv_ => {
     }
 
     if (user.uid === scope || user.email === scope || user.username === scope) {
-      delete ctx.config.currentTeam;
+      delete client.config.currentTeam;
     } else {
       let list = [];
 
@@ -595,7 +572,7 @@ const main = async argv_ => {
         return 1;
       }
 
-      ctx.config.currentTeam = related.id;
+      client.config.currentTeam = related.id;
     }
   }
 
@@ -612,7 +589,7 @@ const main = async argv_ => {
   try {
     const start = new Date();
     const full = require(`./commands/${targetCommand}`).default;
-    exitCode = await full(ctx);
+    exitCode = await full(client);
     const end = new Date() - start;
 
     if (shouldCollectMetrics) {
@@ -666,7 +643,7 @@ const main = async argv_ => {
       output.debug(err.stack);
       output.prettyError(err);
     } else {
-      await reportError(Sentry, err, apiUrl, configFiles);
+      await reportError(Sentry, client, err);
 
       // Otherwise it is an unexpected error and we should show the trace
       // and an unexpected error message
@@ -693,7 +670,7 @@ const handleRejection = async err => {
       await handleUnexpected(err);
     } else {
       console.error(error(`An unexpected rejection occurred\n  ${err}`));
-      await reportError(Sentry, err, apiUrl, configFiles);
+      await reportError(Sentry, client, err);
     }
   } else {
     console.error(error('An unexpected empty rejection occurred'));
@@ -711,12 +688,9 @@ const handleUnexpected = async err => {
     return;
   }
 
-  await reportError(Sentry, err, apiUrl, configFiles);
-  debug('handling unexpected error');
+  await reportError(Sentry, client, err);
 
-  console.error(
-    error(`An unexpected error occurred!\n  ${err.stack} ${err.stack}`)
-  );
+  console.error(error(`An unexpected error occurred!\n${err.stack}`));
 
   process.exit(1);
 };
@@ -724,9 +698,7 @@ const handleUnexpected = async err => {
 process.on('unhandledRejection', handleRejection);
 process.on('uncaughtException', handleUnexpected);
 
-// Don't use `.then` here. We need to shutdown gracefully, otherwise
-// subcommands waiting for further data won't work (like `logs` and `logout`)!
-main(process.argv)
+main()
   .then(exitCode => {
     process.exitCode = exitCode;
     process.emit('nowExit');

packages/cli/src/commands/alias/index.js
@@ -70,11 +70,11 @@ const COMMAND_CONFIG = {
   set: ['set'],
 };
 
-export default async function main(ctx) {
+export default async function main(client) {
   let argv;
 
   try {
-    argv = getArgs(ctx.argv.slice(2), {
+    argv = getArgs(client.argv.slice(2), {
       '--json': Boolean,
       '--yes': Boolean,
       '--next': Number,
@@ -95,10 +95,10 @@ export default async function main(ctx) {
 
   switch (subcommand) {
     case 'ls':
-      return ls(ctx, argv, args);
+      return ls(client, argv, args);
     case 'rm':
-      return rm(ctx, argv, args);
+      return rm(client, argv, args);
     default:
-      return set(ctx, argv, args);
+      return set(client, argv, args);
   }
 }

packages/cli/src/commands/billing/index.js
@@ -12,7 +12,6 @@ import info from '../../util/output/info';
 import logo from '../../util/output/logo';
 import addBilling from './add';
 import exit from '../../util/exit';
-import Client from '../../util/client.ts';
 import getScope from '../../util/get-scope.ts';
 import { getPkgName } from '../../util/pkg-name.ts';
 
@@ -55,8 +54,8 @@ let debug;
 let apiUrl;
 let subcommand;
 
-export default async ctx => {
-  argv = mri(ctx.argv.slice(2), {
+export default async client => {
+  argv = mri(client.argv.slice(2), {
     boolean: ['help', 'debug'],
     alias: {
       help: 'h',
@@ -67,7 +66,7 @@ export default async ctx => {
   argv._ = argv._.slice(1);
 
   debug = argv.debug;
-  apiUrl = ctx.apiUrl;
+  apiUrl = client.apiUrl;
   subcommand = argv._[0];
 
   if (argv.help || !subcommand) {
@@ -76,37 +75,11 @@ export default async ctx => {
   }
 
   const {
+    output,
     authConfig: { token },
-    config,
-  } = ctx;
+    config: { currentTeam },
+  } = client;
 
-  return run({ token, config });
-};
-
-// Builds a `choices` object that can be passesd to inquirer.prompt()
-function buildInquirerChoices(cards) {
-  return cards.sources.map(source => {
-    const _default =
-      source.id === cards.defaultSource ? ` ${chalk.bold('(default)')}` : '';
-    const id = `${chalk.cyan(`ID: ${source.id}`)}${_default}`;
-    const number = `${chalk.gray('#### ').repeat(3)}${
-      source.last4 || source.card.last4
-    }`;
-    const str = [
-      id,
-      indent(source.name || source.owner.name, 2),
-      indent(`${source.brand || source.card.brand} ${number}`, 2),
-    ].join('\n');
-
-    return {
-      name: str, // Will be displayed by Inquirer
-      value: source.id, // Will be used to identify the answer
-      short: source.id, // Will be displayed after the users answers
-    };
-  });
-}
-
-async function run({ token, output, config: { currentTeam } }) {
   const start = new Date();
   const creditCards = new NowCreditCards({
     apiUrl,
@@ -115,7 +88,7 @@ async function run({ token, output, config: { currentTeam } }) {
     currentTeam,
     output,
   });
-  const client = new Client({ apiUrl, token, currentTeam, debug, output });
+
   let contextName = null;
 
   try {
@@ -361,4 +334,27 @@ async function run({ token, output, config: { currentTeam } }) {
 
   // This is required, otherwise we get those weird zlib errors
   return exit(0);
+};
+
+// Builds a `choices` object that can be passesd to inquirer.prompt()
+function buildInquirerChoices(cards) {
+  return cards.sources.map(source => {
+    const _default =
+      source.id === cards.defaultSource ? ` ${chalk.bold('(default)')}` : '';
+    const id = `${chalk.cyan(`ID: ${source.id}`)}${_default}`;
+    const number = `${chalk.gray('#### ').repeat(3)}${
+      source.last4 || source.card.last4
+    }`;
+    const str = [
+      id,
+      indent(source.name || source.owner.name, 2),
+      indent(`${source.brand || source.card.brand} ${number}`, 2),
+    ].join('\n');
+
+    return {
+      name: str, // Will be displayed by Inquirer
+      value: source.id, // Will be used to identify the answer
+      short: source.id, // Will be displayed after the users answers
+    };
+  });
 }

packages/cli/src/commands/deploy/index.js
@@ -1,7 +1,6 @@
 import fs from 'fs-extra';
 import { resolve, basename } from 'path';
 import { fileNameSymbol } from '@vercel/client';
-import Client from '../../util/client.ts';
 import getScope from '../../util/get-scope.ts';
 import code from '../../util/output/code';
 import highlight from '../../util/output/highlight';
@@ -11,19 +10,17 @@ import { handleError } from '../../util/error';
 import { help, args } from './args';
 import deploy from './latest';
 
-export default async ctx => {
+export default async client => {
   const {
-    authConfig,
     output,
     config: { currentTeam },
-    apiUrl,
-  } = ctx;
+  } = client;
 
   let contextName = currentTeam || 'current user';
   let argv = null;
 
   try {
-    argv = getArgs(ctx.argv.slice(2), args);
+    argv = getArgs(client.argv.slice(2), args);
   } catch (error) {
     handleError(error);
     return 1;
@@ -43,11 +40,10 @@ export default async ctx => {
     paths = [process.cwd()];
   }
 
-  let { localConfig } = ctx;
+  let { localConfig } = client;
   if (!localConfig || localConfig instanceof Error) {
     localConfig = readLocalConfig(paths[0]);
   }
-  const debugEnabled = argv['--debug'];
   const stats = {};
 
   if (argv['--help']) {
@@ -66,26 +62,15 @@ export default async ctx => {
     }
   }
 
-  let client = null;
-
-  if (authConfig && authConfig.token) {
-    client = new Client({
-      apiUrl,
-      token: authConfig.token,
-      currentTeam,
-      output,
-      debug: debugEnabled,
-    });
-    try {
-      ({ contextName } = await getScope(client));
-    } catch (err) {
-      if (err.code === 'NOT_AUTHORIZED' || err.code === 'TEAM_DELETED') {
-        output.error(err.message);
-        return 1;
-      }
-
-      throw err;
+  try {
+    ({ contextName } = await getScope(client));
+  } catch (err) {
+    if (err.code === 'NOT_AUTHORIZED' || err.code === 'TEAM_DELETED') {
+      output.error(err.message);
+      return 1;
     }
+
+    throw err;
   }
 
   if (localConfig) {
@@ -112,5 +97,5 @@ export default async ctx => {
     }
   }
 
-  return deploy(ctx, contextName, output, stats, localConfig, args);
+  return deploy(client, contextName, output, stats, localConfig, args);
 };

packages/cli/src/util/index.js
@@ -20,7 +20,7 @@ export default class Now extends EventEmitter {
   constructor({
     apiUrl,
     token,
-    currentTeam,
+    currentTeam = null,
     forceNew = false,
     withCache = false,
     debug = false,
@@ -88,7 +88,6 @@ export default class Now extends EventEmitter {
     const deployment = await processDeployment({
       now: this,
       output: this._output,
-      hashes,
       paths,
       requestBody,
       uploadStamp,
@@ -444,7 +443,7 @@ export default class Now extends EventEmitter {
 
     opts.headers = opts.headers || {};
     opts.headers.accept = 'application/json';
-    opts.headers.Authorization = `Bearer ${this._token}`;
+    opts.headers.authorization = `Bearer ${this._token}`;
     opts.headers['user-agent'] = ua;
 
     if (
@@ -453,7 +452,7 @@ export default class Now extends EventEmitter {
       opts.body.constructor === Object
     ) {
       opts.body = JSON.stringify(opts.body);
-      opts.headers['Content-Type'] = 'application/json';
+      opts.headers['content-type'] = 'application/json; charset=utf-8';
     }
     const res = await this._output.time(
       `${opts.method || 'GET'} ${this._apiUrl}${_url} ${opts.body || ''}`,
@@ -463,7 +462,7 @@ export default class Now extends EventEmitter {
     return res;
   }
 
-  // public retry with built-in retrying that can be
+  // public fetch with built-in retrying that can be
   // used from external utilities. it optioanlly
   // receives a `retry` object in the opts that is
   // passed to the retry utility

commit 40aaa0952fd0415f3317e79220db46d6b72b4838

[cli] Fix error usage before defined (#6099)
This PR fixes a couple regressions from #6016:

- Using `error` before its imported. [See index.js](https://github.com/vercel/vercel/pull/6016/files#diff-032eb615f83c504bbb0ec2dfae005b7cea1748c905270acfffe6be0ee3fef7acL3)
- Passing the incorrect params to `add()`. [See add.js](https://github.com/vercel/vercel/pull/6016/files#diff-ee72a76cd216b786bc71a05d76a1b3c760a73248e5b8363ec2f83dd1d46b63d7R39)

We should eventually move these files to TS to catch these types of bugs.

packages/cli/src/commands/teams.js
@@ -116,7 +116,7 @@ const main = async client => {
     }
     case 'add':
     case 'create': {
-      exitCode = await add(client, argv, teams);
+      exitCode = await add(client, teams);
       break;
     }
 

packages/cli/src/index.js
@@ -6,7 +6,7 @@ try {
   process.cwd();
 } catch (e) {
   if (e && e.message && e.message.includes('uv_cwd')) {
-    console.error(error('The current working directory does not exist.'));
+    console.error('Error! The current working directory does not exist.');
     process.exit(1);
   }
 }