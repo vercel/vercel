2a791cbab5ebf5fedf507f4e99f329082f800b35
Nathan Rajlich <n@n8.io>
2021-05-05 10:55:12 -0700

    Simplify

--- packages/cli/src/util/login/sso.ts
+++ packages/cli/src/util/login/sso.ts
@@ -5,13 +5,11 @@ import { LoginParams } from './types';
 import doOauthLogin from './oauth';
 
 export default function doSsoLogin(teamIdOrSlug: string, params: LoginParams) {
-  const { apiUrl } = params;
-
   const hyphens = new RegExp('-', 'g');
   const host = hostname().replace(hyphens, ' ').replace('.local', '');
   const tokenName = `${getTitleName()} CLI on ${host}`;
 
-  const url = new URL('/auth/sso', apiUrl);
+  const url = new URL('/auth/sso', params.apiUrl);
   url.searchParams.append('teamId', teamIdOrSlug);
   url.searchParams.append('tokenName', tokenName);
 
