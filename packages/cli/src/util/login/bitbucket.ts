27c0c2c3230ddc0eab2708c9cbc26c39a71efdd0
Nathan Rajlich <n@n8.io>
2021-05-05 12:13:48 -0700

    Add comment

--- packages/cli/src/util/login/bitbucket.ts
+++ packages/cli/src/util/login/bitbucket.ts
@@ -5,6 +5,8 @@ import doOauthLogin from './oauth';
 export default function doBitbucketLogin(params: LoginParams) {
   const url = new URL(
     '/api/registration/bitbucket/connect',
+    // Can't use `apiUrl` here because this URL sets a
+    // cookie that the OAuth callback URL depends on
     'https://vercel.com'
   );
   return doOauthLogin(url, 'Bitbucket', params);
--- packages/cli/src/util/login/github.ts
+++ packages/cli/src/util/login/github.ts
@@ -5,6 +5,8 @@ import doOauthLogin from './oauth';
 export default function doGithubLogin(params: LoginParams) {
   const url = new URL(
     '/api/registration/login-with-github',
+    // Can't use `apiUrl` here because this URL sets a
+    // cookie that the OAuth callback URL depends on
     'https://vercel.com'
   );
   return doOauthLogin(url, 'GitHub', params);
