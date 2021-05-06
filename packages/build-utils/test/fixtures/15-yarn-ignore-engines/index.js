9ce0156917b49686c2b7c614fa55fc16a3739660
Nathan Rajlich <n@n8.io>
2021-05-06 13:01:14 -0700

    [build-utils] Remove `15-yarn-ignore-engines` test fixture
    It's failing during runtime, like here: https://github.com/vercel/vercel/runs/2520732314
    
    With an error like so:
    
    ```
    [GET] /
    2021-05-06T20:02:55.411Z	undefined	ERROR	The gRPC binary module was not installed. This may be fixed by running "npm rebuild"
    Original error: Cannot find module '/var/task/node_modules/grpc/src/node/extension_binary/node-v83-linux-x64-glibc/grpc_node.node'
    Require stack:
    - /var/task/node_modules/grpc/src/grpc_extension.js
    - /var/task/node_modules/grpc/src/client_interceptors.js
    - /var/task/node_modules/grpc/src/client.js
    - /var/task/node_modules/grpc/index.js
    - /var/task/node_modules/google-gax/build/src/grpc.js
    - /var/task/node_modules/google-gax/build/src/index.js
    - /var/task/node_modules/@google-cloud/scheduler/src/v1beta1/cloud_scheduler_client.js
    - /var/task/node_modules/@google-cloud/scheduler/src/v1beta1/index.js
    - /var/task/node_modules/@google-cloud/scheduler/src/index.js
    - /var/task/index.js
    - /var/task/___vc_launcher.js
    - /var/runtime/UserFunction.js
    - /var/runtime/index.js
    2021-05-06T20:02:55.411Z	undefined	ERROR	Did you forget to add it to "dependencies" in `package.json`?
    RequestId: 2cdba37e-4bf4-4a2f-b443-4ebcc99fe308 Error: Runtime exited with error: exit status 1
    Runtime.ExitError
    ```
    
    Because grpc is failing with NFT. But also, the test is no longer
    necessary because we no longer support Node 8.

--- packages/build-utils/test/fixtures/15-yarn-ignore-engines/index.js
+++
@@ -1,9 +0,0 @@
-const scheduler = require('@google-cloud/scheduler');
-
-module.exports = (_, res) => {
-  if (scheduler) {
-    res.end('found:RANDOMNESS_PLACEHOLDER');
-  } else {
-    res.end('nope:RANDOMNESS_PLACEHOLDER');
-  }
-};
