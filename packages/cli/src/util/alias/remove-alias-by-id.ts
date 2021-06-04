8608105aac61000119f7355d54491b63e816d198
Markoz Pena <markdrew53@gmail.com>
2021-06-04 03:25:18 -0500

    refactor: Remove unnecessary code

--- packages/cli/src/util/alias/remove-alias-by-id.ts
+++ packages/cli/src/util/alias/remove-alias-by-id.ts
@@ -1,7 +1,11 @@
+import { Alias } from '../../types';
 import Client from '../client';
 
-export default async function removeAliasById(client: Client, id: string) {
+export default async function removeAliasById(
+  client: Client,
+  id: string | Alias
+) {
   return client.fetch(`/now/aliases/${id}`, {
-    method: 'DELETE'
+    method: 'DELETE',
   });
 }
