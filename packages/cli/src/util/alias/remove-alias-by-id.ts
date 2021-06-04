8b57f4e9fa1923c68e4ba25ce82c3bb61015295a
Markoz Pena <markdrew53@gmail.com>
2021-06-04 10:55:32 -0500

    refactor: Remove `Alias` type from of the parameter `id`

--- packages/cli/src/util/alias/remove-alias-by-id.ts
+++ packages/cli/src/util/alias/remove-alias-by-id.ts
@@ -1,10 +1,6 @@
-import { Alias } from '../../types';
 import Client from '../client';
 
-export default async function removeAliasById(
-  client: Client,
-  id: string | Alias
-) {
+export default async function removeAliasById(client: Client, id: string) {
   return client.fetch(`/now/aliases/${id}`, {
     method: 'DELETE',
   });
