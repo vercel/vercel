d98789cd9cae5621a0ea3bd59b0e17fe8cc7e61c
Markoz Pena <markdrew53@gmail.com>
2021-06-22 15:05:06 -0500

    feat: Remove `null` optional

--- packages/cli/src/util/client.ts
+++ packages/cli/src/util/client.ts
@@ -47,7 +47,7 @@ export default class Client extends EventEmitter {
   authConfig: AuthConfig;
   output: Output;
   config: GlobalConfig;
-  localConfig: VercelConfig | null;
+  localConfig: VercelConfig;
 
   constructor(opts: ClientOptions) {
     super();
