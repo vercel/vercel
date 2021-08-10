vercel/vercel

commit 2756d1e323eec35ae053207beaba86d0452d2918

[client][frameworks][api] Update `readdir()` call with `withFileTypes` (#6554)
Avoids running unnecessary code such as `stat().isDirectory()`.

api/_lib/examples/summary.ts
@@ -2,7 +2,6 @@
  * Get example list from extracted folder
  */
 
-import { join } from 'path';
 import { lstatSync, existsSync, readdirSync } from 'fs';
 
 const exists = (path: string) => existsSync(path);
@@ -14,7 +13,7 @@ export function summary(source: string) {
     return [];
   }
 
-  return readdirSync(source)
-    .filter(name => !isDotFile(name))
-    .filter(name => isDirectory(join(source, name)));
+  return readdirSync(source, { withFileTypes: true })
+    .filter(d => !isDotFile(d.name))
+    .filter(d => d.isDirectory());
 }

packages/client/src/create-deployment.ts
@@ -1,4 +1,4 @@
-import { readdir as readRootFolder, lstatSync } from 'fs-extra';
+import { lstatSync } from 'fs-extra';
 
 import { relative, isAbsolute } from 'path';
 import hashes, { mapToObject } from './utils/hashes';
@@ -50,8 +50,6 @@ export default function buildCreateDeployment() {
     clientOptions.isDirectory =
       !Array.isArray(path) && lstatSync(path).isDirectory();
 
-    let rootFiles: string[];
-
     if (Array.isArray(path)) {
       for (const filePath of path) {
         if (!isAbsolute(filePath)) {
@@ -69,15 +67,11 @@ export default function buildCreateDeployment() {
     }
 
     if (clientOptions.isDirectory && !Array.isArray(path)) {
-      debug(`Provided 'path' is a directory. Reading subpaths... `);
-      rootFiles = await readRootFolder(path);
-      debug(`Read ${rootFiles.length} subpaths`);
+      debug(`Provided 'path' is a directory.`);
     } else if (Array.isArray(path)) {
       debug(`Provided 'path' is an array of file paths`);
-      rootFiles = path;
     } else {
       debug(`Provided 'path' is a single file`);
-      rootFiles = [path];
     }
 
     let { fileList } = await buildFileTree(

packages/frameworks/src/frameworks.ts
@@ -6,9 +6,7 @@ import { readConfigFile } from './read-config-file';
 
 export * from './types';
 
-const { readdir, stat, readFile, unlink } = promises;
-const isDir = async (file: string): Promise<boolean> =>
-  (await stat(file)).isDirectory();
+const { readdir, readFile, unlink } = promises;
 
 /**
  * Please note that is extremely important that the `dependency` property needs
@@ -313,11 +311,11 @@ export const frameworks = [
       const base = 'build';
       try {
         const location = join(dirPrefix, base);
-        const content = await readdir(location);
+        const content = await readdir(location, { withFileTypes: true });
 
         // If there is only one file in it that is a dir we'll use it as dist dir
-        if (content.length === 1 && (await isDir(join(location, content[0])))) {
-          return join(base, content[0]);
+        if (content.length === 1 && content[0].isDirectory()) {
+          return join(base, content[0].name);
         }
       } catch (error) {
         console.error(`Error detecting output directory: `, error);
@@ -402,11 +400,11 @@ export const frameworks = [
       const base = 'build';
       try {
         const location = join(dirPrefix, base);
-        const content = await readdir(location);
+        const content = await readdir(location, { withFileTypes: true });
 
         // If there is only one file in it that is a dir we'll use it as dist dir
-        if (content.length === 1 && (await isDir(join(location, content[0])))) {
-          return join(base, content[0]);
+        if (content.length === 1 && content[0].isDirectory()) {
+          return join(base, content[0].name);
         }
       } catch (error) {
         console.error(`Error detecting output directory: `, error);
@@ -744,11 +742,11 @@ export const frameworks = [
       const base = 'dist';
       try {
         const location = join(dirPrefix, base);
-        const content = await readdir(location);
+        const content = await readdir(location, { withFileTypes: true });
 
         // If there is only one file in it that is a dir we'll use it as dist dir
-        if (content.length === 1 && (await isDir(join(location, content[0])))) {
-          return join(base, content[0]);
+        if (content.length === 1 && content[0].isDirectory()) {
+          return join(base, content[0].name);
         }
       } catch (error) {
         console.error(`Error detecting output directory: `, error);
@@ -830,8 +828,7 @@ export const frameworks = [
     logo: 'https://raw.githubusercontent.com/vercel/vercel/main/packages/frameworks/logos/svelte.svg',
     tagline:
       'Svelte lets you write high performance reactive apps with significantly less boilerplate.',
-    description:
-      'A basic Svelte app using the default template.',
+    description: 'A basic Svelte app using the default template.',
     website: 'https://svelte.dev',
     detectors: {
       every: [
@@ -882,8 +879,7 @@ export const frameworks = [
     logo: 'https://raw.githubusercontent.com/vercel/vercel/main/packages/frameworks/logos/svelte.svg',
     tagline:
       'SvelteKit is a framework for building web applications of all sizes.',
-    description:
-      'A SvelteKit app optimized to work for serverless.',
+    description: 'A SvelteKit app optimized to work for serverless.',
     website: 'https://kit.svelte.dev',
     detectors: {
       every: [

commit 2bbe20bb3ebb87de0dd7ad8cc2e0e84e85fb3e72

[api] Fix `GET /api/examples/list` output
A regression from #6554 caused the return value to contain a nested
object with a `name` property for the `name` key of the response in the
list.

api/_lib/examples/summary.ts
@@ -15,5 +15,6 @@ export function summary(source: string) {
 
   return readdirSync(source, { withFileTypes: true })
     .filter(d => !isDotFile(d.name))
-    .filter(d => d.isDirectory());
+    .filter(d => d.isDirectory())
+    .map(d => d.name);
 }