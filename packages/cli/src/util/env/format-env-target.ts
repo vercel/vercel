1a1f4871fdbb48470179df628ee9fad1f722775f
Nathan Rajlich <n@n8.io>
2021-08-13 01:06:54 -0700

    Fix build

--- packages/cli/src/util/env/format-env-target.ts
+++ packages/cli/src/util/env/format-env-target.ts
@@ -4,7 +4,7 @@ import { ProjectEnvVariable } from '../../types';
 
 export default function formatEnvTarget(env: ProjectEnvVariable): string {
   const target = (Array.isArray(env.target) ? env.target : [env.target || ''])
-    .map(title)
+    .map(t => title(t))
     .join(', ');
 
   return env.gitBranch ? `${target} (${env.gitBranch})` : target;
