81ea0082f189dfe0414c0aefefacd4027b60289a
Steven <steven@ceriously.com>
2021-11-19 13:42:17 -0500

    [python] Upgrade python 3.9 (#7008)
    * [python] Upgrade python 3.9
    
    * Fix python path
    
    * Improve testing
    
    * Add support for version selection via Pipfile
    
    * Parse the "Pipfile.lock" instead
    
    * Fix support for Node.js 12

--- test/lib/deployment/now-deploy.js
+++ test/lib/deployment/now-deploy.js
@@ -20,7 +20,12 @@ async function nowDeploy(bodies, randomness, uploadNowJson) {
         (path.extname(n) === '.sh' ? 0o100755 : 0o100644),
     }));
 
-  const { FORCE_BUILD_IN_REGION, NOW_DEBUG, VERCEL_DEBUG } = process.env;
+  const {
+    FORCE_BUILD_IN_REGION,
+    NOW_DEBUG,
+    VERCEL_DEBUG,
+    VERCEL_BUILDER_DEBUG,
+  } = process.env;
   const nowJson = JSON.parse(bodies['vercel.json'] || bodies['now.json']);
 
   const nowDeployPayload = {
@@ -34,6 +39,7 @@ async function nowDeploy(bodies, randomness, uploadNowJson) {
         FORCE_BUILD_IN_REGION,
         NOW_DEBUG,
         VERCEL_DEBUG,
+        VERCEL_BUILDER_DEBUG,
       },
     },
     name: 'test2020',
