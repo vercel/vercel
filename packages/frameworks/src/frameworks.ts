db468c489a9310692dd09bd1268169ee85462a75
Sam Ko <samko9522@gmail.com>
2021-11-19 17:44:22 -0800

    [frameworks] Fix Parcel default output directory to dist (#7029)
    ### Related Issues
    
    The Parcel template via the Project Creating Flow returns a build error because the default output directory for the Parcel **Framework Preset** is searching for `public` instead of `dist`. The default behavior of Parcel's CLI I believe is `dist`.
    
    ### 📋 Checklist
    
    <!--
      Please keep your PR as a Draft until the checklist is complete
    -->
    
    #### Tests
    
    - [ ] The code changed/added as part of this PR has been covered with tests
    - [ ] All tests pass locally with `yarn test-unit`
    
    #### Code Review
    
    - [ ] This PR has a concise title and thorough description useful to a reviewer
    - [ ] Issue from task tracker has a link to this PR

--- packages/frameworks/src/frameworks.ts
+++ packages/frameworks/src/frameworks.ts
@@ -1946,7 +1946,7 @@ export const frameworks = [
         value: 'parcel',
       },
       outputDirectory: {
-        placeholder: 'dist',
+        value: 'dist',
       },
     },
     dependency: 'parcel',
