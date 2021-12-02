ffb001bdc86017eeca1dc1f56c90cd26fb7d4a17
Leo Lamprecht <leo@vercel.com>
2021-12-02 14:04:53 +0100

    Made CLI Plugin publishing work

--- packages/plugin-ruby/src/index.ts
+++ packages/plugin-ruby/src/index.ts
@@ -1,7 +1,6 @@
 import { convertRuntimeToPlugin } from '@vercel/build-utils';
 import * as ruby from '@vercel/ruby';
-import { name } from '../package.json';
 
-export const build = convertRuntimeToPlugin(ruby.build, name, '.rb');
+export const build = convertRuntimeToPlugin(ruby.build, 'vercel-plugin-ruby', '.rb');
 
 //export const startDevServer = ruby.startDevServer;
