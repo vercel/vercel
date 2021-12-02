ae3b25be4be55b3ea150e933096c96e13eb74af9
Leo Lamprecht <leo@vercel.com>
2021-12-02 14:10:23 +0100

    Made CLI Plugin publishing work (#7113)

--- packages/plugin-python/src/index.ts
+++ packages/plugin-python/src/index.ts
@@ -1,7 +1,6 @@
 import { convertRuntimeToPlugin } from '@vercel/build-utils';
 import * as python from '@vercel/python';
-import { name } from '../package.json';
 
-export const build = convertRuntimeToPlugin(python.build, name, '.py');
+export const build = convertRuntimeToPlugin(python.build, 'vercel-plugin-python', '.py');
 
 //export const startDevServer = python.startDevServer;
