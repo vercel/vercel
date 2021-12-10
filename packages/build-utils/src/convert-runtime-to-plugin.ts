a41b557cf983d070aa71e26e91f9b19a94ae9383
Leo Lamprecht <leo@vercel.com>
2021-12-10 16:49:23 +0100

    Cleaned up the code

--- packages/build-utils/src/convert-runtime-to-plugin.ts
+++ packages/build-utils/src/convert-runtime-to-plugin.ts
@@ -124,9 +124,6 @@ export function convertRuntimeToPlugin(
       // file system, so that they can be passed on as `FileFsRef` instead.
       const lambdaFileBlobs: Array<Promise<void>> = [];
 
-      // The path of the directory that contains the entrypoint file
-      const entryDir = dirname(entrypoint);
-
       for (const file in lambdaFiles) {
         // When deploying, the `files` that are passed to the Legacy Runtimes already
         // have certain files that are ignored stripped, but locally, that list of
@@ -188,7 +185,7 @@ export function convertRuntimeToPlugin(
       const handlerExtName = extname(handlerFile.fsPath);
 
       const entryBase = basename(entrypoint).replace(ext, handlerExtName);
-      const entryPath = join(entryDir, entryBase);
+      const entryPath = join(dirname(entrypoint), entryBase);
       const entry = join(entryRoot, entryPath);
 
       // Create the parent directory of the API Route that will be created
