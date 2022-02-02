21b726a8d5250c91e25c9cc389c92382e9e0dbec
Sean Massa <endangeredmassa@gmail.com>
2022-02-02 16:15:18 -0600

    remove advanced syntax

--- utils/changelog/group.js
+++ utils/changelog/group.js
@@ -3,7 +3,7 @@ function groupLog(commits) {
 
   for (let commit of commits) {
     for (let area of commit.areas) {
-      grouped[area] ||= [];
+      grouped[area] = grouped[area] || [];
       grouped[area].push(commit.subject);
     }
   }
