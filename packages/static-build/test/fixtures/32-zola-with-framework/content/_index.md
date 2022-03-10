4ee064bb6103532244a675b4dc6f50d898f971b2
Sean Massa <EndangeredMassa@gmail.com>
2022-03-10 13:21:24 -0600

    [static-build] Add `@vercel/static-build` Builder (#7536)

---
+++ packages/static-build/test/fixtures/32-zola-with-framework/content/_index.md
@@ -0,0 +1,4 @@
++++
+# Whether to sort pages by "date", "weight", or "none". More on that below
+sort_by = "weight"
++++
---
+++ packages/static-build/test/fixtures/49-scully/src/app/blog/blog.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BlogComponent } from './Blog.component';
+
+describe('BlogComponent', () => {
+  let component: BlogComponent;
+  let fixture: ComponentFixture<BlogComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [BlogComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BlogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
