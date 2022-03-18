b4200064010868ec10fd99af2425e7307f0fe79c
Nathan Rajlich <n@n8.io>
2022-03-17 17:27:26 -0700

    [build-utils] Remove `AnalyzeOptions` and `analyze()` docs (#7550)
    The `analyze()` function on a Builder was removed a long time ago.

--- packages/build-utils/src/types.ts
+++ packages/build-utils/src/types.ts
@@ -65,35 +65,6 @@ export interface Meta {
   avoidTopLevelInstall?: boolean;
 }
 
-export interface AnalyzeOptions {
-  /**
-   * All source files of the project
-   */
-  files: {
-    [filePath: string]: FileRef;
-  };
-
-  /**
-   * Name of entrypoint file for this particular build job. Value
-   * `files[entrypoint]` is guaranteed to exist and be a valid File reference.
-   * `entrypoint` is always a discrete file and never a glob, since globs are
-   * expanded into separate builds at deployment time.
-   */
-  entrypoint: string;
-
-  /**
-   * A writable temporary directory where you are encouraged to perform your
-   * build process. This directory will be populated with the restored cache.
-   */
-  workPath: string;
-
-  /**
-   * An arbitrary object passed by the user in the build definition defined
-   * in `vercel.json`.
-   */
-  config: Config;
-}
-
 export interface BuildOptions {
   /**
    * All source files of the project
