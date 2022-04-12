9012e41fd0dab89d5d16943b7781bfe32fce633e
Sean Massa <endangeredmassa@gmail.com>
2022-04-12 15:35:21 -0500

    clear out other tests

--- packages/cli/test/commands/pull.test.ts
+++ packages/cli/test/commands/pull.test.ts
@@ -8,7 +8,7 @@ import { useTeams } from '../mocks/team';
 import { useUser } from '../mocks/user';
 
 describe('pull', () => {
-  it.only('should handle pulling', async () => {
+  it('should handle pulling', async () => {
     const cwd = setupFixture('vercel-pull-next');
     useUser();
     useTeams();
@@ -26,53 +26,5 @@ describe('pull', () => {
     );
     const devFileHasDevEnv = rawDevEnv.toString().includes('SPECIAL_FLAG');
     expect(devFileHasDevEnv).toBeTruthy();
-  });
-
-  it('should handle --environment=preview flag', async () => {
-    const cwd = setupFixture('vercel-pull-next');
-    useUser();
-    useTeams();
-    useProject({
-      ...defaultProject,
-      id: 'vercel-pull-next',
-      name: 'vercel-pull-next',
-    });
-    client.setArgv('pull', '--yes', '--environment=preview', cwd);
-    const exitCode = await pull(client);
-    expect(exitCode, client.outputBuffer).toEqual(0);
-
-    const rawPreviewEnv = await fs.readFile(
-      path.join(cwd, '.vercel', '.env.preview.local')
-    );
-    const previewFileHasPreviewEnv = rawPreviewEnv
-      .toString()
-      .includes('REDIS_CONNECTION_STRING');
-    expect(previewFileHasPreviewEnv).toBeTruthy();
-  });
-
-  it('should handle --environment=production flag', async () => {
-    const cwd = setupFixture('vercel-pull-next');
-    useUser();
-    useTeams();
-    useProject({
-      ...defaultProject,
-      id: 'vercel-pull-next',
-      name: 'vercel-pull-next',
-    });
-    client.setArgv('pull', '--yes', '--environment=production', cwd);
-    const exitCode = await pull(client);
-    expect(exitCode, client.outputBuffer).toEqual(0);
-
-    const rawProdEnv = await fs.readFile(
-      path.join(cwd, '.vercel', '.env.production.local')
-    );
-    const productionFileHasProductionEnv1 = rawProdEnv
-      .toString()
-      .includes('REDIS_CONNECTION_STRING');
-    expect(productionFileHasProductionEnv1).toBeTruthy();
-    const productionFileHasProductionEnv2 = rawProdEnv
-      .toString()
-      .includes('SQL_CONNECTION_STRING');
-    expect(productionFileHasProductionEnv2).toBeTruthy();
   });
 });
