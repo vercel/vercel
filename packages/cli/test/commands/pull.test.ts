3fb19faed6ec4f1de58825d2af5a9fa5ecad1c57
Nathan Rajlich <n@n8.io>
2022-04-19 17:59:28 -0700

    Better test

--- packages/cli/test/commands/pull.test.ts
+++ packages/cli/test/commands/pull.test.ts
@@ -28,7 +28,7 @@ describe('pull', () => {
     expect(devFileHasDevEnv).toBeTruthy();
   });
 
-  it('should handle pulling with env vars', async () => {
+  it('should handle pulling with env vars (headless mode)', async () => {
     try {
       process.env.VERCEL_PROJECT_ID = 'vercel-pull-next';
       process.env.VERCEL_ORG_ID = 'team_dummy';
@@ -46,15 +46,18 @@ describe('pull', () => {
         id: 'vercel-pull-next',
         name: 'vercel-pull-next',
       });
-      client.setArgv('pull', '--yes', cwd);
+      client.setArgv('pull', cwd);
       const exitCode = await pull(client);
       expect(exitCode, client.outputBuffer).toEqual(0);
 
-      const rawDevEnv = await fs.readFile(
-        path.join(cwd, '.vercel', '.env.development.local')
-      );
-      const devFileHasDevEnv = rawDevEnv.toString().includes('SPECIAL_FLAG');
-      expect(devFileHasDevEnv).toBeTruthy();
+      const config = await fs.readJSON(path.join(cwd, '.vercel/project.json'));
+      expect(config).toMatchInlineSnapshot(`
+        Object {
+          "orgId": "team_dummy",
+          "projectId": "vercel-pull-next",
+          "settings": Object {},
+        }
+      `);
     } finally {
       delete process.env.VERCEL_PROJECT_ID;
       delete process.env.VERCEL_ORG_ID;
