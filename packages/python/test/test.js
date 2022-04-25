0473430350d4c75a0abfd1ff83307ddb93d94795
Steven <steven@ceriously.com>
2022-04-25 09:04:48 -0400

    Use verbose requirements.txt

--- packages/python/test/test.js
+++ packages/python/test/test.js
@@ -25,34 +25,6 @@ const fixturesPath = path.resolve(__dirname, 'fixtures');
 it('should match the probes against Python dev servers', async () => {
   const fixture = path.join(fixturesPath, '00-request-path');
 
-  //const builder = require('../dist');
-  //await builder.build({ workPath: fixture, entrypoint: 'api/python.py', files: {} });
-
-  const requirements = `aiofiles==0.8.0; python_version >= "3.6" and python_version < "4.0"
-  certifi==2021.10.8
-  click==8.1.2; python_version >= "3.7"
-  flask==2.1.1
-  h11==0.9.0
-  httpcore==0.11.1; python_version >= "3.6"
-  httptools==0.4.0; python_version >= "3.5"
-  httpx==0.15.4; python_version >= "3.6"
-  idna==3.3
-  importlib-metadata==4.11.3; python_version < "3.10"
-  itsdangerous==2.1.2; python_version >= "3.7"
-  jinja2==3.1.1; python_version >= "3.7"
-  markupsafe==2.1.1; python_version >= "3.7"
-  multidict==5.2.0; python_version >= "3.6"
-  rfc3986[idna2008]==1.5.0
-  sanic==20.12.6
-  sniffio==1.2.0; python_version >= "3.5"
-  ujson==5.2.0; sys_platform != "win32" and implementation_name == "cpython"
-  uvloop==0.16.0; sys_platform != "win32" and implementation_name == "cpython"
-  websockets==9.1; python_full_version >= "3.6.1"
-  werkzeug==2.1.1; python_version >= "3.7"
-  zipp==3.8.0; python_version >= "3.7"`;
-
-  fs.writeFileSync(path.join(fixture, 'requirements.txt'), requirements);
-
   await execa(
     'pip3',
     ['install', '--user', '--upgrade', 'setuptools', 'wheel'],
@@ -65,8 +37,6 @@ it('should match the probes against Python dev servers', async () => {
     cwd: fixture,
     stdio: 'inherit',
   });
-
-  fs.unlinkSync(path.join(fixture, 'requirements.txt'));
 
   const ports = new Map();
   ports.set('/api/python.py', 8001);
