bfcb99327707191b7c73a42df787d206a605060a
agadzik <andrew.gadzik@vercel.com>
2022-05-03 15:18:10 -0400

    BREAKING CHANGE: Adding readdir and chdir functions to DetectorFilesystem

--- packages/build-utils/src/detectors/filesystem.ts
+++ packages/build-utils/src/detectors/filesystem.ts
@@ -1,3 +1,8 @@
+export interface Stat {
+  name: string;
+  path: string;
+  type: 'file' | 'dir';
+}
 /**
  * `DetectorFilesystem` is an abstract class that represents a virtual filesystem
  * to perform read-only operations on in order to detect which framework is being
@@ -27,15 +32,19 @@ export abstract class DetectorFilesystem {
   protected abstract _hasPath(name: string): Promise<boolean>;
   protected abstract _readFile(name: string): Promise<Buffer>;
   protected abstract _isFile(name: string): Promise<boolean>;
+  protected abstract _readdir(name: string): Promise<Stat[]>;
+  protected abstract _chdir(name: string): DetectorFilesystem;
 
   private pathCache: Map<string, Promise<boolean>>;
   private fileCache: Map<string, Promise<boolean>>;
   private readFileCache: Map<string, Promise<Buffer>>;
+  private readdirCache: Map<string, Promise<Stat[]>>;
 
   constructor() {
     this.pathCache = new Map();
     this.fileCache = new Map();
     this.readFileCache = new Map();
+    this.readdirCache = new Map();
   }
 
   public hasPath = async (path: string): Promise<boolean> => {
@@ -63,5 +72,59 @@ export abstract class DetectorFilesystem {
       this.readFileCache.set(name, p);
     }
     return p;
+  };
+
+  /**
+   * Returns a list of Stat objects from the current working directory.
+   *
+   * @example
+   *
+   * const url = "https://github.com/vercel/front"
+   * const fs = new GitDetectorFilesystem(...) // based on url
+   *
+   * // calls "https://api.github.com/repos/vercel/front/contents" behind the scenes
+   * await fs.readdir() => [
+   *    { name: "docs", path: "docs", type: "dir" },
+   *    { name: "front", path: "front", type: "dir" },
+   *    ...,
+   *    { name: "package.json", path: "package.json", type: "file" },
+   * ]
+   */
+  public readdir = async (name: string): Promise<Stat[]> => {
+    let p = this.readdirCache.get(name);
+    if (!p) {
+      p = this._readdir(name);
+      this.readdirCache.set(name, p);
+    }
+    return p;
+  };
+
+  /**
+   * Changes the current directory to the specified path and returns a new instance of DetectorFilesystem.
+   *
+   * @example
+   *
+   * my-repo
+   * |-- backend
+   * |    |-- api-1
+   * |    |-- api-2
+   * |    |-- package.json // workspaces: ["api-1", "api-2"]
+   * |    |-- yarn.lock
+   * |-- frontend
+   * |    |-- nextjs-app
+   * |    |-- gatsby-app
+   * |    |-- package.json
+   * |    |-- pnpm-workspaces.yaml // packages: ["nextjs-app", "gatsby-app"]
+   *
+   * const url = "https://github.com/vercel/my-repo"
+   * const fs = new GitDetectorFilesystem(...) // based on "url"
+   * const backendFs = fs.chdir("backend")
+   * const frontendFs = fs.chdir("frontend")
+   *
+   * const backendWorkspaceManager = detectFramework({ fs: backendFs, frameworkList: workspaceManagers }) // "yarn"
+   * const frontendWorkspaceManager = detectFramework({ fs: frontendFs, frameworkList: workspaceManagers }) // "pnpm"
+   */
+  public chdir = (name: string): DetectorFilesystem => {
+    return this._chdir(name);
   };
 }
