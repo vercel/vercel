da672f1f06b9aa30b20cac0d292c813941ad0a09
Sean Massa <EndangeredMassa@gmail.com>
2022-05-16 10:48:06 -0500

    Revert "Revert "[static-build] Support subset of Build Output API v2" (#7803)"
    This reverts commit dfb6ef949b35acdc11d0e57d45d4c2b8eb8a9ff4.

--- packages/static-build/test/build.test.ts
+++ packages/static-build/test/build.test.ts
@@ -1,54 +1,88 @@
 import path from 'path';
+import { remove } from 'fs-extra';
 import { build } from '../src';
 
 describe('build()', () => {
-  it('should detect Builder Output v3', async () => {
-    const workPath = path.join(
-      __dirname,
-      'build-fixtures',
-      '09-build-output-v3'
-    );
-    const buildResult = await build({
-      files: {},
-      entrypoint: 'package.json',
-      workPath,
-      config: {},
-      meta: {
-        skipDownload: true,
-        cliVersion: '0.0.0',
-      },
+  describe('Build Output API v2', () => {
+    it('should detect the output format', async () => {
+      const workPath = path.join(
+        __dirname,
+        'build-fixtures',
+        '10-build-output-v2'
+      );
+
+      try {
+        const buildResult = await build({
+          files: {},
+          entrypoint: 'package.json',
+          workPath,
+          config: {},
+          meta: {
+            skipDownload: true,
+            cliVersion: '0.0.0',
+          },
+        });
+        if ('buildOutputVersion' in buildResult) {
+          throw new Error('Unexpected `buildOutputVersion` in build result');
+        }
+
+        expect(buildResult.output['index.html']).toBeTruthy();
+        expect(buildResult.output['middleware']).toBeTruthy();
+      } finally {
+        remove(path.join(workPath, '.output'));
+      }
     });
-    if ('output' in buildResult) {
-      throw new Error('Unexpected `output` in build result');
-    }
-    expect(buildResult.buildOutputVersion).toEqual(3);
-    expect(buildResult.buildOutputPath).toEqual(
-      path.join(workPath, '.vercel/output')
-    );
   });
 
-  it('should throw an Error with Builder Output v3 without `vercel build`', async () => {
-    let err;
-    const workPath = path.join(
-      __dirname,
-      'build-fixtures',
-      '09-build-output-v3'
-    );
-    try {
-      await build({
+  describe('Build Output API v3', () => {
+    it('should detect the output format', async () => {
+      const workPath = path.join(
+        __dirname,
+        'build-fixtures',
+        '09-build-output-v3'
+      );
+      const buildResult = await build({
         files: {},
         entrypoint: 'package.json',
         workPath,
         config: {},
         meta: {
           skipDownload: true,
+          cliVersion: '0.0.0',
         },
       });
-    } catch (_err: any) {
-      err = _err;
-    }
-    expect(err.message).toEqual(
-      `Detected Build Output v3 from the "build" script, but this Deployment is not using \`vercel build\`.\nPlease set the \`ENABLE_VC_BUILD=1\` environment variable.`
-    );
+      if ('output' in buildResult) {
+        throw new Error('Unexpected `output` in build result');
+      }
+      expect(buildResult.buildOutputVersion).toEqual(3);
+      expect(buildResult.buildOutputPath).toEqual(
+        path.join(workPath, '.vercel/output')
+      );
+    });
+
+    it('should throw an Error without `vercel build`', async () => {
+      let err;
+      const workPath = path.join(
+        __dirname,
+        'build-fixtures',
+        '09-build-output-v3'
+      );
+      try {
+        await build({
+          files: {},
+          entrypoint: 'package.json',
+          workPath,
+          config: {},
+          meta: {
+            skipDownload: true,
+          },
+        });
+      } catch (_err: any) {
+        err = _err;
+      }
+      expect(err.message).toEqual(
+        `Detected Build Output v3 from the "build" script, but this Deployment is not using \`vercel build\`.\nPlease set the \`ENABLE_VC_BUILD=1\` environment variable.`
+      );
+    });
   });
 });
