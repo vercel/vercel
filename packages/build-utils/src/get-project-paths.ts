7f9c3abd40d01b916133c608981db633b7d3c4e2
chloetedder <chloe.tedder@vercel.com>
2022-05-31 16:27:34 -0500

    Remove unnecessary readdir calls, simplify code

--- packages/build-utils/src/get-project-paths.ts
+++ packages/build-utils/src/get-project-paths.ts
@@ -8,6 +8,7 @@ export interface GetProjectPathsOptions {
   fs: DetectorFilesystem;
   path?: string;
   skipPaths?: string[];
+  depth?: number;
 }
 
 export type ProjectPath = string;
@@ -16,39 +17,19 @@ export const getProjectPaths = async ({
   fs,
   path,
   skipPaths,
+  depth = 0,
 }: GetProjectPathsOptions): Promise<ProjectPath[]> => {
   const allPaths: Array<ProjectPath> = [];
-  for (
-    let currentDepth = 0;
-    currentDepth < MAX_DEPTH_TRAVERSE;
-    currentDepth++
-  ) {
-    if (path && skipPaths?.includes(path)) {
-      return allPaths;
-    }
-    const directoryContents = await fs.readdir(path ?? './');
+  if (path && skipPaths?.includes(path)) {
+    return allPaths;
+  }
+  const directoryContents = await fs.readdir(path ?? './');
 
+  if (depth < MAX_DEPTH_TRAVERSE) {
     const hasPackageJson = directoryContents.some(
       dir => dir.type === 'file' && dir.name === 'package.json'
     );
-    const childDirs = directoryContents.filter(stat => stat.type === 'dir');
 
-    if (!childDirs.length && !hasPackageJson) {
-      return allPaths;
-    }
-
-    if (childDirs.length) {
-      const dirPaths = await Promise.all(
-        childDirs.flatMap(async current => {
-          const paths = await getProjectPaths({
-            fs,
-            path: current.path,
-          });
-          return paths;
-        })
-      );
-      dirPaths.flat().forEach(path => allPaths.push(path));
-    }
     if (hasPackageJson && path) {
       const framework = await detectFramework({
         fs: fs.chdir(path),
@@ -57,6 +38,40 @@ export const getProjectPaths = async ({
       if (framework) {
         allPaths.push(path);
       }
+    }
+
+    const childDirs = directoryContents.filter(stat => stat.type === 'dir');
+
+    if (!childDirs.length && !hasPackageJson) {
+      return allPaths;
+    }
+
+    if (childDirs.length) {
+      const dirFinals = await Promise.all(
+        childDirs.flatMap(async current => {
+          const hasPackageJsonDir = await fs.hasPath(
+            `${current.path}/package.json`
+          );
+          if (hasPackageJsonDir && current.path) {
+            const framework = await detectFramework({
+              fs: fs.chdir(current.path),
+              frameworkList: frameworks,
+            });
+            if (framework) {
+              return current.path;
+            } else {
+              const paths = await getProjectPaths({
+                fs,
+                path: current.path,
+                depth: depth + 1,
+              });
+              return paths;
+            }
+          }
+          return [];
+        })
+      );
+      dirFinals.flat().forEach(path => allPaths.push(path));
     }
   }
   return allPaths;
