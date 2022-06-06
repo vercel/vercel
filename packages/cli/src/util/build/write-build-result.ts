8f3f4d0b7cef154d4298b3f4eaf955a5eebf3f9b
Nathan Rajlich <n@n8.io>
2022-06-06 00:10:30 -0700

    [cli] Output `EdgeFunctions` from version 3 Builders in `vc build`

--- packages/cli/src/util/build/write-build-result.ts
+++ packages/cli/src/util/build/write-build-result.ts
@@ -130,13 +130,18 @@ async function writeBuildResultV2(
  */
 async function writeBuildResultV3(buildResult: BuildResultV3, build: Builder) {
   const { output } = buildResult;
+  const src = build.src;
+  if (typeof src !== 'string') {
+    throw new Error(`Expected "build.src" to be a string`);
+  }
+  const ext = extname(src);
+  const path = build.config?.zeroConfig
+    ? src.substring(0, src.length - ext.length)
+    : src;
   if (isLambda(output)) {
-    const src = build.src!;
-    const ext = extname(src);
-    const path = build.config?.zeroConfig
-      ? src.substring(0, src.length - ext.length)
-      : src;
     await writeLambda(output, path);
+  } else if (isEdgeFunction(output)) {
+    await writeEdgeFunction(output, path);
   } else {
     throw new Error(
       `Unsupported output type: "${(output as any).type}" for ${build.src}`
