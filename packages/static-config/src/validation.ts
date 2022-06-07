e390739bc75f33e96bace9577f45267dac7f28a9
Nathan Rajlich <n@n8.io>
2022-06-06 19:25:34 -0700

    .

--- packages/static-config/src/validation.ts
+++ packages/static-config/src/validation.ts
@@ -3,10 +3,7 @@ import { FromSchema, JSONSchema } from 'json-schema-to-ts';
 
 const ajv = new Ajv();
 
-export function validate<T extends JSONSchema>(
-  schema: T,
-  data: any
-): FromSchema<T> {
+export function validate<T extends JSONSchema>(schema: T, data: any): FromSchema<T> {
   const isValid = ajv.compile(schema);
   if (!isValid(data)) {
     // TODO: better error message
