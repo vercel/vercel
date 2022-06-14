0ba858c4dc9c51d1bc8607eaae8e8d8cfb9977f9
Nathan Rajlich <n@n8.io>
2022-06-14 16:24:08 -0700

    Read `package.json` from the filesystem instead of bundling with ncc

--- packages/cli/src/util/pkg.ts
+++ packages/cli/src/util/pkg.ts
@@ -1,6 +1,11 @@
-import _pkg from '../../package.json';
+import fs from 'fs';
+import { join } from 'path';
 import { PackageJson } from '@vercel/build-utils';
 
-const pkg: PackageJson & typeof _pkg = _pkg;
+// Path here has only one `..` because the ncc'd file lives in "dist"
+const pkgPath = join(__dirname, '../package.json');
 
+const pkg: PackageJson & typeof import('../../package.json') = JSON.parse(
+  fs.readFileSync(pkgPath, 'utf8')
+);
 export default pkg;
