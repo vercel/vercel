727b2906dacde2f7fe3ab2ee2e14828575964162
Nathan Rajlich <n@n8.io>
2022-06-14 16:33:04 -0700

    Fix unit tests

--- packages/cli/src/util/pkg.ts
+++ packages/cli/src/util/pkg.ts
@@ -2,9 +2,12 @@ import fs from 'fs';
 import { join } from 'path';
 import { PackageJson } from '@vercel/build-utils';
 
-// Path here has only one `..` because the ncc'd file lives in "dist"
-const pkgPath = join(__dirname, '../package.json');
+let rootDir = __dirname;
+while (!fs.existsSync(join(rootDir, 'package.json'))) {
+  rootDir = join(rootDir, '..');
+}
 
+const pkgPath = join(rootDir, 'package.json');
 const pkg: PackageJson & typeof import('../../package.json') = JSON.parse(
   fs.readFileSync(pkgPath, 'utf8')
 );
