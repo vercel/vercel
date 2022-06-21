ff7b6bd35309e4da7064296e0c82590db86d89a8
Matthew Stanciu <mattbstanciu@gmail.com>
2022-06-21 16:08:07 -0700

    Revert "Does this passs??????????????????"
    This reverts commit d0cc5ad0f41f1b2c983eafe0ddc42481bf72c14a.

--- packages/cli/test/integration.js
+++ packages/cli/test/integration.js
@@ -550,11 +550,12 @@ test('default command should warn when deploying with conflicting subdirectory',
   t.is(exitCode, 0, formatOutput({ stdout, stderr }));
   t.regex(
     stderr || '',
-    /Did you mean to deploy the subdirectory "list"\? Use `vc --cwd list` instead./
+    /Did you mean to deploy the subdirectory "list"\? Use `vc --cwd list` instead./,
+    formatOutput({ stdout, stderr })
   );
 
-  const listHeader = /No deployments found/;
-  t.regex(stderr || '', listHeader); // ensure `list` command still ran
+  const listHeader = /age +deployment url +state +duration +username/;
+  t.regex(stdout || '', listHeader, formatOutput({ stdout, stderr })); // ensure `list` command still ran
 });
 
 test('deploy command should not warn when deploying with conflicting subdirectory and using --cwd', async t => {
@@ -577,8 +578,8 @@ test('deploy command should not warn when deploying with conflicting subdirector
     /Did you mean to deploy the subdirectory "list"\? Use `vc --cwd list` instead./
   );
 
-  const listHeader = /age +deployment url +state +duration/;
-  t.regex(stderr || '', listHeader); // ensure `list` command still ran
+  const listHeader = /project +latest deployment +state +age +username/;
+  t.regex(stdout || '', listHeader); // ensure `list` command still ran
 });
 
 test('default command should work with --cwd option', async t => {
