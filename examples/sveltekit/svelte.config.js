6973cd59894127f6b0856d1731eacd9036138340
Lee Robinson <lrobinson2011@gmail.com>
2022-06-21 14:10:55 -0500

    [examples] Address follow up from SvelteKit example update (#8002)

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -1,19 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess({
-    replace: [
-      ['import.meta.env.VERCEL_ANALYTICS_ID', JSON.stringify(process.env.VERCEL_ANALYTICS_ID)]
-    ]
-  }),
   kit: {
     adapter: adapter(),
 
     // Override http methods in the Todo forms
     methodOverride: {
       allowed: ['PATCH', 'DELETE']
+    },
+    vite: {
+      define: {
+        'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
+      }
     }
   }
 };
