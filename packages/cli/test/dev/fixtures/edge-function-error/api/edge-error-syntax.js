b47c68317b820a5425cb7b392b03ebdaed795f27
Sean Massa <endangeredmassa@gmail.com>
2022-06-28 02:56:24 -0500

    update tests

--- packages/cli/test/dev/fixtures/edge-function-error/api/edge-error-syntax.js
+++ packages/cli/test/dev/fixtures/edge-function-error/api/edge-error-syntax.js
@@ -2,7 +2,7 @@ export const config = {
   runtime: 'experimental-edge'
 }
 
-export default async function edge(request: Request, event: Event) {
+export default async function edge(request, event) {
   return new Response('some response body');
 
 // intentional missing closing bracket to produce syntax error
