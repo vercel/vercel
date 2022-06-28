348a22b0dae277e62e1e56aa413ccd78bf100d47
Matthew Stanciu <mattbstanciu@gmail.com>
2022-06-27 20:02:50 -0700

    Make tests better, kind of

--- packages/cli/test/unit/commands/list.test.ts
+++ packages/cli/test/unit/commands/list.test.ts
@@ -15,15 +15,17 @@ const fixture = (name: string) =>
 
 describe('list', () => {
   const originalCwd = process.cwd();
+  let teamSlug: string = '';
 
   it('should get deployments from a project linked by a directory', async () => {
-    const cwd = fixture('project');
+    const cwd = fixture('link');
     try {
       process.chdir(cwd);
 
-      const user = useUser();
-      useTeams('team_MtLD9hKuWAvoDd3KmiHs9zUg');
-      useProject({
+      const user = await useUser();
+      const team = useTeams('team_MtLD9hKuWAvoDd3KmiHs9zUg');
+      teamSlug = team[0].slug;
+      const project = useProject({
         ...defaultProject,
         id: 'prj_Am19DF8JBL9g89tn4RdDVD59axFi',
         name: 'prj_Am19DF8JBL9g89tn4RdDVD59axFi',
@@ -32,14 +34,15 @@ describe('list', () => {
 
       await list(client);
 
-      const header: Array<string> = formatOutput(
-        client.outputBuffer.split('\n')[3]
+      const { project: proj, org } = getDataFromIntro(
+        client.outputBuffer.split('\n')[0]
       );
-      const data: Array<string> = formatOutput(
-        client.outputBuffer.split('\n')[4]
-      );
+      const header: string[] = formatTable(client.outputBuffer.split('\n')[3]);
+      const data: string[] = formatTable(client.outputBuffer.split('\n')[4]);
       data.shift();
 
+      expect(proj).toEqual(project.project.name);
+      expect(org).toEqual(team[0].slug);
       expect(header).toEqual([
         'age',
         'deployment url',
@@ -59,7 +62,7 @@ describe('list', () => {
     }
   });
   it('should get all deployments in the project scope', async () => {
-    const cwd = fixture('project');
+    const cwd = fixture('all');
     try {
       process.chdir(cwd);
 
@@ -67,22 +70,23 @@ describe('list', () => {
       useTeams('team_MtLD9hKuWAvoDd3KmiHs9zUg');
       useProject({
         ...defaultProject,
-        id: 'prj_Am19DF8JBL9g89tn4RdDVD59axFi',
-        name: 'prj_Am19DF8JBL9g89tn4RdDVD59axFi',
+        id: 'proj-all',
+        name: 'proj-all',
       });
       const deployment = useDeployment({ creator: user });
 
       client.setArgv('--all');
       await list(client);
-
-      const header: Array<string> = formatOutput(
-        client.outputBuffer.split('\n')[2]
+      console.log(client.outputBuffer);
+      const { project, org } = getDataFromIntro(
+        client.outputBuffer.split('\n')[0]
       );
-      const data: Array<string> = formatOutput(
-        client.outputBuffer.split('\n')[3]
-      );
+      const header: string[] = formatTable(client.outputBuffer.split('\n')[2]);
+      const data: string[] = formatTable(client.outputBuffer.split('\n')[3]);
       data.pop();
 
+      expect(project).toBeUndefined();
+      expect(org).toEqual(teamSlug);
       expect(header).toEqual(['project', 'latest deployment', 'state', 'age']);
       expect(data).toEqual([
         deployment.name,
@@ -94,7 +98,7 @@ describe('list', () => {
     }
   });
   it('should get the deployments for a specified project', async () => {
-    const cwd = fixture('project');
+    const cwd = fixture('specify-project');
     try {
       process.chdir(cwd);
 
@@ -102,21 +106,23 @@ describe('list', () => {
       useTeams('team_MtLD9hKuWAvoDd3KmiHs9zUg');
       useProject({
         ...defaultProject,
-        id: 'prj_Am19DF8JBL9g89tn4RdDVD59axFi',
-        name: 'prj_Am19DF8JBL9g89tn4RdDVD59axFi',
+        id: 'specify-project',
+        name: 'specify-project',
       });
       const deployment = useDeployment({ creator: user });
 
       client.setArgv(deployment.name);
       await list(client);
 
-      const header: Array<string> = formatOutput(
-        client.outputBuffer.split('\n')[3]
+      const { project, org } = getDataFromIntro(
+        client.outputBuffer.split('\n')[0]
       );
-      const data: Array<string> = formatOutput(
-        client.outputBuffer.split('\n')[4]
-      );
+      const header: string[] = formatTable(client.outputBuffer.split('\n')[3]);
+      const data: string[] = formatTable(client.outputBuffer.split('\n')[4]);
       data.shift();
+
+      expect(project).toEqual(deployment.name);
+      expect(org).toEqual(teamSlug);
 
       expect(header).toEqual([
         'age',
@@ -137,7 +143,20 @@ describe('list', () => {
   });
 });
 
-function formatOutput(output: string): Array<string> {
+function getDataFromIntro(output: string): {
+  project: string | undefined;
+  org: string | undefined;
+} {
+  const project = output.match(/(?<=Deployments for )(.*)(?= under)/);
+  const org = output.match(/(?<=under )(.*)(?= \[)/);
+
+  return {
+    project: project?.[0],
+    org: org?.[0],
+  };
+}
+
+function formatTable(output: string): string[] {
   return output
     .trim()
     .replace(/ {3} +/g, ',')
