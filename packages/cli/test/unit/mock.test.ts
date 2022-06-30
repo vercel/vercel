5faeba7a5978d78b07d24e2ae5eb464cfd078cac
Nathan Rajlich <n@n8.io>
2022-06-29 20:40:57 -0700

    Fix `confirm()` mock test

--- packages/cli/test/unit/mock.test.ts
+++ packages/cli/test/unit/mock.test.ts
@@ -4,29 +4,29 @@ import { client } from '../mocks/client';
 describe('MockClient', () => {
   it('should mock `confirm()`', async () => {
     // true (explicit)
-    let confirmedPromise = confirm(client, 'Do the thing?', false);
-    await expect(client.stderr).toOutput('Do the thing? [y/N]');
+    let confirmedPromise = confirm(client, 'Explictly true?', false);
+    await expect(client.stderr).toOutput('Explictly true? [y/N]');
     client.stdin.write('yes\n');
     let confirmed = await confirmedPromise;
     expect(confirmed).toEqual(true);
 
     // false (explicit)
-    confirmedPromise = confirm(client, 'Do the thing?', true);
-    await expect(client.stderr).toOutput('Do the thing? [y/N]');
+    confirmedPromise = confirm(client, 'Explcitly false?', true);
+    await expect(client.stderr).toOutput('Explcitly false? [Y/n]');
     client.stdin.write('no\n');
     confirmed = await confirmedPromise;
     expect(confirmed).toEqual(false);
 
     // true (default)
-    confirmedPromise = confirm(client, 'Do the thing?', true);
-    await expect(client.stderr).toOutput('Do the thing? [y/N]');
+    confirmedPromise = confirm(client, 'Default true?', true);
+    await expect(client.stderr).toOutput('Default true? [Y/n]');
     client.stdin.write('\n');
     confirmed = await confirmedPromise;
     expect(confirmed).toEqual(true);
 
     // false (default)
-    confirmedPromise = confirm(client, 'Do the thing?', false);
-    await expect(client.stderr).toOutput('Do the thing? [y/N]');
+    confirmedPromise = confirm(client, 'Default false?', false);
+    await expect(client.stderr).toOutput('Default false? [y/N]');
     client.stdin.write('\n');
     confirmed = await confirmedPromise;
     expect(confirmed).toEqual(false);
