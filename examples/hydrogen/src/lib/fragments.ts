f8e5df749cd33ab585b03533aedd58391c23b808
Nathan Rajlich <n@n8.io>
2022-07-07 13:52:03 -0700

    [frameworks] Add "hydrogen" framework preset (#8073)
    Adds "hydrogen" framework preset for Shopify Hydrogen framework to allow for zero-config usage of the `@vercel/hydrogen` Builder.

---
+++ examples/hydrogen/src/lib/fragments.ts
@@ -0,0 +1,66 @@
+import {gql} from '@shopify/hydrogen';
+
+export const MEDIA_FRAGMENT = gql`
+  fragment Media on Media {
+    mediaContentType
+    alt
+    previewImage {
+      url
+    }
+    ... on MediaImage {
+      id
+      image {
+        url
+        width
+        height
+      }
+    }
+    ... on Video {
+      id
+      sources {
+        mimeType
+        url
+      }
+    }
+    ... on Model3d {
+      id
+      sources {
+        mimeType
+        url
+      }
+    }
+    ... on ExternalVideo {
+      id
+      embedUrl
+      host
+    }
+  }
+`;
+
+export const PRODUCT_CARD_FRAGMENT = gql`
+  fragment ProductCard on Product {
+    id
+    title
+    publishedAt
+    handle
+    variants(first: 1) {
+      nodes {
+        id
+        image {
+          url
+          altText
+          width
+          height
+        }
+        priceV2 {
+          amount
+          currencyCode
+        }
+        compareAtPriceV2 {
+          amount
+          currencyCode
+        }
+      }
+    }
+  }
+`;
