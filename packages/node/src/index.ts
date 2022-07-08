5d6d0bad4c2780bd049f6c5e97d011ab3586099f
Steven <steven@ceriously.com>
2022-07-08 11:56:47 -0400

    Revert "Debugging node symlinks"
    This reverts commit 942cd0c45e3074b9fab405f979a419824425623f.

--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -51,7 +51,7 @@ import type {
 import { getConfig } from '@vercel/static-config';
 
 import { Register, register } from './typescript';
-import { getRegExpFromMatchers } from './utils';
+import { entrypointToOutputPath, getRegExpFromMatchers } from './utils';
 
 export { shouldServe };
 
@@ -230,10 +230,8 @@ async function compile(
       const fsPath = resolve(baseDir, path);
       const { mode } = lstatSync(fsPath);
       if (isSymbolicLink(mode)) {
-        console.log('[debug][@vercel/node] Found symlink: ' + fsPath);
         entry = new FileFsRef({ fsPath, mode });
       } else {
-        console.log('[debug][@vercel/node] Found file: ' + fsPath);
         const source = readFileSync(fsPath);
         entry = new FileBlob({ data: source, mode });
       }
@@ -244,21 +242,12 @@ async function compile(
         baseDir,
         resolve(dirname(entry.fsPath), readlinkSync(entry.fsPath))
       );
-      console.log(
-        '[debug][@vercel/node] relative symlinkTarget: ' + symlinkTarget
-      );
       if (
         !symlinkTarget.startsWith('..' + sep) &&
         !fileList.has(symlinkTarget)
       ) {
-        console.log(
-          '[debug][@vercel/node] first time symlinkTarget: ' + symlinkTarget
-        );
         const stats = statSync(resolve(baseDir, symlinkTarget));
         if (stats.isFile()) {
-          console.log(
-            '[debug][@vercel/node] symlinkTarget is a file: ' + symlinkTarget
-          );
           fileList.add(symlinkTarget);
         }
       }
@@ -351,12 +340,6 @@ export const build: BuildV3 = async ({
   meta = {},
 }) => {
   const baseDir = repoRootPath || workPath;
-  const rootPathContent = require('fs').readdirSync(baseDir);
-  console.log(
-    `[debug][@vercel/node] contents of repoRootPath "${repoRootPath}" is ${JSON.stringify(
-      rootPathContent
-    )}`
-  );
   const awsLambdaHandler = getAWSLambdaHandler(entrypoint, config);
 
   const { entrypointPath, entrypointFsDirname, nodeVersion, spawnOpts } =
@@ -390,10 +373,7 @@ export const build: BuildV3 = async ({
   let output: BuildResultV3['output'] | undefined;
 
   const handler = renameTStoJS(relative(baseDir, entrypointPath));
-  const outputName = config.zeroConfig
-    ? handler.substring(0, handler.length - 3)
-    : handler;
-
+  const outputPath = entrypointToOutputPath(entrypoint, config.zeroConfig);
   const isMiddleware = config.middleware === true;
 
   // Will output an `EdgeFunction` for when `config.middleware = true`
@@ -429,9 +409,7 @@ export const build: BuildV3 = async ({
     routes = [
       {
         src,
-        middlewarePath: config.zeroConfig
-          ? outputName
-          : relative(baseDir, entrypointPath),
+        middlewarePath: outputPath,
         continue: true,
         override: true,
       },
@@ -444,7 +422,7 @@ export const build: BuildV3 = async ({
       files: preparedFiles,
 
       // TODO: remove - these two properties should not be required
-      name: outputName,
+      name: outputPath,
       deploymentTarget: 'v8-worker',
     });
   } else {
