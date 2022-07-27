b5b792e42f0ef4bfe2fdad894c211294d41d40aa
Steven <steven@ceriously.com>
2022-07-27 17:56:03 -0400

    [tests] Update tests to run with vercel cli tarball (#8257)
    This PR update the tests suite to wait for Vercel CLI tarball and then use that tarball to run E2E tests.
    
    This is valuable because it will package all the packages in this monorepo to make the tests follow more closely what will happen in production once merged.
    
    Since the current "Find Changes" step takes about 2 minutes, we run that first (it happens concurrently with the tarball preparation). Then once we complete "Find Changes" we wait for the tarball but it will likely be ready by that point since it also takes about 2 minutes. After both of those steps, the E2E tests continue as usual but with the `VERCEL_CLI_VERSION` set to the tarball.
    
    - Related to #7967 
    - Closes #8245 
    - Closes #8227

--- packages/python/test/integration.test.ts
+++ packages/python/test/integration.test.ts
@@ -6,19 +6,10 @@ const execa = require('execa');
 const { spawn } = require('child_process');
 
 const {
-  packAndDeploy,
   testDeployment,
 } = require('../../../test/lib/deployment/test-deployment.js');
 
 jest.setTimeout(4 * 60 * 1000);
-const buildUtilsUrl = '@canary';
-let builderUrl: string;
-
-beforeAll(async () => {
-  const builderPath = path.resolve(__dirname, '..');
-  builderUrl = await packAndDeploy(builderPath);
-  console.log('builderUrl', builderUrl);
-});
 
 const fixturesPath = path.resolve(__dirname, 'fixtures');
 
@@ -85,10 +76,7 @@ for (const fixture of fs.readdirSync(fixturesPath)) {
   // eslint-disable-next-line no-loop-func
   it(`should build ${fixture}`, async () => {
     await expect(
-      testDeployment(
-        { builderUrl, buildUtilsUrl },
-        path.join(fixturesPath, fixture)
-      )
+      testDeployment(path.join(fixturesPath, fixture))
     ).resolves.toBeDefined();
   });
 }
