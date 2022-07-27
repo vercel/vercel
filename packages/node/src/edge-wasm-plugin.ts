1a3f2bfc3f62af9730fc1b8543a31a3b9b405e4c
Sean Massa <EndangeredMassa@gmail.com>
2022-07-27 10:11:59 -0500

    add comment about the original source

--- packages/node/src/edge-wasm-plugin.ts
+++ packages/node/src/edge-wasm-plugin.ts
@@ -1,3 +1,6 @@
+// copied from `edge-functions-bridge`:
+// https://github.com/vercel/runtimes/blob/c076db9e3ce5635f7c2690396e3d9f791a0fd808/packages/edge-functions-bridge/src/get-edge-function-source.ts#L282-L317
+
 import type { Plugin } from 'esbuild';
 import { createHash } from 'crypto';
 import { promises as fs } from 'fs';
