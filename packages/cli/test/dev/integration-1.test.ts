44a9a6593794ba3da620d735c891ddaeade80a9f
Sean Massa <endangeredmassa@gmail.com>
2022-07-28 11:02:09 -0500

    tweak regex

--- packages/cli/test/dev/integration-1.test.ts
+++ packages/cli/test/dev/integration-1.test.ts
@@ -329,7 +329,7 @@ test('[vercel dev] should handle invalid middleware config', async () => {
       /<strong>500<\/strong>: INTERNAL_SERVER_ERROR/g
     );
     expect(stderr).toMatch(
-      /Error! Middleware's `config.matcher` .+ Received: not-a-valid-matcher/g
+      /Middleware's `config.matcher` .+ Received: not-a-valid-matcher/g
     );
   } finally {
     await dev.kill('SIGTERM');
