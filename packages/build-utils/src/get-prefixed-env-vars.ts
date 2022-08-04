8907e35fc23e062264d482d434d710470aef85a0
Steven <steven@ceriously.com>
2022-08-04 17:35:25 -0400

    Apply patch from Tom

--- packages/build-utils/src/get-prefixed-env-vars.ts
+++ packages/build-utils/src/get-prefixed-env-vars.ts
@@ -7,17 +7,19 @@ type Envs = { [key: string]: string | undefined };
  * @param envs - Environment Variables, typically from `process.env`
  */
 export function getPrefixedEnvVars(envPrefix: string, envs: Envs): Envs {
+  const vercelSystemEnvPrefix = 'VERCEL_';
   const newEnvs: Envs = {};
   if (envs.VERCEL_URL) {
     if (envPrefix) {
       Object.keys(envs)
-        .filter(key => key.startsWith('VERCEL_'))
+        .filter(key => key.startsWith(vercelSystemEnvPrefix))
         .forEach(key => {
           const newKey = `${envPrefix}${key}`;
           if (!(newKey in envs)) {
             newEnvs[newKey] = envs[key];
           }
         });
+      newEnvs.TURBO_CI_VENDOR_ENV_KEY = `${envPrefix}${vercelSystemEnvPrefix}`;
     }
   }
   return newEnvs;
