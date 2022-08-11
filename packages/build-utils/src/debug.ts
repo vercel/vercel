2f192be72a10abe9a40fc579176be4c589c6cb04
Steven <steven@ceriously.com>
2022-08-11 18:01:43 -0400

    [build-utils] Prefix debug message

--- packages/build-utils/src/debug.ts
+++ packages/build-utils/src/debug.ts
@@ -2,6 +2,6 @@ import { getPlatformEnv } from './get-platform-env';
 
 export default function debug(message: string, ...additional: any[]) {
   if (getPlatformEnv('BUILDER_DEBUG')) {
-    console.log(message, ...additional);
+    console.log(`[vc][builder] ${message}`, ...additional);
   }
 }
