ed499c36295da6673c87a9811ce646d8f7678433
Sean Massa <endangeredmassa@gmail.com>
2022-08-15 11:41:52 -0500

    tweaks

--- packages/cli/test/unit/commands/bisect.test.ts
+++ packages/cli/test/unit/commands/bisect.test.ts
@@ -4,34 +4,27 @@ import bisect from '../../../src/commands/bisect';
 import { useDeployments, useDeployment } from '../../mocks/deployment';
 
 describe('bisect', () => {
-  it('should ...', async () => {
+  it('should find the bad deployment', async () => {
     const user = useUser();
 
     const now = Date.now();
     const deployment1 = useDeployment({ creator: user, createdAt: now });
-    const deployment2 = useDeployment({ creator: user, createdAt: now + 5000 });
+    useDeployment({ creator: user, createdAt: now + 10000 });
     const deployment3 = useDeployment({
       creator: user,
-      createdAt: now + 10000,
+      createdAt: now + 20000,
     });
     useDeployments();
-
-    // TODO: remove
-    console.log({
-      deployment1: deployment1.url,
-      deployment2: deployment2.url,
-      deployment3: deployment3.url,
-    });
 
     const bisectPromise = bisect(client);
 
     await expect(client.stderr).toOutput('Specify a URL where the bug occurs:');
-    client.stdin.write(`${deployment3.url}\n`);
+    client.stdin.write(`https://${deployment3.url}\n`);
 
     await expect(client.stderr).toOutput(
       'Specify a URL where the bug does not occur:'
     );
-    client.stdin.write(`${deployment1.url}\n`);
+    client.stdin.write(`https://${deployment1.url}\n`);
 
     await expect(client.stderr).toOutput(
       'Specify the URL subpath where the bug occurs:'
