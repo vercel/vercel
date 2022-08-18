862369c2891ff40206674c97cbeec3f1e8a14248
Sean Massa <endangeredmassa@gmail.com>
2022-08-18 18:59:36 -0500

    make fixture deployable

--- packages/go/test/fixtures/24-bad-handler/api/bad-receiver.go
+++ packages/go/test/fixtures/24-bad-handler/api/bad-receiver.go
@@ -9,6 +9,15 @@ import (
 type SampleDecoder struct {
 }
 
+// the handler location logic looks for the first function that matches the proper signature;
+// this test makes sure that the BadReceiverHandler is not found because it is a receiver function
+
+// this handler will not be found because it is a receiver function
 func (d *SampleDecoder) BadReceiverHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "version:%v", runtime.Version())
+	fmt.Fprintf(w, "version:%v:from BadHandler", runtime.Version())
+}
+
+// this handler will be found because it has the correct function signature
+func GoodHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "version:%v:from GoodHandler", runtime.Version())
 }
