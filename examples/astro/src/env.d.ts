0e11854eea4c66078f7aa27f27ff1cb4c65249d7
Josh <josh@joshthomas.dev>
2022-08-26 11:34:30 -0500

    convert spaces to tabs to conform to existing template

--- examples/astro/src/env.d.ts
+++ examples/astro/src/env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly PUBLIC_VERCEL_ANALYTICS_ID: string;
+	readonly PUBLIC_VERCEL_ANALYTICS_ID: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+	readonly env: ImportMetaEnv;
 }
