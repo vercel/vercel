52c56b5b185ae40b3ac21bc9cc5ee0b75f2fe351
Chris Barber <chris@cb1inc.com>
2022-09-12 00:22:17 -0500

    added Next.js path normalization unit test

---
+++ packages/next/test/unit/fixtures/01-normalize-routes/index.test.js
@@ -0,0 +1,48 @@
+const fs = require('fs-extra');
+const ms = require('ms');
+const path = require('path');
+const { build } = require('../../../../src');
+const { FileFsRef } = require('@vercel/build-utils');
+
+jest.setTimeout(ms('6m'));
+
+describe(`${__dirname.split(path.sep).pop()}`, () => {
+  it('should normalize routes in config', async () => {
+    const files = [
+      'index.test.js',
+      'next.config.js',
+      'package.json',
+      'pages/foo/bar/index.js',
+      'pages/foo/index.js',
+      'pages/index.js',
+      'vercel.json',
+      'yarn.lock',
+    ].reduce((filesMap, file) => {
+      const fsPath = path.join(__dirname, file);
+      const { mode } = fs.statSync(fsPath);
+      filesMap[path] = new FileFsRef({ mode, fsPath });
+      return filesMap;
+    }, {});
+
+    const { output } = await build({
+      config: {},
+      entrypoint: 'package.json',
+      files,
+      meta: {
+        skipDownload: true,
+      },
+      repoRootPath: __dirname,
+      workPath: __dirname,
+    });
+
+    const pagesDir = path.join(__dirname, '.next', 'server', 'pages');
+    const pages = ['foo', 'foo/bar', 'index'];
+
+    for (const page of pages) {
+      expect(output).toHaveProperty(page);
+      expect(path.resolve(output[page].fsPath)).toEqual(
+        path.join(pagesDir, `${page}.html`)
+      );
+    }
+  });
+});
