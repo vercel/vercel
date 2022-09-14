09ff04fbc5e66f736fe6e08a376801ce1f1ea3f4
chloetedder <chloe.tedder@vercel.com>
2022-09-14 14:18:13 -0500

    Add string misshaped json

--- packages/fs-detectors/test/unit.get-workspaces-package-paths.test.ts
+++ packages/fs-detectors/test/unit.get-workspaces-package-paths.test.ts
@@ -17,6 +17,7 @@ describe.each<[string, string[]]>([
   ['41-nx-workspace', ['/apps/app-one', '/apps/app-two']],
   ['42-npm-workspace-with-nx', ['/apps/app-one', '/apps/app-two']],
   ['43-nx-json-misshaped', []],
+  ['44-nx-json-string', []],
 ])('`getWorkspacesPackagePaths()`', (fixturePath, packagePaths) => {
   const testName =
     packagePaths.length > 0
