e159401a088839ca83304f4c5b73452e0e61d222
Chris Barber <chris.barber@vercel.com>
2022-09-19 09:02:11 -0500

    [build-utils] Increase unit test timeout from 10s to 20s

--- packages/build-utils/test/unit.test.ts
+++ packages/build-utils/test/unit.test.ts
@@ -19,7 +19,7 @@ import {
   Meta,
 } from '../src';
 
-jest.setTimeout(10 * 1000);
+jest.setTimeout(20 * 1000);
 
 async function expectBuilderError(promise: Promise<any>, pattern: string) {
   let result;
