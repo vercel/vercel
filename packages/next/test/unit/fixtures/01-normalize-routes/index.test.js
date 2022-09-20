e0d43ce696d920bf1d8276b4e12f95fdc137e561
Steven <steven@ceriously.com>
2022-09-20 14:39:18 -0400

    skip on windows

--- packages/next/test/unit/fixtures/01-normalize-routes/index.test.js
+++ packages/next/test/unit/fixtures/01-normalize-routes/index.test.js
@@ -9,10 +9,15 @@ jest.setTimeout(ms('6m'));
 describe(`${__dirname.split(path.sep).pop()}`, () => {
   afterEach(() => fs.remove(path.join(__dirname, 'yarn.lock')));
 
-  // This test is failing on Windows with the following error:
-  // $ next build
-  // 'next' is not recognized as an internal or external command,
-  it.skip('should normalize routes in build results output', async () => {
+  it('should normalize routes in build results output', async () => {
+    if (process.platform === 'win32') {
+      console.log('Skipping test on windows temporarily');
+      // This test is failing on Windows with the following error:
+      // $ next build
+      // 'next' is not recognized as an internal or external command,
+      return;
+    }
+
     const files = [
       'index.test.js',
       'next.config.js',
