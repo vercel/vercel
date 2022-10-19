483a96783afc88d5e45d08a42b35ba694a363162
Seiya Nuta <nuta@seiya.me>
2022-10-19 17:04:16 +0900

    Apply suggestions from code review
    Co-authored-by: Gal Schlezinger <gal@spitfire.co.il>

--- packages/edge/src/middleware-helpers.ts
+++ packages/edge/src/middleware-helpers.ts
@@ -1,6 +1,24 @@
 export interface ModifiedRequest {
   /**
-   * If this is set, the request headers will be overridden with this value.
+   * If set, overwrites the incoming headers to the origin request.
+   *
+   * This is useful when you want to pass data between a Middleware and a
+   * Serverless or Edge Function.
+   *
+   * @example
+   * <caption>Add a `x-user-id` header and remove the `Authorization` header</caption>
+   *
+   * ```ts
+   * import { rewrite } from '@vercel/edge';
+   * export default async function middleware(request: Request): Promise<Response> {
+   *   const newHeaders = new Headers(request.headers);
+   *   newHeaders.set('x-user-id', 'user_123');
+   *   newHeaders.delete('authorization');
+   *   return rewrite(request.url, {
+   *     request: { headers: newHeaders }
+   *   })
+   * }
+   * ```
    */
   headers?: Headers;
 }
