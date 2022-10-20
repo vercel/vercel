798d6249e7a2a87d8b862ad579336e3311249ae0
Andy McKay <andy.mckay@vercel.com>
2022-10-20 08:49:45 -0700

    handle limit in fixture and add test

--- packages/cli/test/mocks/alias.ts
+++ packages/cli/test/mocks/alias.ts
@@ -2,29 +2,31 @@ import chance from 'chance';
 import { client } from './client';
 
 export function useAlias() {
-  const alias = {
-    alias: 'dummy.app',
-    created: chance().timestamp(),
-    createdAt: chance().timestamp(),
-    deletedAt: null,
-    deployment: {
-      id: chance().guid(),
-      url: chance().domain(),
-    },
-    deploymentId: chance().guid(),
-    projectId: chance().guid(),
-    redirect: null,
-    redirectStatusCode: null,
-    uid: chance().guid(),
-    updatedAt: chance().timestamp(),
-  };
+  function create(alias: string) {
+    return {
+      alias: `dummy-${alias}.app`,
+      created: chance().timestamp(),
+      createdAt: chance().timestamp(),
+      deletedAt: null,
+      deployment: {
+        id: chance().guid(),
+        url: chance().domain(),
+      },
+      deploymentId: chance().guid(),
+      projectId: chance().guid(),
+      redirect: null,
+      redirectStatusCode: null,
+      uid: chance().guid(),
+      updatedAt: chance().timestamp(),
+    };
+  }
 
   client.scenario.get('/v3/now/aliases', (_req, res) => {
+    let limit = parseInt(_req.query.limit);
+    const aliases = Array.from({ length: limit }, (v, i) => create(`${i}`));
     res.json({
-      aliases: [alias],
-      pagination: { count: 1, total: 1, page: 1, pages: 1 },
+      aliases: aliases,
+      pagination: { count: limit, total: limit, page: 1, pages: 1 },
     });
   });
-
-  return alias;
 }
--- packages/cli/test/unit/commands/alias.test.ts
+++ packages/cli/test/unit/commands/alias.test.ts
@@ -4,13 +4,22 @@ import { useUser } from '../../mocks/user';
 import { useAlias } from '../../mocks/alias';
 
 describe('alias', () => {
-  it('should list aliases', async () => {
+  it('should list up to 20 aliases by default', async () => {
     useUser();
     useAlias();
     client.setArgv('alias', 'ls');
     const exitCodePromise = alias(client);
     await expect(exitCodePromise).resolves.toEqual(0);
-    await expect(client.stderr).toOutput('dummy.app');
+    await expect(client.stderr).toOutput('dummy-19.app');
+  });
+
+  it('should list up to 2 aliases', async () => {
+    useUser();
+    useAlias();
+    client.setArgv('alias', 'ls', '--limit', '2');
+    const exitCodePromise = alias(client);
+    await expect(exitCodePromise).resolves.toEqual(0);
+    await expect(client.stderr).toOutput('dummy-1.app');
   });
 
   it('should throw an error if limit not valid', async () => {
