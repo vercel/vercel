13273d26614e8999bfc45869734143a4a0af9ff4
Andy McKay <andy.mckay@vercel.com>
2022-10-21 15:59:18 -0700

    update to avoid ! based on suggestion from @TooTallNate

--- packages/cli/src/commands/domains/ls.ts
+++ packages/cli/src/commands/domains/ls.ts
@@ -35,7 +35,8 @@ export default async function ls(
 
   if (
     typeof nextTimestamp !== undefined &&
-    (Number.isNaN(limit) || limit! > 100)
+    typeof limit === 'number' &&
+    (Number.isNaN(limit) || limit > 100)
   ) {
     output.error('Please provide a number up to 100 for flag --limit');
     return 1;
