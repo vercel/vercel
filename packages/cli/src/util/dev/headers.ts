22d9178c577289c647a05aeb2664a5b6df8d131a
Seiya Nuta <nuta@seiya.me>
2022-10-24 21:23:54 +0900

    Extract the headers override logic into a function and add some unit tests for it

--- packages/cli/src/util/dev/headers.ts
+++ packages/cli/src/util/dev/headers.ts
@@ -16,3 +16,60 @@ export function nodeHeadersToFetchHeaders(
   }
   return headers;
 }
+
+/**
+ * Adds/Updates/Deletes headers in `reqHeaders` based on the response headers
+ * from a middleware (`respHeaders`).
+ *
+ * `x-middleware-override-headers` is a comma-separated list of *all* header
+ * names that should appear in new request headers. Names not in this list
+ * will be deleted.
+ *
+ * `x-middleware-request-*` is the new value for each header. This can't be
+ * omitted, even if the header is not being modified.
+ *
+ * Headers listed in `skippedHeaders` will be ignored.
+ *
+ */
+export function applyOverriddenHeaders(
+  reqHeaders: { [k: string]: string | string[] | undefined },
+  respHeaders: Headers,
+  skippedHeaders: Set<string>
+) {
+  if (!respHeaders.has('x-middleware-override-headers')) {
+    return;
+  }
+
+  const overriddenHeaders: Set<string> = new Set();
+  for (const key of respHeaders
+    .get('x-middleware-override-headers')!
+    .split(',')) {
+    overriddenHeaders.add(key.trim());
+  }
+
+  respHeaders.delete('x-middleware-override-headers');
+
+  // Delete headers.
+  for (const key of Object.keys(reqHeaders)) {
+    if (!overriddenHeaders.has(key)) {
+      delete reqHeaders[key];
+    }
+  }
+
+  // Update or add headers.
+  for (const key of overriddenHeaders.keys()) {
+    if (skippedHeaders.has(key)) {
+      continue;
+    }
+
+    const valueKey = 'x-middleware-request-' + key;
+    const newValue = respHeaders.get(valueKey);
+    const oldValue = reqHeaders[key];
+
+    if (oldValue !== newValue) {
+      reqHeaders[key] = newValue === null ? undefined : newValue;
+    }
+
+    respHeaders.delete(valueKey);
+  }
+}
