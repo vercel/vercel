09edbf53d59e39ff4995810aa9a1e713b23c7385
Abdul Qadeer <abdulqadeer.el@gmail.com>
2022-11-13 09:15:13 +0300

    update app.js
    for testing

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload - aq.
         </p>
         <a
           className="App-link"
