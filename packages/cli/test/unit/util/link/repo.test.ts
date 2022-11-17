f24e3cc25c519491e4a26d2c9efb96cdfe1fa479
Nathan Rajlich <n@n8.io>
2022-11-16 20:31:39 -0800

    Fix Windows tests

--- packages/cli/test/unit/util/link/repo.test.ts
+++ packages/cli/test/unit/util/link/repo.test.ts
@@ -5,6 +5,8 @@ import {
   traverseDirectories,
 } from '../../../../src/util/link/repo';
 
+const isWindows = process.platform === 'win32';
+
 // Root of `vercel/vercel` repo
 const vercelRepoRoot = join(__dirname, '../../../../../..');
 
@@ -26,16 +28,29 @@ describe('findRepoRoot()', () => {
 });
 
 describe('traverseDirectories()', () => {
-  test.each([
-    {
-      input: '/foo/bar/baz',
-      expected: ['/foo/bar/baz', '/foo/bar', '/foo', '/'],
-    },
-    {
-      input: '/foo/../bar/./baz',
-      expected: ['/bar/baz', '/bar', '/'],
-    },
-  ])('should traverse "$input"', ({ input, expected }) => {
+  test.each(
+    isWindows
+      ? [
+          {
+            input: 'C:\\foo\\bar\\baz',
+            expected: ['C:\\foo\\bar\\baz', 'C:\\foo\\bar', 'C:\\foo', 'C:\\'],
+          },
+          {
+            input: 'C:\\foo\\..\\bar\\.\\baz',
+            expected: ['C:\\bar\\baz', 'C:\\bar', 'C:\\'],
+          },
+        ]
+      : [
+          {
+            input: '/foo/bar/baz',
+            expected: ['/foo/bar/baz', '/foo/bar', '/foo', '/'],
+          },
+          {
+            input: '/foo/../bar/./baz',
+            expected: ['/bar/baz', '/bar', '/'],
+          },
+        ]
+  )('should traverse "$input"', ({ input, expected }) => {
     expect(Array.from(traverseDirectories(input))).toEqual(expected);
   });
 });
