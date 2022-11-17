924f642b38086afa4d1e0beb881f62ae2ded3f0e
Lydia Hallie <lydiajuliettehallie@gmail.com>
2022-11-17 14:07:16 +0000

    Minify GraphQL Engine

--- packages/gatsby/src/handlers/build.ts
+++ packages/gatsby/src/handlers/build.ts
@@ -1,6 +1,6 @@
 import { join } from 'path';
 import { build } from 'esbuild';
-import { FileFsRef, NodeVersion, glob } from '@vercel/build-utils';
+import { FileFsRef, NodeVersion, glob, FileBlob } from '@vercel/build-utils';
 
 type FileFsRefs = Record<string, FileFsRef>;
 
@@ -28,14 +28,27 @@ export const getHandler = async ({
   return res.outputFiles[0].text;
 };
 
+export const minifyGraphQLEngine = async (): Promise<string> => {
+  const res = await build({
+    entryPoints: [join('.cache', 'query-engine', 'index.js')],
+    minify: true,
+    define: {
+      'process.env.NODE_ENV': "'production'",
+    },
+    write: false,
+  });
+
+  return res.outputFiles[0].text;
+};
+
 export async function getFunctionLibsFiles(): Promise<FileFsRefs> {
   const files: FileFsRefs = {};
 
   /* Copies the required libs for Serverless Functions from .cache to the <name>.func folder */
   for (const cur of [
     {
-      name: 'lib/query-engine',
-      src: join('.cache', 'query-engine'),
+      name: 'lib/query-engine/assets',
+      src: join('.cache', 'query-engine', 'assets'),
     },
     {
       name: 'lib/page-ssr',
@@ -55,6 +68,12 @@ export async function getFunctionLibsFiles(): Promise<FileFsRefs> {
       files[join(cur.name, path)] = file;
     }
   }
+
+  // Minify pre-bundled GraphQL Engine
+  // @ts-ignore
+  files['lib/query-engine/index.js'] = new FileBlob({
+    data: await minifyGraphQLEngine(),
+  });
 
   return files;
 }
