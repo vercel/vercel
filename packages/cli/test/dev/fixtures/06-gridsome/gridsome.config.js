0c792aefbc04e66ad07972bb5cc30b0637c71776
Nathan Rajlich <n@n8.io>
2022-11-21 11:14:24 -0800

    formatting

--- packages/cli/test/dev/fixtures/06-gridsome/gridsome.config.js
+++ packages/cli/test/dev/fixtures/06-gridsome/gridsome.config.js
@@ -1,4 +1,4 @@
-// This is where project configuration and plugin options are located.
+// This is where project configuration and plugin options are located. 
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
@@ -6,5 +6,5 @@
 
 module.exports = {
   siteName: 'Gridsome',
-  plugins: [],
-};
+  plugins: []
+}
--- packages/cli/test/dev/fixtures/06-gridsome/gridsome.server.js
+++ packages/cli/test/dev/fixtures/06-gridsome/gridsome.server.js
@@ -8,9 +8,9 @@
 module.exports = function (api) {
   api.loadSource(({ addContentType }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api
-  });
+  })
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api
-  });
-};
+  })
+}
