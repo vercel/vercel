f263e3a6dead5b8af149107d971fdfb010b0c13c
feugy <damien@vercel.com>
2022-11-22 11:22:44 +0100

    chore: adds Web as possible NodejsLambda launcher type

--- packages/build-utils/src/nodejs-lambda.ts
+++ packages/build-utils/src/nodejs-lambda.ts
@@ -4,22 +4,24 @@ interface NodejsLambdaOptions extends LambdaOptionsWithFiles {
   shouldAddHelpers: boolean;
   shouldAddSourcemapSupport: boolean;
   awsLambdaHandler?: string;
+  launcherType?: 'Nodejs' | 'Web';
 }
 
 export class NodejsLambda extends Lambda {
-  launcherType: 'Nodejs';
+  launcherType: 'Nodejs' | 'Web';
   shouldAddHelpers: boolean;
   shouldAddSourcemapSupport: boolean;
   awsLambdaHandler?: string;
 
   constructor({
+    launcherType,
     shouldAddHelpers,
     shouldAddSourcemapSupport,
     awsLambdaHandler,
     ...opts
   }: NodejsLambdaOptions) {
     super(opts);
-    this.launcherType = 'Nodejs';
+    this.launcherType = launcherType || 'Nodejs';
     this.shouldAddHelpers = shouldAddHelpers;
     this.shouldAddSourcemapSupport = shouldAddSourcemapSupport;
     this.awsLambdaHandler = awsLambdaHandler;
