8f42090bd377acd6fb38191510d611fecd5c6a93
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-01-03 14:36:44 -0700

    fix up tests into discrete gatsby versions

--- packages/gatsby/test/fixtures/gatsby-starter-default/gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
-
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
-}
--- packages/gatsby/test/test.js
+++ packages/gatsby/test/test.js
@@ -9,10 +9,8 @@ jest.setTimeout(12 * 60 * 1000);
 
 const fixturesPath = path.resolve(__dirname, 'fixtures');
 
-// eslint-disable-next-line no-restricted-syntax
 for (const fixture of fs.readdirSync(fixturesPath)) {
-  // eslint-disable-next-line no-loop-func
-  it(`should build ${fixture}`, async () => {
+  test(`should build ${fixture}`, async () => {
     await expect(
       testDeployment(path.join(fixturesPath, fixture))
     ).resolves.toBeDefined();
