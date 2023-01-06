2ff027875bd085948ced5a42b38fa724e97b2450
Andy McKay <andy.mckay@vercel.com>
2023-01-06 12:58:53 -0800

    add in tests for getEnvTargetRequested

---
+++ packages/cli/test/unit/util/env/parse-target.test.ts
@@ -0,0 +1,37 @@
+import { getEnvTargetRequested } from '../../../../src/util/env/env-target';
+import { Output } from '../../../../src/util/output';
+
+describe('parseEnvTarget', () => {
+  let output: Output;
+
+  beforeEach(() => {
+    output = new Output();
+    output.warn = jest.fn();
+    output.error = jest.fn();
+  });
+
+  it('defaults to `development`', () => {
+    let result = getEnvTargetRequested(output);
+    expect(result).toEqual('development');
+  });
+
+  it('defaults to arg', () => {
+    try {
+      process.env.VERCEL_ENV = 'some-env';
+      const result = getEnvTargetRequested(output, 'arg-env');
+      expect(result).toEqual('arg-env');
+    } finally {
+      delete process.env.VERCEL_ENV;
+    }
+  });
+
+  it('use VERCEL_ENV when no arg', () => {
+    try {
+      process.env.VERCEL_ENV = 'some-env';
+      const result = getEnvTargetRequested(output);
+      expect(result).toEqual('some-env');
+    } finally {
+      delete process.env.VERCEL_ENV;
+    }
+  });
+});
