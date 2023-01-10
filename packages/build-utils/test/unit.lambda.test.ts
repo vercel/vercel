2cddf2df5a7fd1bd0f1dfef56c6ebd5c5675aeda
Nathan Rajlich <n@n8.io>
2023-01-10 15:10:35 -0800

    [build-utils] Support directory entries in `Lambda#createZip()`

---
+++ packages/build-utils/test/unit.lambda.test.ts
@@ -0,0 +1,89 @@
+import path from 'path';
+import { tmpdir } from 'os';
+import fs from 'fs-extra';
+import { createZip } from '../src/lambda';
+import { FileBlob, glob, spawnAsync } from '../src';
+
+describe('Lambda', () => {
+  it('should create zip file with symlinks', async () => {
+    if (process.platform === 'win32') {
+      console.log('Skipping test on windows');
+      return;
+    }
+    const files = await glob('**', path.join(__dirname, 'symlinks'));
+    expect(Object.keys(files)).toHaveLength(4);
+
+    const outFile = path.join(__dirname, 'symlinks.zip');
+    await fs.remove(outFile);
+
+    const outDir = path.join(__dirname, 'symlinks-out');
+    await fs.remove(outDir);
+    await fs.mkdirp(outDir);
+
+    await fs.writeFile(outFile, await createZip(files));
+    await spawnAsync('unzip', [outFile], { cwd: outDir });
+
+    const [linkStat, linkDirStat, aStat] = await Promise.all([
+      fs.lstat(path.join(outDir, 'link.txt')),
+      fs.lstat(path.join(outDir, 'link-dir')),
+      fs.lstat(path.join(outDir, 'a.txt')),
+    ]);
+    expect(linkStat.isSymbolicLink()).toEqual(true);
+    expect(linkDirStat.isSymbolicLink()).toEqual(true);
+    expect(aStat.isFile()).toEqual(true);
+  });
+
+  it('should create zip file with empty directory', async () => {
+    const dir = await fs.mkdtemp(path.join(tmpdir(), 'create-zip-empty-dir'));
+    try {
+      const files = {
+        a: new FileBlob({
+          data: 'contents',
+          mode: 33188,
+        }),
+        empty: new FileBlob({
+          data: '',
+          mode: 16877,
+        }),
+        'b/a': new FileBlob({
+          data: 'inside dir b',
+          mode: 33188,
+        }),
+        c: new FileBlob({
+          data: '',
+          mode: 16877,
+        }),
+        'c/a': new FileBlob({
+          data: 'inside dir c',
+          mode: 33188,
+        }),
+      };
+
+      const outFile = path.join(dir, 'lambda.zip');
+
+      const outDir = path.join(dir, 'out');
+      await fs.mkdirp(outDir);
+
+      await fs.writeFile(outFile, await createZip(files));
+      await spawnAsync('unzip', [outFile], { cwd: outDir });
+
+      expect(fs.statSync(path.join(outDir, 'empty')).isDirectory()).toEqual(
+        true
+      );
+      expect(fs.statSync(path.join(outDir, 'b')).isDirectory()).toEqual(true);
+      expect(fs.statSync(path.join(outDir, 'c')).isDirectory()).toEqual(true);
+      expect(fs.readFileSync(path.join(outDir, 'a'), 'utf8')).toEqual(
+        'contents'
+      );
+      expect(fs.readFileSync(path.join(outDir, 'b/a'), 'utf8')).toEqual(
+        'inside dir b'
+      );
+      expect(fs.readFileSync(path.join(outDir, 'c/a'), 'utf8')).toEqual(
+        'inside dir c'
+      );
+      expect(fs.readdirSync(path.join(outDir, 'empty'))).toHaveLength(0);
+    } finally {
+      await fs.remove(dir);
+    }
+  });
+});
