ac91efe1e187f3bd4c814ffbf790fefbd64cba40
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-01-10 11:19:53 -0700

    temp commit

---
+++ packages/gatsby/test/fixtures/gatsby-v4/gatsby-node.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions;
+  createPage({
+    path: '/using-dsg',
+    component: require.resolve('./src/templates/using-dsg.js'),
+    context: {},
+    defer: true,
+  });
+};
+
+exports.onPostBuild = async ({ store }) => {
+  console.log(store.getState().pages);
+};
