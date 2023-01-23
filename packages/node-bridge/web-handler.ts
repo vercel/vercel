b577457ef97b0bb0df92e988f90f4e6e5122fbe3
feugy <damien@vercel.com>
2023-01-23 15:21:11 +0100

    chore: remove debug and todo

--- packages/node-bridge/web-handler.ts
+++ packages/node-bridge/web-handler.ts
@@ -1,14 +1,7 @@
 import type { BuildDependencies } from '@edge-runtime/node-utils';
 import { buildToNodeHandler } from '@edge-runtime/node-utils';
 
-export function transformToNodeHandler(webHandler: any, runtime: string) {
-  const nodeVersion = parseInt(runtime.replace(/^nodejs/, ''));
-  if (isNaN(nodeVersion) || nodeVersion < 18) {
-    throw new Error(
-      `web compliant signature can only be used with node.js 18 and later. Please configure your function's runtime accordingly`
-    );
-  }
-  // TODO throw when runtime isn't Node18
+export function transformToNodeHandler(webHandler: any) {
   const toNodeHandler = buildToNodeHandler(
     globalThis as unknown as BuildDependencies,
     { origin: 'http://example.com' } // TODO shouldn't the origin be set per-request at run time?
