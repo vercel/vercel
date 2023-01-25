de5a4cc0956c69b0d9d15df9f1814b459c7ce157
Nathan Rajlich <n@n8.io>
2023-01-24 20:23:04 -0800

    Remove

--- packages/static-build/src/utils/gatsby.ts
+++ packages/static-build/src/utils/gatsby.ts
@@ -182,12 +182,10 @@ async function updateGatsbyConfigMjs(
     configPath,
     `import userConfig from "./gatsby-config.mjs.__vercel_builder_backup__.mjs";
 
-// https://github.com/gatsbyjs/gatsby/blob/354003fb2908e02ff12109ca3a02978a5a6e608c/packages/gatsby/src/bootstrap/prefer-default.ts
 const preferDefault = (m) => (m && m.default) || m;
 
 const vercelConfig = Object.assign(
   {},
-  // https://github.com/gatsbyjs/gatsby/blob/a6ecfb2b01d761e8a3612b8ea132c698659923d9/packages/gatsby/src/services/initialize.ts#L113-L117
   preferDefault(userConfig)
 );
 if (!vercelConfig.plugins) {
@@ -223,12 +221,10 @@ async function updateGatsbyConfigJs(
     configPath,
     `const userConfig = require("./gatsby-config.js.__vercel_builder_backup__.js");
 
-// https://github.com/gatsbyjs/gatsby/blob/354003fb2908e02ff12109ca3a02978a5a6e608c/packages/gatsby/src/bootstrap/prefer-default.ts
 const preferDefault = m => (m && m.default) || m;
 
 const vercelConfig = Object.assign(
   {},
-  // https://github.com/gatsbyjs/gatsby/blob/a6ecfb2b01d761e8a3612b8ea132c698659923d9/packages/gatsby/src/services/initialize.ts#L113-L117
   preferDefault(userConfig)
 );
 if (!vercelConfig.plugins) {
