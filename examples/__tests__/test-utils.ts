862b1fa6efa3ec5916d4b5a6f807977018fe94a8
Steven <steven@ceriously.com>
2023-02-09 06:43:16 -0500

    Apply suggestion from cb1kenobi

--- examples/__tests__/test-utils.ts
+++ examples/__tests__/test-utils.ts
@@ -2,6 +2,6 @@ import { basename, join } from 'path';
 import { testDeployment } from '../../test/lib/deployment/test-deployment.js';
 
 export async function deployExample(filename: string) {
-  const example = basename(filename).slice(0, -8);
+  const example = basename(filename).replace(/\.test\.ts$/, '');
   await testDeployment(join(filename, '..', '..', '..', example));
 }
