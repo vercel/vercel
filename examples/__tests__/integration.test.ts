f950e44aa0861e287c29d3889b9c6e3ddc61c280
Steven <steven@ceriously.com>
2023-02-08 19:39:46 -0500

    Fix tests

--- examples/__tests__/integration.test.ts
+++ examples/__tests__/integration.test.ts
@@ -1,18 +1,19 @@
-const { readdirSync } = require('fs');
-const { join } = require('path');
+import { readdirSync, lstatSync } from 'fs';
+import { join } from 'path';
 
 const {
   testDeployment,
-} = require('../../../test/lib/deployment/test-deployment.js');
+} = require('../../test/lib/deployment/test-deployment.js');
 
 jest.setTimeout(5 * 60 * 1000);
 
 function getExamples() {
-  const examples = readdirSync(__dirname)
+  const dirname = join(__dirname, '..');
+  const examples = readdirSync(dirname)
     .map(example =>
       ({
         exampleName: example,
-        examplePath: join(__dirname, example),
+        examplePath: join(dirname, example),
       })
     )
     .filter(o =>
@@ -23,7 +24,7 @@ function getExamples() {
 
 describe('examples', () => {
   const examples = getExamples();
-
+  // TODO: separate each test into its own file
   it.each(examples)('should build and deploy $exampleName', async ({examplePath}) => {
     const promise = testDeployment(examplePath);
     await expect(promise).resolves.toBeDefined();
---
+++ examples/jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+};
