26773daf05af3aa274f95fc3e506619d5be8566c
Steven <steven@ceriously.com>
2023-02-09 09:58:37 -0500

    [tests] Add tests for `examples` (#9403)
    These tests will run if one of the conditions is met:
    
    - any change to `./examples`
    - any changes to `./packages/frameworks`
    
    The test ensures that examples can build and deploy without errors.
    
    In a future PR, we could improve these tests to make requests to the
    test deployment (like we do for probes).

---
+++ examples/__tests__/unit/index.test.ts
@@ -0,0 +1,26 @@
+import { existsSync, lstatSync, readdirSync } from 'fs';
+import { join } from 'path';
+
+function getExamples() {
+  const dirname = join(__dirname, '..', '..');
+  const examples = readdirSync(dirname)
+    .map(example =>
+      ({
+        exampleName: example,
+        examplePath: join(dirname, example),
+        testPath: join(dirname, '__tests__', 'integration', `${example}.test.ts`),
+      })
+    )
+    .filter(o =>
+      !o.exampleName.startsWith('.') &&
+      !o.exampleName.startsWith('_') &&
+      lstatSync(o.examplePath).isDirectory()
+    );
+  return examples;
+}
+
+describe('should have test for each example', () => {
+  it.each(getExamples())('should exist $exampleName', async ({testPath}) => {
+    expect(existsSync(testPath)).toBeTruthy();
+  });
+});
