a010d8fe8af5786c16c9d1d40c730e2302d02c00
Sean Massa <EndangeredMassa@gmail.com>
2023-02-13 15:37:16 -0600

    [cli] add slash before url for edge function errors (#9428)
    Edge Functions and Serverless Functions were both updated to show the path of their entry points, but it turns out Serverless Function paths started with a slash and Edge Function paths did not.
    
    The [related code for Serverless Functions](https://github.com/vercel/vercel/blob/d62888094236c6f2afdc34a5db2f098adf575470/packages/node-bridge/helpers.ts) does not need to be updated.
    
    ---
    
    Before:
    
    <img width="996" alt="CleanShot 2023-02-13 at 13 23 42@2x" src="https://user-images.githubusercontent.com/41545/218554154-2b112a68-2d68-4968-bae7-acf6e85a72a2.png">
    
    ---
    
    After: 
    
    <img width="921" alt="CleanShot 2023-02-13 at 13 20 22@2x" src="https://user-images.githubusercontent.com/41545/218554029-c3dce6b1-b01f-4a78-aa32-f6891ff16e51.png">

--- packages/node/src/edge-functions/edge-handler.ts
+++ packages/node/src/edge-functions/edge-handler.ts
@@ -7,7 +7,7 @@ import type { EdgeContext } from '@edge-runtime/vm';
 import esbuild from 'esbuild';
 import fetch from 'node-fetch';
 import { createEdgeWasmPlugin, WasmAssets } from './edge-wasm-plugin';
-import { logError } from '../utils';
+import { entrypointToOutputPath, logError } from '../utils';
 import { readFileSync } from 'fs';
 
 const NODE_VERSION_MAJOR = process.version.match(/^v(\d+)\.\d+/)?.[1];
@@ -136,7 +136,8 @@ async function createEdgeRuntime(params?: {
 export async function createEdgeEventHandler(
   entrypointFullPath: string,
   entrypointRelativePath: string,
-  isMiddleware: boolean
+  isMiddleware: boolean,
+  isZeroConfig?: boolean
 ): Promise<(request: IncomingMessage) => Promise<VercelProxyResponse>> {
   const userCode = await compileUserCode(
     entrypointFullPath,
@@ -167,9 +168,12 @@ export async function createEdgeEventHandler(
       // We can't currently get a real stack trace from the Edge Function error,
       // but we can fake a basic one that is still usefult to the user.
       const fakeStackTrace = `    at (${entrypointRelativePath})`;
-      const urlPath = extractUrlPath(entrypointRelativePath);
+      const requestPath = entrypointToRequestPath(
+        entrypointRelativePath,
+        isZeroConfig
+      );
       console.log(
-        `Error from API Route ${urlPath}: ${body}\n${fakeStackTrace}`
+        `Error from API Route ${requestPath}: ${body}\n${fakeStackTrace}`
       );
 
       // this matches the serverless function bridge launcher's behavior when
@@ -186,12 +190,11 @@ export async function createEdgeEventHandler(
   };
 }
 
-// turns "api/some.func.js" into "api/some.func"
-function extractUrlPath(entrypointRelativePath: string) {
-  const parts = entrypointRelativePath.split('.');
-  if (parts.length === 1) {
-    return entrypointRelativePath;
-  }
-  parts.pop();
-  return parts.join('.');
+function entrypointToRequestPath(
+  entrypointRelativePath: string,
+  isZeroConfig?: boolean
+) {
+  // ensure the path starts with a slash to match conventions used elsewhere,
+  // notably when rendering serverless function paths in error messages
+  return '/' + entrypointToOutputPath(entrypointRelativePath, isZeroConfig);
 }
