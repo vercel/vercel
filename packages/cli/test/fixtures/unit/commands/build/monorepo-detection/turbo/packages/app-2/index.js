b45052adda828f5c8b6eec0fd37021435f86fd86
chloetedder <chloe.tedder@vercel.com>
2023-02-14 13:27:47 -0700

    Change tests

--- packages/cli/test/fixtures/unit/commands/build/monorepo-detection/turbo/packages/app-2/index.js
+++ packages/cli/test/fixtures/unit/commands/build/monorepo-detection/turbo/packages/app-2/index.js
@@ -4,4 +4,4 @@ const fs = require('node:fs');
 const dist = path.join(__dirname, 'dist');
 fs.rmSync(dist, { recursive: true, force: true });
 fs.mkdirSync(dist);
-fs.writeFileSync(path.join(dist, 'index.js'), 'module.exports = "world"');
+fs.writeFileSync(path.join(dist, 'index.txt'), `Hello`);
