bd12262d73eadca78ae1122e3ed639a40e8f70a0
Nathan Rajlich <n@n8.io>
2023-02-16 11:25:35 -0800

    .

--- packages/remix/test/fixtures/01-remix-basics/app/routes/instanceof.tsx
+++ packages/remix/test/fixtures/01-remix-basics/app/routes/instanceof.tsx
@@ -1,4 +1,4 @@
-import { json } from '@remix-run/node';
+import { json } from '@remix-run/server-runtime';
 import { useLoaderData } from '@remix-run/react';
 import type { LoaderArgs } from '@remix-run/server-runtime';
 
