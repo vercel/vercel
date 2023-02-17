63211b8b89f7d3e5147a49c1de9cc66424c51d45
Nathan Rajlich <n@n8.io>
2023-02-17 10:46:33 -0800

    [remix] Add unit tests (#9469)
    Moves parts of the `@vercel/remix` builder into util functions that have isolated unit tests. No functionality changes.

--- packages/remix/src/utils.ts
+++ packages/remix/src/utils.ts
@@ -1,5 +1,10 @@
-import { existsSync } from 'fs';
 import { join } from 'path';
+import { existsSync } from 'fs';
+import { pathToRegexp, Key } from 'path-to-regexp';
+import type {
+  ConfigRoute,
+  RouteManifest,
+} from '@remix-run/dev/dist/config/routes';
 
 const configExts = ['.js', '.cjs', '.mjs'];
 
@@ -11,4 +16,40 @@ export function findConfig(dir: string, basename: string): string | undefined {
   }
 
   return undefined;
+}
+
+export function isLayoutRoute(
+  routeId: string,
+  routes: Pick<ConfigRoute, 'id' | 'parentId'>[]
+): boolean {
+  return routes.some(r => r.parentId === routeId);
+}
+
+export function getPathFromRoute(
+  route: ConfigRoute,
+  routes: RouteManifest
+): string {
+  let currentRoute: ConfigRoute | undefined = route;
+  const pathParts: string[] = [];
+  do {
+    if (currentRoute.index) pathParts.push('index');
+    if (currentRoute.path) pathParts.push(currentRoute.path);
+    if (currentRoute.parentId) {
+      currentRoute = routes[currentRoute.parentId];
+    } else {
+      currentRoute = undefined;
+    }
+  } while (currentRoute);
+  const path = pathParts.reverse().join('/');
+  return path;
+}
+
+export function getRegExpFromPath(path: string): RegExp | false {
+  const keys: Key[] = [];
+  // Replace "/*" at the end to handle "splat routes"
+  const splatPath = '/:params+';
+  const rePath =
+    path === '*' ? splatPath : `/${path.replace(/\/\*$/, splatPath)}`;
+  const re = pathToRegexp(rePath, keys);
+  return keys.length > 0 ? re : false;
 }
