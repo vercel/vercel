6e76d4f27d98f1f6d3ba609651f18abd0443feac
Evan Luo <luojiashao@gmail.com>
2023-02-17 15:06:45 -0500

    hexo: update to latest 6.3.0 version

--- examples/hexo/themes/landscape/source/js/script.js
+++ examples/hexo/themes/landscape/source/js/script.js
@@ -1,138 +1,109 @@
-(function($) {
+(function($){
   // Search
   var $searchWrap = $('#search-form-wrap'),
     isSearchAnim = false,
     searchAnimDuration = 200;
 
-  var startSearchAnim = function() {
+  var startSearchAnim = function(){
     isSearchAnim = true;
   };
 
-  var stopSearchAnim = function(callback) {
-    setTimeout(function() {
+  var stopSearchAnim = function(callback){
+    setTimeout(function(){
       isSearchAnim = false;
       callback && callback();
     }, searchAnimDuration);
   };
 
-  $('#nav-search-btn').on('click', function() {
+  $('#nav-search-btn').on('click', function(){
     if (isSearchAnim) return;
 
     startSearchAnim();
     $searchWrap.addClass('on');
-    stopSearchAnim(function() {
+    stopSearchAnim(function(){
       $('.search-form-input').focus();
     });
   });
 
-  $('.search-form-input').on('blur', function() {
+  $('.search-form-input').on('blur', function(){
     startSearchAnim();
     $searchWrap.removeClass('on');
     stopSearchAnim();
   });
 
   // Share
-  $('body')
-    .on('click', function() {
-      $('.article-share-box.on').removeClass('on');
-    })
-    .on('click', '.article-share-link', function(e) {
-      e.stopPropagation();
+  $('body').on('click', function(){
+    $('.article-share-box.on').removeClass('on');
+  }).on('click', '.article-share-link', function(e){
+    e.stopPropagation();
 
-      var $this = $(this),
-        url = $this.attr('data-url'),
-        encodedUrl = encodeURIComponent(url),
-        id = 'article-share-box-' + $this.attr('data-id'),
-        offset = $this.offset();
+    var $this = $(this),
+      url = $this.attr('data-url'),
+      encodedUrl = encodeURIComponent(url),
+      id = 'article-share-box-' + $this.attr('data-id'),
+      title = $this.attr('data-title'),
+      offset = $this.offset();
 
-      if ($('#' + id).length) {
-        var box = $('#' + id);
+    if ($('#' + id).length){
+      var box = $('#' + id);
 
-        if (box.hasClass('on')) {
-          box.removeClass('on');
-          return;
-        }
-      } else {
-        var html = [
-          '<div id="' + id + '" class="article-share-box">',
+      if (box.hasClass('on')){
+        box.removeClass('on');
+        return;
+      }
+    } else {
+      var html = [
+        '<div id="' + id + '" class="article-share-box">',
           '<input class="article-share-input" value="' + url + '">',
           '<div class="article-share-links">',
-          '<a href="https://twitter.com/intent/tweet?url=' +
-            encodedUrl +
-            '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-          '<a href="https://www.facebook.com/sharer.php?u=' +
-            encodedUrl +
-            '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-          '<a href="http://pinterest.com/pin/create/button/?url=' +
-            encodedUrl +
-            '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-          '<a href="https://plus.google.com/share?url=' +
-            encodedUrl +
-            '" class="article-share-google" target="_blank" title="Google+"></a>',
+            '<a href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
+            '<a href="https://www.linkedin.com/shareArticle?mini=true&url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="LinkedIn"></a>',
           '</div>',
-          '</div>',
-        ].join('');
+        '</div>'
+      ].join('');
 
-        var box = $(html);
+      var box = $(html);
 
-        $('body').append(box);
-      }
+      $('body').append(box);
+    }
 
-      $('.article-share-box.on').hide();
+    $('.article-share-box.on').hide();
 
-      box
-        .css({
-          top: offset.top + 25,
-          left: offset.left,
-        })
-        .addClass('on');
-    })
-    .on('click', '.article-share-box', function(e) {
-      e.stopPropagation();
-    })
-    .on('click', '.article-share-box-input', function() {
-      $(this).select();
-    })
-    .on('click', '.article-share-box-link', function(e) {
-      e.preventDefault();
-      e.stopPropagation();
+    box.css({
+      top: offset.top + 25,
+      left: offset.left
+    }).addClass('on');
+  }).on('click', '.article-share-box', function(e){
+    e.stopPropagation();
+  }).on('click', '.article-share-box-input', function(){
+    $(this).select();
+  }).on('click', '.article-share-box-link', function(e){
+    e.preventDefault();
+    e.stopPropagation();
 
-      window.open(
-        this.href,
-        'article-share-box-window-' + Date.now(),
-        'width=500,height=450'
-      );
-    });
-
-  // Caption
-  $('.article-entry').each(function(i) {
-    $(this)
-      .find('img')
-      .each(function() {
-        if (
-          $(this)
-            .parent()
-            .hasClass('fancybox')
-        )
-          return;
-
-        var alt = this.alt;
-
-        if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-        $(this).wrap(
-          '<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>'
-        );
-      });
-
-    $(this)
-      .find('.fancybox')
-      .each(function() {
-        $(this).attr('rel', 'article' + i);
-      });
+    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
   });
 
-  if ($.fancybox) {
+  // Caption
+  $('.article-entry').each(function(i){
+    $(this).find('img').each(function(){
+      if ($(this).parent().hasClass('fancybox') || $(this).parent().is('a')) return;
+
+      var alt = this.alt;
+
+      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+
+      $(this).wrap('<a href="' + this.src + '" data-fancybox=\"gallery\" data-caption="' + alt + '"></a>')
+    });
+
+    $(this).find('.fancybox').each(function(){
+      $(this).attr('rel', 'article' + i);
+    });
+  });
+
+  if ($.fancybox){
     $('.fancybox').fancybox();
   }
 
@@ -141,17 +112,17 @@
     isMobileNavAnim = false,
     mobileNavAnimDuration = 200;
 
-  var startMobileNavAnim = function() {
+  var startMobileNavAnim = function(){
     isMobileNavAnim = true;
   };
 
-  var stopMobileNavAnim = function() {
-    setTimeout(function() {
+  var stopMobileNavAnim = function(){
+    setTimeout(function(){
       isMobileNavAnim = false;
     }, mobileNavAnimDuration);
-  };
+  }
 
-  $('#main-nav-toggle').on('click', function() {
+  $('#main-nav-toggle').on('click', function(){
     if (isMobileNavAnim) return;
 
     startMobileNavAnim();
@@ -159,7 +130,7 @@
     stopMobileNavAnim();
   });
 
-  $('#wrap').on('click', function() {
+  $('#wrap').on('click', function(){
     if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
 
     $container.removeClass('mobile-nav-on');
