f39b40d029434c0be0e6f22bd3c36cec07c545ac
Nathan Rajlich <n@n8.io>
2023-02-23 23:20:19 -0800

    [tests] Properly test for multiple headers with same name in probes
    Updates the `responseHeaders` probe checks to properly test for multiple
    headers with the same name.
    
    Previously the probes were using `headers.get()` which concats multiple
    headers into a single string, which results in the check not really
    checking if there are in fact multiple headers with the same name.
    Using `headers.raw()` allows us to properly test for that.
    
    A couple of Python tests that were already checking for multiple
    `set-cookie` headers needed to be updated to match the full value, since
    the check now properly validates the full string match of each header
    (before it was basically just doing a `string.includes()` check).
    
    This is a precursor for https://github.com/vercel/vercel/pull/9533.

--- test/lib/deployment/test-deployment.js
+++ test/lib/deployment/test-deployment.js
@@ -222,24 +222,46 @@ async function runProbe(probe, deploymentId, deploymentUrl, ctx) {
 
   if (probe.responseHeaders) {
     // eslint-disable-next-line no-loop-func
+    const rawHeaders = resp.headers.raw();
     Object.keys(probe.responseHeaders).forEach(header => {
-      const actual = resp.headers.get(header);
-      const expected = probe.responseHeaders[header];
-      const isEqual = Array.isArray(expected)
-        ? expected.every(h => actual.includes(h))
-        : typeof expected === 'string' &&
-          expected.startsWith('/') &&
-          expected.endsWith('/')
-        ? new RegExp(expected.slice(1, -1)).test(actual)
-        : expected === actual;
-      if (!isEqual) {
-        const headers = Array.from(resp.headers.entries())
-          .map(([k, v]) => `  ${k}=${v}`)
-          .join('\n');
+      const actualArr = rawHeaders[header.toLowerCase()];
+      let expectedArr = probe.responseHeaders[header];
 
-        throw new Error(
-          `Page ${probeUrl} does not have expected response header ${header}.\n\nExpected: ${expected}.\n\nActual: ${headers}`
-        );
+      // Header should not exist
+      if (expectedArr === null) {
+        if (actualArr) {
+          const headers = Array.from(resp.headers.entries())
+            .map(([k, v]) => `  ${k}=${v}`)
+            .join('\n');
+          throw new Error(
+            `Page ${probeUrl} contains response header "${header}", but probe says it should not.\n\nActual: ${headers}`
+          );
+        }
+        return;
+      }
+
+      if (!Array.isArray(expectedArr)) {
+        expectedArr = [expectedArr];
+      }
+      console.log({ header, actualArr, expectedArr });
+      for (const expected of expectedArr) {
+        let isEqual = false;
+        for (const actual of actualArr) {
+          isEqual =
+            expected.startsWith('/') && expected.endsWith('/')
+              ? new RegExp(expected.slice(1, -1)).test(actual)
+              : expected === actual;
+          if (isEqual) break;
+        }
+        if (!isEqual) {
+          const headers = Array.from(resp.headers.entries())
+            .map(([k, v]) => `  ${k}=${v}`)
+            .join('\n');
+
+          throw new Error(
+            `Page ${probeUrl} does not have expected response header ${header}.\n\nExpected: ${expected}.\n\nActual: ${headers}`
+          );
+        }
       }
     });
     hadTest = true;
