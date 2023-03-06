837c34771e4a6042018afc49633d3da049b63a3a
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-03-06 16:38:48 -0700

    fix bad import replace

--- packages/cli/src/util/login/verify.ts
+++ packages/cli/src/util/login/verify.ts
@@ -2,7 +2,7 @@ import { URL } from 'url';
 import Client from '../client';
 import { hostname } from 'os';
 import { getTitleName } from '../pkg-name';
-import { LoginResultSuccess } from '@vercel-internals/types';
+import { LoginResultSuccess } from './types';
 
 export default function verify(
   client: Client,
