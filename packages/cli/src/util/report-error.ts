53a575e260c0f096caa2f8341fe1bb587bec2681
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-03-08 10:36:54 -0700

    fix import for get-args

--- packages/cli/src/util/report-error.ts
+++ packages/cli/src/util/report-error.ts
@@ -1,6 +1,6 @@
 import Client from './client';
 import getScope from './get-scope';
-import getArgs from './get-args';
+import { getArgs } from '@vercel-internals/utils';
 import { isError } from '@vercel/error-utils';
 import type { Team, User } from '@vercel-internals/types';
 
