3ad8dad74066336214eb561be78d31c7a03983e1
Sean Massa <endangeredmassa@gmail.com>
2023-03-09 09:53:37 -0600

    fix

--- packages/cli/test/integration.test.ts
+++ packages/cli/test/integration.test.ts
@@ -2366,7 +2366,7 @@ test('[vercel dev] fails when development commad calls vercel dev recursively', 
   const { exitCode, stdout, stderr } = await dev;
 
   expect(exitCode, formatOutput({ stdout, stderr })).toBe(1);
-  expect(stderr).toContain('must not recursively invoke itself'));
+  expect(stderr).toContain('must not recursively invoke itself');
 });
 
 test('`vercel rm` removes a deployment', async () => {
