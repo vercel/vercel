4ecf4dd214de2cc5d7984f271e3934f4fe11a710
Sean Massa <endangeredmassa@gmail.com>
2023-03-14 13:54:10 -0500

    hardlink dir tree

--- packages/gatsby-plugin-vercel-builder/src/helpers/static.ts
+++ packages/gatsby-plugin-vercel-builder/src/helpers/static.ts
@@ -1,5 +1,40 @@
 import { join, dirname } from 'path';
-import { copy, ensureDir, link } from 'fs-extra';
+import { copy, ensureDir, link, readdir, stat } from 'fs-extra';
+
+async function hardlinkFileTree(
+  rootAbsolutePath: string,
+  targetAbsolutePath: string,
+  currentRelativePath = '.'
+) {
+  console.log(
+    `hardlinkFileTree("${rootAbsolutePath}", "${targetAbsolutePath}", "${currentRelativePath}");`
+  );
+
+  const currentAbsolutePath = join(rootAbsolutePath, currentRelativePath);
+  const realtiveFilePaths = await readdir(currentAbsolutePath);
+
+  for (const relativePath of realtiveFilePaths) {
+    const currentAbsoluteSubPath = join(currentAbsolutePath, relativePath);
+    const currentPathStat = await stat(currentAbsoluteSubPath);
+    if (currentPathStat.isDirectory()) {
+      await hardlinkFileTree(
+        rootAbsolutePath,
+        targetAbsolutePath,
+        join(currentRelativePath, relativePath)
+      );
+    } else {
+      const linkDestPath = join(
+        targetAbsolutePath,
+        currentRelativePath,
+        relativePath
+      );
+
+      console.log(`link("${currentAbsoluteSubPath}", "${linkDestPath}");`);
+
+      await link(currentAbsoluteSubPath, linkDestPath);
+    }
+  }
+}
 
 export async function createStaticDir(prefix?: string) {
   const publicDir = join(process.cwd(), 'public');
@@ -12,10 +47,10 @@ export async function createStaticDir(prefix?: string) {
   );
   await ensureDir(dirname(targetDir));
   try {
-    await link(publicDir, targetDir);
+    await hardlinkFileTree(publicDir, targetDir);
   } catch (err: any) {
     console.error(
-      `Failed to move "public" dir from "${publicDir}" to "${targetDir}". Copying instead.`,
+      `Failed to hardlink "public" dir files from "${publicDir}" to "${targetDir}". Copying instead.`,
       err
     );
     await copy(publicDir, targetDir);
