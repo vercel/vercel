753cc0c6f0f9029b150655602795f47882758cba
Sean Massa <endangeredmassa@gmail.com>
2023-03-15 11:33:56 -0500

    use pnpm version of hardLinkDir

--- packages/gatsby-plugin-vercel-builder/src/helpers/static.ts
+++ packages/gatsby-plugin-vercel-builder/src/helpers/static.ts
@@ -1,35 +1,6 @@
-import { join, dirname } from 'path';
-import { copy, ensureDir, link, readdir, stat } from 'fs-extra';
-
-async function hardlinkFileTree(
-  rootAbsolutePath: string,
-  targetAbsolutePath: string,
-  currentRelativePath = '.'
-) {
-  const currentAbsolutePath = join(rootAbsolutePath, currentRelativePath);
-  const realtiveFilePaths = await readdir(currentAbsolutePath);
-
-  for (const relativePath of realtiveFilePaths) {
-    const currentAbsoluteSubPath = join(currentAbsolutePath, relativePath);
-    const currentPathStat = await stat(currentAbsoluteSubPath);
-    if (currentPathStat.isDirectory()) {
-      await hardlinkFileTree(
-        rootAbsolutePath,
-        targetAbsolutePath,
-        join(currentRelativePath, relativePath)
-      );
-    } else {
-      const linkDestPath = join(
-        targetAbsolutePath,
-        currentRelativePath,
-        relativePath
-      );
-
-      await ensureDir(dirname(linkDestPath));
-      await link(currentAbsoluteSubPath, linkDestPath);
-    }
-  }
-}
+import { join } from 'path';
+import { copy } from 'fs-extra';
+import { hardLinkDir } from './hard-link-dir';
 
 export async function createStaticDir(prefix?: string) {
   const publicDir = join(process.cwd(), 'public');
@@ -42,7 +13,7 @@ export async function createStaticDir(prefix?: string) {
   );
 
   try {
-    await hardlinkFileTree(publicDir, targetDir);
+    await hardLinkDir(publicDir, [targetDir]);
   } catch (err: any) {
     console.error(
       `Failed to hardlink "public" dir files from "${publicDir}" to "${targetDir}". Copying instead.`,
