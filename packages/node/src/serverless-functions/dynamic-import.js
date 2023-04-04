95dfb5cfd3c75f685b9c453d12b2b965a42834db
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-04-04 11:25:37 +0200

    test: add dynamic-import assertion

--- packages/node/src/serverless-functions/dynamic-import.js
+++ packages/node/src/serverless-functions/dynamic-import.js
@@ -1,3 +1,22 @@
+'use strict';
+
+const { pathToFileURL } = require('url');
+const { isAbsolute } = require('path');
+
+function dynamicImport(filepath) {
+  const id = isAbsolute(filepath) ? pathToFileURL(filepath).href : filepath;
+  let fn = import(id).then(mod => mod.default);
+
+  /**
+   * In some cases we might have nested default props due to TS => JS
+   */
+  for (let i = 0; i < 5; i++) {
+    if (fn.default) fn = fn.default;
+  }
+
+  return fn;
+}
+
 module.exports = {
-  dynamicImport: filepath => import(filepath).then(mod => mod.default),
+  dynamicImport,
 };
