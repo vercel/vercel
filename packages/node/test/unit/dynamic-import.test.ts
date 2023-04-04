66198d3e8636b0bdd75b170a220f9ff4abb7fead
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-04-04 11:25:37 +0200

    test: add dynamic-import assertion

---
+++ packages/node/test/unit/dynamic-import.test.ts
@@ -0,0 +1,13 @@
+// @ts-expect-error
+import { dynamicImport } from '../../src/serverless-functions/dynamic-import.js';
+import { resolve } from 'path';
+
+describe('dynamic-import', () => {
+  test('load esm code', async () => {
+    const entrypointPath = resolve(__dirname, '../dev-fixtures/esm-module.mjs');
+    const fn = await dynamicImport(entrypointPath);
+    expect(fn.toString()).toStrictEqual(
+      "(_req, res) => {\n  res.setHeader('x-hello', 'world');\n  res.send('Hello, world!').end();\n}"
+    );
+  });
+});
