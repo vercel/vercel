8ed5588b1ce7178f3f79ea0c5c0f0e76285b7f6d
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-04-04 18:58:07 -0600

    drop slice

--- internals/get-package-json/src/index.ts
+++ internals/get-package-json/src/index.ts
@@ -11,19 +11,19 @@ export function getPackageJSON() {
   Error.prepareStackTrace = (_, stack) => stack;
 
   // The `stack` reference above is of type `Array<NodeJS.CallSite>`
-  const stack = (new Error().stack as unknown as Array<NodeJS.CallSite>).slice(
-    1
-  ); // remove self from stack
+  const stack = new Error().stack as unknown as Array<NodeJS.CallSite>;
 
   Error.prepareStackTrace = _prepareStackTrace;
 
-  // TypeScript rule noUncheckedIndexedAccess makes `stack[0]` potentially undefined;
+  // TypeScript rule noUncheckedIndexedAccess makes `stack[1]` potentially undefined;
   // however, we can safely assert it is not undefined as the callstack will always
-  // have atleast 2 entries (we slice the first above), and then rely on the second here.
-  const callsite = stack[0]!;
+  // have at least 2 entries, the first being this function, and the second wherever
+  // its being called from.
+  const callsite = stack[1]!;
 
-  // it is guarranteed that the only way for `getFileName` to return `undefined` is if this function is called using `eval`
-  // thus it is safe to assert `filePath` is defined at this point.
+  // it is guarranteed that the only way for `getFileName` to return `undefined` is if
+  // this function is called using `eval` thus it is safe to assert `filePath` is defined
+  // at this point.
   const filePath = (callsite.getFileName() ||
     callsite.getEvalOrigin()) as string; // get the file name of where this function was called
 
