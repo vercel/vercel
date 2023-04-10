d4e3f7245bfe8214881eeda32ccab6d2a11b8dde
Chris Barber <chris.barber@vercel.com>
2023-04-10 14:23:53 -0500

    Removed unnecessary jest config

--- packages/node/jest.config.js
+++ packages/node/jest.config.js
@@ -1,10 +1,6 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
-
-const { defaults } = require('jest-config');
-
 module.exports = {
   preset: 'ts-jest',
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
   transform: {
     '^.+\\.m?[tj]s$': [
       'ts-jest',
