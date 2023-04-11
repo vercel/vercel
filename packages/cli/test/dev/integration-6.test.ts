53661d173f86a32360bf20dfcccc74316d264ef5
Chris Barber <chris.barber@vercel.com>
2023-04-11 09:42:00 -0500

    Finished the esm vc dev tests

--- packages/cli/test/dev/integration-6.test.ts
+++ packages/cli/test/dev/integration-6.test.ts
@@ -4,16 +4,116 @@ const {
   validateResponseHeaders,
 } = require('./utils.js');
 
-test(
-  '[vercel dev] ESM .js edge type=module',
-  testFixtureStdio(
-    'esm-js-edge-module',
-    async (_testPath: any, port: any) => {
-      const res = await fetch(`http://localhost:${port}/api/data`);
-      validateResponseHeaders(res);
-      const body = await res.json();
-      expect(body).toHaveProperty('isLeapYear');
-    },
-    { skipDeploy: true }
-  )
-);
+describe('[vercel dev] ESM edge functions', () => {
+  test(
+    '[vercel dev] ESM .js type=module',
+    testFixtureStdio(
+      'esm-js-edge-module',
+      async (_testPath: any, port: any) => {
+        const res = await fetch(`http://localhost:${port}/api/data`);
+        validateResponseHeaders(res);
+        const body = await res.json();
+        expect(body).toHaveProperty('isLeapYear');
+      },
+      { skipDeploy: true }
+    )
+  );
+
+  test(
+    '[vercel dev] ESM .ts type=module',
+    testFixtureStdio(
+      'esm-ts-edge-module',
+      async (_testPath: any, port: any) => {
+        const res = await fetch(`http://localhost:${port}/api/data`);
+        validateResponseHeaders(res);
+        const body = await res.json();
+        expect(body).toHaveProperty('isLeapYear');
+      },
+      { skipDeploy: true }
+    )
+  );
+
+  test(
+    '[vercel dev] ESM .js type=commonjs',
+    testFixtureStdio(
+      'esm-js-edge-no-module',
+      async (_testPath: any, port: any) => {
+        const res = await fetch(`http://localhost:${port}/api/data`);
+        validateResponseHeaders(res);
+        const body = await res.json();
+        expect(body).toHaveProperty('isLeapYear');
+      },
+      { skipDeploy: true }
+    )
+  );
+
+  test(
+    '[vercel dev] ESM .ts type=commonjs',
+    testFixtureStdio(
+      'esm-ts-edge-no-module',
+      async (_testPath: any, port: any) => {
+        const res = await fetch(`http://localhost:${port}/api/data`);
+        validateResponseHeaders(res);
+        const body = await res.json();
+        expect(body).toHaveProperty('isLeapYear');
+      },
+      { skipDeploy: true }
+    )
+  );
+});
+
+describe('[vercel dev] ESM serverless functions', () => {
+  test(
+    '[vercel dev] ESM .js type=module',
+    testFixtureStdio(
+      'esm-js-nodejs-module',
+      async (_testPath: any, port: any) => {
+        const res = await fetch(`http://localhost:${port}/api/data`);
+        validateResponseHeaders(res);
+        const body = await res.json();
+        expect(body).toHaveProperty('isLeapYear');
+      },
+      { skipDeploy: true }
+    )
+  );
+
+  test(
+    '[vercel dev] ESM .ts type=module',
+    testFixtureStdio(
+      'esm-ts-nodejs-module',
+      async (_testPath: any, port: any) => {
+        const res = await fetch(`http://localhost:${port}/api/data`);
+        validateResponseHeaders(res);
+        const body = await res.json();
+        expect(body).toHaveProperty('isLeapYear');
+      },
+      { skipDeploy: true }
+    )
+  );
+
+  test(
+    '[vercel dev] ESM .js type=commonjs',
+    testFixtureStdio(
+      'esm-js-nodejs-no-module',
+      async (testPath: any) => {
+        // bad gateway
+        // require() of ESM Module
+        await testPath(502, '/api/data');
+      },
+      { skipDeploy: true }
+    )
+  );
+
+  test(
+    '[vercel dev] ESM .ts type=commonjs',
+    testFixtureStdio(
+      'esm-ts-nodejs-no-module',
+      async (testPath: any) => {
+        // bad gateway
+        // require() of ESM Module
+        await testPath(502, '/api/data');
+      },
+      { skipDeploy: true }
+    )
+  );
+});
