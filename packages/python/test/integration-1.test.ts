ce26fb922d349e53b07a60c10697f3569ae881d4
Sean Massa <EndangeredMassa@gmail.com>
2023-04-13 16:24:42 -0500

    Update packages/python/test/integration-1.test.ts
    Co-authored-by: Steven <steven@ceriously.com>

--- packages/python/test/integration-1.test.ts
+++ packages/python/test/integration-1.test.ts
@@ -1,6 +1,6 @@
 require('./integration-setup')(1);
 
-// NOTE: The fixture `00-requrest-path` has special case handling, which is why
+// NOTE: The fixture `00-request-path` has special case handling, which is why
 // it's broken out below. This is also why this fixture is filtered out of the
 // chunking in "integration-setup".
 
