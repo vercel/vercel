529a1dc94fd9034b1b9ed53a2bedfb74117b0b42
Chris Barber <chris.barber@vercel.com>
2023-05-04 15:16:47 -0500

    [tests] Pin Next.js version in test fixtures

--- utils/update-next.js
+++ utils/update-next.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-console */
+
+const { dirname, join } = require('path');
 const { execFileSync } = require('child_process');
+const { glob } = require('glob');
+const { pathExists, readJson, writeJson } = require('fs-extra');
 
 function exec(cmd, args, opts) {
-  // eslint-disable-next-line no-console
   console.log({ input: `${cmd} ${args.join(' ')}` });
   const output = execFileSync(cmd, args, opts).toString().trim();
   console.log({ output });
@@ -10,36 +14,92 @@ function exec(cmd, args, opts) {
 }
 
 module.exports = async ({ github, context }) => {
-  const oldVersion = require('../examples/nextjs/package.json').dependencies
-    .next;
   const newVersion = exec('npm', ['view', 'next', 'dist-tags.latest']);
   const branch = `next-${newVersion.replaceAll('.', '-')}`;
 
-  if (oldVersion === newVersion) {
-    // eslint-disable-next-line no-console
+  if (
+    exec('git', ['ls-remote', '--heads', 'origin', branch]).toString().trim()
+  ) {
+    console.log(`Branch ${branch} already exists, skipping update.`);
+    return;
+  }
+
+  let updatedCount = 0;
+
+  // update `examples/nextjs`
+  const oldVersion = require('../examples/nextjs/package.json').dependencies
+    .next;
+  if (oldVersion !== newVersion) {
+    updatedCount++;
+    exec('rm', ['-rf', './examples/nextjs']);
+    exec('npx', ['--yes', 'create-next-app@latest', './examples/nextjs']);
+    exec('git', [
+      'config',
+      '--global',
+      'user.email',
+      'infra+release@vercel.com',
+    ]);
+    exec('git', ['config', '--global', 'user.name', 'vercel-release-bot']);
+    exec('git', ['checkout', 'main']);
+    exec('git', ['checkout', '-b', branch]);
+    exec('git', ['add', '-A']);
+    exec('git', ['commit', '-m', branch]);
+    exec('git', ['push', 'origin', branch]);
+  }
+
+  // update tests
+  const testGlobs = [
+    'packages/cli/test/dev/fixtures/*/package.json',
+    'packages/cli/test/fixtures/unit/**/package.json',
+    'packages/fs-detectors/test/fixtures/**/package.json',
+    'packages/next/test/fixtures/*/package.json',
+    'packages/next/test/integration/*/package.json',
+    'packages/static-build/test/fixtures/*/package.json',
+  ];
+  for (const pattern of testGlobs) {
+    const files = await glob(pattern);
+    for (const file of files) {
+      const pkgJson = await readJson(file);
+      if (pkgJson.ignoreNextjsUpdates) {
+        continue;
+      }
+
+      const dependencies = [pkgJson.dependencies, pkgJson.devDependencies];
+      for (const deps of dependencies) {
+        const oldVersion = deps?.next;
+        if (oldVersion && oldVersion !== newVersion) {
+          updatedCount++;
+          deps.next = newVersion;
+          await writeJson(file, pkgJson);
+
+          // update lock file
+          const cwd = dirname(file);
+          if (await pathExists(join(cwd, 'yarn.lock'))) {
+            exec('yarn', ['generate-lock-entry'], { cwd });
+          } else if (await pathExists(join(cwd, 'pnpm-lock.yaml'))) {
+            exec('pnpm', ['install', '--lockfile-only'], { cwd });
+          } else if (await pathExists(join(cwd, 'package-lock.json'))) {
+            exec('npm', ['install', '--package-lock-only'], { cwd });
+          }
+
+          break;
+        }
+      }
+    }
+  }
+
+  if (!updatedCount) {
     console.log(
       `Next.js version ${newVersion} did not change, skipping update.`
     );
     return;
   }
 
-  if (
-    exec('git', ['ls-remote', '--heads', 'origin', branch]).toString().trim()
-  ) {
-    // eslint-disable-next-line no-console
-    console.log(`Branch ${branch} already exists, skipping update.`);
-    return;
-  }
-
-  exec('rm', ['-rf', './examples/nextjs']);
-  exec('npx', ['--yes', 'create-next-app@latest', './examples/nextjs']);
-  exec('git', ['config', '--global', 'user.email', 'infra+release@vercel.com']);
-  exec('git', ['config', '--global', 'user.name', 'vercel-release-bot']);
-  exec('git', ['checkout', 'main']);
-  exec('git', ['checkout', '-b', branch]);
-  exec('git', ['add', '-A']);
-  exec('git', ['commit', '-m', branch]);
-  exec('git', ['push', 'origin', branch]);
+  console.log(
+    `Updated ${updatedCount} package${
+      updatedCount === 1 ? '' : 's'
+    } to Next.js version ${newVersion}`
+  );
 
   const { repo, owner } = context.repo;
 
@@ -48,8 +108,10 @@ module.exports = async ({ github, context }) => {
     repo,
     head: branch,
     base: 'main',
-    title: `[examples] Upgrade Next.js to version ${newVersion}`,
-    body: `This auto-generated PR updates Next.js to version ${newVersion}`,
+    title: `[examples][tests] Upgrade Next.js to version ${newVersion}`,
+    body: `This auto-generated PR updates ${updatedCount} package${
+      updatedCount === 1 ? '' : 's'
+    } to Next.js version ${newVersion}`,
   });
 
   await github.rest.pulls.requestReviewers({
@@ -63,6 +125,6 @@ module.exports = async ({ github, context }) => {
     owner,
     repo,
     issue_number: pr.data.number,
-    labels: ['area: examples', 'semver: none', 'pr: automerge'],
+    labels: ['area: examples', 'area: tests', 'semver: none', 'pr: automerge'],
   });
 };
