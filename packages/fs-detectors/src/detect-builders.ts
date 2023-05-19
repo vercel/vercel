ab552876d353563c08c0fad24a6b722f787ab553
Sean Massa <EndangeredMassa@gmail.com>
2023-05-19 10:32:31 -0500

    Update packages/fs-detectors/src/detect-builders.ts
    Co-authored-by: Steven <steven@ceriously.com>

--- packages/fs-detectors/src/detect-builders.ts
+++ packages/fs-detectors/src/detect-builders.ts
@@ -339,7 +339,7 @@ export async function detectBuilders(
       warnings.push({
         code: 'conflicting_files',
         message:
-          'When using Next.js, it is recommended to place Node.js Functions inside of the `pages/api` (provided by Next.js) directory instead of `api` (provided by Vercel). Other (non-Node.js) Functions should still go in the `api` directory.',
+          'When using Next.js, it is recommended to place JavaScript Functions inside of the `pages/api` (provided by Next.js) directory instead of `api` (provided by Vercel). Other languages (Python, Go, etc) should still go in the `api` directory.',
         link: 'https://nextjs.org/docs/api-routes/introduction',
         action: 'Learn More',
       });
