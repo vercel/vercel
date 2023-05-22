722691a88461b5a1a30be4704d1034f912a68fb9
Chris Barber <chris.barber@vercel.com>
2023-05-22 16:09:56 -0500

    Update packages/cli/src/util/get-deployment.ts
    Co-authored-by: Nathan Rajlich <n@n8.io>

--- packages/cli/src/util/get-deployment.ts
+++ packages/cli/src/util/get-deployment.ts
@@ -10,9 +10,7 @@ import mapCertError from './certs/map-cert-error';
 import toHost from './to-host';
 
 /**
- * Retrieves a deployment by id or URL. If the deployment is not found and the
- * deployment `hostOrId` contains alpha-numeric characters only, it will retry
- * with the `dpl_` prefix.
+ * Retrieves a deployment by id or URL.
  *
  * @param client - The Vercel CLI client instance.
  * @param contextName - The scope context/team name.
