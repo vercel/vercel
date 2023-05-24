1aab908e3926e4ddd694bec702a1e0108f3f9e49
Shohei Maeda <11495867+smaeda-ks@users.noreply.github.com>
2023-05-24 15:48:23 +0900

    fix test

--- packages/next/test/integration/integration-1.test.js
+++ packages/next/test/integration/integration-1.test.js
@@ -72,7 +72,7 @@ if (parseInt(process.versions.node.split('.')[0], 10) >= 16) {
       )
     ).toBeFalsy();
 
-    expect(lambdas.size).toBe(3);
+    expect(lambdas.size).toBe(4);
 
     // RSC, root-level page.js
     expect(buildResult.output['index']).toBeDefined();
