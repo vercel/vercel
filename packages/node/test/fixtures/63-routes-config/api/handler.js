d1aec97674aeca6b1c7241680d61211c1fc5d45e
Florentin / 珞辰 <ecklf@icloud.com>
2023-07-05 10:08:25 +0200

    add test for build output

---
+++ packages/node/test/fixtures/63-routes-config/api/handler.js
@@ -0,0 +1,5 @@
+export default req => {
+  return new Response(`RANDOMNESS_PLACEHOLDER:edge`);
+};
+
+export const maxDuration = 5;
---
+++ packages/node/test/integration/integration-build-output.test.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const builder = require('../../');
+const {
+  createRunBuildLambda,
+} = require('../../../../test/lib/run-build-lambda');
+
+const runBuildLambda = createRunBuildLambda(builder);
+
+it('should have configured `maxDuration` in build output', async () => {
+  const { buildResult } = await runBuildLambda(
+    path.join(__dirname, '../fixtures/63-routes-config')
+  );
+
+  expect(buildResult.output).toBeDefined();
+  expect(typeof buildResult.output.maxDuration).toBe('number');
+  expect(buildResult.output.maxDuration).toMatchInlineSnapshot(`5`);
+});
