028a36d5d4f3acfaef2f9d3d9c1b01218e2437a6
Zack Tanner <zacktanner@gmail.com>
2023-07-17 12:02:34 -0700

    remove unnecessary changes

--- packages/next/test/fixtures/00-app-dir-edge-404/next.config.js
+++ packages/next/test/fixtures/00-app-dir-edge-404/next.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   experimental: {
     appDir: true,
-    runtime: 'nodejs',
-  },
-  rewrites: async () => {
-    return [
-      {
-        source: '/rewritten-to-dashboard',
-        destination: '/dashboard',
-      },
-    ];
   },
 };
