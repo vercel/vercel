e2cd069af42490392c16096eb7846e07f31f64b9
Trek Glowacki <trek.glowacki@vercel.com>
2023-07-26 08:18:23 -0500

    Migrate `pull` command to new structue

---
+++ packages/cli/src/commands/pull/command.ts
@@ -0,0 +1,52 @@
+import { Command } from '../help';
+import { getPkgName } from '../../util/pkg-name';
+import { getEnvTargetPlaceholder } from '../../util/env/env-target';
+
+export const pullCommand: Command = {
+  name: 'pull',
+  description: 'Show information about a deployment.',
+  arguments: [
+    {
+      name: 'project-path',
+      required: false,
+    },
+  ],
+  options: [
+    {
+      name: 'environment',
+      description: 'Deployment environment [development]',
+      argument: 'environment',
+      shorthand: null,
+      type: 'string',
+      deprecated: false,
+      multi: false,
+    },
+    {
+      name: 'yes',
+      description:
+        'Skip questions when setting up new project using default scope and settings',
+      shorthand: 'y',
+      type: 'string',
+      deprecated: false,
+      multi: false,
+    },
+  ],
+  examples: [
+    {
+      name: 'Pull the latest Environment Variables and Project Settings from the cloud',
+      value: `${getPkgName()} pull`,
+    },
+    {
+      name: 'Pull the latest Environment Variables and Project Settings from the cloud targeting a directory',
+      value: `${getPkgName()} pull ./path-to-project`,
+    },
+    {
+      name: 'Pull for a specific environment',
+      value: `${getPkgName()} pull --environment=${getEnvTargetPlaceholder()}`,
+    },
+    {
+      name: 'If you want to download environment variables to a specific file, use `vercel env pull` instead',
+      value: `${getPkgName()} env pull`,
+    },
+  ],
+};
