6dea2a961ed50e7a037713ae0b5abeb98ed00f2e
Nathan Rajlich <n@n8.io>
2023-08-14 17:12:54 -0700

    [cli] Use `vercel-php@0.6.0` in e2e test fixture

--- packages/cli/test/helpers/prepare.js
+++ packages/cli/test/helpers/prepare.js
@@ -334,7 +334,7 @@ module.exports = async function prepare(session, binaryPath, tmpFixturesDir) {
       'vercel.json': JSON.stringify({
         functions: {
           'api/**/*.php': {
-            runtime: 'vercel-php@0.5.2',
+            runtime: 'vercel-php@0.6.0',
           },
         },
       }),
