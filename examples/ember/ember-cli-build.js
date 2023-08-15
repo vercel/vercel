160beb2df62d936d5cb5127160a9878f28f76f67
Trek Glowacki <trek.glowacki@vercel.com>
2023-08-15 09:38:50 -0500

    Show welcome page in prod

--- examples/ember/ember-cli-build.js
+++ examples/ember/ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    'ember-welcome-page': {
+      enabled: true
+    }
   });
 
   return app.toTree();
