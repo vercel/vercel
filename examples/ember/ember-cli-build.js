4a59df4e320481e8aa79f9b784d8d68536de16b6
Steven <steven@ceriously.com>
2023-08-15 10:44:30 -0400

    add lockfile and set welcomepage true

--- examples/ember/ember-cli-build.js
+++ examples/ember/ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    'ember-welcome-page': {
+      enabled: true
+    }
   });
 
   return app.toTree();
