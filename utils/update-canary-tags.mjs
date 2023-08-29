1393c1c41560ff1df03d65a914bbf0bf92723fb9
Nathan Rajlich <n@n8.io>
2023-08-28 17:45:18 -0700

    Fix lint

--- utils/update-canary-tags.mjs
+++ utils/update-canary-tags.mjs
@@ -4,12 +4,12 @@ import { spawnSync } from 'child_process';
 const packagesDir = new URL('../packages/', import.meta.url);
 
 for (const name of fs.readdirSync(packagesDir)) {
-    const pkg = JSON.parse(
-      fs.readFileSync(new URL(`${name}/package.json`, packagesDir), 'utf8')
-    );
-    spawnSync(
-      'npm',
-      `dist-tag add ${pkg.name}@${pkg.version} canary`.split(' '),
-      { stdio: 'inherit' }
-    );
+  const pkg = JSON.parse(
+    fs.readFileSync(new URL(`${name}/package.json`, packagesDir), 'utf8')
+  );
+  spawnSync(
+    'npm',
+    `dist-tag add ${pkg.name}@${pkg.version} canary`.split(' '),
+    { stdio: 'inherit' }
+  );
 }
