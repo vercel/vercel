566aedf48cbc0433e5437a9e618a0f4543b24736
Dan Stowell <dan.stowell@vercel.com>
2023-08-31 11:35:45 -0700

    remove extraneous $

--- packages/cli/src/commands/env/command.ts
+++ packages/cli/src/commands/env/command.ts
@@ -82,58 +82,58 @@ export const envCommand: Command = {
     {
       name: 'Pull all Development Environment Variables down from the cloud',
       value: [
-        `$ ${packageName} env pull <file>`,
-        `$ ${packageName} env pull .env.development.local`,
+        `${packageName} env pull <file>`,
+        `${packageName} env pull .env.development.local`,
       ],
     },
     {
       name: 'Add a new variable to multiple Environments',
       value: [
-        `$ ${packageName} env add <name>`,
-        `$ ${packageName} env add API_TOKEN`,
+        `${packageName} env add <name>`,
+        `${packageName} env add API_TOKEN`,
       ],
     },
     {
       name: 'Add a new variable for a specific Environment',
       value: [
-        `$ ${packageName} env add <name> ${targetPlaceholder}`,
-        `$ ${packageName} env add DB_PASS production`,
+        `${packageName} env add <name> ${targetPlaceholder}`,
+        `${packageName} env add DB_PASS production`,
       ],
     },
     {
       name: 'Add a new variable for a specific Environment and Git Branch',
       value: [
-        `$ ${packageName} env add <name> ${targetPlaceholder} <gitbranch>`,
-        `$ ${packageName} env add DB_PASS preview feat1`,
+        `${packageName} env add <name> ${targetPlaceholder} <gitbranch>`,
+        `${packageName} env add DB_PASS preview feat1`,
       ],
     },
     {
       name: 'Add a new Environment Variable from stdin',
       value: [
-        `$ cat <file> | ${packageName} env add <name> ${targetPlaceholder}`,
-        `$ cat ~/.npmrc | ${packageName} env add NPM_RC preview`,
-        `$ ${packageName} env add API_URL production < url.txt`,
+        `cat <file> | ${packageName} env add <name> ${targetPlaceholder}`,
+        `cat ~/.npmrc | ${packageName} env add NPM_RC preview`,
+        `${packageName} env add API_URL production < url.txt`,
       ],
     },
     {
       name: 'Remove a variable from multiple Environments',
       value: [
-        `$ ${packageName} env rm <name>`,
-        `$ ${packageName} env rm API_TOKEN`,
+        `${packageName} env rm <name>`,
+        `${packageName} env rm API_TOKEN`,
       ],
     },
     {
       name: 'Remove a variable from a specific Environment',
       value: [
-        `$ ${packageName} env rm <name> ${targetPlaceholder}`,
-        `$ ${packageName} env rm NPM_RC preview`,
+        `${packageName} env rm <name> ${targetPlaceholder}`,
+        `${packageName} env rm NPM_RC preview`,
       ],
     },
     {
       name: 'Remove a variable from a specific Environment and Git Branch',
       value: [
-        `$ ${packageName} env rm <name> ${targetPlaceholder} <gitbranch>`,
-        `$ ${packageName} env rm NPM_RC preview feat1`,
+        `${packageName} env rm <name> ${targetPlaceholder} <gitbranch>`,
+        `${packageName} env rm NPM_RC preview feat1`,
       ],
     },
   ],
