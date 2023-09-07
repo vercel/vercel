1941879dd986dabc41ef078316ec98fcdabbcc86
Chris Barber <chris.barber@vercel.com>
2023-09-07 16:04:05 -0500

    Updated build script

--- packages/ruby/build.mjs
+++ packages/ruby/build.mjs
@@ -1,29 +1,3 @@
-#!/usr/bin/env node
-const fs = require('fs-extra');
-const execa = require('execa');
-const { join } = require('path');
+import { esbuild } from '../../utils/build.mjs';
 
-async function main() {
-  const outDir = join(__dirname, 'dist');
-
-  // Start fresh
-  await fs.remove(outDir);
-
-  await execa(
-    'ncc',
-    [
-      'build',
-      join(__dirname, 'index.ts'),
-      '-e',
-      '@vercel/build-utils',
-      '-o',
-      outDir,
-    ],
-    { stdio: 'inherit' }
-  );
-}
-
-main().catch(err => {
-  console.error(err);
-  process.exit(1);
-});
+await esbuild({ bundle: true, external: ['@vercel/build-utils'] });
