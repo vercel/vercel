a9f1f6f0844d25a405195296455b53ea619ea8a9
Nathan Rajlich <n@n8.io>
2023-09-07 15:34:25 -0700

    Prettier

--- utils/build-builder.mjs
+++ utils/build-builder.mjs
@@ -1,10 +1,10 @@
 /**
  * This script is the build configuration common to all our Builder packages.
  * We bundle the output using `esbuild`, and do not publish type definitions.
- * 
+ *
  * `@vercel/build-utils` is marked as external because it's always an implicit
  * dependency when the Builder is invoked by `vercel build`.
- * 
+ *
  * `@vercel/nft` is marked as external because esbuild has trouble bundling
  * it due to some optional dependencies which we don't install, but also this
  * is beneficial because the package will be de-duped at the node_modules level
