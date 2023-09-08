2b04292973d870849404cf7af88e0a7d9b228e7b
Nathan Rajlich <n@n8.io>
2023-09-08 11:35:52 -0700

    .

--- packages/static-build/src/utils/gatsby.ts
+++ packages/static-build/src/utils/gatsby.ts
@@ -14,14 +14,14 @@ type PluginName = typeof PLUGINS[number];
 const GATSBY_CONFIG_FILE = 'gatsby-config';
 const GATSBY_NODE_FILE = 'gatsby-node';
 
-const _require = createRequire(__filename);
+const require_ = createRequire(__filename);
 
 const PLUGIN_PATHS: Record<PluginName, string> = {
   '@vercel/gatsby-plugin-vercel-analytics': path.dirname(
-    _require.resolve(`@vercel/gatsby-plugin-vercel-analytics/package.json`)
+    require_.resolve(`@vercel/gatsby-plugin-vercel-analytics/package.json`)
   ),
   '@vercel/gatsby-plugin-vercel-builder': path.dirname(
-    _require.resolve(`@vercel/gatsby-plugin-vercel-builder/package.json`)
+    require_.resolve(`@vercel/gatsby-plugin-vercel-builder/package.json`)
   ),
 };
 
