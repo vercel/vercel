89bb4a2e062afe0d97c74c342a20a83c491f507a
Steven <steven@ceriously.com>
2023-09-11 17:15:13 -0400

    Update build.js to not use bun.fil()

--- packages/build-utils/test/fixtures/30-bun-v1/build.js
+++ packages/build-utils/test/fixtures/30-bun-v1/build.js
@@ -6,10 +6,3 @@ const content = say({ text })
 await rm('./public', { recursive: true, force: true })
 await mkdir('./public', { recursive: true })
 await writeFile('./public/index.txt', content)
-
-const file = Bun.file('./public/index.txt')
-const content2 = await file.text()
-
-if (content !== content2) {
-  throw new Error('the content of file write did not match the content of file read')
-}
