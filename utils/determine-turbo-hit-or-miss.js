29ea1af971cfd7076c139c598da0778f6e2b8207
Trek Glowacki <trek.glowacki@gmail.com>
2023-09-14 10:47:20 -0500

    Skip DataDog reporting if Turbo cache indicates the uploaded files would be identical (#10501)

---
+++ utils/determine-turbo-hit-or-miss.js
@@ -0,0 +1,30 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+async function main() {
+  const rootDir = path.join(__dirname, '..');
+  const turboRunDir = path.join(rootDir, '.turbo/runs');
+  const turboRunFiles = await fs.readdir(turboRunDir);
+
+  turboRunFiles.forEach(async fileName => {
+    const runFile = path.join(turboRunDir, fileName);
+    const runData = await fs.readJson(runFile);
+    const tasksReports = runData.tasks || [];
+
+    const missCount = tasksReports.reduce((total, taskData) => {
+      if (taskData.cache.status === 'MISS') {
+        return total + 1;
+      }
+
+      return total;
+    }, 0);
+
+    console.log(missCount);
+    return process.exit(missCount);
+  });
+}
+
+main().catch(err => {
+  console.log('error determining', err);
+  process.exit(1);
+});
