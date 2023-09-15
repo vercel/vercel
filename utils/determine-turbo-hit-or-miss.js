fec1b8fe8cfa792fc172f84943e9b6b079fbaf7e
Steven <steven@ceriously.com>
2023-09-15 13:49:46 -0400

    Revert "fix cache miss"
    This reverts commit a793240e83a42e8f79ab2c6135c70df357713510.

--- utils/determine-turbo-hit-or-miss.js
+++ utils/determine-turbo-hit-or-miss.js
@@ -5,21 +5,23 @@ async function main() {
   const rootDir = path.join(__dirname, '..');
   const turboRunDir = path.join(rootDir, '.turbo/runs');
   const turboRunFiles = await fs.readdir(turboRunDir);
-  let missCount = 0;
 
-  await Promise.all(
-    turboRunFiles.forEach(async fileName => {
-      const runFile = path.join(turboRunDir, fileName);
-      const runData = await fs.readJson(runFile);
-      const { tasks = [] } = runData;
-      const count = tasks.filter(
-        taskData => taskData.cache.status === 'MISS'
-      ).length;
-      missCount += count;
-    })
-  );
+  turboRunFiles.forEach(async fileName => {
+    const runFile = path.join(turboRunDir, fileName);
+    const runData = await fs.readJson(runFile);
+    const tasksReports = runData.tasks || [];
 
-  console.log(missCount);
+    const missCount = tasksReports.reduce((total, taskData) => {
+      if (taskData.cache.status === 'MISS') {
+        return total + 1;
+      }
+
+      return total;
+    }, 0);
+
+    console.log(missCount);
+    return process.exit(missCount);
+  });
 }
 
 main().catch(err => {
