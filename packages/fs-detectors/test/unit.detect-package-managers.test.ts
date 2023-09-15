aa3e13e1ea0351359cba03ef2969dc59f0dfa247
Steven <steven@ceriously.com>
2023-09-15 11:02:05 -0400

    [fs-detectors] Add support for bun detection in monorepo

--- packages/fs-detectors/test/unit.detect-package-managers.test.ts
+++ packages/fs-detectors/test/unit.detect-package-managers.test.ts
@@ -14,6 +14,8 @@ describe('package-managers', () => {
     ['54-yarn-with-corepack', 'yarn'],
     ['55-pnpm-with-lockfile', 'pnpm'],
     ['56-pnpm-with-corepack', 'pnpm'],
+    ['57-bun-with-lockfile', 'bun'],
+    ['58-bun-with-corepack', 'bun'],
   ])('with detectFramework', (fixturePath, frameworkSlug) => {
     const testName = `should detect package manager '${frameworkSlug}' for ${fixturePath}`;
 
