828f6b14184ebef25ec988ff3ea0c66b118aad07
Nathan Rajlich <n@n8.io>
2023-09-20 11:39:24 -0300

    Bring bundling back

--- packages/next/build.mjs
+++ packages/next/build.mjs
@@ -1,17 +1,27 @@
-//import { join } from 'node:path';
-//import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { readFileSync, unlinkSync } from 'node:fs';
 import { esbuild } from '../../utils/build.mjs';
 import buildEdgeFunctionTemplate from './scripts/build-edge-function-template.js';
 
-//const pkgPath = join(process.cwd(), 'package.json');
-//const pkg = JSON.parse(readFileSync(pkgPath, 'utf8'));
-//const externals = Object.keys(pkg.dependencies || {});
-
-// Compile all `.ts` files individually first,
-// so that the template files exist
-await esbuild();
+const pkgPath = join(process.cwd(), 'package.json');
+const pkg = JSON.parse(readFileSync(pkgPath, 'utf8'));
+const externals = Object.keys(pkg.dependencies || {});
 
 await Promise.all([
+  // Compile all launcher `.ts` files into the "dist" dir
+  esbuild({
+    entryPoints: [
+      'src/legacy-launcher.ts',
+      'src/server-launcher.ts',
+      'src/templated-launcher-shared.ts',
+      'src/templated-launcher.ts',
+    ],
+  }),
   buildEdgeFunctionTemplate(),
-  //esbuild({ bundle: true, external: ['@vercel/build-utils', ...externals] }),
 ]);
+
+await esbuild({ bundle: true, external: ['@vercel/build-utils', ...externals] });
+
+// The file from `buildEdgeFunctionTemplate()` has been bundled,
+// and is no longer needed, so clean it up
+unlinkSync(new URL('dist/___get-nextjs-edge-function.js', import.meta.url));
