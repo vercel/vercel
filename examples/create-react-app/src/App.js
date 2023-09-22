4691359a9146ebbfa04968321e3bbc21c7121055
AnanyaBangalore <125627636+AnanyaBangalore@users.noreply.github.com>
2023-09-22 23:22:08 +0530

    Update App.js
    test

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React  test
         </a>
       </header>
     </div>
