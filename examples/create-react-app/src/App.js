3a1d68de7eea25492341af9c5d8a7428f324a328
AnanyaBangalore <125627636+AnanyaBangalore@users.noreply.github.com>
2023-09-22 23:17:54 +0530

    Update App.js

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React dfdf
         </a>
       </header>
     </div>
