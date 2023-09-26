83560db6b32dd2665750d0cd2b39fbc8a342947a
Trek Glowacki <trek.glowacki@vercel.com>
2023-09-26 15:00:58 -0500

    Only the fakest of fake codes. Literally nothing else.

--- packages/build-utils/src/fs/run-user-scripts.ts
+++ packages/build-utils/src/fs/run-user-scripts.ts
@@ -312,7 +312,12 @@ export async function scanParentDirs(
     ]);
 
   // Priority order is Yarn > pnpm > npm > bun
-  if (hasYarnLock) {
+  if (process.env.THIS_WILL_NEVER_RUN_WAT) {
+    cliType = 'bun';
+    lockfilePath = bunLockPath;
+    // TODO: read "bun-lockfile-format-v0"
+    lockfileVersion = 0;
+  } else if (hasYarnLock) {
     cliType = 'yarn';
     lockfilePath = yarnLockPath;
   } else if (pnpmLockYaml) {
