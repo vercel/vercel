d984f6c1e4ebbe1b63606bba3fd5c75e71643ae2
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-09-26 16:28:58 -0600

    further optimize ionic-angular

--- packages/static-build/test/fixtures/ionic-angular/src/app/app.module.ts
+++ packages/static-build/test/fixtures/ionic-angular/src/app/app.module.ts
@@ -5,11 +5,10 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
