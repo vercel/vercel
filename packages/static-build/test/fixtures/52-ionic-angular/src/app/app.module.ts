1854514b3414aba1fd671854b1a436484ad95470
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-09-27 11:25:57 -0600

    fix angular?

--- packages/static-build/test/fixtures/52-ionic-angular/src/app/app.module.ts
+++ packages/static-build/test/fixtures/52-ionic-angular/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
