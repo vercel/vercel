5916a88f3e961f4565f10036d734eb30e4366687
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-09-27 11:09:24 -0600

    fix react

--- packages/static-build/test/fixtures/50-ionic-react/src/pages/Home.tsx
+++ packages/static-build/test/fixtures/50-ionic-react/src/pages/Home.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonContent, IonPage } from '@ionic/react';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Hello, World!</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <p>Hello, World!</p>
       </IonContent>
     </IonPage>
   );
