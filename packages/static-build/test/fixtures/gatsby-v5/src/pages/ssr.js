2597983e1a729b0b36c351c6b103d48a09156579
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-09-28 12:38:17 -0600

    improve gatsby-v5

--- packages/static-build/test/fixtures/gatsby-v5/src/pages/ssr.js
+++ packages/static-build/test/fixtures/gatsby-v5/src/pages/ssr.js
@@ -1,30 +1,11 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 
 const UsingSSR = ({ serverData }) => {
   return (
     <>
       <h1>
-        This page is <b>rendered server-side</b>
+        This page is {serverData.message}
       </h1>
-      <p>
-        This page is rendered server side every time the page is requested.
-        Reload it to see a(nother) random photo from{' '}
-        <code>dog.ceo/api/breed/shiba/images/random</code>:
-      </p>
-      <img
-        style={{ width: '320px', borderRadius: 'var(--border-radius)' }}
-        alt="A random dog"
-        src={serverData.message}
-      />
-      <p>
-        To learn more, head over to our{' '}
-        <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/server-side-rendering/">
-          documentation about Server Side Rendering
-        </a>
-        .
-      </p>
-      <Link to="/">Go back to the homepage</Link>
     </>
   );
 };
@@ -34,19 +15,7 @@ export const Head = () => <title>SSR Gatsby</title>;
 export default UsingSSR;
 
 export async function getServerData() {
-  try {
-    const res = await fetch(`https://dog.ceo/api/breed/shiba/images/random`);
-    if (!res.ok) {
-      throw new Error(`Response failed`);
-    }
-    return {
-      props: await res.json(),
-    };
-  } catch (error) {
-    return {
-      status: 500,
-      headers: {},
-      props: {},
-    };
+  return {
+    props: { message: 'rendered server-side' },
   }
 }
