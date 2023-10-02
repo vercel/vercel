3c10219bcc2521d4fb56732058d90836bd729afb
Steven <steven@ceriously.com>
2023-10-02 11:49:34 -0400

    [ci] Run tests with macos-13-xlarge

--- utils/chunk-tests.js
+++ utils/chunk-tests.js
@@ -8,12 +8,15 @@ const runnersMap = new Map([
     {
       min: 1,
       max: 1,
-      runners: ['ubuntu-latest', 'macos-latest', 'windows-latest'],
+      runners: ['ubuntu-latest', 'macos-13-xlarge', 'windows-latest'],
     },
   ],
   ['test-e2e', { min: 1, max: 7, runners: ['ubuntu-latest'] }],
   ['test-next-local', { min: 1, max: 5, runners: ['ubuntu-latest'] }],
-  ['test-dev', { min: 1, max: 7, runners: ['ubuntu-latest', 'macos-latest'] }],
+  [
+    'test-dev',
+    { min: 1, max: 7, runners: ['ubuntu-latest', 'macos-13-xlarge'] },
+  ],
 ]);
 
 const packageOptionsOverrides = {
