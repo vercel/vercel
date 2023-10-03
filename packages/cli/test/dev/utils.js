0b2f92a2127222c3b3ac9498afb4330190f7b334
Trek Glowacki <trek.glowacki@vercel.com>
2023-10-03 09:45:58 -0500

    Only use npm install when lockfile is present

--- packages/cli/test/dev/utils.js
+++ packages/cli/test/dev/utils.js
@@ -144,7 +144,13 @@ async function exec(directory, args = []) {
 
 async function runNpmInstall(fixturePath) {
   if (await fs.pathExists(join(fixturePath, 'package.json'))) {
-    await execa('npm', ['install', '--force'], {
+    let command;
+    if (await fs.pathExists(join(fixturePath, 'package-lock.json'))) {
+      command = 'npm';
+    } else {
+      command = 'yarn';
+    }
+    await execa(command, ['install', '--force'], {
       cwd: fixturePath,
       shell: true,
       stdio: 'inherit',
