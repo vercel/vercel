edd70cc4a3c730a8d3fe23722f6b43799754c0eb
Trek Glowacki <trek.glowacki@vercel.com>
2023-10-04 12:30:39 -0500

    Try a bran new vue 3 app

--- packages/static-build/test/fixtures/vue-3/src/App.vue
+++ packages/static-build/test/fixtures/vue-3/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.svg">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
