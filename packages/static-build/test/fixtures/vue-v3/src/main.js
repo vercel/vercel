9ed96f4de83eebf02888c7cc477832fb639de020
Trek Glowacki <trek.glowacki@vercel.com>
2023-10-06 09:22:30 -0500

    Vue SSR

--- packages/static-build/test/fixtures/vue-v3/src/main.js
+++ packages/static-build/test/fixtures/vue-v3/src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createSSRApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createSSRApp(App).mount('#app')
--- packages/static-build/test/fixtures/vue-v3/vite.config.js
+++ packages/static-build/test/fixtures/vue-v3/vite.config.js
@@ -8,6 +8,17 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    // This is necessary for our test probes
+    // so that the generated file names are deterministic.
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
