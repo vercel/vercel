f1cc739c585814269efdfb38a12e03f97ba2bba5
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-10-10 16:22:25 -0600

    delete storybook-static, add it to gitignore

--- packages/static-build/test/fixtures/storybook-v7/storybook-static/sb-manager/WithTooltip-J57HCPYA-3VOMFVVS.js
+++
@@ -1,1 +0,0 @@
-import{WithToolTipState,WithTooltipPure}from"./chunk-NBCGHFLK.js";import"./chunk-QPL63VNK.js";import"./chunk-SPUAGIB2.js";import"./chunk-QMHPSTMR.js";import"./chunk-3J6GS6FI.js";import"./chunk-RKFFWI2D.js";export{WithToolTipState,WithToolTipState as WithTooltip,WithTooltipPure};
