d41c67e73aa1995caba51a877b9d0c4883c406bf
Sri Krishna <skrishna@buf.build>
2023-10-14 09:35:08 +0530

    Add test

--- packages/node/test/unit/dev.test.ts
+++ packages/node/test/unit/dev.test.ts
@@ -1,7 +1,7 @@
 import { forkDevServer, readMessage } from '../../src/fork-dev-server';
 import { resolve, extname } from 'path';
 import { fetch } from 'undici';
-import { createServer } from 'http';
+import { createServer, request } from 'http';
 import { listen } from 'async-listen';
 import zlib from 'zlib';
 
@@ -303,6 +303,46 @@ test('allow setting multiple cookies with same name', async () => {
     });
 
     expect(response.headers.getSetCookie()).toEqual(['a=x', 'b=y', 'c=z']);
+  } finally {
+    child.kill(9);
+  }
+});
+
+test('dev server should remove transfer encoding header', async () => {
+  const child = testForkDevServer('./edge-echo.js');
+  try {
+    const result = await readMessage(child);
+    if (result.state !== 'message') {
+      throw new Error('Exited. error: ' + JSON.stringify(result.value));
+    }
+
+    const { address, port } = result.value;
+    let resResolve, resReject;
+    const resPromise = new Promise((resolve, reject) => {
+      resResolve = resolve;
+      resReject = reject;
+    });
+    // Undici will throw if we set Transfer-Encoding, we must use http.request
+    const req = request(`http://${address}:${port}/api/edge-echo`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    req.once('response', res => {
+      expect(res.statusCode).toEqual(200);
+      res.setEncoding('utf-8');
+      res.on('data', chunk => {
+        expect(chunk).toEqual('Hello!');
+      });
+      res.on('error', resReject!);
+      res.on('end', resResolve!);
+    });
+    req.on('error', resReject!);
+    req.flushHeaders();
+    req.write('Hello!');
+    req.end();
+    await resPromise;
   } finally {
     child.kill(9);
   }
