cd526284f3ea9818d75044da79a5fcfb892d9c9d
devjiwonchoi <devjiwonchoi@gmail.com>
2023-10-16 19:23:32 +0900

    feat: add standalone express app example

---
+++ examples/express/api/index.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
---
+++ examples/express/vercel.json
@@ -0,0 +1,8 @@
+{
+  "rewrites": [
+    {
+      "source": "/api/(.*)",
+      "destination": "/api"
+    }
+  ]
+}
