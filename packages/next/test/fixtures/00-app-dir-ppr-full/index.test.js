8cd54073630a3ac3756794e1ea012a738fdbac4f
Wyatt Johnson <accounts+github@wyattjoh.ca>
2023-11-07 21:52:19 -0700

    fix: import fetch

--- packages/next/test/fixtures/00-app-dir-ppr-full/index.test.js
+++ packages/next/test/fixtures/00-app-dir-ppr-full/index.test.js
@@ -1,6 +1,7 @@
 /* eslint-env jest */
 const path = require('path');
 const { deployAndTest } = require('../../utils');
+const fetch = require('../../../../../test/lib/deployment/fetch-retry');
 
 const pages = [
   { pathname: '/', dynamic: true },
@@ -22,19 +23,6 @@ const pages = [
 
 const ctx = {};
 
-const MAX_ATTEMPTS = 3;
-async function retry(fn) {
-  for (let attempt = 0; attempt < MAX_ATTEMPTS; attempt++) {
-    try {
-      return await fn();
-    } catch (err) {
-      if (attempt + 1 >= MAX_ATTEMPTS) {
-        throw err;
-      }
-    }
-  }
-}
-
 describe(`${__dirname.split(path.sep).pop()}`, () => {
   beforeAll(async () => {
     const info = await deployAndTest(__dirname);
@@ -45,19 +33,17 @@ describe(`${__dirname.split(path.sep).pop()}`, () => {
     it.each(pages.filter(p => p.dynamic))(
       'should resume $pathname',
       async ({ pathname }) => {
-        await retry(async () => {
-          const expected = `${Date.now()}:${Math.random()}`;
-          const res = await fetch(`${ctx.deploymentUrl}${pathname}`, {
-            headers: { 'X-Test-Input': expected },
-          });
-          expect(res.status).toEqual(200);
-          expect(res.headers.get('content-type')).toEqual(
-            'text/html; charset=utf-8'
-          );
-          const html = await res.text();
-          expect(html).toContain(expected);
-          expect(html).toContain('</html>');
+        const expected = `${Date.now()}:${Math.random()}`;
+        const res = await fetch(`${ctx.deploymentUrl}${pathname}`, {
+          headers: { 'X-Test-Input': expected },
         });
+        expect(res.status).toEqual(200);
+        expect(res.headers.get('content-type')).toEqual(
+          'text/html; charset=utf-8'
+        );
+        const html = await res.text();
+        expect(html).toContain(expected);
+        expect(html).toContain('</html>');
       }
     );
   });
@@ -66,56 +52,52 @@ describe(`${__dirname.split(path.sep).pop()}`, () => {
     it.each(pages)(
       'should prefetch $pathname',
       async ({ pathname, dynamic }) => {
-        await retry(async () => {
-          const unexpected = `${Date.now()}:${Math.random()}`;
-          const res = await fetch(`${ctx.deploymentUrl}${pathname}`, {
-            headers: {
-              RSC: '1',
-              'Next-Router-Prefetch': '1',
-              'X-Test-Input': unexpected,
-            },
-          });
-          expect(res.status).toEqual(200);
-          expect(res.headers.get('content-type')).toEqual('text/x-component');
-          console.log(
-            'X-NextJS-Postponed-Reason',
-            res.headers.get('X-NextJS-Postponed-Reason')
-          );
-          if (dynamic) {
-            expect(res.headers.get('X-NextJS-Postponed')).toEqual('1');
-          } else {
-            expect(res.headers.has('X-NextJS-Postponed')).toEqual(false);
-          }
-
-          // Expect that static RSC prefetches do not contain the dynamic text.
-          const text = await res.text();
-          expect(text).not.toContain(unexpected);
+        const unexpected = `${Date.now()}:${Math.random()}`;
+        const res = await fetch(`${ctx.deploymentUrl}${pathname}`, {
+          headers: {
+            RSC: '1',
+            'Next-Router-Prefetch': '1',
+            'X-Test-Input': unexpected,
+          },
         });
+        expect(res.status).toEqual(200);
+        expect(res.headers.get('content-type')).toEqual('text/x-component');
+        console.log(
+          'X-NextJS-Postponed-Reason',
+          res.headers.get('X-NextJS-Postponed-Reason')
+        );
+        if (dynamic) {
+          expect(res.headers.get('X-NextJS-Postponed')).toEqual('1');
+        } else {
+          expect(res.headers.has('X-NextJS-Postponed')).toEqual(false);
+        }
+
+        // Expect that static RSC prefetches do not contain the dynamic text.
+        const text = await res.text();
+        expect(text).not.toContain(unexpected);
       }
     );
   });
 
   describe('dynamic RSC payloads should return', () => {
     it.each(pages)('should fetch $pathname', async ({ pathname, dynamic }) => {
-      await retry(async () => {
-        const expected = `${Date.now()}:${Math.random()}`;
-        const res = await fetch(`${ctx.deploymentUrl}${pathname}`, {
-          headers: { RSC: '1', 'X-Test-Input': expected },
-        });
-        expect(res.status).toEqual(200);
-        expect(res.headers.get('content-type')).toEqual('text/x-component');
-        expect(res.headers.has('X-NextJS-Postponed')).toEqual(false);
-
-        const text = await res.text();
-        if (dynamic) {
-          // Expect that dynamic RSC prefetches do contain the dynamic text.
-          expect(text).toContain(expected);
-        } else {
-          // Expect that dynamic RSC prefetches do not contain the dynamic text
-          // when we're forced static.
-          expect(text).not.toContain(expected);
-        }
+      const expected = `${Date.now()}:${Math.random()}`;
+      const res = await fetch(`${ctx.deploymentUrl}${pathname}`, {
+        headers: { RSC: '1', 'X-Test-Input': expected },
       });
+      expect(res.status).toEqual(200);
+      expect(res.headers.get('content-type')).toEqual('text/x-component');
+      expect(res.headers.has('X-NextJS-Postponed')).toEqual(false);
+
+      const text = await res.text();
+      if (dynamic) {
+        // Expect that dynamic RSC prefetches do contain the dynamic text.
+        expect(text).toContain(expected);
+      } else {
+        // Expect that dynamic RSC prefetches do not contain the dynamic text
+        // when we're forced static.
+        expect(text).not.toContain(expected);
+      }
     });
   });
 });
