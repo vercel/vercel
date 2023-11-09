0a03b48312851d3e1add1b105b56ff0155b9c742
Chris Barber <chris.barber@vercel.com>
2023-11-09 16:48:56 -0600

    Only clear cache if yarn

--- packages/build-utils/src/fs/run-user-scripts.ts
+++ packages/build-utils/src/fs/run-user-scripts.ts
@@ -544,7 +544,9 @@ export async function runNpmInstall(
     debug(JSON.stringify(results, null, 2));
 
     try {
-      await spawnAsync(cliType, ['cache', 'clean'], opts);
+      if (cliType === 'yarn') {
+        await spawnAsync(cliType, ['cache', 'clean'], opts);
+      }
       await spawnAsync(cliType, commandArgs, opts);
     } catch (err: unknown) {
       const potentialErrorPath = path.join(
