7a2968fbfebf57391ad272e59c4e6a9ae5e4c1f6
Nathan Rajlich <n@n8.io>
2023-11-09 17:42:21 -0800

    debug

--- packages/remix/src/build.ts
+++ packages/remix/src/build.ts
@@ -225,6 +225,7 @@ export const build: BuildV2 = async ({
     // Remove the env vars that will cause pnpm to consider
     // as CI, which enables `--frozen-lockfile` enforcement
     const nonCiEnv = { ...spawnOpts.env };
+    console.log(nonCiEnv);
     delete nonCiEnv.VERCEL;
     delete nonCiEnv.NOW_BUILDER;
 
