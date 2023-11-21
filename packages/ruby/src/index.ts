4636ae54c6c17709c1a058169cdca19c3df73ddb
Nathan Rajlich <n@n8.io>
2023-11-21 13:57:23 -0800

    [ruby] Enable `ruby3.2` runtime (#10859)
    Allows for Ruby 3.2 Serverless Functions to be created.
    
    **Note:** New test fixtures will be added after this is deployed to production.

--- packages/ruby/src/index.ts
+++ packages/ruby/src/index.ts
@@ -44,11 +44,13 @@ async function matchPaths(
 async function bundleInstall(
   bundlePath: string,
   bundleDir: string,
-  gemfilePath: string
+  gemfilePath: string,
+  runtime: string
 ) {
   debug(`running "bundle install --deployment"...`);
   const bundleAppConfig = await getWriteableDirectory();
   const gemfileContent = await readFile(gemfilePath, 'utf8');
+
   if (gemfileContent.includes('ruby "~> 2.7.x"')) {
     // Gemfile contains "2.7.x" which will cause an error message:
     // "Your Ruby patchlevel is 0, but your Gemfile specified -1"
@@ -58,19 +60,54 @@ async function bundleInstall(
       gemfilePath,
       gemfileContent.replace('ruby "~> 2.7.x"', 'ruby "~> 2.7.0"')
     );
+  } else if (gemfileContent.includes('ruby "~> 3.2.x"')) {
+    // Gemfile contains "3.2.x" which will cause an error message:
+    // "Your Ruby patchlevel is 0, but your Gemfile specified -1"
+    // See https://github.com/rubygems/bundler/blob/3f0638c6c8d340c2f2405ecb84eb3b39c433e36e/lib/bundler/errors.rb#L49
+    // We must correct to the actual version in the build container.
+    await writeFile(
+      gemfilePath,
+      gemfileContent.replace('ruby "~> 3.2.x"', 'ruby "~> 3.2.0"')
+    );
   }
-  await execa(
+
+  const bundlerEnv = cloneEnv(process.env, {
+    // Ensure the correct version of `ruby` is in front of the $PATH
+    PATH: `${dirname(bundlePath)}:${process.env.PATH}`,
+    BUNDLE_SILENCE_ROOT_WARNING: '1',
+    BUNDLE_APP_CONFIG: bundleAppConfig,
+    BUNDLE_JOBS: '4',
+  });
+
+  // Lambda "ruby3.2" runtime does not include "webrick",
+  // which is needed for the `vc_init.rb` entrypoint file
+  if (runtime === 'ruby3.2') {
+    const result = await execa('bundler', ['add', 'webrick'], {
+      stdio: 'pipe',
+      env: bundlerEnv,
+      reject: false,
+    });
+    if (result.exitCode !== 0) {
+      console.log(result.stdout);
+      console.error(result.stderr);
+      throw result;
+    }
+  }
+
+  const result = await execa(
     bundlePath,
     ['install', '--deployment', '--gemfile', gemfilePath, '--path', bundleDir],
     {
       stdio: 'pipe',
-      env: cloneEnv(process.env, {
-        BUNDLE_SILENCE_ROOT_WARNING: '1',
-        BUNDLE_APP_CONFIG: bundleAppConfig,
-        BUNDLE_JOBS: '4',
-      }),
+      env: bundlerEnv,
+      reject: false,
     }
   );
+  if (result.exitCode !== 0) {
+    console.log(result.stdout);
+    console.error(result.stderr);
+    throw result;
+  }
 }
 
 export const version = 3;
@@ -141,7 +178,7 @@ export async function build({
       } else {
         // try installing. this won't work if native extesions are required.
         // if that's the case, gems should be vendored locally before deploying.
-        await bundleInstall(bundlerPath, bundleDir, gemfilePath);
+        await bundleInstall(bundlerPath, bundleDir, gemfilePath, runtime);
       }
     }
   } else {
