ae6b35bc82c64068c7f3da49f5799c353ceda65e
Ethan Arrowood <ethan.arrowood@vercel.com>
2023-11-29 14:01:13 -0700

    add ImageVersion to turbo cache key

--- utils/gen.js
+++ utils/gen.js
@@ -7,9 +7,14 @@
 const { writeFileSync } = require('fs');
 const { join } = require('path');
 
-const { versions, platform, arch } = process;
+const { versions, platform, arch, env } = process;
 const file = join(__dirname, '..', 'turbo-cache-key.json');
 const node = versions.node.split('.')[0];
-const str = JSON.stringify({ node, platform, arch });
+const str = JSON.stringify({
+  node,
+  platform,
+  arch,
+  imageVersion: env.ImageVersion ?? null,
+});
 console.log(`Generating cache key: ${str}`);
 writeFileSync(file, str);
