3b55f8a1db8e43d339d03cea98a7ba623482c9d3
Mitch (a.k.a Voz) <mitch.vostrez@vercel.com>
2023-12-14 16:09:42 -0600

    fix doc links in auth file

--- packages/cli/src/util/config/get-default.ts
+++ packages/cli/src/util/config/get-default.ts
@@ -4,12 +4,12 @@ export const defaultGlobalConfig: GlobalConfig = {
   '// Note':
     'This is your Vercel config file. For more information see the global configuration documentation.',
   '// Docs':
-    'https://vercel.com/docs/project-configuration#global-configuration/config-json',
+    'https://vercel.com/docs/project-configuration/global-configuration#config.json',
   collectMetrics: true,
 };
 
 export const defaultAuthConfig: AuthConfig = {
   '// Note': 'This is your Vercel credentials file. DO NOT SHARE!',
   '// Docs':
-    'https://vercel.com/docs/project-configuration#global-configuration/auth-json',
+    'https://vercel.com/docs/project-configuration/global-configuration#auth.json',
 };
