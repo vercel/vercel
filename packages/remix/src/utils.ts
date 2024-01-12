5168b64c95c10e668b18ec4bd5129334a47fd4b8
Nathan Rajlich <n@n8.io>
2024-01-12 14:02:51 -0800

    [remix] Fix functions without a output path edge case
    Fixes an edge case where a route without a path was causing the build to
    fail. Reproducible with this `remix.config.js` file:
    
    ```js
    export default {
        ignoredRouteFiles: ["**/.*"],
        routes(defineRoutes) {
            return defineRoutes((route) => {
                route("/", "foo.tsx", { index: true });
            });
        },
    };
    ```

--- packages/remix/src/utils.ts
+++ packages/remix/src/utils.ts
@@ -148,7 +148,9 @@ export function getPathFromRoute(
 ): ResolvedRoutePaths {
   if (
     route.id === 'root' ||
-    (route.parentId === 'root' && !route.path && route.index)
+    (route.parentId === 'root' &&
+      (!route.path || route.path === '/') &&
+      route.index)
   ) {
     return { path: 'index', rePath: '/index' };
   }
