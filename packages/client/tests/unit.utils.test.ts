1cbb531fb7f9ace4b28e98acbdea69f7fbf84687
Nathan Rajlich <n@n8.io>
2024-01-22 12:37:14 -0800

    [client] Upload files referenced by "filePathMap" during `vc deploy --prebuilt`

--- packages/client/tests/unit.utils.test.ts
+++ packages/client/tests/unit.utils.test.ts
@@ -124,8 +124,12 @@ describe('buildFileTree()', () => {
     );
 
     const expectedFileList = toAbsolutePaths(cwd, [
+      '.vercel/output/functions/api/another.func/.vc-config.json',
+      '.vercel/output/functions/api/example.func/.vc-config.json',
       '.vercel/output/static/baz.txt',
       '.vercel/output/static/sub/qux.txt',
+      'node_modules/another/index.js',
+      'node_modules/example/index.js',
     ]);
     expect(normalizeWindowsPaths(expectedFileList).sort()).toEqual(
       normalizeWindowsPaths(fileList).sort()
