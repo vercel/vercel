6e350f1ba096906d5c004483d377b77a4c8fcd72
zyxkad <zyxkad@gmail.com>
2024-01-30 08:16:43 -0700

    add more tests for parse go.mod version

--- packages/go/test/parse-go-mod-version.test.ts
+++ packages/go/test/parse-go-mod-version.test.ts
@@ -5,4 +5,34 @@ describe('parseGoModVersion', function () {
     const version = parseGoModVersion('');
     expect(version).toBeUndefined();
   });
+  it('returns exactly same version if patch exists', async () => {
+    const version = parseGoModVersion('go 1.16.1');
+    expect(version).toEqual('1.16.1');
+  });
+  it('returns the latest patch version if patch dose not exists', async () => {
+    const version = parseGoModVersion('go 1.16');
+    expect(version).toEqual('1.16.15');
+  });
+  it('returns correct version when unrelated line exists', async () => {
+    const version = parseGoModVersion('something\ngo 1.16.1\nsomething');
+    expect(version).toEqual('1.16.1');
+  });
+  it('returns correct version when space exists', async () => {
+    const version = parseGoModVersion('  \tgo\t 1.16.1 \t');
+    expect(version).toEqual('1.16.1');
+  });
+  it('returns correct version when comment exists', async () => {
+    const version = parseGoModVersion(
+      '// some comment\ngo 1.16.1//some other comment'
+    );
+    expect(version).toEqual('1.16.1');
+  });
+  it('returns correct version when comment exists with spaces between', async () => {
+    const version = parseGoModVersion('go 1.16.1\t  // some comment');
+    expect(version).toEqual('1.16.1');
+  });
+  it('returns correct version when similar version structure exists in comment', async () => {
+    const version = parseGoModVersion('// go 1.17.1\ngo 1.16.1 // go 1.15.1');
+    expect(version).toEqual('1.16.1');
+  });
 });
