4da053a3aec9ee8daea76e2f21fea38b932fc0c0
Nathan Rajlich <n@n8.io>
2024-03-01 01:10:56 -0800

    Remove `NODE_ENV`

--- packages/remix/src/build-vite.ts
+++ packages/remix/src/build-vite.ts
@@ -121,9 +121,6 @@ export const build: BuildV2 = async ({
   // package version.
   const remixVersion = await getRemixVersion(entrypointFsDirname, repoRootPath);
 
-  // Make `remix build` output production mode
-  spawnOpts.env.NODE_ENV = 'production';
-
   // Run "Build Command"
   if (buildCommand) {
     debug(`Executing build command "${buildCommand}"`);
