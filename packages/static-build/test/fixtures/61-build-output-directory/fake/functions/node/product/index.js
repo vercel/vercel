48037c5515f8af1c6be88f81bf6b76b44bb6bacf
Nathan Rajlich <n@n8.io>
2024-03-08 00:44:55 -0800

    [static-build] Remove Build Output API v1 tests
    Remove these old tests for the legacy BOA version. Specifically, `66-build-output-directory-with-functions` has become problematic because the "go1.x" Lambda runtime no longer deploys. I figured we take the opportunity to remove the others as well though. Will make running the tests slightly faster.

--- packages/static-build/test/fixtures/61-build-output-directory/fake/functions/node/product/index.js
+++
@@ -1,7 +0,0 @@
-const { getInfo } = require('./info');
-
-module.exports = async function (req, res) {
-  return res.end(
-    `Hello from /product/index.js on ${req.url} - ${await getInfo()}`
-  );
-};
