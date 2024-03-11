13292cc6d90155dd63489636d042191f298769d7
Ethan Arrowood <ethan@arrowood.dev>
2024-03-11 14:00:10 -0600

    whoops - remove console.log

--- packages/next/test/fixtures/00-x-forwarded-port/app/api/app-headers/route.js
+++ packages/next/test/fixtures/00-x-forwarded-port/app/api/app-headers/route.js
@@ -1,8 +1,4 @@
 import { NextResponse } from 'next/server';
 export function GET(req) {
-    console.log('🧪 app-router')
-    // console.log('🧪 app-router req.headers', JSON.stringify(req.headers, null, 2))
-    console.log('🧪 app-router req.headers', req.headers);
     return NextResponse.json({ 'port': req.headers.get('x-forwarded-port') });
   }
-  
