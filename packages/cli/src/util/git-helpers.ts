e45dd486a603fb13530457687e85d28ee6414a7f
Dillon Mulroy <dillon.mulroy@vercel.com>
2024-03-15 08:57:56 -0400

    wrap git helper in try/catch + add GitExecOptions

--- packages/cli/src/util/git-helpers.ts
+++ packages/cli/src/util/git-helpers.ts
@@ -1,24 +1,49 @@
 import { execSync } from 'node:child_process';
 
+export type GitExecOptions = Readonly<{
+  /** Throw if an error occurs. Defaults to false */
+  unsafe?: boolean;
+  /** The directory to check. Defaults to `process.cwd()` */
+  cwd?: string;
+}>;
+
+const DEFAULT_GIT_EXEC_OPTS = {
+  unsafe: false,
+  cwd: process.cwd(),
+} as const;
+
 /**
- * Checks if the current working directory is a Git worktree.
+ * Checks if a given directory is a Git worktree.
  *
  * A Git worktree is a linked working tree, which allows you to have multiple
  * working trees attached to the same repository. This function checks if the
- * current working directory or the specified directory is a Git worktree by
+ * specified directory (or the current working directory if none is specified) is a Git worktree by
  * looking for the '.git/worktrees/' path in the Git configuration.
  *
- * @param {string} [cwd] - The current working directory to check. If not specified,
- *                         the process's current working directory is used.
- * @returns {boolean} - Returns `true` if the directory is a Git worktree, otherwise `false`.
+ * @param {GitExecOptions} [opts={}] The options to use. Options include:
+ *        - `cwd`: The directory to check. Defaults to `process.cwd()`.
+ *        - `unsafe`: If true, throws if an error occurs during execution. Defaults to `false`.
+ * @returns {boolean} Returns `true` if the directory is a Git worktree, otherwise `false`.
+ *
+ * @throws {Error} Can throw an error if `opts.unsafe` is set to `true`
  */
-export function isGitWorktree(cwd?: string): boolean {
-  const gitConfigPath = execSync('git rev-parse --git-dir', {
-    cwd,
-    encoding: 'utf8',
-  });
+export function isGitWorktree(opts: GitExecOptions = {}): boolean {
+  const { cwd, unsafe } = { ...DEFAULT_GIT_EXEC_OPTS, ...opts };
 
-  return gitConfigPath.includes('.git/worktrees/');
+  try {
+    const gitConfigPath = execSync('git rev-parse --git-dir', {
+      cwd,
+      encoding: 'utf8',
+    });
+
+    return gitConfigPath.includes('.git/worktrees/');
+  } catch (error) {
+    if (unsafe) {
+      throw error;
+    }
+
+    return false;
+  }
 }
 
 /**
@@ -28,15 +53,29 @@ export function isGitWorktree(cwd?: string): boolean {
  * checks if the current working directory or the specified directory is part of a
  * Git submodule by looking for the '.git/modules/' path in the Git configuration.
  *
- * @param {string} [cwd] - The current working directory to check. If not specified,
- *                         the process's current working directory is used.
+ * @param {GitExecOptions} [opts={}] The options to use. Options include:
+ *        - `cwd`: The directory to check. Defaults to `process.cwd()`.
+ *        - `unsafe`: If true, throws if an error occurs during execution. Defaults to `false`.
+ *
  * @returns {boolean} - Returns `true` if the directory is part of a Git submodule, otherwise `false`.
+ *
+ * @throws {Error} Can throw an error if `opts.unsafe` is set to `true`
  */
-export function isGitSubmodule(cwd?: string): boolean {
-  const gitConfigPath = execSync('git rev-parse --git-dir', {
-    cwd,
-    encoding: 'utf8',
-  });
+export function isGitSubmodule(opts: GitExecOptions = {}): boolean {
+  const { cwd, unsafe } = { ...DEFAULT_GIT_EXEC_OPTS, ...opts };
 
-  return gitConfigPath.includes('.git/modules/');
+  try {
+    const gitConfigPath = execSync('git rev-parse --git-dir', {
+      cwd,
+      encoding: 'utf8',
+    });
+
+    return gitConfigPath.includes('.git/modules/');
+  } catch (error) {
+    if (unsafe) {
+      throw error;
+    }
+
+    return false;
+  }
 }
