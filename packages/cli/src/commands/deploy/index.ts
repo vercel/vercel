95bf64499808a8b571c6195c10bf9dacedde9b6f
Erika Rowland <erika.rowland@vercel.com>
2024-03-21 12:46:05 -0700

    [cli] Change condition to be clearer positive framing
    `status` here is guaranteed to either be
    `'linked'` or `'not_linked'` by this point in the code,
    switching the double negative to a positive condition makes it clearer.

--- packages/cli/src/commands/deploy/index.ts
+++ packages/cli/src/commands/deploy/index.ts
@@ -572,7 +572,7 @@ export default async (client: Client): Promise<number> => {
         nodeVersion,
       };
 
-      if (status !== 'not_linked') {
+      if (status === 'linked') {
         createArgs.projectSettings = {
           ...createArgs.projectSettings,
           ...localConfigurationOverrides,
