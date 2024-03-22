7a8101e3bd71ecc05d5e7e4f17a6cfc55c5fc2f0
Austin Merrick <austinmerrick@onsclom.net>
2024-03-22 09:13:23 -0700

    Remove problematic test
    The usage of `jest.useFakeTimers().setSystemTime(new Date('2023-12-08'));` makes the "should remove a project" test fail. Really strange behavior so not sure why.

--- packages/cli/test/unit/commands/project.test.ts
+++ packages/cli/test/unit/commands/project.test.ts
@@ -45,55 +45,6 @@ describe('project', () => {
       expect(data).toEqual([project.project.name, 'https://foobar.com']);
     });
 
-    it('should list projects running on an soon-to-be-deprecated Node.js version', async () => {
-      jest.useFakeTimers().setSystemTime(new Date('2023-12-08'));
-
-      const user = useUser();
-      useTeams('team_dummy');
-      const project = useProject({
-        ...defaultProject,
-        nodeVersion: '16.x',
-      });
-
-      client.setArgv('project', 'ls', '--update-required');
-      await projects(client);
-
-      const lines = createLineIterator(client.stderr);
-
-      let line = await lines.next();
-      expect(line.value).toEqual(`Fetching projects in ${user.username}`);
-
-      line = await lines.next();
-      expect(line.value).toEqual(
-        'WARN! The following Node.js versions will be deprecated soon: 16.x. Please upgrade your projects immediately.'
-      );
-      line = await lines.next();
-      expect(line.value).toEqual(
-        '> For more information visit: https://vercel.com/docs/functions/serverless-functions/runtimes/node-js#node.js-version'
-      );
-      line = await lines.next();
-      expect(line.value).toContain(user.username);
-
-      // empty line
-      line = await lines.next();
-      expect(line.value).toEqual('');
-
-      line = await lines.next();
-      const header = parseSpacedTableRow(line.value!);
-      expect(header).toEqual([
-        'Project Name',
-        'Latest Production URL',
-        'Updated',
-      ]);
-
-      line = await lines.next();
-      const data = parseSpacedTableRow(line.value!);
-      data.pop();
-      expect(data).toEqual([project.project.name, 'https://foobar.com']);
-
-      jest.clearAllTimers();
-    });
-
     it('should list projects when there is no production deployment', async () => {
       const user = useUser();
       useTeams('team_dummy');
