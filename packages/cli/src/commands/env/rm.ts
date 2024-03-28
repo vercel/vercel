9aca937900710e65198b6174d8a37920c64feb23
Austin Merrick <austinmerrick@onsclom.net>
2024-03-28 12:04:53 -0700

    Fix usage

--- packages/cli/src/commands/env/rm.ts
+++ packages/cli/src/commands/env/rm.ts
@@ -81,7 +81,7 @@ export default async function rm(
   }
 
   while (envs.length > 1) {
-    const id = await client.select({
+    const id = await client.input.select({
       message: `Remove ${envName} from which Environments?`,
       choices: envs.map(env => ({ value: env.id, name: formatEnvTarget(env) })),
     });
