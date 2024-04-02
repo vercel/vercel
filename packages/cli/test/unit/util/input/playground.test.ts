372453ddd583cf6b2b4da64ba46800a28a703bc3
Jeff See <jeffsee.55@gmail.com>
2024-04-02 11:47:21 -0400

    Add more input snapshot tests

--- packages/cli/test/unit/util/input/playground.test.ts
+++ packages/cli/test/unit/util/input/playground.test.ts
@@ -2,15 +2,14 @@ import { describe, it, expect } from 'vitest';
 import { client } from '../../../mocks/client';
 import { render } from '../../../helpers/render';
 
-const SPINNER_FRAME = '[LOADING_SPINNER]';
 const theme = {
   // Override spinner with a single frame
-  spinner: { frames: [SPINNER_FRAME] },
+  spinner: { frames: ['O'] },
 };
 
 describe('client.input', () => {
   describe('text', () => {
-    it('text', async () => {
+    it('should match the snapshot', async () => {
       const { answer, events, getScreen } = await render(client.input.text, {
         message: 'What is your name',
         theme,
@@ -18,9 +17,17 @@ describe('client.input', () => {
       expect(getScreen()).toMatchInlineSnapshot(`"? What is your name"`);
       events.type('Joe');
       events.keypress('enter');
-      expect(getScreen()).toMatchInlineSnapshot(
-        `"[LOADING_SPINNER] What is your name Joe"`
-      );
+      expect(getScreen()).toMatchInlineSnapshot(`"O What is your name Joe"`);
+      await expect(answer).resolves.toEqual('Joe');
+    });
+    it('should use the default value if nothing is provided', async () => {
+      const { answer, events, getScreen } = await render(client.input.text, {
+        message: 'What is your name',
+        default: 'Joe',
+        theme,
+      });
+      expect(getScreen()).toMatchInlineSnapshot(`"? What is your name (Joe)"`);
+      events.keypress('enter');
       await expect(answer).resolves.toEqual('Joe');
     });
   });
@@ -52,6 +59,162 @@ describe('client.input', () => {
       events.keypress('enter');
       await expect(answer).resolves.toEqual(['a']);
       expect(getScreen()).toMatchInlineSnapshot(`"? Choose an option a"`);
+    });
+  });
+  describe('select', () => {
+    it(`uses the values if no "name" is provided`, async () => {
+      const { events, getScreen } = await render(client.input.expand, {
+        message: 'Select an action:',
+        choices: [
+          { key: 'g', name: 'Good', value: 'good' },
+          { key: 'b', name: 'Bad', value: 'bad' },
+          { key: 's', name: 'Skip', value: 'skip' },
+        ],
+      });
+      expect(getScreen()).toMatchInlineSnapshot(`"? Select an action: (gbsH)"`);
+      events.type('H');
+      expect(getScreen()).toMatchInlineSnapshot(`
+        "? Select an action: (gbsH) H
+        >> Help, list all options"
+      `);
+    });
+    it(`uses the values if no "name" is provided`, async () => {
+      const { answer, events, getScreen } = await render(client.input.expand, {
+        message: 'Select an action:',
+        choices: [
+          { key: 'g', name: 'Good', value: 'good' },
+          { key: 'b', name: 'Bad', value: 'bad' },
+          { key: 's', name: 'Skip', value: 'skip' },
+        ],
+      });
+      expect(getScreen()).toMatchInlineSnapshot(`"? Select an action: (gbsH)"`);
+      events.type('b');
+      expect(getScreen()).toMatchInlineSnapshot(`
+        "? Select an action: (gbsH) b
+        >> Bad"
+      `);
+      events.keypress('enter');
+      await expect(answer).resolves.toEqual('bad');
+    });
+  });
+  describe('select', () => {
+    it(`uses the values if no "name" is provided`, async () => {
+      const { getScreen } = await render(client.input.select, {
+        message: 'Choose an option',
+        choices: [{ value: 'a' }, { value: 'b' }],
+      });
+      expect(getScreen()).toMatchInlineSnapshot(`
+        "? Choose an option (Use arrow keys)
+        ❯ a
+          b"
+      `);
+    });
+    it(`using a separator`, async () => {
+      const { events, getScreen } = await render(client.input.select, {
+        message: 'Choose an option',
+        choices: [
+          { value: 'a' },
+          { type: 'separator', separator: '---' },
+          { value: 'b' },
+        ],
+      });
+      expect(getScreen()).toMatchInlineSnapshot(`
+        "? Choose an option (Use arrow keys)
+        ❯ a
+         ---
+          b"
+      `);
+      events.keypress('down');
+      expect(getScreen()).toMatchInlineSnapshot(`
+        "? Choose an option
+          a
+         ---
+        ❯ b"
+      `);
+    });
+    it(`matches the expected output`, async () => {
+      const { answer, events, getScreen } = await render(client.input.select, {
+        message: 'Choose an option',
+        choices: [
+          { value: 'a', name: 'Option A' },
+          { value: 'b', name: 'Option B' },
+        ],
+      });
+      expect(getScreen()).toMatchInlineSnapshot(`
+        "? Choose an option (Use arrow keys)
+        ❯ Option A
+          Option B"
+      `);
+      events.keypress('down');
+      expect(getScreen()).toMatchInlineSnapshot(`
+        "? Choose an option
+          Option A
+        ❯ Option B"
+      `);
+      events.keypress('enter');
+      expect(getScreen()).toMatchInlineSnapshot(
+        `"? Choose an option Option B"`
+      );
+      await expect(answer).resolves.toEqual('b');
+    });
+    it(`with a default matches the expected output`, async () => {
+      const { answer, events, getScreen } = await render(client.input.select, {
+        message: 'Choose an option',
+        default: 'b',
+        choices: [
+          { value: 'a', name: 'Option A' },
+          { value: 'b', name: 'Option B' },
+        ],
+      });
+      expect(getScreen()).toMatchInlineSnapshot(`
+        "? Choose an option (Use arrow keys)
+          Option A
+        ❯ Option B"
+      `);
+      events.keypress('enter');
+      expect(getScreen()).toMatchInlineSnapshot(
+        `"? Choose an option Option B"`
+      );
+      await expect(answer).resolves.toEqual('b');
+    });
+  });
+  describe('confirm', () => {
+    it(`defaults to true if no default is provided`, async () => {
+      const { answer, events, getScreen } = await render(client.input.confirm, {
+        message: 'Confirm the value',
+      });
+      expect(getScreen()).toMatchInlineSnapshot(`"? Confirm the value (Y/n)"`);
+      events.keypress('enter');
+      await expect(answer).resolves.toEqual(true);
+    });
+    it(`defaulting to false works`, async () => {
+      const { answer, events, getScreen } = await render(client.input.confirm, {
+        message: 'Confirm the value',
+        default: false,
+      });
+      expect(getScreen()).toMatchInlineSnapshot(`"? Confirm the value (y/N)"`);
+      events.keypress('enter');
+      await expect(answer).resolves.toEqual(false);
+    });
+    it(`typing "n" for the value`, async () => {
+      const { answer, events, getScreen } = await render(client.input.confirm, {
+        message: 'Confirm the value',
+      });
+      expect(getScreen()).toMatchInlineSnapshot(`"? Confirm the value (Y/n)"`);
+      events.type('n');
+      events.keypress('enter');
+      expect(getScreen()).toMatchInlineSnapshot(`"? Confirm the value no"`);
+      await expect(answer).resolves.toEqual(false);
+    });
+    it(`typing "y" for the value`, async () => {
+      const { answer, events, getScreen } = await render(client.input.confirm, {
+        message: 'Confirm the value',
+      });
+      expect(getScreen()).toMatchInlineSnapshot(`"? Confirm the value (Y/n)"`);
+      events.type('y');
+      events.keypress('enter');
+      expect(getScreen()).toMatchInlineSnapshot(`"? Confirm the value yes"`);
+      await expect(answer).resolves.toEqual(true);
     });
   });
 });
