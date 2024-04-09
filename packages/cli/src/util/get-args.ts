a2f29700f9be390348c221d70efb134f886c46ec
Erika Rowland <erika.rowland@vercel.com>
2024-04-09 15:41:06 -0700

    [cli] Add JSDoc comment explaining parseArguments

--- packages/cli/src/util/get-args.ts
+++ packages/cli/src/util/get-args.ts
@@ -29,6 +29,18 @@ type ParserOptions = {
   permissive?: boolean;
 };
 
+/**
+ * Parses command line arguments.
+ * Automatically includes a number of common flags such as `--help`.
+ *
+ * This function is designed to replace getArgs
+ * and will live alongside getArgs until the migration is completed.
+ *
+ * It takes the same three arguments as getArgs with improved names: args, flagsSpecification, and parserOptions.
+ * It also changes the return type to be an object with two keys: {args, flags}
+ * `args` contains the positional arguments, previously returned under the key `_`
+ * `flags` contains the parsed options, previously these keys were mixed with the positional arguments
+ */
 export function parseArguments<T extends Spec>(
   args: string[],
   flagsSpecification?: T,
