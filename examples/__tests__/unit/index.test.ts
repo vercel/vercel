9773af100986fc0c5d46b2d7d73019374c1454ff
Jeff See <jeffsee.55@gmail.com>
2024-04-11 10:03:21 -0700

    Add solidstart-1 fixture

--- examples/__tests__/unit/index.test.ts
+++ examples/__tests__/unit/index.test.ts
@@ -3,6 +3,7 @@ import { getExamples } from '../test-utils';
 
 describe('should have test for each example', () => {
   it.each(getExamples())('should exist $exampleName', async ({testPath}) => {
+    console.log(testPath)
     expect(existsSync(testPath)).toBeTruthy();
   });
 });
