be72d833480fac99dc7c94a05937343d982a3286
JJ Kasper <jj@jjsweb.site>
2024-04-16 13:04:08 -0700

    Ensure _not-found is included properly

--- packages/next/src/constants.ts
+++ packages/next/src/constants.ts
@@ -9,6 +9,8 @@ export const EDGE_FUNCTION_SIZE_LIMIT = 4 * MIB;
 
 export const MAX_UNCOMPRESSED_LAMBDA_SIZE = 250 * MIB;
 
-export const LAMBDA_RESERVED_UNCOMPRESSED_SIZE = 2.5 * MIB;
+// we need to leave wiggle room as other files are added
+// post build so we don't want to completely pack the function
+export const LAMBDA_RESERVED_UNCOMPRESSED_SIZE = 25 * MIB;
 
-export const LAMBDA_RESERVED_COMPRESSED_SIZE = 250 * KIB;
+export const LAMBDA_RESERVED_COMPRESSED_SIZE = 5 * MIB;
