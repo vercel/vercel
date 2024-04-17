949d84ad1cdcd4f9ea44f8a165a193a488cb1a31
JJ Kasper <jj@jjsweb.site>
2024-04-17 13:37:39 -0700

    [next] Ensure _not-found is included properly in app router functions (#11441)
    This ensures we properly include `_not-found` assets when present for
    app router specific functions as otherwise when attempting to render
    `notFound()` case we can hit `MODULE_NOT_FOUND` errors. A test case for
    this behavior has been added under `00-app-router-not-found`.
    
    Closes: [NEXT-3084](https://linear.app/vercel/issue/NEXT-3084)

--- packages/next/src/constants.ts
+++ packages/next/src/constants.ts
@@ -9,4 +9,6 @@ export const EDGE_FUNCTION_SIZE_LIMIT = 4 * MIB;
 
 export const DEFAULT_MAX_UNCOMPRESSED_LAMBDA_SIZE = 250 * MIB;
 
-export const LAMBDA_RESERVED_UNCOMPRESSED_SIZE = 2.5 * MIB;
+// we need to leave wiggle room as other files are added
+// post build so we don't want to completely pack the function
+export const LAMBDA_RESERVED_UNCOMPRESSED_SIZE = 25 * MIB;
