4d6d06139366deaddccae284f06b0a185e9df17a
JJ Kasper <jj@jjsweb.site>
2024-04-17 09:03:04 -0700

    update test comment

--- packages/next/test/integration/test-limit-exceeded-404-static-files/next.config.js
+++ packages/next/test/integration/test-limit-exceeded-404-static-files/next.config.js
@@ -20,7 +20,7 @@ assert(
   `unexpected locale count, expected 82, received ${locales.length}`
 );
 
-// generate 100MB text file which will be traced in `/api/hello`
+// generate large text file which will be traced in `/api/hello`
 // which when combined with the 404 HTML files will push us over the 250MB
 // uncompressed limit
 fs.writeFileSync('data.txt', Buffer.alloc(200 * 1024 * 1024));
