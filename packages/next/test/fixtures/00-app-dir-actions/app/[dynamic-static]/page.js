5bb96ea07289e7db66d28d08b372cf949f3d7e88
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2024-04-18 12:04:30 -0700

    [next] add .action handling for dynamic routes (#11461)
    Follow-up to:
    - https://github.com/vercel/vercel/pull/11454
    
    Adds similar handling for dynamic routes

---
+++ packages/next/test/fixtures/00-app-dir-actions/app/[dynamic-static]/page.js
@@ -0,0 +1,17 @@
+import { revalidatePath } from 'next/cache'
+
+export const dynamic = 'force-static'
+
+export default async function Page() {
+  async function serverAction() {
+    'use server';
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    revalidatePath('/dynamic');
+  }
+
+  return (
+    <form action={serverAction}>
+      <button>Submit</button>
+    </form>
+  );
+}
