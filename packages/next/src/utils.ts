6402436e6dc4305b5a3a80c023b4d135770ccf1a
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2024-04-18 11:22:55 -0700

    add .action handling for dynamic routes

--- packages/next/src/utils.ts
+++ packages/next/src/utils.ts
@@ -432,6 +432,15 @@ export async function getDynamicRoutes({
             dest: route.dest?.replace(/($|\?)/, '.rsc$1'),
           });
 
+          routes.push({
+            ...route,
+            src: route.src.replace(
+              new RegExp(escapeStringRegexp('(?:/)?$')),
+              '(?:\\.action)(?:/)?$'
+            ),
+            dest: route.dest?.replace(/($|\?)/, '.action$1'),
+          });
+
           routes.push(route);
         }
 
