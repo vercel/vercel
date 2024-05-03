b86d4a6200e65dfee581bc4912ccaaa2fc1affc6
Erika Rowland <erika.rowland@vercel.com>
2024-05-03 16:02:28 -0700

    [cli] Migrate vc domains to updated argument parsing utilities

--- packages/cli/src/commands/domains/index.ts
+++ packages/cli/src/commands/domains/index.ts
@@ -1,5 +1,5 @@
 import Client from '../../util/client';
-import getArgs from '../../util/get-args';
+import { parseArguments } from '../../util/get-args';
 import getSubcommand from '../../util/get-subcommand';
 import handleError from '../../util/handle-error';
 import add from './add';
@@ -11,6 +11,7 @@ import rm from './rm';
 import move from './move';
 import { domainsCommand } from './command';
 import { help } from '../help';
+import { getFlagsSpecification } from '../../util/get-flags-specification';
 
 const COMMAND_CONFIG = {
   add: ['add'],
@@ -23,45 +24,42 @@ const COMMAND_CONFIG = {
 };
 
 export default async function main(client: Client) {
-  let argv;
+  let parsedArgs;
+
+  const flagsSpecification = getFlagsSpecification(domainsCommand.options);
 
   try {
-    argv = getArgs(client.argv.slice(2), {
-      '--code': String,
-      '--yes': Boolean,
-      '--force': Boolean,
-      '--next': Number,
-      '-N': '--next',
-      '-y': '--yes',
-      '--limit': Number,
-    });
+    parsedArgs = parseArguments(client.argv.slice(2), flagsSpecification);
   } catch (error) {
     handleError(error);
     return 1;
   }
 
-  if (argv['--help']) {
+  if (parsedArgs.flags['--help']) {
     client.output.print(
       help(domainsCommand, { columns: client.stderr.columns })
     );
     return 2;
   }
 
-  const { subcommand, args } = getSubcommand(argv._.slice(1), COMMAND_CONFIG);
+  const { subcommand, args } = getSubcommand(
+    parsedArgs.args.slice(1),
+    COMMAND_CONFIG
+  );
   switch (subcommand) {
     case 'add':
-      return add(client, argv, args);
+      return add(client, parsedArgs.flags, args);
     case 'inspect':
-      return inspect(client, argv, args);
+      return inspect(client, parsedArgs.flags, args);
     case 'move':
-      return move(client, argv, args);
+      return move(client, parsedArgs.flags, args);
     case 'buy':
-      return buy(client, argv, args);
+      return buy(client, parsedArgs.flags, args);
     case 'rm':
-      return rm(client, argv, args);
+      return rm(client, parsedArgs.flags, args);
     case 'transferIn':
-      return transferIn(client, argv, args);
+      return transferIn(client, parsedArgs.flags, args);
     default:
-      return ls(client, argv, args);
+      return ls(client, parsedArgs.flags, args);
   }
 }
