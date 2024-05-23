2cab2b0c217295b711379408944e886f58e759b6
Erika Rowland <erika.rowland@vercel.com>
2024-05-23 14:56:04 -0700

    [cli] Migrate vc whoami to new parse arguments utilities

--- packages/cli/src/commands/whoami/index.ts
+++ packages/cli/src/commands/whoami/index.ts
@@ -2,15 +2,17 @@ import { help } from '../help';
 import { whoamiCommand } from './command';
 
 import getScope from '../../util/get-scope';
-import getArgs from '../../util/get-args';
+import { parseArguments } from '../../util/get-args';
 import Client from '../../util/client';
+import { getFlagsSpecification } from '../../util/get-flags-specification';
 
 export default async function whoami(client: Client): Promise<number> {
   const { output } = client;
-  const argv = getArgs(client.argv.slice(2), {});
-  argv._ = argv._.slice(1);
+  const flagsSpecification = getFlagsSpecification(whoamiCommand.options);
+  const parsedArgs = parseArguments(client.argv.slice(2), flagsSpecification);
+  parsedArgs.args = parsedArgs.args.slice(1);
 
-  if (argv['--help'] || argv._[0] === 'help') {
+  if (parsedArgs.flags['--help'] || parsedArgs.args[0] === 'help') {
     output.print(help(whoamiCommand, { columns: client.stderr.columns }));
     return 2;
   }
