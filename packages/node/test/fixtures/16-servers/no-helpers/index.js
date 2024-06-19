74ee1d59a5c0004fa15ed838e4eb6cc8a1087924
Tom Lienard <tom.lienrd@gmail.com>
2024-06-19 07:55:02 +0100

    fix: test

--- packages/node/test/fixtures/16-servers/no-helpers/index.js
+++ packages/node/test/fixtures/16-servers/no-helpers/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.all('*', (req, res) => {
-  const areHelpersAvailable = typeof req.query !== 'undefined';
+  const areHelpersAvailable = typeof req.body !== 'undefined';
 
   res.end(`${areHelpersAvailable ? 'yes' : 'no'}:RANDOMNESS_PLACEHOLDER`);
 });
