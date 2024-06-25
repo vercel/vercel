49e1cf182d12ba51c890a422dcfea0ab5bacb325
Jeff See <jeffsee.55@gmail.com>
2024-06-25 13:28:23 -0700

    Log fetchWithAuth

--- test/lib/deployment/now-deploy.js
+++ test/lib/deployment/now-deploy.js
@@ -192,6 +192,7 @@ async function fetchWithAuth(url, opts = {}) {
   if (VERCEL_TEAM_ID) {
     url += `${url.includes('?') ? '&' : '?'}teamId=${VERCEL_TEAM_ID}`;
   }
+  console.log('fetchWithAuth', url, opts);
 
   return await fetchApi(url, opts);
 }
