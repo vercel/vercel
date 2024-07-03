0cc81f3ffc5b0955e1037d9c3d4f5184b54ee690
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-07-03 21:33:58 +0200

    add interface test

---
+++ packages/functions/test/index.test.ts
@@ -0,0 +1,24 @@
+import { expect, test } from 'vitest';
+import $ from 'tinyspawn';
+
+const evalScript = (code: string, flags: string[] = []) =>
+  $('node', ['--eval', code, ...flags]).then(({ stdout }) => stdout);
+evalScript.esm = code => evalScript(code, ['--input-type', 'module']);
+
+test('load as CommonJS', async () => {
+  const code =
+    "console.log(JSON.stringify(Object.keys(require('@vercel/functions'))))";
+  const exportedMethods = await evalScript(code).then(output =>
+    JSON.parse(output)
+  );
+  expect(exportedMethods).toEqual(['waitUntil']);
+});
+
+test('load as ESM', async () => {
+  const code =
+    "import f from '@vercel/functions'; console.log(JSON.stringify(Object.keys(f)))";
+  const exportedMethods = await evalScript
+    .esm(code)
+    .then(output => JSON.parse(output));
+  expect(exportedMethods).toEqual(['waitUntil']);
+});
