2e0f4f7377198516677a8b0f4f16673da855a19f
feugy <damien@vercel.com>
2024-07-03 17:44:20 +0200

    feat(cli)!: vc logs now returns runtime logs

--- packages/cli/src/util/logs.ts
+++ packages/cli/src/util/logs.ts
@@ -1,7 +1,15 @@
 import type { Deployment } from '@vercel-internals/types';
-import { printLogShort } from '../commands/logs';
+import chalk from 'chalk';
+import { format } from 'date-fns';
+import ms from 'ms';
+import jsonlines from 'jsonlines';
+import split from 'split2';
+import { URLSearchParams } from 'url';
 import Client from '../util/client';
 import printEvents from './events';
+import { CommandTimeout } from '../commands/logs/command';
+
+type Printer = (l: string) => void;
 
 export function displayBuildLogs(
   client: Client,
@@ -30,11 +38,254 @@ export function displayBuildLogs(
     deployment.id,
     {
       mode: 'logs',
-      onEvent: (event: any) => printLogShort(event, client),
+      onEvent: (event: any) => printBuildLog(event, client.output.print),
       quiet: false,
       findOpts: { direction: 'forward', follow },
     },
     abortController
   );
   return { promise, abortController };
+}
+
+export interface DisplayRuntimeLogsOptions {
+  projectId?: string;
+  deploymentId: string;
+  parse?: boolean;
+}
+
+const runtimeLogSpinnerMessage = `waiting for new logs...`;
+
+export interface RuntimeLog {
+  level: 'error' | 'warning' | 'info';
+  message: string;
+  rowId: string;
+  source:
+    | 'delimiter'
+    | 'edge-function'
+    | 'edge-middleware'
+    | 'serverless'
+    | 'request';
+  timestampInMs: number;
+  domain: string;
+  messageTruncated: boolean;
+  requestMethod: string;
+  requestPath: string;
+  responseStatusCode: number;
+}
+
+export async function displayRuntimeLogs(
+  client: Client,
+  options: DisplayRuntimeLogsOptions,
+  abortController: AbortController
+) {
+  const { log, debug, print, spinner, stopSpinner, warn } = client.output;
+  const { projectId, deploymentId, parse } = options;
+
+  const query = new URLSearchParams({ format: 'lines' });
+
+  const url = `/v1/projects/${projectId}/deployments/${deploymentId}/runtime-logs?${query}`;
+  spinner(runtimeLogSpinnerMessage);
+  setTimeout(() => {
+    abortController.abort();
+    warn(
+      `${chalk.bold(
+        `Command automatically interrupted after ${CommandTimeout}.`
+      )}\n`
+    );
+  }, ms(CommandTimeout));
+
+  const response = await client.fetch(url, {
+    json: false,
+    // @ts-expect-error: typescipt is getting confused with the signal types from node (web & server) and node-fetch (server only)
+    signal: abortController.signal,
+    retry: {
+      retries: 3,
+      onRetry: err => {
+        log(`Runtime logs error: ${err.message}`);
+
+        if (err instanceof Error && err.name === 'AbortError') {
+          return;
+        }
+        throw err;
+      },
+    },
+  });
+
+  if (response.ok) {
+    // handle the event stream and make the promise get rejected
+    // if errors occur so we can retry
+    return new Promise<void>((resolve, reject) => {
+      const stream = response.body.pipe(parse ? jsonlines.parse() : split());
+      let finished = false;
+
+      function finish(err?: unknown) {
+        if (finished) return;
+        stopSpinner();
+        finished = true;
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      }
+
+      const handleData = (data: RuntimeLog | string) => {
+        let log: RuntimeLog = parse ? data : JSON.parse(data as string);
+        if (isRuntimeLimitDelimiter(log)) {
+          abortController.abort();
+          warn(`${chalk.bold(log.message)}\n`);
+          return;
+        }
+        // eslint-disable-next-line no-console -- we intent to write unparsed logs to stdout so JQ could read them
+        parse ? prettyPrintLogline(log, print) : console.log(data);
+        spinner(runtimeLogSpinnerMessage);
+      };
+
+      let errored = false;
+      const handleError = (err: Error) => {
+        if (finished || errored) return;
+        if (err.name === 'AbortError') {
+          finish();
+          return;
+        }
+        errored = true;
+
+        const errorMessage = `Runtime logs stream error: ${err.message ?? err}`;
+
+        debug(errorMessage);
+        stream.destroy(err);
+
+        setTimeout(() => {
+          if (abortController.signal.aborted) return;
+          // retry without maximum amount nor clear past logs etc
+          displayRuntimeLogs(client, options, abortController).then(
+            resolve,
+            reject
+          );
+        }, 2000);
+      };
+
+      stream.on('end', finish);
+      stream.on('data', handleData);
+      stream.on('error', handleError);
+      response.body.on('error', handleError);
+    });
+  }
+}
+
+function printBuildLog(log: any, print: Printer) {
+  if (!log.created) return; // keepalive
+
+  let data: string;
+
+  data = (log.text || '')
+    .replace(/\n$/, '')
+    .replace(/^\n/, '')
+    // eslint-disable-next-line no-control-regex
+    .replace(/\x1b\[1000D/g, '')
+    .replace(/\x1b\[0K/g, '')
+    .replace(/\x1b\[1A/g, '');
+  if (/warning/i.test(data)) {
+    data = chalk.yellow(data);
+  } else if (log.type === 'stderr') {
+    data = chalk.red(data);
+  }
+
+  const date = new Date(log.created).toISOString();
+
+  data.split('\n').forEach(line => {
+    print(`${chalk.dim(date)}  ${line.replace('[now-builder-debug] ', '')}\n`);
+  });
+}
+
+function isRuntimeLimitDelimiter(log: RuntimeLog) {
+  return (
+    log.rowId === '' && log.level === 'error' && log.source === 'delimiter'
+  );
+}
+
+const dateTimeFormat = 'MMM dd HH:mm:ss.SS';
+const statusWidth = 3;
+const methodWidth = 7;
+const domainWidth = 20;
+const pathWidth = 30;
+// padding for date, method, status, domain and path, all separated by 2 spaces
+const multiLintPadding = ' '.repeat(
+  [
+    dateTimeFormat.length,
+    methodWidth,
+    statusWidth,
+    domainWidth,
+    pathWidth,
+  ].reduce((sum, width) => sum + 2 + width)
+);
+
+function prettyPrintLogline(
+  {
+    level,
+    domain,
+    requestPath: path,
+    responseStatusCode: status,
+    requestMethod: method,
+    message,
+    messageTruncated,
+    timestampInMs,
+    source,
+  }: RuntimeLog,
+  print: Printer
+) {
+  const lines = message.replace(/\n$/, '').split('\n');
+  for (const i of lines.keys()) {
+    const displayedLine = getDisplayedLine(i, lines, messageTruncated);
+    if (i === 0) {
+      print(
+        `${getLevelIcon(level)}  ${chalk.dim(
+          format(timestampInMs, dateTimeFormat)
+        )}  ${chalk.bold(toFixedWidth(method, 7))} ${chalk.grey(
+          status <= 0 ? '---' : status
+        )}  ${chalk.dim(toFixedWidth(domain, 20))} ${getSourceIcon(source)} ${
+          displayedLine ? toFixedWidth(path, 30) : path
+        }  ${displayedLine}\n`
+      );
+    } else {
+      print(`    ${multiLintPadding}  ${displayedLine}\n`);
+    }
+  }
+}
+
+function getDisplayedLine(rank: number, lines: string[], truncated: boolean) {
+  if (lines.length === 1 && lines[0] === '-') return '';
+  const separator = getSeparator(rank, lines.length);
+  const line =
+    rank === lines.length - 1 && truncated
+      ? `${lines[rank]}\u2026`
+      : lines[rank];
+  return `  ${separator} ${line}`;
+}
+
+function getLevelIcon(level: string) {
+  return level === 'error' ? '❌' : level === 'warning' ? '⚠️' : 'ℹ️';
+}
+
+function getSourceIcon(source: string) {
+  if (source === 'edge-function') return 'ന';
+  if (source === 'edge-middleware') return 'ɛ';
+  if (source === 'serverless') return 'ƒ';
+  return ' ';
+}
+
+function getSeparator(current: number, max: number) {
+  // https://en.wikipedia.org/wiki/Miscellaneous_Technical#Block
+  // special UTF characters for a better vertical line
+  if (current === 0 && max === 1) return '⏵';
+  if (current === 0) return '⎛';
+  if (current === max - 1) return '⎝';
+  return '⎟';
+}
+
+function toFixedWidth(value: string, width: number) {
+  if (!value) return ' '.repeat(width);
+  return value.length < width
+    ? value.padEnd(width, ' ')
+    : value.slice(0, width - 1) + '\u2026';
 }
---
+++ packages/cli/test/unit/commands/logs.test.ts
@@ -0,0 +1,156 @@
+import type { Deployment } from '@vercel-internals/types';
+import { afterAll, beforeAll, describe, expect, it, vi } from 'vitest';
+import { client } from '../../mocks/client';
+import { useUser } from '../../mocks/user';
+import { useDeployment, useRuntimeLogs } from '../../mocks/deployment';
+import logs from '../../../src/commands/logs';
+import { stateString } from '../../../src/commands/list';
+import { CommandTimeout } from '../../../src/commands/logs/command';
+
+const logsFixtures = [
+  {
+    rowId: 1,
+    timestampInMs: 1717426870339,
+    level: 'info',
+    message: 'Hello, world!',
+    messageTruncated: false,
+    domain: 'acme.com',
+    requestMethod: 'GET',
+    requestPath: '/',
+    responseStatusCode: 200,
+  },
+  {
+    rowId: 2,
+    timestampInMs: 1717426870540,
+    message: 'Bye...',
+    messageTruncated: false,
+    domain: 'acme.com',
+    requestMethod: 'OPTION',
+    requestPath: '/logout',
+    responseStatusCode: 204,
+  },
+];
+
+describe('logs', () => {
+  beforeAll(() => {
+    vi.useFakeTimers();
+  });
+
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+
+  it('should print error when deployment not found', async () => {
+    const user = useUser();
+    client.setArgv('logs', 'bad.com');
+    await expect(logs(client)).rejects.toThrow(
+      `Can't find the deployment "bad.com" under the context "${user.username}"`
+    );
+  });
+
+  it.each([
+    { state: 'DEPLOYING' },
+    { state: 'QUEUED' },
+    { state: 'ERROR' },
+    { state: 'CANCELED' },
+  ] as {
+    state: Deployment['readyState'];
+  }[])('prints disclaimer when deployment is $state', async ({ state }) => {
+    const user = useUser();
+    const deployment = useDeployment({ creator: user, state });
+    client.setArgv('logs', deployment.url);
+    const exitCode = await logs(client);
+    await expect(client.stderr).toOutput(
+      `Fetching deployment "${deployment.url}" in ${user.username}
+Error: Deployment not ready. Currently: ${stateString(state)}.
+`
+    );
+    expect(exitCode).toEqual(3);
+  });
+
+  it('pretty prints log lines', async () => {
+    const user = useUser();
+    const deployment = useDeployment({ creator: user });
+    const stdout = vi.spyOn(console, 'log');
+    const spy = vi.fn();
+    useRuntimeLogs({
+      spy,
+      deployment,
+      logProducer: async function* () {
+        for (const log of logsFixtures) {
+          yield log;
+        }
+      },
+    });
+    client.setArgv('logs', deployment.url);
+    const exitCode = await logs(client);
+    expect(exitCode).toEqual(0);
+    expect(spy).toHaveBeenCalledWith(
+      `/v1/projects/${deployment.projectId}/deployments/${deployment.id}/runtime-logs`,
+      { format: 'lines' }
+    );
+    expect(spy).toHaveBeenCalledOnce();
+    await expect(client.stderr).toOutput(
+      `Fetching deployment "${deployment.url}" in ${user.username}
+`
+    );
+    // 2nd line is time dependent and others are blank lines
+    expect(client.getFullOutput().split('\n').slice(3).join('\n'))
+      .toMatchInlineSnapshot(`
+        "waiting for new logs...
+        ℹ️  Jun Mo 17:01:10.33  GET     200  acme.com               /                                 ⏵ Hello, world!
+        waiting for new logs...
+        ℹ️  Jun Mo 17:01:10.54  OPTION  204  acme.com               /logout                           ⏵ Bye...
+        waiting for new logs...
+        "
+      `);
+    expect(stdout).not.toHaveBeenCalled();
+  });
+
+  it('prints log lines in json', async () => {
+    const user = useUser();
+    const deployment = useDeployment({ creator: user });
+    const stdout = vi.spyOn(console, 'log').mockImplementation(() => void 0);
+    const spy = vi.fn();
+    useRuntimeLogs({
+      spy,
+      deployment,
+      logProducer: async function* () {
+        for (const log of logsFixtures) {
+          yield log;
+        }
+      },
+    });
+    client.setArgv('logs', deployment.url, '--json');
+    const exitCode = await logs(client);
+    expect(exitCode).toEqual(0);
+    expect(spy).toHaveBeenCalledWith(
+      `/v1/projects/${deployment.projectId}/deployments/${deployment.id}/runtime-logs`,
+      { format: 'lines' }
+    );
+    expect(spy).toHaveBeenCalledOnce();
+    await expect(client.stderr).toOutput(
+      `Fetching deployment "${deployment.url}" in ${user.username}
+`
+    );
+    expect(stdout).toHaveBeenNthCalledWith(
+      1,
+      `{"rowId":1,"timestampInMs":1717426870339,"level":"info","message":"Hello, world!","messageTruncated":false,"domain":"acme.com","requestMethod":"GET","requestPath":"/","responseStatusCode":200}`
+    );
+    expect(stdout).toHaveBeenNthCalledWith(
+      2,
+      `{"rowId":2,"timestampInMs":1717426870540,"message":"Bye...","messageTruncated":false,"domain":"acme.com","requestMethod":"OPTION","requestPath":"/logout","responseStatusCode":204}`
+    );
+    expect(stdout).toHaveBeenCalledTimes(2);
+  });
+
+  it.todo('stops when receiving "limit exceeded" delimiter from server');
+
+  it.todo('retries on a server error');
+
+  it.todo('does not retry on a server validation error');
+
+  it.todo('resumes showing logs when failing to process a log line');
+
+  it.todo(`aborts the command after ${CommandTimeout}`);
+});
