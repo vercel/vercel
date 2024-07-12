a379a8464d56f36edddb8dc904018ad2ba5ce2d9
Austin Merrick <austinmerrick@onsclom.net>
2024-07-12 11:24:44 -0700

    Remove unnecessary if statement

--- packages/cli/src/commands/inspect/index.ts
+++ packages/cli/src/commands/inspect/index.ts
@@ -194,20 +194,18 @@ async function printDetails({
   print(`    ${chalk.cyan('name')}\t${name}\n`);
   const customEnvironmentName = deployment.customEnvironment?.name;
   const target = customEnvironmentName ?? deployment.target ?? 'preview';
-  if (target) {
-    print(`    ${chalk.cyan('target')}\t`);
-    // TODO: once custom environments is shipped for all users,
-    // make all deployments link to the environment settings page
-    print(
-      deployment.customEnvironment && deployment.team?.slug
-        ? `${link(
-            `${target}`,
-            `https://vercel.com/${deployment.team.slug}/${name}/settings/environments/${deployment.customEnvironment.id}}`,
-            { fallback: false }
-          )}\n`
-        : `${target}\n`
-    );
-  }
+  print(`    ${chalk.cyan('target')}\t`);
+  // TODO: once custom environments is shipped for all users,
+  // make all deployments link to the environment settings page
+  print(
+    deployment.customEnvironment && deployment.team?.slug
+      ? `${link(
+          `${target}`,
+          `https://vercel.com/${deployment.team.slug}/${name}/settings/environments/${deployment.customEnvironment.id}}`,
+          { fallback: false }
+        )}\n`
+      : `${target}\n`
+  );
   print(`    ${chalk.cyan('status')}\t${stateString(readyState)}\n`);
   print(`    ${chalk.cyan('url')}\t\thttps://${url}\n`);
   if (createdAt) {
