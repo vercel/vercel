e488fb49ea22ec5202c06c27fc1b713bf4411387
bricefriha <contact@bricefriha.dev>
2024-07-13 10:09:42 +0100

    #6899 - CLI sign up | email verification

--- packages/cli/src/util/login/signUp.ts
+++ packages/cli/src/util/login/signUp.ts
@@ -1,37 +1,21 @@
 import { errorToString } from '@vercel/error-utils';
 import Client from '../client';
 import { AccountNotFound, InvalidEmail, isAPIError } from '../errors-ts';
-import { SignUpData, VerifyResult } from './types';
-import { clearInterval } from 'timers';
+import { SignUpData } from './types';
+// import { clearInterval } from 'timers';
 
 export default async function signUp(
   client: Client,
-  email: string,
-  plan: string,
-  teamName: string
+  email: string
 ): Promise<SignUpData> {
   try {
-    const signUpRes = await client.fetch<SignUpData>(
+    return await client.fetch<SignUpData>(
       `/registration?mode=signUp&source=web`,
       {
         method: 'POST',
         body: { email, tokenName: 'other' },
       }
     );
-    let verifyRes: VerifyResult;
-    const inter = setInterval(async () => {
-      verifyRes = await client.fetch(
-        `/verify?email=${email}&token=${signUpRes.token}&teamName=${teamName}projects&teamPlan=${plan}`,
-        {
-          method: 'GET',
-        }
-      );
-      if (!verifyRes.error) {
-        clearInterval(inter);
-      }
-    }, 1000);
-
-    return signUpRes;
   } catch (err: unknown) {
     if (isAPIError(err)) {
       if (err.code === 'not_exists') {
