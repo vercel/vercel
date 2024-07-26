cf0a84929abbbacb19d6e25d5c9a1390defab0ae
Nathan Rajlich <n@n8.io>
2024-07-26 11:38:49 -0700

    add test

--- packages/remix/test/fixtures-vite/06-unstable_singleFetch/vite.config.ts
+++ packages/remix/test/fixtures-vite/06-unstable_singleFetch/vite.config.ts
@@ -1,11 +1,15 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
-import { vercelPreset } from "@vercel/remix/vite";
+import { vercelPreset } from '@vercel/remix/vite';
 import tsconfigPaths from "vite-tsconfig-paths";
 
-installGlobals();
+installGlobals({ nativeFetch: true });
 
 export default defineConfig({
-  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+  plugins: [remix({
+    presets: [vercelPreset()], future: {
+      unstable_singleFetch: true
+    }
+  }), tsconfigPaths()],
 });
