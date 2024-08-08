be0bd03f841db11c4e80a19436398d8c05a80b00
Erika Rowland <erika.rowland@vercel.com>
2024-08-08 11:15:57 -0700

    [cli] fix missed argv

--- packages/cli/src/commands/remove/index.ts
+++ packages/cli/src/commands/remove/index.ts
@@ -164,7 +164,7 @@ export default async function remove(client: Client) {
 
   if (deployments.length === 0 && projects.length === 0) {
     log(
-      `Could not find ${argv['--safe'] ? 'unaliased' : 'any'} deployments ` +
+      `Could not find ${parsedArgs.flags['--safe'] ? 'unaliased' : 'any'} deployments ` +
         `or projects matching ` +
         `${ids
           .map(id => chalk.bold(`"${id}"`))
