f58018d328d966bbf367a8e0d8491bcc8c2c22f7
Nathan Rajlich <n@n8.io>
2024-08-23 13:14:40 -0700

    .

--- packages/cli/test/integration-1.test.ts
+++ packages/cli/test/integration-1.test.ts
@@ -544,7 +544,9 @@ test('deploy `api-env` fixture and test `vercel env` command', async () => {
     );
 
     expect(exitCode, formatOutput({ stdout, stderr })).toBe(0);
-    expect(stderr).toMatch(/No Environment Variables found for/gm);
+    expect(stderr).toMatch(
+      /No Environment Variables found for (.*)\/api-env/gm
+    );
   }
 
   async function vcEnvAddWithPrompts() {
@@ -620,7 +622,7 @@ test('deploy `api-env` fixture and test `vercel env` command', async () => {
     );
 
     expect(exitCode, formatOutput({ stdout, stderr })).toBe(0);
-    expect(stderr).toMatch(/Environment Variables found in Project/gm);
+    expect(stderr).toMatch(/Environment Variables found for (.*)\/api-env/gm);
 
     const lines = stdout.split('\n');
 
