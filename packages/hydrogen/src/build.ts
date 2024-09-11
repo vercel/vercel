8e90f415663226411ee6f294e30331a95806e53e
Sean Massa <EndangeredMassa@gmail.com>
2024-09-11 10:39:25 -0500

    Revert #11871 "Fix corepack `packageManager` detection on monorepos" (#12099)
    Reverts vercel/vercel#11871
    
    See: https://vercel.slack.com/archives/C07LRLJJ3HU

--- packages/hydrogen/src/build.ts
+++ packages/hydrogen/src/build.ts
@@ -51,13 +51,15 @@ export const build: BuildV2 = async ({
   );
 
   const spawnOpts = getSpawnOptions(meta, nodeVersion);
-  const { cliType, lockfileVersion, packageJsonPackageManager } =
-    await scanParentDirs(entrypointDir, true);
+  const { cliType, lockfileVersion, packageJson } = await scanParentDirs(
+    entrypointDir,
+    true
+  );
 
   spawnOpts.env = getEnvForPackageManager({
     cliType,
     lockfileVersion,
-    packageJsonPackageManager,
+    packageJsonPackageManager: packageJson?.packageManager,
     nodeVersion,
     env: spawnOpts.env || {},
   });
