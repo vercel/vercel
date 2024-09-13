b94fc5260c9c1b7e9fcb76467c90d4c231c04a1f
Austin Merrick <austinmerrick@onsclom.net>
2024-09-13 09:17:58 -0700

    Update versions

--- packages/go/src/go-helpers.ts
+++ packages/go/src/go-helpers.ts
@@ -22,8 +22,8 @@ import type { Env } from '@vercel/build-utils';
 const streamPipeline = promisify(pipeline);
 
 const versionMap = new Map([
-  ['1.23', '1.23.0'],
-  ['1.22', '1.22.6'],
+  ['1.23', '1.23.1'],
+  ['1.22', '1.22.7'],
   ['1.21', '1.21.13'],
   ['1.20', '1.20.14'],
   ['1.19', '1.19.13'],
