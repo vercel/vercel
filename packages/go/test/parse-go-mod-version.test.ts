b4c0970c06e75960943a32115a5f780ebaf04aa3
Kevin Z <zyxkad@gmail.com>
2024-09-17 10:48:54 -0600

    Update parse-go-mod-version.test.ts

--- packages/go/test/parse-go-mod-version.test.ts
+++ packages/go/test/parse-go-mod-version.test.ts
@@ -7,32 +7,32 @@ describe('parseGoModVersion', function () {
   });
   it('returns exactly same version if patch exists', async () => {
     const version = parseGoModVersion('go 1.21.1');
-    expect(version).toEqual('1.16.1');
+    expect(version).toEqual('1.21.1');
   });
   it('returns the latest patch version if patch is not defined', async () => {
     const version = parseGoModVersion('go 1.16');
     expect(version).toEqual('1.16.15');
   });
   it('returns correct version when unrelated line exists', async () => {
-    const version = parseGoModVersion('something\ngo 1.16.1\nsomething');
-    expect(version).toEqual('1.16.1');
+    const version = parseGoModVersion('something\ngo 1.21.1\nsomething');
+    expect(version).toEqual('1.21.1');
   });
   it('returns correct version when space exists', async () => {
-    const version = parseGoModVersion('  \tgo\t 1.16.1 \t');
-    expect(version).toEqual('1.16.1');
+    const version = parseGoModVersion('  \tgo\t 1.21.1 \t');
+    expect(version).toEqual('1.21.1');
   });
   it('returns correct version when comment exists', async () => {
     const version = parseGoModVersion(
-      '// some comment\ngo 1.16.1//some other comment'
+      '// some comment\ngo 1.21.1//some other comment'
     );
-    expect(version).toEqual('1.16.1');
+    expect(version).toEqual('1.21.1');
   });
   it('returns correct version when comment exists with spaces between', async () => {
-    const version = parseGoModVersion('go 1.16.1\t  // some comment');
-    expect(version).toEqual('1.16.1');
+    const version = parseGoModVersion('go 1.21.1\t  // some comment');
+    expect(version).toEqual('1.21.1');
   });
   it('returns correct version when similar version structure exists in comment', async () => {
-    const version = parseGoModVersion('// go 1.17.1\ngo 1.16.1 // go 1.15.1');
-    expect(version).toEqual('1.16.1');
+    const version = parseGoModVersion('// go 1.17.1\ngo 1.21.1 // go 1.15.1');
+    expect(version).toEqual('1.21.1');
   });
 });
