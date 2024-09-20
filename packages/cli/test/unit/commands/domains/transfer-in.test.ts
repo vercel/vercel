34a4acea18acd83cfc5c8eb894fb3bb0a6e46558
Trek Glowacki <trek.glowacki@vercel.com>
2024-09-20 12:53:58 -0500

    [cli] Standardize tests directory/file structure (#12149)
    I started the work to audit missing CLI command/subcommand tests by
    adding in `it.todo` for any missing command/subcommand/option combos and
    discovered our test directory/file/`describe` nesting pattern is all
    over the place making that identification a bit complicated. The is the
    first step in normalizing the structure to:
    
    ```
    <command>/
      index.test.ts
      <sub-command>.test.ts
    ```
    
    For files and:
    
    ```
    describe('<command> <subcommand>`, () => {
      describe('--<flag>`, () => {});
    });
    ```
    
    Within `index.test.ts` verifying one of two scenarios:
    
    1. For commands with a default subcommand invocation, that the correct
    function is called (via mocking).
    2. For commands without a default subcommand invocation, that calling
    the base command errors.
    
    If a command has no subcommands we'd have
    
    ```
    <command>/
      index.test.ts
    ```
    
    ```
    describe('<command>`, () => {
      describe('--<flag>`, () => {});
    });
    ```
    
    We could possibly flatten that into just `<command>.test.ts` but then
    would need to watch for if and when any subcommand are introduced.
    Consistency over brevity seemed the correct choice here.
    
    
    This PR primarily shifts existing test _files_, but I also put in some
    examples of what the new unimplemented tests could look like.
    
    A follow up PR will break apart exist nested `describe`s in
    `index.test.ts` into their own files. A subsequent followup PR (or PRs)
    will add the `it.todo` calls for missing tests.

---
+++ packages/cli/test/unit/commands/domains/transfer-in.test.ts
@@ -0,0 +1,9 @@
+import { describe, it } from 'vitest';
+
+describe('domains transfer-in', () => {
+  describe('[DOMAIN]', () => {
+    describe('--code', () => {
+      it.todo('does a thing?');
+    });
+  });
+});
