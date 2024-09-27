3c47e03e17c9668fd77bf86d9c3d5bfd284e4931
Trek Glowacki <trek.glowacki@vercel.com>
2024-09-27 11:53:16 -0500

    Add tests for all routing/erroring base command invocations

--- packages/cli/test/unit/commands/certs/index.test.ts
+++ packages/cli/test/unit/commands/certs/index.test.ts
@@ -1,5 +1,11 @@
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
+import certs from '../../../../src/commands/certs';
+import { client } from '../../../mocks/client';
 
 describe('certs', () => {
-  it.todo('errors when invoked without subcommand');
+  it('errors when invoked without subcommand', async () => {
+    client.setArgv('certs');
+    const exitCodePromise = certs(client);
+    expect(exitCodePromise).resolves.toBe(2);
+  });
 });
--- packages/cli/test/unit/commands/integration/index.test.ts
+++ packages/cli/test/unit/commands/integration/index.test.ts
@@ -1,5 +1,11 @@
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
+import integration from '../../../../src/commands/integration';
+import { client } from '../../../mocks/client';
 
 describe('integration', () => {
-  it.todo('errors when invoked without subcommand');
+  it('errors when invoked without subcommand', async () => {
+    client.setArgv('integration');
+    const exitCodePromise = integration(client);
+    expect(exitCodePromise).resolves.toBe(2);
+  });
 });
