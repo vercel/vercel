f114a6a50e8ad0ac034e93bbfa2feb337adacb72
Jake Uskoski <jake.uskoski@vercel.com>
2024-10-04 16:00:35 -0400

    Add list entry to integration subcommands

--- packages/cli/src/commands/integration/command.ts
+++ packages/cli/src/commands/integration/command.ts
@@ -22,16 +22,49 @@ export const integrationCommand: Command = {
         },
       ],
       options: [],
-      examples: [],
+      examples: [
+        {
+          name: 'Install a marketplace integration',
+          value: [
+            `${packageName} integration add <integration-name>`,
+            `${packageName} integration add acme`,
+          ],
+        },
+      ],
     },
-  ],
-  examples: [
     {
-      name: 'Install a marketplace integration',
-      value: [
-        `${packageName} integration add <integration-name>`,
-        `${packageName} integration add acme`,
+      name: 'list',
+      description: 'Lists all resources from marketplace integrations',
+      arguments: [
+        {
+          name: 'provider',
+          required: false,
+        },
+      ],
+      options: [
+        {
+          name: 'filter',
+          description: 'limits the resources listed to a designated provider',
+          argument: 'NAME',
+          shorthand: 'f',
+          type: String,
+          deprecated: false,
+        },
+      ],
+      examples: [
+        {
+          name: 'List all resources',
+          value: [`${packageName} integrations list`],
+        },
+        {
+          name: 'List all resources from a single integration',
+          value: [
+            `${packageName} integrations list --filter <integration>`,
+            `${packageName} integrations list --filter acme`,
+          ],
+        },
       ],
     },
   ],
+  examples: [],
 };
