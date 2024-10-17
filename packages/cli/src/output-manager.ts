04f0d9f87b90ea24ad63ada88071c030e711a636
Sean Massa <endangeredmassa@gmail.com>
2024-10-17 12:28:42 -0500

    simplify more

--- packages/cli/src/output-manager.ts
+++ packages/cli/src/output-manager.ts
@@ -1,14 +1,9 @@
 import { Output } from './util/output';
-import { OutputOptions } from './util/output/create-output';
 
 let output = new Output(process.stderr, {
   debug: false,
   noColor: true,
 });
-
-export function outputInit(options: OutputOptions) {
-  output.initialize(options);
-}
 
 /**
  * A managed singleton instance of the Output class.
