98d28568508885d509bd367c67a95c43b86cee10
Trek Glowacki <trek.glowacki@vercel.com>
2024-10-25 09:19:35 -0500

    [cli] add subcommand tracking for `integration` group

---
+++ packages/cli/src/util/telemetry/commands/integration/index.ts
@@ -0,0 +1,31 @@
+import { TelemetryClient } from '../..';
+
+export class IntegrationTelemetryClient extends TelemetryClient {
+  trackCliSubcommandAdd(actual: string) {
+    this.trackCliSubcommand({
+      subcommand: 'add',
+      value: actual,
+    });
+  }
+
+  trackCliSubcommandList(actual: string) {
+    this.trackCliSubcommand({
+      subcommand: 'list',
+      value: actual,
+    });
+  }
+
+  trackCliSubcommandMove(actual: string) {
+    this.trackCliSubcommand({
+      subcommand: 'move',
+      value: actual,
+    });
+  }
+
+  trackCliSubcommandOpen(actual: string) {
+    this.trackCliSubcommand({
+      subcommand: 'open',
+      value: actual,
+    });
+  }
+}
