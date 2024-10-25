d2190d39e7cc8bd0d123277c142755ce526e897a
Nathan Rajlich <n@n8.io>
2024-10-25 16:23:04 -0700

    [cli] Add telemetry for `vercel certs remove` (#12394)

--- packages/cli/test/mocks/certs.ts
+++ packages/cli/test/mocks/certs.ts
@@ -1,18 +1,18 @@
 import chance from 'chance';
 import { client } from './client';
 
-export function useCert() {
-  function create(cert: string) {
-    return {
-      uid: `dummy-${cert}.cert`,
-      created: chance().timestamp(),
-      expiration: chance().timestamp(),
-      renew: chance().bool(),
-      cns: [chance().domain()],
-      age: chance().integer({ min: 0, max: 1000 }),
-    };
-  }
+function create(cert: string) {
+  return {
+    uid: `dummy-${cert}.cert`,
+    created: chance().timestamp(),
+    expiration: chance().timestamp(),
+    renew: chance().bool(),
+    cns: [chance().domain()],
+    age: chance().integer({ min: 0, max: 1000 }),
+  };
+}
 
+export function useCert() {
   client.scenario.get('/v4/now/certs', (_req, res) => {
     const limit = parseInt(_req.query.limit);
     const certs = Array.from({ length: limit }, (v, i) => create(`${i}`));
@@ -20,5 +20,14 @@ export function useCert() {
       certs: certs,
       pagination: { count: limit, total: limit, page: 1, pages: 1 },
     });
+  });
+
+  client.scenario.get('/v6/certs/:id', (req, res) => {
+    const cert = create(req.params.id);
+    res.json(cert);
+  });
+
+  client.scenario.delete('/v5/certs/:id', (req, res) => {
+    res.json({});
   });
 }
