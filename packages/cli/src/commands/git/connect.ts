d1d57a33de8c9a25ba6c00c9a4860df7b71f7656
Trek Glowacki <trek.glowacki@vercel.com>
2024-10-29 12:06:33 -0500

    [cli] don't track unreachable condition

--- packages/cli/src/commands/git/connect.ts
+++ packages/cli/src/commands/git/connect.ts
@@ -65,7 +65,6 @@ export default async function connect(
   });
 
   telemetry.trackCliFlagYes(argv['--yes']);
-  telemetry.trackCliFlagConfirm(argv['--confirm']);
 
   const confirm = Boolean(argv['--yes']);
   const repoArg = args[0];
