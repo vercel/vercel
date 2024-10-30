2283825780ab8ae92e3cfbd7213f729b46b016f0
Trek Glowacki <trek.glowacki@vercel.com>
2024-10-30 15:01:59 -0500

    [tests] fix small naming error (#12434)
    This is testing the `domains` function but had `dns` listed as an arg.

--- packages/cli/test/unit/commands/domains/index.test.ts
+++ packages/cli/test/unit/commands/domains/index.test.ts
@@ -14,7 +14,7 @@ describe('domains', () => {
     const args: string[] = [];
     const opts = {};
 
-    client.setArgv('dns', ...args);
+    client.setArgv('domains', ...args);
     await domains(client);
     expect(lsSpy).toHaveBeenCalledWith(client, opts, args);
   });
