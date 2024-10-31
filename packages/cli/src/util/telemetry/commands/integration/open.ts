faf6f878404565baf24747d72440d5408b7c5622
Nathan Rajlich <n@n8.io>
2024-10-31 09:36:39 -0700

    [cli] Add strongly typed `subcommands` for Telemetry methods based on command specification (#12446)
    * Adds the `subcommads` Telemetry methods (i.e. `trackCliSubcommandOpen()`) to the strong typing generic type.
    * Applies the interface to the `vercel integration` family of (sub)commands, since their commands specification data is already in the correct format.
    
    For the rest of the (sub)commands, we still need to wait for https://github.com/vercel/vercel/pull/12103, but at least the generic type is now prepared for `subcommands` to be properly defined in the commands' spec.

--- packages/cli/src/util/telemetry/commands/integration/open.ts
+++ packages/cli/src/util/telemetry/commands/integration/open.ts
@@ -1,7 +1,12 @@
 import { TelemetryClient } from '../..';
+import type { TelemetryMethods } from '../../types';
+import type { openSubcommand } from '../../../../commands/integration/command';
 
-export class IntegrationOpenTelemetryClient extends TelemetryClient {
-  trackCliArgumentName(v: string, known: boolean | undefined) {
+export class IntegrationOpenTelemetryClient
+  extends TelemetryClient
+  implements TelemetryMethods<typeof openSubcommand>
+{
+  trackCliArgumentName(v: string | undefined, known?: boolean) {
     this.trackCliArgument({
       arg: 'name',
       value: known ? v : this.redactedValue,
