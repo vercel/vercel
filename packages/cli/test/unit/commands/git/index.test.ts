d32f295c48bb247ba1976d84425c9b452cf6ec73
Trek Glowacki <trek.glowacki@vercel.com>
2024-10-31 08:02:27 -0500

    [tests] enable git command index test

--- packages/cli/test/unit/commands/git/index.test.ts
+++ packages/cli/test/unit/commands/git/index.test.ts
@@ -1,9 +1,15 @@
 import { describe, it, expect } from 'vitest';
+import fs from 'fs-extra';
+import { join } from 'path';
 import git from '../../../../src/commands/git';
 import { client } from '../../../mocks/client';
+import { useTeams } from '../../../mocks/team';
+import { defaultProject, useProject } from '../../../mocks/project';
 
-// this requires mocking a linked project
-describe.todo('git', () => {
+describe('git', () => {
+  const fixture = (name: string) =>
+    join(__dirname, '../../../fixtures/unit/commands/git/connect', name);
+
   describe('--help', () => {
     it('tracks telemetry', async () => {
       const command = 'git';
@@ -21,9 +27,22 @@ describe.todo('git', () => {
     });
   });
 
-  it('errors when invoked without subcommand', async () => {
-    client.setArgv('git');
-    const exitCodePromise = git(client);
-    await expect(exitCodePromise).resolves.toBe(2);
+  it('displays help when invoked without subcommand', async () => {
+    const cwd = fixture('new-connection');
+    client.cwd = cwd;
+    try {
+      await fs.rename(join(cwd, 'git'), join(cwd, '.git'));
+      useTeams('team_dummy');
+      useProject({
+        ...defaultProject,
+        id: 'new-connection',
+        name: 'new-connection',
+      });
+      client.setArgv('git');
+      const exitCodePromise = git(client);
+      await expect(exitCodePromise).resolves.toBe(2);
+    } finally {
+      await fs.rename(join(cwd, '.git'), join(cwd, 'git'));
+    }
   });
 });
