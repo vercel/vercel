f30614a5004c550e63da6eff03ba03f07438f6a2
Erika Rowland <erika.rowland@vercel.com>
2024-11-04 12:57:27 -0800

    [cli] add telemetry for `vercel teams switch`

--- packages/cli/src/commands/teams/switch.ts
+++ packages/cli/src/commands/teams/switch.ts
@@ -10,6 +10,7 @@ import listInput from '../../util/input/list';
 import { Team, GlobalConfig } from '@vercel-internals/types';
 import { writeToConfigFile } from '../../util/config/files';
 import output from '../../output-manager';
+import { TeamsSwitchTelemetryClient } from '../../util/telemetry/commands/teams/switch';
 
 const updateCurrentTeam = (config: GlobalConfig, team?: Team) => {
   if (team) {
@@ -22,7 +23,13 @@ const updateCurrentTeam = (config: GlobalConfig, team?: Team) => {
 };
 
 export default async function main(client: Client, desiredSlug?: string) {
-  const { config } = client;
+  const { config, telemetryEventStore } = client;
+  const telemetry = new TeamsSwitchTelemetryClient({
+    opts: {
+      store: telemetryEventStore,
+    },
+  });
+  telemetry.trackCliArgumentDesiredSlug(desiredSlug);
   const personalScopeSelected = !config.currentTeam;
 
   output.spinner('Fetching teams information');
