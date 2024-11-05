ca9669b95305913fc1e3a9ba42f3a5244f142c6e
ViableSystemModel <44686976+ViableSystemModel@users.noreply.github.com>
2024-11-04 23:47:28 -0500

    Remove old buggy solidstart example

--- examples/solidstart/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import solid from "solid-start/vite";
-import vercel from "solid-start-vercel";
-
-export default defineConfig({
-  plugins: [solid({ adapter: vercel() })],
-});
