4e6643d983538eb5eeb32bb60c4cb6daac25bccd
ViableSystemModel <44686976+ViableSystemModel@users.noreply.github.com>
2024-11-05 00:05:14 -0500

    Update solidstart-1 example to use current results of `pnpm create solid`

--- examples/solidstart-1/src/components/Counter.tsx
+++ examples/solidstart-1/src/components/Counter.tsx
@@ -1,11 +1,10 @@
-"use client";
 import { createSignal } from "solid-js";
 import "./Counter.css";
 
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button class="increment" onClick={() => setCount(count() + 1)}>
+    <button class="increment" onClick={() => setCount(count() + 1)} type="button">
       Clicks: {count()}
     </button>
   );
