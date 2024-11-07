ecb913706c33c00f2a5a703dc7d43115ae83ad38
Erika Rowland <erika.rowland@vercel.com>
2024-11-07 11:53:10 -0800

    [cli] fix tests

--- packages/cli/test/unit/commands/project/rm.test.ts
+++ packages/cli/test/unit/commands/project/rm.test.ts
@@ -17,7 +17,7 @@ describe('rm', () => {
       expect(client.telemetryEventStore).toHaveTelemetryEvents([
         {
           key: 'flag:help',
-          value: `${command}:remove`,
+          value: `${command}:${subcommand}`,
         },
       ]);
     });
