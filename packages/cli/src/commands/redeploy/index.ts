7fdae1e9925fb0365af566cafc9c45f1083d41db
Erika Rowland <erika.rowland@vercel.com>
2024-11-14 12:06:00 -0800

    [cli] type import

--- packages/cli/src/commands/redeploy/index.ts
+++ packages/cli/src/commands/redeploy/index.ts
@@ -18,7 +18,7 @@ import { redeployCommand } from './command';
 import { getFlagsSpecification } from '../../util/get-flags-specification';
 import output from '../../output-manager';
 import { RedeployTelemetryClient } from '../../util/telemetry/commands/redeploy';
-import type { CustomEnvironment } from '@vercel-internals/types';
+import type { CustomEnvironment, Deployment } from '@vercel-internals/types';
 import {
   getCustomEnvironments,
   pickCustomEnvironment,
