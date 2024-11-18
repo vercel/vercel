vercel/vercel

commit 786de1af5089b9647b12602b4dd383bb8fec79b5

[cli] Fix framework detection for `vc deploy` when using `--archive` and/or `--prebuilt` (#12598)
Leverage the `ensureLink()` function in `vc deploy`. This ends up being
a multi-win:

* Since `ensureLink` doesn't know/care about `--archive`/`--prebuilt`,
the source code is uploaded for server-side framework detection (the
_fix_ aspect of this PR).
* Quite a bit of duplicated code gets removed, and the logic is cleaner
(previously, the linking logic was intertwined with the deployment logic
itself, making it hard to follow).
* This is a prefactor for enabling local framework detection, which I
plan to do as a follow-up PR.

packages/cli/src/commands/deploy/index.ts
@@ -9,7 +9,7 @@ import {
   VALID_ARCHIVE_FORMATS,
   VercelConfig,
 } from '@vercel/client';
-import { errorToString, isErrnoException, isError } from '@vercel/error-utils';
+import { errorToString, isError } from '@vercel/error-utils';
 import bytes from 'bytes';
 import chalk from 'chalk';
 import fs from 'fs-extra';
@@ -51,12 +51,6 @@ import { parseArguments } from '../../util/get-args';
 import getDeployment from '../../util/get-deployment';
 import { getFlagsSpecification } from '../../util/get-flags-specification';
 import getProjectName from '../../util/get-project-name';
-import toHumanPath from '../../util/humanize-path';
-import confirm from '../../util/input/confirm';
-import editProjectSettings from '../../util/input/edit-project-settings';
-import inputProject from '../../util/input/input-project';
-import { inputRootDirectory } from '../../util/input/input-root-directory';
-import selectOrg from '../../util/input/select-org';
 import code from '../../util/output/code';
 import highlight from '../../util/output/highlight';
 import param from '../../util/output/param';
@@ -64,10 +58,6 @@ import stamp from '../../util/output/stamp';
 import { parseEnv } from '../../util/parse-env';
 import parseMeta from '../../util/parse-meta';
 import { getCommandName } from '../../util/pkg-name';
-import {
-  getLinkedProject,
-  linkFolderToProject,
-} from '../../util/projects/link';
 import { pickOverrides } from '../../util/projects/project-settings';
 import validatePaths, {
   validateRootDirectory,
@@ -77,6 +67,7 @@ import { deployCommand } from './command';
 import parseTarget from '../../util/parse-target';
 import { DeployTelemetryClient } from '../../util/telemetry/commands/deploy';
 import output from '../../output-manager';
+import { ensureLink } from '../../util/link/ensure-link';
 
 export default async (client: Client): Promise<number> => {
   const telemetryClient = new DeployTelemetryClient({
@@ -159,6 +150,7 @@ export default async (client: Client): Promise<number> => {
   let localConfig = client.localConfig || readLocalConfig(paths[0]);
 
   if (localConfig) {
+    client.localConfig = localConfig;
     const { version } = localConfig;
     const file = highlight(localConfig[fileNameSymbol]!);
     const prop = code('version');
@@ -228,100 +220,29 @@ export default async (client: Client): Promise<number> => {
     return 1;
   }
 
-  // retrieve `project` and `org` from .vercel
-  const link = await getLinkedProject(client, cwd);
-
-  if (link.status === 'error') {
-    return link.exitCode;
-  }
-
-  let { org, project, status } = link;
-
-  let newProjectName = null;
-  let rootDirectory = project ? project.rootDirectory : null;
-  let sourceFilesOutsideRootDirectory: boolean | undefined = true;
-
-  if (status === 'not_linked') {
-    const shouldStartSetup =
-      autoConfirm ||
-      (await confirm(
-        client,
-        `Set up and deploy ${chalk.cyan(`“${toHumanPath(cwd)}”`)}?`,
-        true
-      ));
-
-    if (!shouldStartSetup) {
-      output.print(`Canceled. Project not set up.\n`);
-      return 0;
-    }
-
-    try {
-      org = await selectOrg(
-        client,
-        'Which scope do you want to deploy to?',
-        autoConfirm
-      );
-    } catch (err: unknown) {
-      if (
-        isErrnoException(err) &&
-        (err.code === 'NOT_AUTHORIZED' || err.code === 'TEAM_DELETED')
-      ) {
-        output.error(err.message);
-        return 1;
-      }
-
-      throw err;
-    }
-
-    // We use `localConfig` here to read the name
-    // even though the `vercel.json` file can change
-    // afterwards, this is fine since the property
-    // will be deprecated and can be replaced with
-    // user input.
-    const detectedProjectName = getProjectName({
+  // Retrieve `project` and `org` from linked Project.
+  // If not linked, prompt user to set up a new Project.
+  const link = await ensureLink('deploy', client, cwd, {
+    autoConfirm,
+    setupMsg: 'Set up and deploy',
+    projectName: getProjectName({
       nameParam: parsedArguments.flags['--name'],
       nowConfig: localConfig,
       paths,
-    });
-
-    const projectOrNewProjectName = await inputProject(
-      client,
-      org,
-      detectedProjectName,
-      autoConfirm
-    );
-
-    if (typeof projectOrNewProjectName === 'string') {
-      newProjectName = projectOrNewProjectName;
-      rootDirectory = await inputRootDirectory(client, cwd, autoConfirm);
-    } else {
-      project = projectOrNewProjectName;
-      rootDirectory = project.rootDirectory;
-      sourceFilesOutsideRootDirectory = project.sourceFilesOutsideRootDirectory;
-
-      // we can already link the project
-      await linkFolderToProject(
-        client,
-        cwd,
-        {
-          projectId: project.id,
-          orgId: org.id,
-        },
-        project.name,
-        org.slug
-      );
-      status = 'linked';
-    }
+    }),
+  });
+  if (typeof link === 'number') {
+    return link;
   }
+
+  const { org, project } = link;
+  const rootDirectory = project.rootDirectory;
+  const sourceFilesOutsideRootDirectory =
+    project.sourceFilesOutsideRootDirectory ?? true;
 
   // For repo-style linking, reset the path to the root of the repository
-  if (link.status === 'linked' && link.repoRoot) {
+  if (link.repoRoot) {
     cwd = link.repoRoot;
-  }
-
-  // At this point `org` should be populated
-  if (!org) {
-    throw new Error(`"org" is not defined`);
   }
 
   // #region Build `--prebuilt`
@@ -331,11 +252,7 @@ export default async (client: Client): Promise<number> => {
 
     // For repo-style linking, update `cwd` to be the Project
     // subdirectory when `rootDirectory` setting is defined
-    if (
-      link.status === 'linked' &&
-      link.repoRoot &&
-      link.project.rootDirectory
-    ) {
+    if (link.repoRoot && link.project.rootDirectory) {
       vercelOutputDir = join(cwd, link.project.rootDirectory, '.vercel/output');
     }
 
@@ -528,7 +445,7 @@ export default async (client: Client): Promise<number> => {
   const regions = regionFlag.length > 0 ? regionFlag : localConfig.regions;
   // #endregion
 
-  const currentTeam = org?.type === 'team' ? org.id : undefined;
+  const currentTeam = org.type === 'team' ? org.id : undefined;
   const now = new Now({
     client,
     currentTeam,
@@ -539,7 +456,7 @@ export default async (client: Client): Promise<number> => {
 
   const localConfigurationOverrides = pickOverrides(localConfig);
 
-  const name = project ? project.name : newProjectName;
+  const name = project.name;
   if (!name) {
     throw new Error(
       '`name` not found on project or provided by existing project'
@@ -587,14 +504,8 @@ export default async (client: Client): Promise<number> => {
       createArgs.projectSettings = {
         sourceFilesOutsideRootDirectory,
         rootDirectory,
+        ...localConfigurationOverrides,
       };
-
-      if (status === 'linked') {
-        createArgs.projectSettings = {
-          ...createArgs.projectSettings,
-          ...localConfigurationOverrides,
-        };
-      }
     }
 
     // Read the `engines.node` field from `package.json` and send as a
@@ -632,42 +543,6 @@ export default async (client: Client): Promise<number> => {
       cwd,
       archive
     );
-
-    if (deployment.code === 'missing_project_settings') {
-      let { projectSettings, framework } = deployment;
-      if (rootDirectory) {
-        projectSettings.rootDirectory = rootDirectory;
-      }
-
-      if (typeof sourceFilesOutsideRootDirectory !== 'undefined') {
-        projectSettings.sourceFilesOutsideRootDirectory =
-          sourceFilesOutsideRootDirectory;
-      }
-
-      const settings = await editProjectSettings(
-        client,
-        projectSettings,
-        framework,
-        false,
-        localConfigurationOverrides
-      );
-
-      // deploy again, but send projectSettings this time
-      createArgs.projectSettings = settings;
-
-      deployStamp = stamp();
-      createArgs.deployStamp = deployStamp;
-      deployment = await createDeploy(
-        client,
-        now,
-        contextName,
-        sourcePath,
-        createArgs,
-        org,
-        false,
-        cwd
-      );
-    }
 
     if (deployment instanceof NotDomainOwner) {
       output.error(deployment.message);

packages/cli/src/util/index.ts
@@ -171,7 +171,6 @@ export default class Now {
       isSettingUpProject,
       archive,
       skipAutoDetectionConfirmation,
-      cwd,
       prebuilt,
       vercelOutputDir,
       rootDirectory,

packages/cli/src/util/link/setup-and-link.ts
@@ -19,8 +19,9 @@ import selectOrg from '../input/select-org';
 import inputProject from '../input/input-project';
 import { validateRootDirectory } from '../validate-paths';
 import { inputRootDirectory } from '../input/input-root-directory';
-import editProjectSettings, {
-  PartialProjectSettings,
+import {
+  editProjectSettings,
+  type PartialProjectSettings,
 } from '../input/edit-project-settings';
 import stamp from '../output/stamp';
 import { EmojiLabel } from '../emoji';
@@ -28,6 +29,9 @@ import createDeploy from '../deploy/create-deploy';
 import Now, { CreateOptions } from '../index';
 import { isAPIError } from '../errors-ts';
 import output from '../../output-manager';
+import { getPrettyError } from '@vercel/build-utils';
+import { SchemaValidationFailed } from '../errors';
+import { fileNameSymbol } from '@vercel/client';
 
 export interface SetupAndLinkOptions {
   autoConfirm?: boolean;
@@ -261,6 +265,14 @@ export default async function setupAndLink(
 
     return { status: 'linked', org, project };
   } catch (err) {
+    if (err instanceof SchemaValidationFailed) {
+      const niceError = getPrettyError(err.meta);
+      const fileName = localConfig?.[fileNameSymbol] || 'vercel.json';
+      niceError.message = `Invalid ${fileName} - ${niceError.message}`;
+      output.prettyError(niceError);
+      return { status: 'error', exitCode: 1 };
+    }
+
     if (isAPIError(err) && err.code === 'too_many_projects') {
       output.prettyError(err);
       return { status: 'error', exitCode: 1, reason: 'TOO_MANY_PROJECTS' };

packages/cli/test/unit/commands/deploy/index.test.ts
@@ -715,8 +715,8 @@ describe('deploy', () => {
 
     await Promise.all<void>([runCommand(), slowlyDeploy()]);
 
-    // remove first 4 lines which contains randomized data
-    expect(client.getFullOutput().split('\n').slice(4).join('\n'))
+    // remove first 3 lines which contains randomized data
+    expect(client.getFullOutput().split('\n').slice(3).join('\n'))
       .toMatchInlineSnapshot(`
         "Building
         2024-06-03T15:01:10.339Z  Hello, world!
@@ -1148,14 +1148,33 @@ describe('deploy', () => {
   describe('first deploy', () => {
     describe('project setup', () => {
       const directoryName = 'unlinked';
+      let missingProjectSettings = false;
+
       beforeEach(() => {
+        missingProjectSettings = true;
+
         const user = useUser();
         client.scenario.get(`/v9/projects/:id`, (_req, res) => {
           return res.status(404).json({});
         });
 
+        client.scenario.post(`/v1/projects`, (req, res) => {
+          return res.status(200).json(req.body);
+        });
+
         const createdDeploymentId = 'dpl_1';
         client.scenario.post(`/v13/deployments`, (req, res) => {
+          if (missingProjectSettings) {
+            res.status(400).json({
+              error: {
+                code: 'missing_project_settings',
+                framework: null,
+                projectSettings: {},
+              },
+            });
+            missingProjectSettings = false;
+            return;
+          }
           res.json({
             creator: {
               uid: user.id,
@@ -1201,7 +1220,7 @@ describe('deploy', () => {
         client.stdin.write('y\n');
 
         await expect(client.stderr).toOutput(
-          '? Which scope do you want to deploy to?'
+          '? Which scope should contain your project?'
         );
         client.stdin.write('\n');
 
@@ -1233,7 +1252,7 @@ describe('deploy', () => {
         client.stdin.write('y\n');
 
         await expect(client.stderr).toOutput(
-          '? Which scope do you want to deploy to?'
+          '? Which scope should contain your project?'
         );
         client.stdin.write('\n');
 

packages/cli/src/util/deploy/process-deployment.ts
@@ -12,7 +12,6 @@ import Now from '../../util';
 import { emoji, prependEmoji } from '../emoji';
 import { displayBuildLogs } from '../logs';
 import { progress } from '../output/progress';
-import { linkFolderToProject } from '../projects/link';
 import ua from '../ua';
 import output from '../../output-manager';
 
@@ -34,7 +33,6 @@ function printInspectUrl(
 
 export default async function processDeployment({
   org,
-  cwd,
   projectName,
   isSettingUpProject,
   archive,
@@ -59,7 +57,6 @@ export default async function processDeployment({
   isSettingUpProject: boolean;
   archive?: ArchiveFormat;
   skipAutoDetectionConfirmation?: boolean;
-  cwd: string;
   rootDirectory?: string | null;
   noWait?: boolean;
   withLogs?: boolean;
@@ -180,17 +177,6 @@ export default async function processDeployment({
       if (event.type === 'created') {
         const deployment: Deployment = event.payload;
 
-        await linkFolderToProject(
-          client,
-          cwd,
-          {
-            orgId: org.id,
-            projectId: deployment.projectId!,
-          },
-          projectName,
-          org.slug
-        );
-
         now.url = deployment.url;
 
         stopSpinner();
@@ -209,7 +195,7 @@ export default async function processDeployment({
           ) + `\n`
         );
 
-        if (quiet) {
+        if (quiet || process.env.FORCE_TTY === '1') {
           process.stdout.write(`https://${event.payload.url}`);
         }
 

packages/cli/src/util/input/edit-project-settings.ts
@@ -21,7 +21,7 @@ const settingKeys = Object.keys(settingMap).sort() as unknown as readonly [
 
 export type PartialProjectSettings = Pick<ProjectSettings, ConfigKeys>;
 
-export default async function editProjectSettings(
+export async function editProjectSettings(
   client: Client,
   projectSettings: PartialProjectSettings | null,
   framework: Framework | null,

packages/cli/test/integration-1.test.ts
@@ -457,12 +457,53 @@ test('deploy from a nested directory', async () => {
 
   const vc = execCli(binaryPath, ['deploy', `--name=${projectName}`], {
     cwd: root,
+    env: {
+      FORCE_TTY: '1',
+    },
   });
 
   await waitForPrompt(vc, /Set up and deploy[^?]+\?/);
   vc.stdin?.write('yes\n');
 
-  await waitForPrompt(vc, 'Which scope do you want to deploy to?');
+  await waitForPrompt(vc, 'Which scope should contain your project?');
+  vc.stdin?.write('\n');
+
+  await waitForPrompt(vc, 'Link to existing project?');
+  vc.stdin?.write('no\n');
+
+  await waitForPrompt(vc, `What’s your project’s name? (${projectName})`);
+  vc.stdin?.write(`\n`);
+
+  await waitForPrompt(vc, 'In which directory is your code located?');
+  vc.stdin?.write('app\n');
+
+  // This means the framework detection worked!
+  await waitForPrompt(vc, 'Auto-detected Project Settings (Next.js)');
+
+  vc.kill();
+});
+
+test('deploy from a nested directory with `--archive=tgz` option', async () => {
+  const root = await setupE2EFixture('zero-config-next-js-nested');
+  const projectName = `project-link-dev-${
+    Math.random().toString(36).split('.')[1]
+  }`;
+
+  const vc = execCli(
+    binaryPath,
+    ['deploy', '--archive=tgz', `--name=${projectName}`],
+    {
+      cwd: root,
+      env: {
+        FORCE_TTY: '1',
+      },
+    }
+  );
+
+  await waitForPrompt(vc, /Set up and deploy[^?]+\?/);
+  vc.stdin?.write('yes\n');
+
+  await waitForPrompt(vc, 'Which scope should contain your project?');
   vc.stdin?.write('\n');
 
   await waitForPrompt(vc, 'Link to existing project?');

packages/cli/test/integration-2.test.ts
@@ -159,7 +159,11 @@ test('should show prompts to set up project during first deploy', async () => {
   // remove previously linked project if it exists
   await remove(path.join(dir, '.vercel'));
 
-  const now = execCli(binaryPath, [dir]);
+  const now = execCli(binaryPath, [dir], {
+    env: {
+      FORCE_TTY: '1',
+    },
+  });
 
   await setupProject(now, projectName, {
     buildCommand: `mkdir -p o && echo '<h1>custom hello</h1>' > o/index.html`,
@@ -258,7 +262,7 @@ test('should prefill "project name" prompt with now.json `name`', async () => {
   await waitForPrompt(now, /Set up and deploy[^?]+\?/);
   now.stdin?.write('yes\n');
 
-  await waitForPrompt(now, 'Which scope do you want to deploy to?');
+  await waitForPrompt(now, 'Which scope should contain your project?');
   now.stdin?.write('\n');
 
   await waitForPrompt(now, 'Link to existing project?');
@@ -356,7 +360,11 @@ test('deploy shows notice when project in `.vercel` does not exists', async () =
     })
   );
 
-  const now = execCli(binaryPath, [directory]);
+  const now = execCli(binaryPath, [directory], {
+    env: {
+      FORCE_TTY: '1',
+    },
+  });
 
   let detectedNotice = false;
 
@@ -1181,13 +1189,17 @@ test('vercel.json configuration overrides in a new project prompt user and merge
   }`;
   const parent = path.join(directory, '..');
   const newDirectory = path.join(parent, randomDirectoryName);
-  await fs.renameSync(directory, newDirectory);
+  fs.renameSync(directory, newDirectory);
   directory = newDirectory;
 
   // remove previously linked project if it exists
   await remove(path.join(directory, '.vercel'));
 
-  const vc = execCli(binaryPath, [directory]);
+  const vc = execCli(binaryPath, [directory], {
+    env: {
+      FORCE_TTY: '1',
+    },
+  });
 
   await waitForPrompt(vc, 'Set up and deploy');
   vc.stdin?.write('y\n');

commit 0fdd8c45f90ff1cd3d03deac2ca98eba75773936

[cli] Fix file uploading in `vc deploy` when `sourceFilesOutsideRootDirectory` setting is `false` (#12608)
Fixes a regression introduced in
https://github.com/vercel/vercel/pull/12598, where the incorrect set of
file paths was being used for a deployment when the
`sourceFilesOutsideRootDirectory` project setting is disabled.

packages/cli/src/commands/deploy/index.ts
@@ -310,18 +310,11 @@ export default async (client: Client): Promise<number> => {
   const contextName = org.slug;
   client.config.currentTeam = org.type === 'team' ? org.id : undefined;
 
-  // if we have `sourceFilesOutsideRootDirectory` set to `true`, we use the current path
-  // and upload the entire directory.
-  const sourcePath =
-    rootDirectory && !sourceFilesOutsideRootDirectory
-      ? join(cwd, rootDirectory)
-      : cwd;
-
   if (
     rootDirectory &&
     (await validateRootDirectory(
       cwd,
-      sourcePath,
+      join(cwd, rootDirectory),
       project
         ? `To change your Project Settings, go to https://vercel.com/${org?.slug}/${project.name}/settings`
         : ''
@@ -536,11 +529,10 @@ export default async (client: Client): Promise<number> => {
       client,
       now,
       contextName,
-      sourcePath,
+      cwd,
       createArgs,
       org,
       !project,
-      cwd,
       archive
     );
 

packages/cli/src/util/deploy/create-deploy.ts
@@ -16,18 +16,10 @@ export default async function createDeploy(
   createArgs: CreateOptions,
   org: Org,
   isSettingUpProject: boolean,
-  cwd: string,
   archive?: ArchiveFormat
 ): Promise<any | DeploymentError> {
   try {
-    return await now.create(
-      path,
-      createArgs,
-      org,
-      isSettingUpProject,
-      cwd,
-      archive
-    );
+    return await now.create(path, createArgs, org, isSettingUpProject, archive);
   } catch (err: unknown) {
     if (ERRORS_TS.isAPIError(err)) {
       if (err.code === 'rate_limited') {
@@ -112,8 +104,7 @@ export default async function createDeploy(
           path,
           createArgs,
           org,
-          isSettingUpProject,
-          cwd
+          isSettingUpProject
         );
       }
 

packages/cli/src/util/index.ts
@@ -130,7 +130,6 @@ export default class Now {
     }: CreateOptions,
     org: Org,
     isSettingUpProject: boolean,
-    cwd: string,
     archive?: ArchiveFormat
   ) {
     let hashes: any = {};

packages/cli/src/util/link/setup-and-link.ts
@@ -211,8 +211,7 @@ export default async function setupAndLink(
         sourcePath,
         createArgs,
         org,
-        true,
-        path
+        true
       );
 
       if (

packages/cli/test/unit/commands/deploy/index.test.ts
@@ -779,7 +779,6 @@ describe('deploy', () => {
       createArgs: expect.any(Object),
       org: expect.any(Object),
       isSettingUpProject: expect.any(Boolean),
-      cwd: expect.any(String),
       archive: undefined,
     };
 