27eaa58a73634c6d8a2602e10b6109349d36bc89
Jeff See <jeffsee.55@gmail.com>
2024-11-19 13:23:53 -0800

    Revert "Fix edge cases with internal redirect sorting" (#12615)
    This reverts commit 7221a5fede82fc3062c228d0b20c207995bd4de0.
    
    https://github.com/vercel/vercel/pull/12599#issuecomment-2486389468

--- packages/next/test/integration/server-build/next.config.js
+++ packages/next/test/integration/server-build/next.config.js
@@ -3,20 +3,4 @@ module.exports = (phase, { defaultConfig }) => ({
   generateBuildId() {
     return 'testing-build-id';
   },
-
-  async redirects() {
-    // these routes are for testing trailing slash ordering
-    return [
-      {
-        source: '/:country([a-z]{2})/legacy/kola',
-        destination: '/:country/flavors/kola',
-        permanent: true,
-      },
-      {
-        source: '/:country([a-z]{2})/legacy/:slug*',
-        destination: '/:country',
-        permanent: true,
-      },
-    ];
-  },
 });
