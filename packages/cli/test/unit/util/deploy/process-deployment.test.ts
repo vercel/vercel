e8538bbb68e32c13fc9167068926ec57b5bc36b4
Austin Merrick <austinmerrick@onsclom.net>
2024-11-21 14:41:53 -0800

    Add tests

---
+++ packages/cli/test/unit/util/deploy/process-deployment.test.ts
@@ -0,0 +1,56 @@
+import { describe, it, expect } from 'vitest';
+import {
+  handleErrorSolvableWithArchive,
+  archiveSuggestionText,
+} from '../../../../src/util/deploy/process-deployment';
+
+describe('processDeployment()', () => {
+  describe('handleErrorSolvableWithArchive()', () => {
+    it('should throw on too many files error', () => {
+      const originalMessage = `Invalid request: 'files' should NOT have more than 15000 items, received 15001.`;
+      expect(() =>
+        handleErrorSolvableWithArchive({
+          code: 'too_many_files',
+          message: originalMessage,
+        })
+      ).toThrow(`${originalMessage}\n${archiveSuggestionText}`);
+    });
+
+    it('should throw on upload rate limit error', () => {
+      const originalMessage =
+        'Too many requests - try again in 22 hours (more than 5000, code: "api-upload-paid").';
+      expect(() =>
+        handleErrorSolvableWithArchive({
+          code: 'rate_limited',
+          message: originalMessage,
+        })
+      ).toThrow(`${originalMessage}\n${archiveSuggestionText}`);
+    });
+
+    it('should not throw for wrong code', () => {
+      expect(() =>
+        handleErrorSolvableWithArchive({
+          code: 'other',
+          message: 'string containing api-upload',
+        })
+      ).not.toThrow();
+    });
+
+    it('should not throw if rate_limited message missing `api-upload`', () => {
+      expect(() =>
+        handleErrorSolvableWithArchive({
+          code: 'rate_limited',
+          message: 'other message',
+        })
+      ).not.toThrow();
+    });
+
+    it('should not throw if no message', () => {
+      expect(() =>
+        handleErrorSolvableWithArchive({
+          code: 'too_many_files',
+        })
+      ).not.toThrow();
+    });
+  });
+});
