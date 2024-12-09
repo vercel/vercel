fa5a6ad45bb85c4c3a501459c6f55a6fd135f021
Sean Massa <EndangeredMassa@gmail.com>
2024-12-09 09:59:27 -0600

    [sdk] move sdk (#12700)
    The `sdk` package has moved to: https://github.com/vercel/sdk
    
    ---------
    
    Co-authored-by: Trek Glowacki <trek.glowacki@vercel.com>

--- utils/update-canary-tags.mjs
+++ utils/update-canary-tags.mjs
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { spawnSync } from 'child_process';
 
 const packagesDir = new URL('../packages/', import.meta.url);
-const ignoredPackages = ['sdk'];
+const ignoredPackages = [];
 
 for (const name of fs.readdirSync(packagesDir)) {
   if (ignoredPackages.includes(name)) {
