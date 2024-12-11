3cb39bd2e05d0adc7f9bb5213bea6b84f3f4dd00
Jake Uskoski <jake.uskoski@vercel.com>
2024-12-11 11:25:36 -0500

    Use variable names to add clarity instead of comments

--- packages/cli/src/commands/integration/add.ts
+++ packages/cli/src/commands/integration/add.ts
@@ -118,9 +118,11 @@ export async function add(client: Client, args: string[]) {
   // However, when we introduce new categories, we avoid breaking this version of the CLI by linking all
   // non-storage categories to the dashboard.
   // product.type is the old way of defining categories, while the protocols are the new way.
-  const isStorageProduct =
-    product.type === 'storage' ||
+  const isPreProtocolStorageProduct = product.type === 'storage';
+  const isPostProtocolStorageProduct =
     product.protocols?.storage?.status === 'enabled';
+  const isStorageProduct =
+    isPreProtocolStorageProduct || isPostProtocolStorageProduct;
 
   // The provisioning via cli is possible when
   // 1. The integration was installed once (terms have been accepted)
