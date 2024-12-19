f74c4c5106f241648dee923ac2b6c6d186a6e557
Mitch (a.k.a Voz) <mitch.vostrez@vercel.com>
2024-12-19 11:35:37 -0600

    testing mic check 1 2

--- packages/cli/test/integration-3.test.ts
+++ packages/cli/test/integration-3.test.ts
@@ -563,7 +563,6 @@ test('create a production deployment', async () => {
   ]);
 
   expect(targetCall.exitCode, formatOutput(targetCall)).toBe(0);
-  expect(targetCall.stderr).toMatch(/Setting target to production/gm);
   expect(targetCall.stderr).toMatch(/Inspect: https:\/\/vercel.com\//gm);
   expect(targetCall.stdout).toMatch(/https:\/\//gm);
 
