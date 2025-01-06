6925cdcb3f44a29a15273bf7c8cbe680700f43aa
Erika Rowland <erika.rowland@vercel.com>
2025-01-06 13:32:07 -0800

    [cli] standardizes uses of confirm on confirm.ts

--- packages/cli/src/util/input/confirm.ts
+++ packages/cli/src/util/input/confirm.ts
@@ -3,7 +3,7 @@ import type Client from '../client';
 export default async function confirm(
   client: Client,
   message: string,
-  preferred: boolean
+  preferred: boolean = true
 ): Promise<boolean> {
   return client.input.confirm({
     message,
