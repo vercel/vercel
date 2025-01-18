d5eab3b4c1c89ce564e093ecf436e02a96e82b14
Erika Rowland <130414394+erikareads@users.noreply.github.com>
2025-01-17 16:29:23 -0800

    [build-utils] document `NODE_VERSIONS` implicit sort in comment (#12865)
    `NODE_VERSIONS` is assumed in other parts of `build-utils` to be sorted
    from newest Node version to oldest. This PR adds a comment to document
    that assumption.
    
    For example,
    https://github.com/vercel/vercel/blob/a567aaddc90016975583f442ecb61a4f1d0eefcb/packages/build-utils/src/fs/node-version.ts#L73-L89
    assumes that the latest version _is_ the first version. If the sort was
    disrupted in `NODE_VERSIONS` this function would start providing wrong
    behavior.
    
    We could make this assumption explicit by sorting `NODE_VERSIONS` before
    checking for latest, but this adds a performance penalty to the Node
    version lookup. Without tracing this functions use, I can't tell if this
    would be a performance regression on the critical path.

--- packages/build-utils/src/fs/node-version.ts
+++ packages/build-utils/src/fs/node-version.ts
@@ -6,6 +6,8 @@ import debug from '../debug';
 
 export type NodeVersionMajor = ReturnType<typeof getOptions>[number]['major'];
 
+// `NODE_VERSIONS` is assumed to be sorted by version number
+// with the newest supported version first
 export const NODE_VERSIONS: NodeVersion[] = [
   { major: 22, range: '22.x', runtime: 'nodejs22.x' },
   { major: 20, range: '20.x', runtime: 'nodejs20.x' },
