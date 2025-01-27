0f86a5390552d30eea00a096824b68c2e40e391a
Nathan Rajlich <n@n8.io>
2025-01-27 10:04:48 -0800

    Fix lint

--- packages/node/test/dev-fixtures/edge-echo.js
+++ packages/node/test/dev-fixtures/edge-echo.js
@@ -1,9 +1,5 @@
-/* global Response */
 export const config = { runtime: 'edge' };
-/**
- * @param {Request} req
- * @returns {Response}
- */
+
 export default async req => {
   return new Response(await req.text());
 };
