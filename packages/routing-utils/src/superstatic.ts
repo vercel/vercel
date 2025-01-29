16b38a92574695f9961c7cc00cf631fe434c26c8
Sean Massa <EndangeredMassa@gmail.com>
2025-01-29 10:16:43 -0600

    [node][remix][routing-utils] log diff between current and updated versions of `path-to-regexp` (#12926)
    Using [pnpm aliases](https://pnpm.io/aliases), this PR installs two
    different versions of `path-to-regexp`. Both are run and the resulting
    regex is compared. If they are different, an innocuous log message is
    sent to the build logs.
    
    This allows us to test out the new version and be notified of any
    breaking changes it would have for our users.
    
    ---
    
    Testing: This will be manually tested in the build container before
    release.
    
    ---------
    
    Co-authored-by: Erika Rowland <130414394+erikareads@users.noreply.github.com>

--- packages/routing-utils/src/superstatic.ts
+++ packages/routing-utils/src/superstatic.ts
@@ -3,8 +3,57 @@
  * See https://github.com/firebase/superstatic#configuration
  */
 import { parse as parseUrl, format as formatUrl } from 'url';
-import { pathToRegexp, compile, Key } from 'path-to-regexp';
+import {
+  pathToRegexp as pathToRegexpCurrent,
+  compile,
+  Key,
+} from 'path-to-regexp';
+import { pathToRegexp as pathToRegexpUpdated } from 'path-to-regexp-updated';
 import { Route, Redirect, Rewrite, HasField, Header } from './types';
+
+// run the updated version of path-to-regexp, compare the results, and log if different
+function pathToRegexp(
+  callerId: string,
+  path: string,
+  keys?: Key[],
+  options?: { strict: boolean; sensitive: boolean; delimiter: string }
+) {
+  const currentRegExp = pathToRegexpCurrent(path, keys, options);
+
+  try {
+    const currentKeys = keys;
+    const newKeys: Key[] = [];
+    const newRegExp = pathToRegexpUpdated(path, newKeys, options);
+
+    // FORCE_PATH_TO_REGEXP_LOG can be used to force these logs to render
+    // for verification that they show up in the build logs as expected
+
+    const isDiffRegExp = currentRegExp.toString() !== newRegExp.toString();
+    if (process.env.FORCE_PATH_TO_REGEXP_LOG || isDiffRegExp) {
+      const message = JSON.stringify({
+        path,
+        currentRegExp: currentRegExp.toString(),
+        newRegExp: newRegExp.toString(),
+      });
+      console.error(`[vc] PATH TO REGEXP PATH DIFF @ #${callerId}: ${message}`);
+    }
+
+    const isDiffKeys = keys?.toString() !== newKeys?.toString();
+    if (process.env.FORCE_PATH_TO_REGEXP_LOG || isDiffKeys) {
+      const message = JSON.stringify({
+        isDiffKeys,
+        currentKeys,
+        newKeys,
+      });
+      console.error(`[vc] PATH TO REGEXP KEYS DIFF @ #${callerId}: ${message}`);
+    }
+  } catch (err) {
+    const error = err as Error;
+    console.error(`[vc] PATH TO REGEXP ERROR @ #${callerId}: ${error.message}`);
+  }
+
+  return currentRegExp;
+}
 
 const UN_NAMED_SEGMENT = '__UN_NAMED_SEGMENT__';
 
@@ -196,7 +245,7 @@ export function sourceToRegex(source: string): {
   segments: string[];
 } {
   const keys: Key[] = [];
-  const r = pathToRegexp(source, keys, {
+  const r = pathToRegexp('632', source, keys, {
     strict: true,
     sensitive: true,
     delimiter: '/',
@@ -299,9 +348,9 @@ function replaceSegments(
   const hostnameKeys: Key[] = [];
 
   try {
-    pathToRegexp(pathname, pathnameKeys);
-    pathToRegexp(hash || '', hashKeys);
-    pathToRegexp(hostname || '', hostnameKeys);
+    pathToRegexp('528', pathname, pathnameKeys);
+    pathToRegexp('834', hash || '', hashKeys);
+    pathToRegexp('712', hostname || '', hostnameKeys);
   } catch (_) {
     // this is not fatal so don't error when failing to parse the
     // params from the destination
