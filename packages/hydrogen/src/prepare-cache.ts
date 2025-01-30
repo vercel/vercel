9b019a35aae1b7b1366b7d188336748778c48d24
Austin Merrick <austinmerrick@onsclom.net>
2025-01-30 13:16:45 -0800

    Fix glob import

--- packages/hydrogen/src/prepare-cache.ts
+++ packages/hydrogen/src/prepare-cache.ts
@@ -1,6 +1,5 @@
-import { glob } from '@vercel/build-utils';
 import type { PrepareCache } from '@vercel/build-utils';
-import { defaultCachePathGlob } from '@vercel/build-utils';
+import { glob, defaultCachePathGlob } from '@vercel/build-utils';
 
 export const prepareCache: PrepareCache = ({ repoRootPath, workPath }) => {
   return glob(defaultCachePathGlob, repoRootPath || workPath);
