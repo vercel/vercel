7181dc29c89a42a93fc4799d7af2554154c7d734
Austin Merrick <austinmerrick@onsclom.net>
2025-01-30 13:17:43 -0800

    Another glob

--- packages/node/src/prepare-cache.ts
+++ packages/node/src/prepare-cache.ts
@@ -1,4 +1,8 @@
-import { defaultCachePathGlob, type PrepareCache } from '@vercel/build-utils';
+import {
+  defaultCachePathGlob,
+  glob,
+  type PrepareCache,
+} from '@vercel/build-utils';
 
 export const prepareCache: PrepareCache = ({ repoRootPath, workPath }) => {
   return glob(defaultCachePathGlob, repoRootPath || workPath);
