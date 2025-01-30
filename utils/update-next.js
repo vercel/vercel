180a750d8270de7c6ff2d4ec5b900643db209d10
Sean Massa <endangeredmassa@gmail.com>
2025-01-30 17:15:16 -0600

    pin nextjs versions

--- utils/update-next.js
+++ utils/update-next.js
@@ -18,125 +18,6 @@ function exec(cmd, args, opts) {
   return output;
 }
 
-module.exports = async ({ github, context } = {}) => {
-  process.env.COREPACK_ENABLE_STRICT = '0';
-  const newVersion = exec('npm', ['view', 'next', 'dist-tags.latest']);
-  const branch = `next-${newVersion.replaceAll('.', '-')}`;
-
-  if (
-    github &&
-    exec('git', ['ls-remote', '--heads', 'origin', branch]).toString().trim()
-  ) {
-    console.log(`Branch ${branch} already exists, skipping update.`);
-    return;
-  }
-
-  let updatedCount = 0;
-
-  // update `examples/nextjs`
-  const oldVersion = require('../examples/nextjs/package.json').dependencies
-    .next;
-  if (github && oldVersion !== newVersion) {
-    updatedCount++;
-    const changeset = join(__dirname, '..', '.changeset', `${branch}.md`);
-    writeFileSync(changeset, `---\n---\n\n`, 'utf-8');
-    exec('rm', ['-rf', './examples/nextjs']);
-    exec('npx', ['--yes', 'create-next-app@latest', './examples/nextjs']);
-    exec('git', [
-      'config',
-      '--global',
-      'user.email',
-      'infra+release@vercel.com',
-    ]);
-    exec('git', ['config', '--global', 'user.name', 'vercel-release-bot']);
-    exec('git', ['checkout', 'main']);
-    exec('git', ['checkout', '-b', branch]);
-    exec('git', ['add', '-A']);
-    exec('git', ['commit', '-m', branch]);
-    exec('git', ['push', 'origin', branch]);
-  }
-
-  // update tests
-  const testPaths = [
-    'packages/cli/test/dev/fixtures',
-    'packages/cli/test/fixtures/unit',
-    'packages/fs-detectors/test/fixtures',
-    'packages/next/test/fixtures',
-    'packages/next/test/integration',
-    'packages/static-build/test/fixtures',
-  ];
-
-  while (testPaths.length) {
-    const dir = testPaths.pop();
-
-    const pkgJsonFile = join(dir, 'package.json');
-    if (existsSync(pkgJsonFile)) {
-      if (updatePackageJson(pkgJsonFile, newVersion)) {
-        updatedCount++;
-      }
-      continue;
-    }
-
-    for (const name of readdirSync(dir)) {
-      const file = join(dir, name);
-      if (statSync(file).isDirectory() && name !== 'node_modules') {
-        testPaths.push(file);
-      }
-    }
-  }
-
-  if (!updatedCount) {
-    console.log(
-      `Next.js version ${newVersion} did not change, skipping update.`
-    );
-    return;
-  }
-
-  console.log(
-    `Updated ${updatedCount} package${
-      updatedCount === 1 ? '' : 's'
-    } to Next.js version ${newVersion}`
-  );
-
-  if (!github || !context) {
-    console.error('Error: missing github or context');
-    return;
-  }
-
-  const { repo, owner } = context.repo;
-
-  const pr = await github.rest.pulls.create({
-    owner,
-    repo,
-    head: branch,
-    base: 'main',
-    title: `[examples][tests] Upgrade Next.js to version ${newVersion}`,
-    body: `This auto-generated PR updates ${updatedCount} package${
-      updatedCount === 1 ? '' : 's'
-    } to Next.js version ${newVersion}`,
-  });
-
-  try {
-    await github.rest.pulls.requestReviewers({
-      owner,
-      repo,
-      pull_number: pr.data.number,
-      reviewers: ['ijjk', 'styfle', 'huozhi'],
-    });
-  } catch (err) {
-    console.log(
-      `Skipping requesting reviews due to permission issues: ${err.message}`
-    );
-  }
-
-  await github.rest.issues.addLabels({
-    owner,
-    repo,
-    issue_number: pr.data.number,
-    labels: ['area: examples', 'area: tests', 'semver: none'],
-  });
-};
-
 function updatePackageJson(file, newVersion) {
   const pkgJson = JSON.parse(readFileSync(file, 'utf-8'));
 
@@ -183,3 +64,157 @@ function updatePackageJson(file, newVersion) {
 
   return false;
 }
+
+async function createPullRequest(
+  github,
+  owner,
+  repo,
+  branch,
+  newVersion,
+  updatedCount
+) {
+  console.log(
+    `mocked out creating PR: ${{ branch, newVersion, updatedCount }}`
+  );
+
+  const pr = await github.rest.pulls.create({
+    owner,
+    repo,
+    head: branch,
+    base: 'main',
+    title: `[examples][tests] Upgrade Next.js to version ${newVersion}`,
+    body: `This auto-generated PR updates ${updatedCount} package${updatedCount === 1 ? '' : 's'} to Next.js version ${newVersion}`,
+  });
+
+  try {
+    await github.rest.pulls.requestReviewers({
+      owner,
+      repo,
+      pull_number: pr.data.number,
+      reviewers: ['ijjk', 'styfle', 'huozhi'],
+    });
+  } catch (err) {
+    console.log(
+      `Skipping requesting reviews due to permission issues: ${err.message}`
+    );
+  }
+
+  await github.rest.issues.addLabels({
+    owner,
+    repo,
+    issue_number: pr.data.number,
+    labels: ['area: examples', 'area: tests', 'semver: none'],
+  });
+}
+
+function updateExamples(github, newVersion, branch) {
+  let updatedCount = 0;
+
+  const oldVersion = require('../examples/nextjs/package.json').dependencies
+    .next;
+  if (github && oldVersion !== newVersion) {
+    updatedCount++;
+    const changeset = join(__dirname, '..', '.changeset', `${branch}.md`);
+    writeFileSync(changeset, `---\n---\n\n`, 'utf-8');
+    exec('rm', ['-rf', './examples/nextjs']);
+    exec('npx', ['--yes', 'create-next-app@latest', './examples/nextjs']);
+    exec('git', [
+      'config',
+      '--global',
+      'user.email',
+      'infra+release@vercel.com',
+    ]);
+    exec('git', ['config', '--global', 'user.name', 'vercel-release-bot']);
+    exec('git', ['checkout', 'main']);
+    exec('git', ['checkout', '-b', branch]);
+    exec('git', ['add', '-A']);
+    exec('git', ['commit', '-m', branch]);
+    exec('git', ['push', 'origin', branch]);
+  }
+
+  return updatedCount;
+}
+
+function getTestPaths(tag) {
+  if (tag === 'canary') {
+    return ['packages/next/test/fixtures', 'packages/next/test/integration'];
+  }
+
+  return [
+    'packages/cli/test/dev/fixtures',
+    'packages/cli/test/fixtures/unit',
+    'packages/fs-detectors/test/fixtures',
+    'packages/static-build/test/fixtures',
+  ];
+}
+
+module.exports = async ({ github, context, tag } = {}) => {
+  process.env.COREPACK_ENABLE_STRICT = '0';
+
+  const newVersion = exec('npm', ['view', 'next', `dist-tags.${tag}`]);
+  const branch = `next-${newVersion.replaceAll('.', '-')}`;
+
+  if (
+    github &&
+    exec('git', ['ls-remote', '--heads', 'origin', branch]).toString().trim()
+  ) {
+    console.log(`Branch ${branch} already exists, skipping update.`);
+    return;
+  }
+
+  let updatedCount = 0;
+
+  if (tag === 'latest') {
+    updatedCount += updateExamples(github, newVersion, branch);
+  }
+
+  // update tests
+  const testPaths = getTestPaths(tag);
+
+  while (testPaths.length) {
+    const dir = testPaths.pop();
+
+    const pkgJsonFile = join(dir, 'package.json');
+    if (existsSync(pkgJsonFile)) {
+      if (updatePackageJson(pkgJsonFile, newVersion)) {
+        updatedCount++;
+      }
+      continue;
+    }
+
+    for (const name of readdirSync(dir)) {
+      const file = join(dir, name);
+      if (statSync(file).isDirectory() && name !== 'node_modules') {
+        testPaths.push(file);
+      }
+    }
+  }
+
+  if (!updatedCount) {
+    console.log(
+      `Next.js version ${newVersion} did not change, skipping update.`
+    );
+    return;
+  }
+
+  console.log(
+    `Updated ${updatedCount} package${
+      updatedCount === 1 ? '' : 's'
+    } to Next.js version ${newVersion}`
+  );
+
+  if (!github || !context) {
+    console.error('Error: missing github or context');
+    return;
+  }
+
+  const { repo, owner } = context.repo;
+  await createPullRequest(
+    github,
+    owner,
+    repo,
+    branch,
+    newVersion,
+    updatedCount
+  );
+};
