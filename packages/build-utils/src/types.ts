5c404c56702ff8685628ffe0db8a90e8cb87568a
Luke PS <LukeSheard@users.noreply.github.com>
2025-02-06 10:23:19 +0000

    Add build-utils tracer (#13013)
    # Overview 
    
    Extract build utils tracer from the builder framework PR so we can
    incrementally shift.

--- packages/build-utils/src/types.ts
+++ packages/build-utils/src/types.ts
@@ -4,6 +4,7 @@ import type FileBlob from './file-blob';
 import type { Lambda } from './lambda';
 import type { Prerender } from './prerender';
 import type { EdgeFunction } from './edge-function';
+import type { Span } from './trace';
 
 export interface Env {
   [name: string]: string | undefined;
@@ -115,6 +116,11 @@ export interface BuildOptions {
    * fully processed
    */
   buildCallback?: (opts: Omit<BuildOptions, 'buildCallback'>) => Promise<void>;
+
+  /**
+   * The current trace state from the internal vc tracing
+   */
+  span?: Span;
 }
 
 export interface PrepareCacheOptions {
