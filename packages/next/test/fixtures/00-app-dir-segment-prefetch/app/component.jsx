e25c466c24e380a684dfca1288034ba87db62bd6
Wyatt Johnson <accounts+github@wyattjoh.ca>
2025-02-07 12:43:42 -0700

    tests: added test

---
+++ packages/next/test/fixtures/00-app-dir-segment-prefetch/app/component.jsx
@@ -0,0 +1,39 @@
+import { Suspense } from "react";
+import { connection } from "next/server";
+
+async function DynamicComponent() {
+  await connection();
+  return <div>Dynamic Component</div>;
+}
+
+const links = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/blog/hello-world",
+    label: "Hello World",
+  },
+  {
+    href: "/blog/hello-not-prerendered",
+    label: "Hello Not Prerendered",
+  },
+];
+
+export function Component() {
+  return (
+    <main>
+      <ul>
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+      <Suspense>
+        <DynamicComponent />
+      </Suspense>
+    </main>
+  );
+}
