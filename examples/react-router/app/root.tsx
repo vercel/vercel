5fd263ea583b1cee61572bd496b244ba8296f92f
Nathan Rajlich <n@n8.io>
2025-02-10 13:45:14 -0800

    [examples] Update "react-router" template to latest
    Mainly updates the template to be pre-configured with Tailwind v4.

--- examples/react-router/app/root.tsx
+++ examples/react-router/app/root.tsx
@@ -8,7 +8,7 @@ import {
 } from "react-router";
 
 import type { Route } from "./+types/root";
-import stylesheet from "./app.css?url";
+import "./app.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -21,7 +21,6 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  { rel: "stylesheet", href: stylesheet },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
