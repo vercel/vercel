d6751ddb61f9941440c7e371e5fa054b0860c01b
Nathan Rajlich <n@n8.io>
2025-02-10 14:08:36 -0800

    [examples] Update "react-router" template to latest (#13041)
    Mainly updates the template to be pre-configured with Tailwind v4.

--- examples/react-router/vite.config.ts
+++ examples/react-router/vite.config.ts
@@ -1,14 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
