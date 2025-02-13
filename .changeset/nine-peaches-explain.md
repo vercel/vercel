e4972fa9adbecd19687aff71ec22b46ce0f3a4fb
Sean Massa <EndangeredMassa@gmail.com>
2025-02-13 11:16:16 -0600

    [build-utils] support pnpm@10 (#12852)
    Updates `build-utils` to use `pnpm@10` for v9 `pnpm-lock.yaml` lockfiles
    instead of `pnpm@9`. v7 pnpm lock files should still use `pnpm@9`.
    
    ---------
    
    Co-authored-by: Austin Merrick <onsclom@onsclom.net>
    Co-authored-by: Austin Merrick <austinmerrick@onsclom.net>

---
+++ .changeset/nine-peaches-explain.md
@@ -0,0 +1,5 @@
+---
+'@vercel/build-utils': major
+---
+
+Detect v9 pnpm lockfiles as pnpm 10 generated
--- packages/build-utils/src/fs/run-user-scripts.ts
+++ packages/build-utils/src/fs/run-user-scripts.ts
@@ -832,6 +832,7 @@ type DetectedPnpmVersion =
   | 'pnpm 7'
   | 'pnpm 8'
   | 'pnpm 9'
+  | 'pnpm 10'
   | 'corepack_enabled';
 
 function detectPnpmVersion(
@@ -846,8 +847,10 @@ function detectPnpmVersion(
       return 'pnpm 7';
     case lockfileVersion === 6.0 || lockfileVersion === 6.1:
       return 'pnpm 8';
-    case lockfileVersion === 7.0 || lockfileVersion === 9.0:
+    case lockfileVersion === 7.0:
       return 'pnpm 9';
+    case lockfileVersion === 9.0:
+      return 'pnpm 10';
     default:
       return 'not found';
   }
@@ -866,6 +869,8 @@ function validLockfileForPackageManager(
       return true;
     case 'pnpm':
       switch (packageManagerMajorVersion) {
+        case 10:
+          return lockfileVersion === 9.0;
         case 9:
           // bug in pnpm 9.0.0 causes incompatibility with lockfile version 6.0
           if (
@@ -1083,6 +1088,14 @@ export function detectPackageManager(
             detectedLockfile: 'pnpm-lock.yaml',
             detectedPackageManager: 'pnpm@9.x',
             pnpmVersionRange: '9.x',
+          };
+        case 'pnpm 10':
+          // pnpm 10
+          return {
+            path: '/pnpm10/node_modules/.bin',
+            detectedLockfile: 'pnpm-lock.yaml',
+            detectedPackageManager: 'pnpm@10.x',
+            pnpmVersionRange: '10.x',
           };
         case 'pnpm 6':
           return {
