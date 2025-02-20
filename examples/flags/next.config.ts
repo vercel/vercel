f74fc327fe964abbcf7fbdbd281a0ef96368a96b
Aaron Morris <aaron@vercel.com>
2025-02-20 12:35:36 -0500

    Revert "Add Flags SDK example"
    This reverts commit 3e4fd6ae72ceea89bf782f11b9b2461311f95698.

--- examples/flags/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-import withVercelToolbar from "@vercel/toolbar/plugins/next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default withVercelToolbar()(nextConfig);
