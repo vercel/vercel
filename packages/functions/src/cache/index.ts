f120d15e6617e608630a43bbd2ee7024c64bc2eb
Kelly Davis <kldavis4@gmail.com>
2025-04-04 13:16:47 -0400

    documentation updates

--- packages/functions/src/cache/index.ts
+++ packages/functions/src/cache/index.ts
@@ -2,7 +2,7 @@ import { getContext } from '../get-context';
 import { CacheOptions, RuntimeCache } from './types';
 import { InMemoryCache } from './in-memory-cache';
 
-export const defaultKeyHashFunction = (key: string) => {
+const defaultKeyHashFunction = (key: string) => {
   let hash = 5381;
   for (let i = 0; i < key.length; i++) {
     hash = (hash * 33) ^ key.charCodeAt(i);
@@ -12,6 +12,20 @@ export const defaultKeyHashFunction = (key: string) => {
 
 const defaultNamespaceSeparator = '$';
 
+/**
+ * Retrieves the Vercel Runtime Cache.
+ *
+ * Keys are hashed to ensure they are unique and consistent. The hashing function can be overridden by providing a custom
+ * `keyHashFunction` in the `cacheOptions` parameter.
+ *
+ * To specify a namespace for the cache keys, you can pass a `namespace` option in the `cacheOptions` parameter. If
+ * a namespace is provided, the cache keys will be prefixed with the namespace followed by a separator (default is `$`). The
+ * namespaceSeparator can also be customized using the `namespaceSeparator` option.
+ *
+ * @param cacheOptions - Optional configuration for the cache.
+ * @returns A promise that resolves to an instance of the Vercel Runtime Cache.
+ * @throws {Error} If no cache is available in the context and `InMemoryCache` cannot be created.
+ */
 export const getRuntimeCache = (cacheOptions?: CacheOptions) => {
   let runtimeCache: RuntimeCache;
   if (getContext().cache) {
@@ -48,5 +62,5 @@ export const getRuntimeCache = (cacheOptions?: CacheOptions) => {
     revalidateTag: (tag: string | string[]) => {
       return runtimeCache.revalidateTag(tag);
     },
-  });
+  } as RuntimeCache);
 };
