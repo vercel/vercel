a640944c6e2c69afab5b7f03080d8363ef4bcf5b
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2025-04-08 06:30:47 -0700

    [next] ensure app router 404 page is still included in functions when using pages i18n (#13222)
    When using pages router, we bundle the localized static 404 file with
    every function output, to ensure that if not found behavior is triggered
    from a function, that it's available.
    
    However, Next.js prioritizes serving the app router 404 page over the
    pages router 404 page. As a result, when triggering a not found error
    from an app router function, it'd trigger a 500 and crash the function
    because it only knows about `en/404.html` rather than `404.html` (as an
    example).
    
    This ensures that we bundle the app router 404 page rather than the
    localized pages router 404 page if i18n + appDir is used. Separately in
    Next.js we need to ensure the `pages-manifest` is pointing localized
    paths to the correct not found file as well. This is accomplished in
    https://github.com/vercel/next.js/pull/77905
    
    Without that change, this just solves the issue of throwing a 500 rather
    than resolving a 404 page, but doesn't fix the app router/pages router
    specificity issue.
    
    ### Test plan
    - Land this change
    - Land the fix in Next.js
    - Update the test here
    
    In the meantime, I manually validated it here:
    https://vtest314-e2e-tests-bkc89ab40-ztanner.vercel.app/app-dir/foo

---
+++ .changeset/healthy-geese-train.md
@@ -0,0 +1,5 @@
+---
+'@vercel/next': patch
+---
+
+[next] ensure app router 404 page is still included in functions when using pages i18n
--- packages/next/src/server-build.ts
+++ packages/next/src/server-build.ts
@@ -499,6 +499,7 @@ export async function serverBuild({
   const lstatSema = new Sema(25);
   const lstatResults: { [key: string]: ReturnType<typeof lstat> } = {};
   const nonLambdaSsgPages = new Set<string>();
+  const static404Pages = new Set<string>(static404Page ? [static404Page] : []);
 
   Object.keys(prerenderManifest.staticRoutes).forEach(route => {
     const result = onPrerenderRouteInitial(
@@ -513,6 +514,8 @@ export async function serverBuild({
     );
 
     if (result && result.static404Page) {
+      // there can be multiple 404 pages (eg i18n) so we want to keep track of all of them
+      static404Pages.add(result.static404Page);
       static404Page = result.static404Page;
     }
 
@@ -725,36 +728,40 @@ export async function serverBuild({
       fsPath: nextServerFile,
     });
 
-    if (static404Page) {
-      // ensure static 404 page file is included in all lambdas
-      // for notFound GS(S)P support
-
+    if (static404Pages.size > 0) {
+      // If we've generated a static 404 page, it's possible that we also
+      // have a static 404 page for each locale.
       if (i18n) {
         for (const locale of i18n.locales) {
-          let static404File =
-            staticPages[path.posix.join(entryDirectory, locale, '/404')];
+          const static404Page = path.posix.join(entryDirectory, locale, '404');
+          static404Pages.add(static404Page);
+        }
+      }
 
-          if (!static404File) {
+      for (const static404Page of static404Pages) {
+        let static404File = staticPages[static404Page];
+
+        if (!static404File) {
+          // if we have a file ref already, we can use it. Otherwise, we need
+          // to create a new one, but we need to ensure it exists on disk
+          const static404FilePath = path.join(
+            pagesDir,
+            `${static404Page}.html`
+          );
+
+          if (fs.existsSync(static404FilePath)) {
             static404File = new FileFsRef({
-              fsPath: path.join(pagesDir, locale, '/404.html'),
+              fsPath: static404FilePath,
             });
-            if (!fs.existsSync(static404File.fsPath)) {
-              static404File = new FileFsRef({
-                fsPath: path.join(pagesDir, '/404.html'),
-              });
-            }
           }
+        }
+
+        // ensure each static 404 page file is included in all lambdas
+        // for notFound GS(S)P support
+        if (static404File) {
           requiredFiles[path.relative(baseDir, static404File.fsPath)] =
             static404File;
         }
-      } else {
-        const static404File =
-          staticPages[static404Page] ||
-          new FileFsRef({
-            fsPath: path.join(pagesDir, '/404.html'),
-          });
-        requiredFiles[path.relative(baseDir, static404File.fsPath)] =
-          static404File;
       }
     }
 
