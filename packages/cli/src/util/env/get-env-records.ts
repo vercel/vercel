1a6a49fd2f6e4a016f24b2b2cb9a6b19c486eee5
Nikolas Schröter <25958801+nwidynski@users.noreply.github.com>
2025-04-17 19:29:17 +0200

    fix(cli): ignore `--git-branch` in non-preview environments

--- packages/cli/src/util/env/get-env-records.ts
+++ packages/cli/src/util/env/get-env-records.ts
@@ -77,7 +77,7 @@ export async function pullEnvRecords(
 
   if (target) {
     url += `/${encodeURIComponent(target)}`;
-    if (gitBranch) {
+    if (target === 'preview' && gitBranch) {
       url += `/${encodeURIComponent(gitBranch)}`;
     }
   }
