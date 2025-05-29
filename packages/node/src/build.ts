6c8e763ab63c79e12c7d5455fd79cf158f43cc77
Gal Schlezinger <gal@spitfire.co.il>
2025-05-29 13:29:50 +0200

    [cli][node] support `config.regions` and `config.preferredRegion` in functions (#13386)
    **[cli][node] support `config.regions` and `config.preferredRegion` in
    functions**

--- packages/node/src/build.ts
+++ packages/node/src/build.ts
@@ -37,7 +37,7 @@ import type {
   NodeVersion,
   BuildResultV3,
 } from '@vercel/build-utils';
-import { getConfig } from '@vercel/static-config';
+import { getConfig, type BaseFunctionConfig } from '@vercel/static-config';
 
 import { Register, register } from './typescript';
 import {
@@ -485,8 +485,27 @@ export const build: BuildV3 = async ({
       awsLambdaHandler,
       supportsResponseStreaming,
       maxDuration: staticConfig?.maxDuration,
+      regions: normalizeRequestedRegions(
+        staticConfig?.preferredRegion ?? staticConfig?.regions
+      ),
     });
   }
 
   return { routes, output };
 };
+
+function normalizeRequestedRegions(
+  regions: BaseFunctionConfig['regions'] | BaseFunctionConfig['preferredRegion']
+): NodejsLambda['regions'] {
+  if (regions === 'all') {
+    return ['all'];
+  } else if (regions === 'auto' || regions === 'default') {
+    return undefined;
+  }
+
+  if (typeof regions === 'string') {
+    return [regions];
+  }
+
+  return regions;
+}
