d8b7c36c6f22c2ad21580ee625db0fbeecf4d407
Brooke Mosby <brookemosby@icloud.com>
2025-06-24 10:04:02 -0600

    [rr] fix timeout (#13482)
    We see timeouts on cli deploys because we are not correctly handling
    deployment ready + rolling release enabled
    
    ---------
    
    Co-authored-by: Trek Glowacki <trek.glowacki@vercel.com>

--- packages/cli/test/unit/commands/deploy/index.test.ts
+++ packages/cli/test/unit/commands/deploy/index.test.ts
@@ -687,6 +687,15 @@ describe('deploy', () => {
       });
       deployment = useDeployment({ creator: user, state: 'BUILDING' });
       deployment.aliasAssigned = false;
+
+      client.scenario.get(`/v9/projects/:id`, (_req, res) => {
+        res.json({
+          ...defaultProject,
+          name: 'node',
+          id: 'QmbKpqpiUqbcke',
+        });
+      });
+
       client.scenario.post(`/v13/deployments`, (req, res) => {
         res.json(
           res.json({
@@ -1243,6 +1252,13 @@ describe('deploy', () => {
         const user = useUser();
         client.scenario.get(`/v9/projects/:id`, (_req, res) => {
           return res.status(404).json({});
+        });
+
+        client.scenario.get(`/v9/projects`, (_req, res) => {
+          return res.json({
+            projects: [],
+            pagination: {},
+          });
         });
 
         client.scenario.post(`/v1/projects`, (req, res) => {
