201b78d2ecbff0c47358d01fd1c9e682d6ca0afe
Alice Hawthorne <alice.hawthorne@vercel.com>
2025-07-11 12:17:14 -0400

    remove unnecessary code from oidc, update docs

--- packages/oidc/src/get-context.ts
+++ packages/oidc/src/get-context.ts
@@ -1,8 +1,4 @@
-import { RuntimeCache } from './cache/types';
-
 type Context = {
-  waitUntil?: (promise: Promise<unknown>) => void;
-  cache?: RuntimeCache;
   headers?: Record<string, string>;
 };
 
