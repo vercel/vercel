4b041107e81a3c0b7da1e6d85ad5de7653127b0b
Kit Foster <katya.a.foster@gmail.com>
2025-07-16 21:43:38 +0200

    Add support to create a project with Vercel Auth disabled (#13528)
    The default for Vercel Auth is Standard Protection. Add support to
    create a project with Vercel Auth disabled.
    
    ---------
    
    Co-authored-by: Chris Olszewski <chris.olszewski@vercel.com>
    Co-authored-by: Chris Olszewski <chrisdolszewski@gmail.com>

--- packages/cli/src/util/projects/create-project.ts
+++ packages/cli/src/util/projects/create-project.ts
@@ -3,11 +3,19 @@ import type { Project, ProjectSettings } from '@vercel-internals/types';
 
 export default async function createProject(
   client: Client,
-  settings: ProjectSettings & { name: string }
+  settings: ProjectSettings & { name: string; vercelAuth?: 'none' | 'standard' }
 ) {
+  const { vercelAuth, ...rest } = settings;
   const project = await client.fetch<Project>('/v1/projects', {
     method: 'POST',
-    body: { ...settings },
+    body: {
+      ...rest,
+      /**
+       * If `null`, vercel auth is disabled. Otherwise standard protection is enabled.
+       * vercelAuth used to be called ssoProtection.
+       */
+      ...(vercelAuth === 'none' ? { ssoProtection: null } : undefined),
+    },
   });
   return project;
 }
