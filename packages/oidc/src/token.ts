043d25476d404b2270aeb82ed2090e7c31052380
Alice Hawthorne <alice.hawthorne@vercel.com>
2025-07-30 14:41:17 -0400

    remove async dynamic imports

--- packages/oidc/src/token.ts
+++ packages/oidc/src/token.ts
@@ -1,28 +1,16 @@
+import ms from 'ms';
+import os from 'os';
+import path from 'path';
+import fs from 'fs';
+
 interface TokenPayload {
   sub: string;
   name: string;
   exp: number;
 }
 
-function isNodeEnv(): boolean {
-  return (
-    typeof process !== 'undefined' &&
-    process.versions != null &&
-    process.versions.node != null
-  );
-}
-
-function isBrowserEnv(): boolean {
-  return typeof window !== 'undefined';
-}
-
-async function getVercelDataDir(): Promise<string | null> {
-  if (isBrowserEnv()) {
-    return '';
-  }
+function getVercelDataDir(): string | null {
   const vercelFolder = 'com.vercel.cli';
-  const os = await import('os');
-  const path = await import('path');
   if (process.env.XDG_DATA_HOME) {
     return path.join(process.env.XDG_DATA_HOME, vercelFolder);
   }
@@ -38,13 +26,11 @@ async function getVercelDataDir(): Promise<string | null> {
   }
 }
 
-async function getVercelCliToken(): Promise<string | null> {
-  const dataDir = await getVercelDataDir();
+function getVercelCliToken(): string | null {
+  const dataDir = getVercelDataDir();
   if (!dataDir) {
     return null;
   }
-  const path = await import('path');
-  const fs = await import('fs');
   const tokenPath = path.join(dataDir, 'auth.json');
   if (!fs.existsSync(tokenPath)) {
     return null;
@@ -62,7 +48,7 @@ async function getVercelOidcToken(
   projectId: string
 ): Promise<VercelTokenResponse | null> {
   try {
-    const url = `https://api.vercel.com/projects/token?projectId=${projectId}&source=vercel-oidc-refresh`;
+    const url = `https://api.vercel.com/v1/projects/${projectId}/token?source=vercel-oidc-refresh`;
     const res = await fetch(url, {
       method: 'POST',
       headers: {
@@ -83,14 +69,12 @@ async function getVercelOidcToken(
   }
 }
 
-async function findProjectId(): Promise<string> {
-  const dir = await findRootDir();
+function findProjectId(): string {
+  const dir = findRootDir();
   if (!dir) {
     throw new Error('unable to find root directory');
   }
   try {
-    const fs = await import('fs');
-    const path = await import('path');
     const pkgPath = path.join(dir, '.vercel', 'project.json');
     if (!fs.existsSync(pkgPath)) {
       throw new Error('project.json not found');
@@ -103,10 +87,8 @@ async function findProjectId(): Promise<string> {
   }
 }
 
-async function findRootDir(): Promise<string> {
+function findRootDir(): string {
   try {
-    const fs = await import('fs');
-    const path = await import('path');
     let dir = process.cwd();
     while (dir !== path.dirname(dir)) {
       const pkgPath = path.join(dir, '.vercel');
@@ -121,21 +103,11 @@ async function findRootDir(): Promise<string> {
   throw new Error('unable to find root directory');
 }
 
-async function saveToken(
-  token: VercelTokenResponse,
-  projectId: string
-): Promise<void> {
+function saveToken(token: VercelTokenResponse, projectId: string): void {
   try {
-    const os = await import('os');
-    const path = await import('path');
     const tmpDir = os.tmpdir();
-    const tokenPath = path.join(
-      tmpDir,
-      `vercel-oidc-token-${projectId}`,
-      'token.json'
-    );
+    const tokenPath = path.join(tmpDir, `${projectId}.json`);
     const tokenJson = JSON.stringify(token);
-    const fs = await import('fs');
     fs.mkdirSync(path.dirname(tokenPath), { recursive: true });
     fs.writeFileSync(tokenPath, tokenJson);
     return;
@@ -145,20 +117,10 @@ async function saveToken(
   }
 }
 
-async function loadToken(
-  projectId: string
-): Promise<VercelTokenResponse | null> {
+function loadToken(projectId: string): VercelTokenResponse | null {
   try {
-    const os = await import('os');
-    const path = await import('path');
-    const fs = await import('fs');
-
     const tmpDir = os.tmpdir();
-    const tokenPath = path.join(
-      tmpDir,
-      `vercel-oidc-token-${projectId}`,
-      'token.json'
-    );
+    const tokenPath = path.join(tmpDir, `${projectId}.json`);
     if (!fs.existsSync(tokenPath)) {
       return null;
     }
@@ -178,40 +140,26 @@ export function getTokenPayload(token: string): TokenPayload {
   }
 
   const base64 = tokenParts[1].replace(/-/g, '+').replace(/_/g, '/');
-
-  if (isBrowserEnv()) {
-    if (typeof Uint8Array === undefined || typeof TextDecoder === undefined) {
-      throw new Error(
-        'The browser environment is not supported. Requires Uint8Array and TextDecoder'
-      );
-    }
-
-    const binaryString = window.atob(base64);
-    const toUtf8 = Uint8Array.from(binaryString, c => c.charCodeAt(0));
-    const decoder = new TextDecoder();
-    const payload = decoder.decode(toUtf8);
-    return JSON.parse(payload);
-  }
-
-  return JSON.parse(Buffer.from(base64, 'base64').toString('utf8'));
+  const padded = base64.padEnd(
+    base64.length + ((4 - (base64.length % 4)) % 4),
+    '='
+  );
+  return JSON.parse(Buffer.from(padded, 'base64').toString('utf8'));
 }
 
 export function isExpired(token: TokenPayload): boolean {
-  return token.exp * 1000 < Date.now();
+  const timeout = ms('15m');
+  return token.exp * 1000 < Date.now() + timeout;
 }
 
 export async function refreshToken(): Promise<void> {
-  if (!isNodeEnv()) {
-    throw new Error('refresh only supported in node server environments');
-  }
-
-  let maybeToken = await loadToken(await findProjectId());
+  let maybeToken = loadToken(findProjectId());
   if (!maybeToken || isExpired(getTokenPayload(maybeToken.token))) {
-    const authToken = await getVercelCliToken();
+    const authToken = getVercelCliToken();
     if (!authToken) {
       throw new Error('failed to refresh token: login to vercel cli');
     }
-    const projectId = await findProjectId();
+    const projectId = findProjectId();
     if (!projectId) {
       throw new Error('failed to refresh token: project id not found');
     }
@@ -219,7 +167,7 @@ export async function refreshToken(): Promise<void> {
     if (!maybeToken) {
       throw new Error('failed to refresh token');
     }
-    await saveToken(maybeToken, projectId);
+    saveToken(maybeToken, projectId);
   }
   process.env.VERCEL_OIDC_TOKEN = maybeToken.token;
   return;
