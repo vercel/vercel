6666a6bb5660c97873c3c4e440d94edf23b49615
Jiachi Liu <inbox@huozhi.im>
2025-08-25 15:20:57 +0200

    probes test

--- packages/next/test/fixtures/00-static-metadata-test/index.test.js
+++ packages/next/test/fixtures/00-static-metadata-test/index.test.js
@@ -1,40 +1,9 @@
 /* eslint-env jest */
 const path = require('path');
 const { deployAndTest } = require('../../utils');
-const { createRunBuildLambda } = require('../../../../../test/lib/run-build-lambda');
-const builder = require('../../..');
-
-const runBuildLambda = createRunBuildLambda(builder);
 
 describe(`${__dirname.split(path.sep).pop()}`, () => {
   it('should deploy and pass probe checks', async () => {
     await deployAndTest(__dirname);
-  });
-
-  it('should output metadata files as static rather than prerender', async () => {
-    const { buildResult } = await runBuildLambda(__dirname);
-    
-    // Check that metadata files are output as static files
-    const staticFiles = [
-      'favicon.ico',
-      'icon.svg',
-      'twitter-image.png',
-      'foo/icon.png'
-    ];
-
-    staticFiles.forEach(file => {
-      expect(buildResult.output[file]).toBeDefined();
-      expect(buildResult.output[file].type).toBe('FileFsRef');
-    });
-
-    // Ensure these are NOT prerender files
-    staticFiles.forEach(file => {
-      expect(buildResult.output[file].type).not.toBe('Prerender');
-    });
-
-    // Source file opengrapg-image/route.js is not a valid metadata route.
-    // Verify that the dynamic opengraph-image route handler is present as a lambda
-    expect(buildResult.output['opengraph-image.png']).toBeDefined();
-    expect(buildResult.output['opengraph-image.png'].type).toBe('Lambda');
   });
 });
