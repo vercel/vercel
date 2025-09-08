ea83aec099cb114296ed30d0dd393b7436e3a53f
Steven <steven@ceriously.com>
2025-09-08 17:20:02 -0400

    [cli] add new commands `vercel cache invalidate --tag`

--- packages/cli/src/commands/cache/command.ts
+++ packages/cli/src/commands/cache/command.ts
@@ -33,12 +33,80 @@ export const purgeSubcommand = {
   ],
 } as const;
 
+export const invalidateSubcommand = {
+  name: 'invalidate',
+  aliases: [],
+  description: 'Invalidate cache by tags',
+  arguments: [],
+  options: [
+    yesOption,
+    {
+      name: 'tag',
+      description: 'Tags to invalidate (comma-separated)',
+      shorthand: null,
+      type: String,
+      argument: 'TAGS',
+      deprecated: false,
+    },
+  ],
+  examples: [
+    {
+      name: 'Invalidate cache by a single tag',
+      value: `${packageName} cache invalidate --tag foo`,
+    },
+    {
+      name: 'Invalidate cache by multiple tags',
+      value: `${packageName} cache invalidate --tag foo,bar,baz`,
+    },
+  ],
+} as const;
+
+export const dangerouslyDeleteSubcommand = {
+  name: 'dangerously-delete',
+  aliases: [],
+  description: 'Dangerously delete cache by tags',
+  arguments: [],
+  options: [
+    yesOption,
+    {
+      name: 'tag',
+      description: 'Tags to delete (comma-separated)',
+      shorthand: null,
+      type: String,
+      argument: 'TAGS',
+      deprecated: false,
+    },
+    {
+      name: 'revalidation-deadline-seconds',
+      description: 'Revalidation deadline in seconds',
+      shorthand: null,
+      type: Number,
+      argument: 'REVALIDATION-DEADLINE-SECONDS',
+      deprecated: false,
+    },
+  ],
+  examples: [
+    {
+      name: 'Dangerously delete cache by tag',
+      value: `${packageName} cache dangerously-delete --tag foo`,
+    },
+    {
+      name: 'Dangerously delete cache by tag with deadline of 1 hour',
+      value: `${packageName} cache dangerously-delete --tag foo --revalidation-deadline-seconds 3600`,
+    },
+  ],
+} as const;
+
 export const cacheCommand = {
   name: 'cache',
   aliases: [],
   description: 'Manage cache for a Project',
   arguments: [],
-  subcommands: [purgeSubcommand],
+  subcommands: [
+    purgeSubcommand,
+    invalidateSubcommand,
+    dangerouslyDeleteSubcommand,
+  ],
   options: [],
   examples: [],
 } as const;
---
+++ packages/cli/src/commands/cache/invalidate.ts
@@ -0,0 +1,92 @@
+import type Client from '../../util/client';
+import { parseArguments } from '../../util/get-args';
+import { printError } from '../../util/error';
+import { invalidateSubcommand } from './command';
+import { getFlagsSpecification } from '../../util/get-flags-specification';
+import output from '../../output-manager';
+import { getCommandName } from '../../util/pkg-name';
+import { getLinkedProject } from '../../util/projects/link';
+import { emoji, prependEmoji } from '../../util/emoji';
+import { CacheInvalidateTelemetryClient } from '../../util/telemetry/commands/cache/invalidate';
+
+export default async function invalidate(
+  client: Client,
+  argv: string[]
+): Promise<number> {
+  const telemetry = new CacheInvalidateTelemetryClient({
+    opts: {
+      store: client.telemetryEventStore,
+    },
+  });
+
+  let parsedArgs;
+  const flagsSpecification = getFlagsSpecification(
+    invalidateSubcommand.options
+  );
+  try {
+    parsedArgs = parseArguments(argv, flagsSpecification);
+  } catch (err) {
+    printError(err);
+    return 1;
+  }
+
+  const link = await getLinkedProject(client);
+
+  if (link.status === 'not_linked') {
+    output.error(
+      'No project linked. Run `vercel link` to link a project to this directory.'
+    );
+    return 1;
+  }
+
+  if (link.status === 'error') {
+    return link.exitCode;
+  }
+
+  const { project, org } = link;
+  client.config.currentTeam = org.type === 'team' ? org.id : undefined;
+  const yes = Boolean(parsedArgs.flags['--yes']);
+  telemetry.trackCliFlagYes(yes);
+
+  const tag = parsedArgs.flags['--tag'];
+  telemetry.trackCliOptionTag(tag);
+  if (!tag) {
+    output.error(`Invalid cache tag "${tag}".}`);
+    return 1;
+  }
+
+  const msg = `You are about to invalidate tag ${tag} for project ${project.name}`;
+  const query = new URLSearchParams({ projectIdOrName: project.id }).toString();
+
+  if (!yes) {
+    if (!process.stdin.isTTY) {
+      output.print(
+        `${msg}. To continue, run ${getCommandName(`cache invalidate --tag ${tag} --yes`)}.`
+      );
+      return 1;
+    }
+    const confirmed = await client.input.confirm(`${msg}. Continue?`, true);
+    if (!confirmed) {
+      output.print(`Canceled.\n`);
+      return 0;
+    }
+  }
+
+  await client.fetch(`/v1/edge-cache/invalidate-by-tags?${query}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      tags: tag
+        .split(',')
+        .map(str => str.trim())
+        .filter(Boolean),
+    }),
+  });
+
+  output.print(
+    prependEmoji(`Successfully invalidated tag ${tag}`, emoji('success')) + `\n`
+  );
+  return 0;
+}
