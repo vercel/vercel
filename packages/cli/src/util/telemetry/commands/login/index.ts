bb1b9b5b3fb66cd47406dd7e6d1ae09d3726baee
Balázs Orbán <info@balazsorban.com>
2025-09-09 10:36:54 +0200

    chore(cli): telemetry on login state (#13886)
    This re-lands #13855 with a different approach. We had to revert
    (#13872) the original approach as it caused the login process to hang
    after a successful login for some reason, which I suspect was due to
    some changes in the interruption handling.
    
    This PR passes the `telemetry` client to the `future` function instead.
    Note that there is a refactor coming in #13802 and this approach plays
    nicer with that anyway.

--- packages/cli/src/util/telemetry/commands/login/index.ts
+++ packages/cli/src/util/telemetry/commands/login/index.ts
@@ -1,3 +1,14 @@
 import { TelemetryClient } from '../..';
 
-export class LoginTelemetryClient extends TelemetryClient {}
+export class LoginTelemetryClient extends TelemetryClient {
+  /**
+   * Tracks the state of the login process.
+   * - `started` when the user initiates the login process.
+   * - `canceled` when the user cancels the login process.
+   * - `error` when the user encounters an error during the login process.
+   * - `success` when the user successfully logs in.
+   */
+  trackState(...args: Parameters<TelemetryClient['trackLoginState']>) {
+    this.trackLoginState(...args);
+  }
+}
