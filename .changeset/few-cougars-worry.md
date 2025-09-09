adf78642a8f4274f4a3ae2eb0af4d482dae5bedd
Austin Merrick <onsclom@onsclom.net>
2025-09-09 14:11:33 -0700

    [cli] Fix vc deploy to show full build logs on build error (#13901)
    This fixes a bug where sometimes the `vc deploy` with build errors do
    not return the full logs:
    https://linear.app/vercel/issue/FLOW-3934/vercel-deploy-builds-logs-do-not-contain-full-build-logs
    
    We receive the build error before the logs are ready to grab. Ideally,
    we'd want some general way to know when logs are done, but in this case
    we know what the last log line will be, so we can watch the logs until
    we see that.

---
+++ .changeset/few-cougars-worry.md
@@ -0,0 +1,5 @@
+---
+'vercel': patch
+---
+
+Fix build errored deployments to always show full build logs
--- packages/cli/src/commands/deploy/index.ts
+++ packages/cli/src/commands/deploy/index.ts
@@ -69,7 +69,7 @@ import { DeployTelemetryClient } from '../../util/telemetry/commands/deploy';
 import output from '../../output-manager';
 import { ensureLink } from '../../util/link/ensure-link';
 import { UploadErrorMissingArchive } from '../../util/deploy/process-deployment';
-import { displayBuildLogs } from '../../util/logs';
+import { displayBuildLogsUntilFinalError } from '../../util/logs';
 
 export default async (client: Client): Promise<number> => {
   const telemetryClient = new DeployTelemetryClient({
@@ -668,8 +668,6 @@ export default async (client: Client): Promise<number> => {
     }
 
     if (err instanceof BuildError) {
-      output.error(err.message || 'Build failed');
-      output.print('\n');
       if (withLogs === false) {
         try {
           if (now.url) {
@@ -678,12 +676,11 @@ export default async (client: Client): Promise<number> => {
               contextName,
               now.url
             );
-            const { promise } = displayBuildLogs(
+            await displayBuildLogsUntilFinalError(
               client,
               failedDeployment,
-              false
+              err.message
             );
-            await promise;
           }
         } catch (_) {
           output.log(
