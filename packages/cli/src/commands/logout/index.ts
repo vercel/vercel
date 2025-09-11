275a5ab7aa0b90b91ac4c700fbd40e1faddb8198
Balázs Orbán <info@balazsorban.com>
2025-09-11 09:51:05 +0200

    fall back to legacy logout

--- packages/cli/src/commands/logout/index.ts
+++ packages/cli/src/commands/logout/index.ts
@@ -1,6 +1,13 @@
 import { printError } from '../../util/error';
+import {
+  writeToConfigFile,
+  writeToAuthConfigFile,
+} from '../../util/config/files';
 import { parseArguments } from '../../util/get-args';
 import type Client from '../../util/client';
+import { getCommandName } from '../../util/pkg-name';
+import { isAPIError } from '../../util/errors-ts';
+import { errorToString } from '@vercel/error-utils';
 import { help } from '../help';
 import { logoutCommand } from './command';
 import { getFlagsSpecification } from '../../util/get-flags-specification';
@@ -9,6 +16,8 @@ import { LogoutTelemetryClient } from '../../util/telemetry/commands/logout';
 import { logout as future } from './future';
 
 export default async function logout(client: Client): Promise<number> {
+  const { authConfig, config } = client;
+
   let parsedArgs = null;
 
   const flagsSpecification = getFlagsSpecification(logoutCommand.options);
@@ -33,5 +42,57 @@ export default async function logout(client: Client): Promise<number> {
     return 0;
   }
 
-  return await future(client);
+  let exitCode = 0;
+  // Unless the authConfig has expiresAt or refreshToken, fall back to legacy logout
+  if ('expiresAt' in authConfig || 'refreshToken' in authConfig) {
+    return await future(client);
+  }
+
+  output.debug('Falling back to legacy logout');
+
+  if (!authConfig.token) {
+    output.note(
+      `Not currently logged in, so ${getCommandName('logout')} did nothing`
+    );
+    return 0;
+  }
+
+  output.spinner('Logging out…', 200);
+
+  try {
+    await client.fetch('/v3/user/tokens/current', {
+      method: 'DELETE',
+      useCurrentTeam: false,
+    });
+  } catch (err: unknown) {
+    if (isAPIError(err)) {
+      if (err.status === 403) {
+        output.debug('Token is invalid so it cannot be revoked');
+      } else if (err.status !== 200) {
+        output.debug(err?.message ?? '');
+        exitCode = 1;
+      }
+    }
+  }
+
+  delete config.currentTeam;
+
+  delete authConfig.token;
+
+  try {
+    writeToConfigFile(config);
+    writeToAuthConfigFile(authConfig);
+    output.debug('Configuration has been deleted');
+  } catch (err: unknown) {
+    output.debug(errorToString(err));
+    exitCode = 1;
+  }
+
+  if (exitCode === 0) {
+    output.log('Logged out!');
+  } else {
+    output.error('Failed during logout');
+  }
+
+  return exitCode;
 }
