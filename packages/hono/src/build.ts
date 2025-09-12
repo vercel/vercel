7d66bd96c96cfcfe6d8b996a968701b974e32fa8
Jeff See <jeffsee.55@gmail.com>
2025-09-12 11:20:28 -0700

    Add framework slug and version for Express and Hono builders to build output (#13918)
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- packages/hono/src/build.ts
+++ packages/hono/src/build.ts
@@ -1,9 +1,11 @@
 import { Files, FileFsRef, type BuildV3, glob } from '@vercel/build-utils';
-// @ts-expect-error - FIXME: hono-framework build is not exported
+// @ts-expect-error - FIXME: framework build is not exported
 import { build as nodeBuild } from '@vercel/node';
+import { createRequire } from 'module';
 import { join } from 'path';
 import fs from 'fs';
 
+const frameworkName = 'hono';
 const REGEX = /(?:from|require|import)\s*(?:\(\s*)?["']hono["']\s*(?:\))?/g;
 
 const validFilenames = [
@@ -15,6 +17,8 @@ const validFilenames = [
   'src/server',
 ];
 
+export const require_ = createRequire(__filename);
+
 const validExtensions = ['js', 'cjs', 'mjs', 'ts', 'cts', 'mts'];
 
 const entrypointsForMessage = validFilenames
@@ -25,8 +29,20 @@ export const build: BuildV3 = async args => {
   // Introducing new behavior for the node builder where Typescript errors always
   // fail the build. Previously, this relied on noEmitOnError being true in the tsconfig.json
   process.env.EXPERIMENTAL_NODE_TYPESCRIPT_ERRORS = '1';
-  return nodeBuild({
+
+  // Express's rendering engine support using the views directory as the entrypoint.
+  const includeFiles = ['views/**/*'];
+  const includeFilesFromConfig = args.config.includeFiles;
+  if (includeFilesFromConfig) {
+    includeFiles.push(...includeFilesFromConfig);
+  }
+
+  const res = await nodeBuild({
     ...args,
+    config: {
+      ...args.config,
+      includeFiles,
+    },
     // this is package.json, but we'll replace it with the return value of the entrypointCallback
     // after install and build scripts have had a chance to run
     entrypoint: 'package.json',
@@ -35,10 +51,28 @@ export const build: BuildV3 = async args => {
       return entrypointCallback(args);
     },
   });
+  let version = undefined;
+  try {
+    const resolved = require_.resolve(`${frameworkName}/package.json`, {
+      paths: [args.workPath],
+    });
+    const honoVersion: string = require_(resolved).version;
+    if (honoVersion) {
+      version = honoVersion;
+    }
+  } catch (e) {
+    // ignore
+  }
+  res.output.framework = {
+    name: frameworkName,
+    version,
+  };
+  return res;
 };
 
 export const entrypointCallback = async (args: Parameters<BuildV3>[0]) => {
   const mainPackageEntrypoint = findMainPackageEntrypoint(args.files);
+  // builds a glob pattern like {app,index,server,src/app,src/index,src/server}.{js,cjs,mjs,ts,cts,mts}
   const entrypointGlob = `{${validFilenames
     .map(entrypoint => `${entrypoint}`)
     .join(',')}}.{${validExtensions.join(',')}}`;
