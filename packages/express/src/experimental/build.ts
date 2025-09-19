fc8254ac9306be02b69500fef078b01a9a41b8a1
Jeff See <jeffsee.55@gmail.com>
2025-09-19 13:32:01 -0700

    Add back original express builder for now

---
+++ packages/express/src/experimental/build.ts
@@ -0,0 +1,32 @@
+import { BuildV2, Lambda } from '@vercel/build-utils';
+import { downloadInstallAndBundle, maybeExecBuildCommand } from './utils';
+import { rolldown } from './rolldown';
+import { entrypointCallback } from './find-entrypoint';
+import { introspectApp } from './introspection';
+
+export const build: BuildV2 = async args => {
+  const downloadResult = await downloadInstallAndBundle(args);
+
+  await maybeExecBuildCommand(args, downloadResult);
+
+  args.entrypoint = await entrypointCallback(args);
+
+  const rolldownResult = await rolldown(args);
+
+  const { routes } = await introspectApp(args, rolldownResult);
+
+  const lambda = new Lambda({
+    runtime: downloadResult.nodeVersion.runtime,
+    ...rolldownResult,
+  });
+
+  const output: Record<string, Lambda> = { index: lambda };
+  for (const route of routes) {
+    output[route.dest] = lambda;
+  }
+
+  return {
+    routes,
+    output,
+  };
+};
