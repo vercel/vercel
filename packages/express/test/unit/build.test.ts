5ef1a78fab260e3f9944a1f6f75a8c40ff653849
Jeff See <jeffsee.55@gmail.com>
2025-09-22 11:38:48 -0700

    Fix window, undo vitest config chagne

--- packages/express/test/unit/build.test.ts
+++ packages/express/test/unit/build.test.ts
@@ -1,7 +1,7 @@
 import { FileFsRef, Files } from '@vercel/build-utils/dist';
 import { build } from '../../src/build';
 import { build as experimentalBuild } from '../../src/experimental/build';
-import { join, sep } from 'path';
+import { join } from 'path';
 import { describe, expect, it } from 'vitest';
 import fs from 'fs';
 
@@ -229,7 +229,7 @@ describe('successful builds', () => {
       });
 
       if ('handler' in result.output) {
-        expect(result.output.handler).toBe(fixtureConfig.handler.join(sep));
+        expect(result.output.handler).toBe(fixtureConfig.handler.join('/'));
         const file = result.output.files?.[result.output.handler];
         if (file && 'data' in file) {
           const content = file.data.toString();
