d588f7ceeef486f657153b567127b44a7c3654ad
ricardo-agz <ricardo.gonzalez@vercel.com>
2025-10-03 19:30:44 -0700

    fixed e2e

--- packages/ruby/test/fixtures/14-zero-config-rails/config/routes.rb
+++ packages/ruby/test/fixtures/14-zero-config-rails/config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: proc { [200, { "Content-Type" => "text/html" }, ["Rails on Vercel"]] }
 end
