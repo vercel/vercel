bf1aacc0e69b8cf81b6f9410d72a37816140de5d
Austin Merrick <austinmerrick@onsclom.net>
2025-10-07 13:03:58 -0700

    invalidate remix tests

--- packages/remix/src/build.ts
+++ packages/remix/src/build.ts
@@ -3,6 +3,8 @@ import { build as buildLegacy } from './build-legacy';
 import { isVite } from './utils';
 import type { BuildV2 } from '@vercel/build-utils';
 
+// invalidate
+
 export const build: BuildV2 = opts => {
   return isVite(opts.workPath) ? buildVite(opts) : buildLegacy(opts);
 };
