28e749e3dcd374d24cb765bad630cab5e93cb1a4
ricardo-agz <ricardo.gonzalez@vercel.com>
2025-10-09 15:40:07 -0700

    made dev fixtures more robust

--- packages/cli/test/dev/fixtures/python-flask/server.py
+++ packages/cli/test/dev/fixtures/python-flask/server.py
@@ -1,6 +1,10 @@
 from flask import Flask, Response, request
+from endpoints.users import users_bp
 
 app = Flask(__name__)
+
+app.register_blueprint(users_bp)
+
 
 @app.route("/")
 def read_root():
@@ -14,12 +18,12 @@ def read_api():
 def read_api_hello(name):
     return {"message": f"Hello, {name}!"}
 
-@app.route("/headers")
-def headers():
-    url = request.headers.get('x-vercel-deployment-url')
-    return Response(url, mimetype='text/plain')
-
 @app.route("/query")
 def query():
     param = request.args.get('param', default='missing')
     return {"received_param": param}
+
+@app.route("/headers")
+def headers():
+    url = request.headers.get('x-vercel-deployment-url')
+    return Response(url, mimetype='text/plain')
