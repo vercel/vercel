4af2e431ecb3d2a06fee87aa4b22fe02de47446c
Austin Merrick <austinmerrick@onsclom.net>
2025-10-16 09:52:32 -0700

    Improve NestJS example

--- examples/nestjs/src/app.controller.ts
+++ examples/nestjs/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Header('Content-Type', 'text/html')
   getHello(): string {
     return this.appService.getHello();
   }
