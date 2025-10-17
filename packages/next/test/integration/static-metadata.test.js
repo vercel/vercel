90f1ecfd004619714cd8101fa3864d53601e4c0b
JJ Kasper <jj@jjsweb.site>
2025-10-16 19:23:34 -0700

    invalid test

--- packages/next/test/integration/static-metadata.test.js
+++ packages/next/test/integration/static-metadata.test.js
@@ -58,12 +58,13 @@ describe('Static Metadata Integration Test', () => {
     expect(output['dynamic/[id]/opengraph-image2.png'].type).toBe('FileFsRef');
 
     // Verify catch-all route static metadata
-    expect(output['dynamic-catch/[...arg]/icon.svg']).toBeDefined();
-    expect(output['dynamic-catch/[...arg]/icon.svg'].type).toBe('FileFsRef');
+    // TODO: https://linear.app/vercel/issue/NEXT-4738
+    // expect(output['dynamic-catch/[...arg]/icon.svg']).toBeDefined();
+    // expect(output['dynamic-catch/[...arg]/icon.svg'].type).toBe('FileFsRef');
 
-    expect(output['dynamic-catch-all/[[...arg]]/icon.svg']).toBeDefined();
-    expect(output['dynamic-catch-all/[[...arg]]/icon.svg'].type).toBe(
-      'FileFsRef'
-    );
+    // expect(output['dynamic-catch-all/[[...arg]]/icon.svg']).toBeDefined();
+    // expect(output['dynamic-catch-all/[[...arg]]/icon.svg'].type).toBe(
+    //   'FileFsRef'
+    // );
   });
 });
