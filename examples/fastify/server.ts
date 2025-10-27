32db4be8da823ffedcef59b7bab244784290fb12
Austin Merrick <austinmerrick@onsclom.net>
2025-10-27 13:40:41 -0700

    Simplify entrypoint

--- examples/fastify/server.ts
+++ examples/fastify/server.ts
@@ -99,9 +99,4 @@ fastify.get('/', async function handler (request, reply) {
     `
 })
 
-try {
-  await fastify.listen({ port: 3000 })
-} catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
-}
+fastify.listen({ port: 3000 })
