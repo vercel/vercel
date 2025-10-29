089c66cf1fbb73f8316b7e182e002b9aab07496a
ricardo-agz <ricardo.gonzalez@vercel.com>
2025-10-29 12:41:44 -0700

    support /api folder entrypoints

--- packages/python/src/entrypoint.ts
+++ packages/python/src/entrypoint.ts
@@ -4,7 +4,7 @@ import type { FileFsRef } from '@vercel/build-utils';
 import { glob, debug } from '@vercel/build-utils';
 
 export const FASTAPI_ENTRYPOINT_FILENAMES = ['app', 'index', 'server', 'main'];
-export const FASTAPI_ENTRYPOINT_DIRS = ['', 'src', 'app', 'api', 'backend'];
+export const FASTAPI_ENTRYPOINT_DIRS = ['', 'src', 'app', 'api'];
 export const FASTAPI_CONTENT_REGEX =
   /(from\s+fastapi\s+import\s+FastAPI|import\s+fastapi|FastAPI\s*\()/;
 
