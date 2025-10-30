32692a06da015a42105692a20ad08f7bb10d11f5
Ricardo Gonzalez <30984749+ricardo-agz@users.noreply.github.com>
2025-10-30 11:16:40 -0700

    [python] allowing /api folder entrypoints for FastAPI + Flask (#14168)
    This PR adds support for entrypoints inside the /api folder for
    FastAPI/Flask apps.
    It adds `ignoreRuntimes: ['@vercel/python'],` because otherwise the API
    builder routes will overshadow the framework routes and 404 when they
    shouldn't

--- packages/cli/src/util/dev/builder.ts
+++ packages/cli/src/util/dev/builder.ts
@@ -434,11 +434,11 @@ export async function getBuildMatches(
       buildConfig.config?.framework === 'flask'
     ) {
       // Mirror @vercel/python's entrypoint candidates
-      const candidateDirs = ['', 'src', 'app'];
+      const candidateDirs = ['', 'src', 'app', 'api'];
       const candidateNames = ['app', 'index', 'server', 'main'];
       const candidates: string[] = [];
-      for (const dir of candidateDirs) {
-        for (const name of candidateNames) {
+      for (const name of candidateNames) {
+        for (const dir of candidateDirs) {
           candidates.push(dir ? `${dir}/${name}.py` : `${name}.py`);
         }
       }
