d980868337948e83dd54b71707cc90d446534ef1
Pranav Karthik <pranav.karthik10@gmail.com>
2025-11-14 16:31:13 -0800

    update for dev command

--- packages/cli/src/util/config/read-config.ts
+++ packages/cli/src/util/config/read-config.ts
@@ -2,9 +2,12 @@ import { CantParseJSONFile } from '../errors-ts';
 import readJSONFile from '../read-json-file';
 import type { VercelConfig } from '../dev/types';
 import getLocalConfigPath from './local-path';
+import { compileVercelConfig } from '../compile-vercel-config';
 
 export default async function readConfig(dir: string) {
-  const pkgFilePath = getLocalConfigPath(dir);
+  const compileResult = await compileVercelConfig(dir);
+
+  const pkgFilePath = compileResult.configPath || getLocalConfigPath(dir);
   const result = await readJSONFile<VercelConfig>(pkgFilePath);
 
   if (result instanceof CantParseJSONFile) {
