93bab3706383aadfb0ff07cdfd440fc8a7da6634
Pranav Karthik <pranav.karthik10@gmail.com>
2025-11-19 16:33:59 -0800

    revert to old conditional

--- packages/cli/src/util/config/local-path.ts
+++ packages/cli/src/util/config/local-path.ts
@@ -39,5 +39,9 @@ export default function getLocalPathConfig(prefix: string) {
     }
   }
 
-  return nowConfigExists ? nowConfigPath : vercelConfigPath;
+  if (nowConfigExists) {
+    return nowConfigPath;
+  }
+
+  return vercelConfigPath;
 }
