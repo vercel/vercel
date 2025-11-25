cb78728cb2050a3ba97f824c571b906962b6ef49
Cody Wong <cody.wong@vercel.com>
2025-11-25 11:48:33 -0800

    fix one integration2

--- packages/cli/test/integration-2.test.ts
+++ packages/cli/test/integration-2.test.ts
@@ -1040,9 +1040,9 @@ test('[vc link] should not duplicate paths in .gitignore', async () => {
   // Ensure the message is correct pattern
   expect(stderr).toMatch(/Linked to /m);
 
-  // Ensure .gitignore is created
+  // Ensure .gitignore contains .vercel and .env*.local (from env pull)
   const gitignore = await readFile(path.join(dir, '.gitignore'), 'utf8');
-  expect(gitignore).toBe('.vercel\n');
+  expect(gitignore).toBe('.vercel\n.env*.local\n');
 });
 
 test('[vc dev] should show prompts to set up project', async () => {
