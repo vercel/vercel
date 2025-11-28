2d8c48b0d5fc71019499e3965dfec68f5973ec2f
ricardo-agz <ricardo.gonzalez@vercel.com>
2025-11-28 09:58:23 -0800

    example

---
+++ examples/pyproject/main.py
@@ -0,0 +1,27 @@
+from starlette.applications import Starlette
+from starlette.responses import PlainTextResponse, JSONResponse
+from starlette.routing import Route
+
+
+async def homepage(request):
+    return PlainTextResponse("Hello Express!")
+
+
+async def get_user(request):
+    user_id = request.path_params['id']
+    return JSONResponse({"id": user_id})
+
+
+async def get_comment(request):
+    post_id = request.path_params['postId']
+    comment_id = request.path_params['commentId']
+    return JSONResponse({"postId": post_id, "commentId": comment_id})
+
+
+routes = [
+    Route("/", homepage),
+    Route("/api/users/{id}", get_user),
+    Route("/api/posts/{postId}/comments/{commentId}", get_comment),
+]
+
+app = Starlette(routes=routes)
