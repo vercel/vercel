bf4450bf41880ad45287394ec948adcf8acc2dc4
Ricardo Gonzalez <Ricardo.gm@live.com>
2025-12-12 14:27:54 -0800

    fidn uv lock

--- packages/python/src/index.ts
+++ packages/python/src/index.ts
@@ -69,6 +69,7 @@ export async function downloadFilesInWorkPath({
 
 export const build: BuildV3 = async ({
   workPath,
+  repoRootPath,
   files: originalFiles,
   entrypoint,
   meta = {},
@@ -303,6 +304,7 @@ export const build: BuildV3 = async ({
       workPath,
       entryDirectory,
       fsFiles,
+      repoRootPath,
       pythonPath: pythonVersion.pythonPath,
       pipPath: pythonVersion.pipPath,
       uvPath,
