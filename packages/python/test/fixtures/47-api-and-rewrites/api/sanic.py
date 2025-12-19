f96a8e4a0a0c4b5d7a50bccf1e9c2f56fd756539
Ricardo Gonzalez <Ricardo.gm@live.com>
2025-12-19 15:11:50 -0800

    fixtures

---
+++ packages/python/test/fixtures/47-api-and-rewrites/api/sanic.py
@@ -0,0 +1,14 @@
+from sanic import Sanic
+from sanic.response import text, json
+
+app = Sanic("my_app")
+
+
+@app.get("/")
+async def home(request):
+    return text("ok")
+
+
+@app.get("/greet/<name>")
+async def greet(request, name: str):
+    return json({"message": f"Hello, {name}!"})
