59a2c01da49f4a3af25e3cbb9a43530da2e33067
Matthew Stanciu <mattbstanciu@gmail.com>
2026-01-07 16:36:48 -0800

    env configs

--- packages/cli/src/util/dev/server.ts
+++ packages/cli/src/util/dev/server.ts
@@ -1633,7 +1633,7 @@ export default class DevServer {
         missRoutes,
         phase,
         req,
-        this.envValues
+        this.envConfigs.allEnv
       );
 
       if (routeResult.continue) {
@@ -1692,7 +1692,7 @@ export default class DevServer {
           [],
           'miss',
           req,
-          this.envValues
+          this.envConfigs.allEnv
         );
 
         match = await findBuildMatch(
@@ -1728,7 +1728,7 @@ export default class DevServer {
           [],
           'hit',
           req,
-          this.envValues
+          this.envConfigs.allEnv
         );
         routeResult.status = prevStatus;
       }
@@ -1758,7 +1758,7 @@ export default class DevServer {
         [],
         'error',
         req,
-        this.envValues
+        this.envConfigs.allEnv
       );
       const { matched_route } = routeResultForError;
 
@@ -1879,7 +1879,7 @@ export default class DevServer {
         undefined,
         undefined,
         req,
-        this.envValues
+        this.envConfigs.allEnv
       );
       if (matchedRoute.found && callLevel === 0) {
         debug(`Found matching route ${matchedRoute.dest} for ${newUrl}`);
