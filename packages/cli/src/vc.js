50c773f6771ca65b5e977fef68e23886643836dd
Thomas Knickman <tom.knickman@vercel.com>
2026-01-12 14:17:19 -0500

    Update packages/cli/src/vc.js
    Co-authored-by: Steven <steven@ceriously.com>

--- packages/cli/src/vc.js
+++ packages/cli/src/vc.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 // This shim defers loading the real module until the compile cache is enabled.
 // https://nodejs.org/api/module.html#moduleenablecompilecachecachedir
-// enableCompileCache was added in Node.js 22.1.0, so we need to handle older versions.
+// enableCompileCache was added in Node.js 22.8.0, so we need to handle older versions.
 try {
   const { enableCompileCache } = await import('node:module');
   if (enableCompileCache) {
