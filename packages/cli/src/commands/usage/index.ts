f826847a3bd2ba4183fb57b5095babe546039276
Mingchung Xia <mingchung.xia@gmail.com>
2026-01-27 11:21:09 -0800

    fix(cli): Add Response type from node fetch for typecheck

--- packages/cli/src/commands/usage/index.ts
+++ packages/cli/src/commands/usage/index.ts
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
 import jsonlines from 'jsonlines';
+import type { Response } from 'node-fetch';
 import { parseArguments } from '../../util/get-args';
 import { printError } from '../../util/error';
 import type Client from '../../util/client';
