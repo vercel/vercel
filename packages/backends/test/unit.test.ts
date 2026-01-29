2e6a715a380f58cce14974e88a3419d4ce07fb9e
Jeff See <jeffsee.55@gmail.com>
2026-01-29 10:03:14 -0800

    Fix skip logic

--- packages/backends/test/unit.test.ts
+++ packages/backends/test/unit.test.ts
@@ -52,7 +52,8 @@ describe('successful builds', async () => {
   );
   for (const fixtureName of fixtures) {
     // Windows is just too slow to build these fixtures
-    it.skipIf(process.platform === 'win32' && fixtureName.includes('09'))(
+    const isNestFixture = fixtureName.includes('nest');
+    it.skipIf(process.platform === 'win32' && isNestFixture)(
       `builds ${fixtureName}`,
       async () => {
         // Copy entire fixture to work dir so no parent node_modules can interfere
