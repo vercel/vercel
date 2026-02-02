eac966d2384db0271dc85f176259437f812cd1e0
Chandan Rao <chandan.rao@vercel.com>
2026-02-02 13:49:57 -0800

    [cli] removing deprecated custom error page implementation

--- packages/client/src/types.ts
+++ packages/client/src/types.ts
@@ -171,12 +171,6 @@ export interface VercelConfig {
   images?: Images;
   crons?: Cron[];
   bunVersion?: string;
-  customErrorPage?:
-    | string
-    | {
-        default5xx?: string;
-        default4xx?: string;
-      };
   /**
    * Path to a file containing bulk redirects (relative to the project root).
    * This file will be included in prebuilt deployments.
@@ -228,10 +222,4 @@ export interface DeploymentOptions {
   gitMetadata?: GitMetadata;
   autoAssignCustomDomains?: boolean;
   customEnvironmentSlugOrId?: string;
-  customErrorPage?:
-    | string
-    | {
-        default5xx?: string;
-        default4xx?: string;
-      };
 }
