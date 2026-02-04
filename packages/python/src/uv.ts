f5bcb5187a9ed176cce9db593c85143642bad4c5
gscho <greg.c.schofield@gmail.com>
2026-02-04 13:22:31 -0500

    Only apply the uv python prefix filter for builds run on vercel infra.
    Signed-off-by: gscho <greg.c.schofield@gmail.com>

--- packages/python/src/uv.ts
+++ packages/python/src/uv.ts
@@ -68,17 +68,24 @@ export class UvRunner {
       );
     }
 
+    // Only apply the startsWith filter when not in Vercel Build Image
+    if (!process.env.VERCEL_BUILD_IMAGE) {
+      pyList = pyList.filter(
+        entry =>
+          entry.path !== null &&
+          entry.path.startsWith(UV_PYTHON_PATH_PREFIX) &&
+          entry.implementation === 'cpython'
+      );
+    } else {
+      pyList = pyList.filter(
+        entry => entry.path !== null && entry.implementation === 'cpython'
+      );
+    }
+
     return new Set(
-      pyList
-        .filter(
-          entry =>
-            entry.path !== null &&
-            entry.path.startsWith(UV_PYTHON_PATH_PREFIX) &&
-            entry.implementation === 'cpython'
-        )
-        .map(
-          entry => `${entry.version_parts.major}.${entry.version_parts.minor}`
-        )
+      pyList.map(
+        entry => `${entry.version_parts.major}.${entry.version_parts.minor}`
+      )
     );
   }
 
