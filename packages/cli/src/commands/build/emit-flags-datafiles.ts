783a3d4033ad7e829fb6775846d74f056c05f9b5
Dominik Ferber <dominik.ferber@gmail.com>
2026-02-18 21:35:34 +0200

    reorder

--- packages/cli/src/commands/build/emit-flags-datafiles.ts
+++ packages/cli/src/commands/build/emit-flags-datafiles.ts
@@ -1,7 +1,7 @@
 import { createHash } from 'node:crypto';
+import { NowBuildError } from '@vercel/build-utils';
 import { mkdir, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
-import { NowBuildError } from '@vercel/build-utils';
 import fetch from 'node-fetch';
 import output from '../../output-manager';
 import pkg from '../../util/pkg';
