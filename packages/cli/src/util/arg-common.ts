74a33c8e838cba67be9d2a9901668d9ecbf12cf7
Brooke Mosby <25040341+brookemosby@users.noreply.github.com>
2026-02-19 12:07:40 -0700

    merge

--- packages/cli/src/util/arg-common.ts
+++ packages/cli/src/util/arg-common.ts
@@ -79,14 +79,6 @@ export const globalCommandOptions = [
   },
   { name: 'team', shorthand: 'T', type: String, deprecated: false },
   { name: 'api', shorthand: null, type: String, deprecated: false },
-  {
-    name: 'non-interactive',
-    shorthand: null,
-    type: Boolean,
-    description:
-      'Never prompt for input; use structured JSON for action required',
-    deprecated: false,
-  },
 ] as const;
 
 const GLOBAL_OPTIONS = getFlagsSpecification(globalCommandOptions);
