19994db03c97e92c1e166e37bc8583c12838dc2c
Mingchung Xia <mingchung.xia@gmail.com>
2026-02-23 15:47:45 -0800

    feat(cli): Add vercel buy pro subcommand

--- packages/cli/src/util/buy/types.ts
+++ packages/cli/src/util/buy/types.ts
@@ -3,7 +3,7 @@ import type { CreditType, AddonAlias } from '../../commands/buy/command';
 export type PurchaseItem =
   | { type: 'credits'; creditType: CreditType; amount: number }
   | { type: 'addon'; productAlias: AddonAlias; quantity: number }
-  | { type: 'pro' }
+  | { type: 'subscription'; planSlug: 'pro' }
   | { type: 'v0' };
 
 export interface BuyResponse {
