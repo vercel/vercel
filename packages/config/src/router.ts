f9699f3fa641de1edb899ccddca1ad394bd0f3f5
Matthew Stanciu <mattbstanciu@gmail.com>
2026-03-26 16:28:47 -0700

    Revert "try pushing again"
    This reverts commit 32c97cc1f75a09eebf8bdbaaa96f0cb0a0472029.

--- packages/config/src/router.ts
+++ packages/config/src/router.ts
@@ -662,8 +662,8 @@ export class Router {
       has?: Condition[];
       missing?: Condition[];
       respectOriginCacheControl?: boolean;
-    } & // when transforms are present — otherwise falls through to the Rewrite overload. // Require at least one transform field so this overload only matches
-    // push
+    } & // Require at least one transform field so this overload only matches
+    // when transforms are present — otherwise falls through to the Rewrite overload.
     (
       | {
           requestHeaders: Record<string, string | string[]>;
