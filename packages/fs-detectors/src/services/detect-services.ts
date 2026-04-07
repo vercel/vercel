178e744229a8d4c6735fef4fa133ec97c7ba1a2b
Nik Sidnev <nik.sidnev@vercel.com>
2026-04-06 22:35:59 -0400

    [services] detect services from Procfile

--- packages/fs-detectors/src/services/detect-services.ts
+++ packages/fs-detectors/src/services/detect-services.ts
@@ -31,6 +31,7 @@ import { resolveAllConfiguredServices } from './resolve';
 import { autoDetectServices } from './auto-detect';
 import { detectRailwayServices } from './detect-railway';
 import { detectRenderServices } from './detect-render';
+import { detectProcfileServices } from './detect-procfile';
 
 // don't apply subdomain rewrites on preview urls
 const PREVIEW_DOMAIN_MISSING: HasField = [
@@ -106,6 +107,10 @@ function toInferredLayoutConfig(
       serviceConfig.buildCommand = service.buildCommand;
     }
 
+    if (typeof service.runtime === 'string') {
+      serviceConfig.runtime = service.runtime;
+    }
+
     inferredConfig[name] = serviceConfig;
   }
 
@@ -156,7 +161,7 @@ export async function detectServices(
     configuredServices && Object.keys(configuredServices).length > 0;
 
   // Try auto-detection of services.
-  // Priority: Railway > Render > blessed layouts.
+  // Priority: Railway > Render > Procfile > blessed layouts.
   // Any hard error (.errors) from detection will result into
   // exit from detection and return of the error
   // back to the user
@@ -169,6 +174,7 @@ export async function detectServices(
     }> = [
       { detect: detectRailwayServices, source: 'railway' },
       { detect: detectRenderServices, source: 'render' },
+      { detect: detectProcfileServices, source: 'procfile' },
       { detect: autoDetectServices, source: 'layout' },
     ];
 
