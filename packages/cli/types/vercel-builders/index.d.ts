bb8759239bf7f81a260763475c3d1458b20b5b72
melkeydev <amokstakov@gmail.com>
2026-04-26 21:46:08 -0700

    fixing type checking linting

---
+++ packages/cli/types/vercel-builders/index.d.ts
@@ -0,0 +1,44 @@
+declare module '@vercel/go' {
+  const builder: unknown;
+  export = builder;
+}
+
+declare module '@vercel/hydrogen' {
+  const builder: unknown;
+  export = builder;
+}
+
+declare module '@vercel/next' {
+  const builder: unknown;
+  export = builder;
+}
+
+declare module '@vercel/python' {
+  const builder: unknown;
+  export = builder;
+}
+
+declare module '@vercel/redwood' {
+  const builder: unknown;
+  export = builder;
+}
+
+declare module '@vercel/remix-builder' {
+  const builder: unknown;
+  export = builder;
+}
+
+declare module '@vercel/ruby' {
+  const builder: unknown;
+  export = builder;
+}
+
+declare module '@vercel/rust' {
+  const builder: unknown;
+  export = builder;
+}
+
+declare module '@vercel/static-build' {
+  const builder: unknown;
+  export = builder;
+}
