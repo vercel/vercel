d99527707cde56eb17f0f449d240480341588853
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-05-18 16:10:38 +0200

    make it required

--- packages/build-utils/src/types.ts
+++ packages/build-utils/src/types.ts
@@ -715,7 +715,7 @@ export interface BuildResultV2Typical {
     value: string;
   }>;
   framework?: {
-    slug?: string;
+    slug: string;
     version: string;
   };
   flags?: { definitions: FlagDefinitions };
